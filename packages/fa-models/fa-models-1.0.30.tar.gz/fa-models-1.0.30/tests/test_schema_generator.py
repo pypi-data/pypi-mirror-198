67c1d011020e72fea72d022aaafb80ca2e046fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:41 +0000

    Add package fa-models/1.0.30/fa-models-1.0.30.tar.gz

---
+++ packages/fa-models/fa-models-1.0.30.tar.gz/fa-models-1.0.30/tests/test_schema_generator.py
@@ -0,0 +1,30 @@
+import pytest
+from famodels.generators.schema_generator import SchemaGenerator
+from famodels.models.trading_signal import TradingSignal
+import json
+
+@pytest.mark.parametrize("name, namespace", [
+    ("TradingSignal", "fa.signalprocessing"),
+    #("TradingSignal", "tks.signal-processing.TradingSignal")
+])
+def test_create_json_avro_head(name, namespace):
+    header = SchemaGenerator().create_json_avro_head(name=name, namespace=namespace)
+    expected_header = {
+            "namespace": namespace,
+            "type": "record",
+            "name": name
+        }
+    assert header == expected_header
+
+def test_create_json_schema_for_avro():
+    generated_avro_schema = SchemaGenerator().generate_json_schema_for_avro(
+        model_class=TradingSignal, 
+        namespace="fa.signalprocessing")
+    # print(generated_avro_schema)
+
+    with open('./tests/avro_schema_expected.json', 'r') as f:
+        # Load the JSON data from the file
+        __expected_avro_schema__ = json.load(f)
+    # print(__expected_avro_schema__)
+
+    assert generated_avro_schema == __expected_avro_schema__
