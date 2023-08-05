2a413326abe0744076268bef63ee906c6d5d1cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:25 +0000

    Add package django-formset/0.13.4/django_formset-0.13.4-py3-none-any.whl

---
+++ packages/django-formset/django_formset-0.13.4-py3-none-any.whl/formset/static/formset/js/SortableSelect-IBHV4XXQ.js
@@ -0,0 +1,1 @@
+import{b as a,c as b}from"./chunk-LIEWHQGW.js";import"./chunk-IXC5FQWZ.js";import"./chunk-KFPGMBXM.js";import"./chunk-NLMHZ7JJ.js";export{b as SortableOptGroupElement,a as SortableSelectElement};
