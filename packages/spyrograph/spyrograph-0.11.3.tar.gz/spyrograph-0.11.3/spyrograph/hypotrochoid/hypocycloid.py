d1c2f8bcca608ce86917bfda7b1395d9f5eab757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:44 +0000

    Add package spyrograph/0.11.3/spyrograph-0.11.3.tar.gz

---
+++ packages/spyrograph/spyrograph-0.11.3.tar.gz/spyrograph-0.11.3/spyrograph/hypotrochoid/hypocycloid.py
@@ -0,0 +1,20 @@
+"""Model of a hypocycloid. Specific case of a hypotrochoid where the distance
+from the rolling circle is equal to the radius of the rolling circle
+"""
+
+from typing import List
+from numbers import Number
+
+from spyrograph.hypotrochoid.hypotrochoid import Hypotrochoid
+
+class Hypocycloid(Hypotrochoid):
+    """Model of a hypocycloid which is a special case of a hypotrochoid where
+    the circle is rolling around the inside of the fixed circle and has 1/3
+    the radius of the fixed circle
+    """
+    def __init__(
+            self, R: Number, r: Number, thetas: List[Number] = None,
+            theta_start: Number = None, theta_stop: Number = None,
+            theta_step: Number = None
+        ) -> None:
+        super().__init__(R, r, r, thetas, theta_start, theta_stop, theta_step)
