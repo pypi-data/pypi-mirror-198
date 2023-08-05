8041490001551073b1ada7cbdc6fc6dd2048c0af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:24 +0000

    Add package p-terraformer/0.2/p_terraformer-0.2.tar.gz

---
+++ packages/p-terraformer/p_terraformer-0.2.tar.gz/p_terraformer-0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+URL = 'https://github.com/S-mishina/p_terraformer'
+AUTHOR = 'S-mishina'
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    author=AUTHOR,
+    url=URL,
+    name='p_terraformer',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=requirements,
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'p_terraformer = p_terraformer.cli:main'
+        ]
+}
+)
