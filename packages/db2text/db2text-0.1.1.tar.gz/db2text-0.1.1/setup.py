5f82c667eee5775727d3193f8a7fc7a4060367ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:01 +0000

    Add package db2text/0.1.1/db2text-0.1.1.tar.gz

---
+++ packages/db2text/db2text-0.1.1.tar.gz/db2text-0.1.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r",encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="db2text",
+    version="0.1.1",
+    author="Chen chuan",
+    author_email="kcchen@139.com",
+    description="database to text",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/chenc224/dbt",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    scripts=["bin/dbt"],
+    zip_safe= False,
+    include_package_data = True,
+)
