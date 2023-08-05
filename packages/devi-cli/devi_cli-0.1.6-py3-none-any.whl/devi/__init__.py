4a538484b42f1ad98d4a17a05400d1de30072b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:51 +0000

    Add package devi-cli/0.1.6/devi_cli-0.1.6-py3-none-any.whl

---
+++ packages/devi-cli/devi_cli-0.1.6-py3-none-any.whl/devi/__init__.py
@@ -0,0 +1,9 @@
+__productname__ = 'devi'
+__description__ = 'devi is a tool for managing your project templates'
+__version__ = '0.1.0'
+__author__ = 'the devi team'
+__license__ = 'MIT'
+
+import devi.config as config
+
+config.init()
