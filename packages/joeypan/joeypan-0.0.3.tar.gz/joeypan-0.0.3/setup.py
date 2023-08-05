8acaef8b617355d2884600708cf213484f84446d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:20 +0000

    Add package joeypan/0.0.3/joeypan-0.0.3.tar.gz

---
+++ packages/joeypan/joeypan-0.0.3.tar.gz/joeypan-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="joeypan",
+    version="0.0.3",
+    author="elijahxb",
+    package_dir={"joeypan": "joeypan"},
+    install_requires=["pydantic==1.10.6"],
+    author_email="elijahxb@outlook.com",
+    description="A small package for web framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/elijahxb/joey",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
