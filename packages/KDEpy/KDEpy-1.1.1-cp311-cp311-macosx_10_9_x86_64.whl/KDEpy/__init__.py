a34d177dec4d36d906dc7ac3295facf50a55d993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:28 +0000

    Add package KDEpy/1.1.1/KDEpy-1.1.1-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/KDEpy/KDEpy-1.1.1-cp311-cp311-macosx_10_9_x86_64.whl/KDEpy/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from KDEpy.FFTKDE import FFTKDE
+from KDEpy.NaiveKDE import NaiveKDE
+from KDEpy.TreeKDE import TreeKDE
+
+__version__ = "1.1.1"
+__author__ = "tommyod"
+
+__all__ = ["TreeKDE", "NaiveKDE", "FFTKDE"]
