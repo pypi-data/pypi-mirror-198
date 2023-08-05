f1d89ff746e98c140e17245001406f17dff23eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:37 +0000

    Add package nonebot-plugin-rrc/0.0.3/nonebot_plugin_rrc-0.0.3.tar.gz

---
+++ packages/nonebot-plugin-rrc/nonebot_plugin_rrc-0.0.3.tar.gz/nonebot_plugin_rrc-0.0.3/README.md
@@ -0,0 +1,24 @@
+<div align="center">
+  <img src="https://s2.loli.net/2022/06/16/opBDE8Swad5rU3n.png" width="180" height="180" alt="NoneBotPluginLogo">
+  <br>
+  <p><img src="https://s2.loli.net/2022/06/16/xsVUGRrkbn1ljTD.png" width="240" alt="NoneBotPluginText"></p>
+</div>
+
+<div align="center">
+
+# nonebot-plugin-rrc
+
+_✨ QQ群聊点名 ✨_
+
+<p align="center">
+  <img src="https://img.shields.io/badge/python-3.8+-blue.svg" alt="Python">
+  <img src="https://img.shields.io/badge/nonebot-2.0.0r4+-red.svg" alt="NoneBot">
+  </a>
+</p>
+</div>
+
+## 🎉 鸣谢
+
+- [NoneBot2](https://github.com/nonebot/nonebot2)：本插件使用的开发框架。
+- [go-cqhttp](https://github.com/Mrs4s/go-cqhttp)：稳定完善的 CQHTTP 实现。
+- [TrWebOCR](https://github.com/alisen39/TrWebOCR)：轻量又便捷的OCR镜像方案。
---
+++ packages/nonebot-plugin-rrc/nonebot_plugin_rrc-0.0.3.tar.gz/nonebot_plugin_rrc-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import os
+import setuptools
+with open("README.md", "r", encoding="utf-8", errors="ignore") as f:
+    long_description = f.read()
+    setuptools.setup(
+    name='nonebot_plugin_rrc',
+    version='0.0.3',
+    author='QingmuCat',
+    author_email='1242550160@qq.com',
+    url='https://github.com/QingMuCat/nonebot_plugin_rrc',
+    description='''修仙插件''',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: Chinese (Simplified)"
+    ],
+    include_package_data=True,
+    platforms="any",
+    install_requires=[
+            'fastapi==0.89.1',
+            'nonebot-adapter-onebot==2.2.1',
+            'nonebot2==2.0.0rc3',
+    ],
+    )
