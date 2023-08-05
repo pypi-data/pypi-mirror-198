2c8329ec6554f270635b167df3049bf443d1e003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:21 +0000

    Add package panther/1.5.2/panther-1.5.2.tar.gz

---
+++ packages/panther/panther-1.5.2.tar.gz/panther-1.5.2/panther/exceptions.py
@@ -0,0 +1,26 @@
+from panther import status
+
+
+class DBException(Exception):
+    pass
+
+
+class APIException(Exception):
+    detail: str | dict | list = 'Internal Server Error'
+    status_code: int = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+    def __init__(self, detail=None, status_code=None):
+        self.detail = detail or self.detail
+        self.status_code = status_code or self.status_code
+
+
+class AuthenticationException(APIException):
+    detail = 'Authentication Error'
+    status_code = status.HTTP_401_UNAUTHORIZED
+
+
+class InvalidPathVariableException(APIException):
+    def __init__(self, value: str, arg_type: type):
+        detail = f"Path variable '{value}' should be '{arg_type.__name__}'"
+        super().__init__(detail=detail, status_code=status.HTTP_400_BAD_REQUEST)
+
