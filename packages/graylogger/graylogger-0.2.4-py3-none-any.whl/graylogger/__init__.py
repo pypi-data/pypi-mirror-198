9a8a46d535b0183f181f362a3e361b32085ba2ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:53 +0000

    Add package graylogger/0.2.4/graylogger-0.2.4-py3-none-any.whl

---
+++ packages/graylogger/graylogger-0.2.4-py3-none-any.whl/graylogger/__init__.py
@@ -0,0 +1,11 @@
+"""
+:authors: v.oficerov
+:license: MIT License
+:copyright: (c) 2023 v.oficerov
+"""
+
+from .HTTPHandler import HTTPHandler
+
+__author__ = 'v.oficerov'
+__version__ = '0.2.3'
+__email__ = 'valeryoficerov@gmail.com'
