0d9ad4691002d9085ffdc05f61aa4a8185930e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:30 +0000

    Add package pylinklist/0.2/pylinklist-0.2.tar.gz

---
+++ packages/pylinklist/pylinklist-0.2.tar.gz/pylinklist-0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='pylinklist',
+    version='0.2',
+    author='YMZ',
+    author_email='935460098@qq.com',
+    # description='你的第三方库的简介',
+    long_description_content_type="""text/markdown""",
+    # url='你的网站',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
