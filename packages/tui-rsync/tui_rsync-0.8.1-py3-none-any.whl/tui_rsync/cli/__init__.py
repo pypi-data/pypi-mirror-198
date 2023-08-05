435959f9a632d3713d6d4d8c4b0a5bed1beff7d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:38 +0000

    Add package tui-rsync/0.8.1/tui_rsync-0.8.1-py3-none-any.whl

---
+++ packages/tui-rsync/tui_rsync-0.8.1-py3-none-any.whl/tui_rsync/cli/__init__.py
@@ -0,0 +1,5 @@
+from tui_rsync.cli.cli import cli_app
+from tui_rsync.cli.source import source
+from tui_rsync.cli.sync import sync
+from tui_rsync.cli.rsync import Rsync
+from tui_rsync.cli.label_prompt import LabelPrompt
