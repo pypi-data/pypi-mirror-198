34957051ca8dc75a82046536fe0413c26f8a364e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:25 +0000

    Add package panther/1.6.1/panther-1.6.1.tar.gz

---
+++ packages/panther/panther-1.6.1.tar.gz/panther-1.6.1/panther/permissions.py
@@ -0,0 +1,15 @@
+from panther.request import Request
+
+
+class BasePermission:
+
+    @classmethod
+    def authorization(cls, request: Request) -> bool:
+        return True
+
+
+class AdminPermission:
+
+    @classmethod
+    def authorization(cls, request: Request) -> bool:
+        return request.user and hasattr(request.user, 'is_admin') and request.user.is_admin
