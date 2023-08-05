d4a56626919b733d6cf642e909555a0afe6b23cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:19 +0000

    Add package vsrgtools/1.4.2/vsrgtools-1.4.2.tar.gz

---
+++ packages/vsrgtools/vsrgtools-1.4.2.tar.gz/vsrgtools-1.4.2/vsrgtools/enum.py
@@ -0,0 +1,131 @@
+from __future__ import annotations
+
+from enum import auto
+from typing import Sequence
+
+from vstools import CustomIntEnum, PlanesT, vs
+
+__all__ = [
+    'LimitFilterMode',
+    'RemoveGrainMode', 'RemoveGrainModeT',
+    'RepairMode', 'RepairModeT',
+    'VerticalCleanerMode', 'VerticalCleanerModeT'
+]
+
+
+class LimitFilterModeMeta:
+    force_expr = True
+
+
+class LimitFilterMode(LimitFilterModeMeta, CustomIntEnum):
+    """Two sources, one filtered"""
+    SIMPLE_MIN = auto()
+    SIMPLE_MAX = auto()
+    """One source, two filtered"""
+    SIMPLE2_MIN = auto()
+    SIMPLE2_MAX = auto()
+    DIFF_MIN = auto()
+    DIFF_MAX = auto()
+    """One/Two sources, one filtered"""
+    CLAMPING = auto()
+
+    @property
+    def op(self) -> str:
+        return '<' if 'MIN' in self._name_ else '>'
+
+    def __call__(self, force_expr: bool = True) -> LimitFilterMode:
+        self.force_expr = force_expr
+
+        return self
+
+
+class RemoveGrainMode(CustomIntEnum):
+    NONE = 0
+    MINMAX_AROUND1 = 1
+    MINMAX_AROUND2 = 2
+    MINMAX_AROUND3 = 3
+    MINMAX_MEDIAN = 4
+    EDGE_CLIP_STRONG = 5
+    EDGE_CLIP_MODERATE = 6
+    EDGE_CLIP_MEDIUM = 7
+    EDGE_CLIP_LIGHT = 8
+    LINE_CLIP_CLOSE = 9
+    MIN_SHARP = 10
+    SQUARE_BLUR = 11
+    BOB_TOP_CLOSE = 13
+    BOB_BOTTOM_CLOSE = 14
+    BOB_TOP_INTER = 15
+    BOB_BOTTOM_INTER = 16
+    MINMAX_MEDIAN_OPP = 17
+    LINE_CLIP_OPP = 18
+    CIRCLE_BLUR = 19
+    BOX_BLUR = 20
+    OPP_CLIP_AVG = 21
+    OPP_CLIP_AVG_FAST = 22
+    EDGE_DEHALO = 23
+    EDGE_DEHALO2 = 24
+    MIN_SHARP2 = 25
+    SMART_RGC = 26
+    SMART_RGCL = 27
+    SMART_RGCL2 = 28
+
+    def __call__(self, clip: vs.VideoNode, planes: PlanesT = None) -> vs.VideoNode:
+        from .rgtools import removegrain
+        from .util import norm_rmode_planes
+        return removegrain(clip, norm_rmode_planes(clip, self, planes))
+
+
+RemoveGrainModeT = int | RemoveGrainMode | Sequence[int | RemoveGrainMode]
+
+
+class RepairMode(CustomIntEnum):
+    NONE = 0
+    MINMAX_SQUARE1 = 1
+    MINMAX_SQUARE2 = 2
+    MINMAX_SQUARE3 = 3
+    MINMAX_SQUARE4 = 4
+    LINE_CLIP_MIN = 5
+    LINE_CLIP_LIGHT = 6
+    LINE_CLIP_MEDIUM = 7
+    LINE_CLIP_STRONG = 8
+    LINE_CLIP_CLOSE = 9
+    MINMAX_SQUARE_REF_CLOSE = 10
+    MINMAX_SQUARE_REF1 = 11
+    MINMAX_SQUARE_REF2 = 12
+    MINMAX_SQUARE_REF3 = 13
+    MINMAX_SQUARE_REF4 = 14
+    CLIP_REF_RG5 = 15
+    CLIP_REF_RG6 = 16
+    CLIP_REF_RG17 = 17
+    CLIP_REF_RG18 = 18
+    CLIP_REF_RG19 = 19
+    CLIP_REF_RG20 = 20
+    CLIP_REF_RG21 = 21
+    CLIP_REF_RG22 = 22
+    CLIP_REF_RG23 = 23
+    CLIP_REF_RG24 = 24
+    CLIP_REF_RG26 = 26
+    CLIP_REF_RG27 = 27
+    CLIP_REF_RG28 = 28
+
+    def __call__(self, clip: vs.VideoNode, repairclip: vs.VideoNode, planes: PlanesT = None) -> vs.VideoNode:
+        from .rgtools import repair
+        from .util import norm_rmode_planes
+        return repair(clip, repairclip, norm_rmode_planes(clip, self, planes))
+
+
+RepairModeT = int | RepairMode | Sequence[int | RepairMode]
+
+
+class VerticalCleanerMode(CustomIntEnum):
+    NONE = 0
+    MEDIAN = 1
+    PRESERVING = 2
+
+    def __call__(self, clip: vs.VideoNode, planes: PlanesT = None) -> vs.VideoNode:
+        from .rgtools import vertical_cleaner
+        from .util import norm_rmode_planes
+        return vertical_cleaner(clip, norm_rmode_planes(clip, self, planes))
+
+
+VerticalCleanerModeT = int | VerticalCleanerMode | Sequence[int | VerticalCleanerMode]
