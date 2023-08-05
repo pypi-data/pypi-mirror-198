b230a9d0e1bf273c7542ee2724ef8852bafe0cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:30 +0000

    Add package ksystem/0.0.1/ksystem-0.0.1.tar.gz

---
+++ packages/ksystem/ksystem-0.0.1.tar.gz/ksystem-0.0.1/setup.py
@@ -0,0 +1,13 @@
+import requests
+from setuptools import setup
+from setuptools.command.install import install
+
+print("test anupamas01 ")
+
+
+setup(name='ksystem',
+      version='0.0.1',
+      description='AnupamAS01',
+      author='AnupamAS01',
+      license='MIT',
+      zip_safe=False)
