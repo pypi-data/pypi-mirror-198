cad2f523ce3a7d354f5fde6be0f52181fe332c73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:02 +0000

    Add package jwpackage/0.0.14/jwpackage-0.0.14.tar.gz

---
+++ packages/jwpackage/jwpackage-0.0.14.tar.gz/jwpackage-0.0.14/jwpackage/authentication/exceptions.py
@@ -0,0 +1,14 @@
+class JwtExpiredError(Exception):
+    def __init__(self,message="JWT token expired"):
+        self.message = message
+        super().__init__(self.message)
+
+class TelegramIdMatchError(Exception):
+    def __init__(self,message="Telegram id mismatch"):
+        self.message = message
+        super().__init__(self.message)
+
+class AuthorizationFieldError(Exception):
+    def __init__(self,message="No Authorization field in headers"):
+        self.message = message
+        super().__init__(self.message)
