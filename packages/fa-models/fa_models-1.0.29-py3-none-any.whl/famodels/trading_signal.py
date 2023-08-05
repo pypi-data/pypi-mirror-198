7e29b9faf8e28c840ace3842bf14d8bfb5f9c97a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:53 +0000

    Add package fa-models/1.0.29/fa_models-1.0.29-py3-none-any.whl

---
+++ packages/fa-models/fa_models-1.0.29-py3-none-any.whl/famodels/trading_signal.py
@@ -0,0 +1,41 @@
+import datetime
+from enum import Enum
+from typing import Optional
+from uuid import UUID
+from sqlmodel import Field, SQLModel
+from famodels.direction import Direction
+from famodels.side import Side
+
+class BaseSignalSQLModel(SQLModel):
+    def __init__(self, **kwargs):
+        self.__config__.table = False
+        super().__init__(**kwargs)
+        self.__config__.table = True
+
+    class Config:
+        validate_assignment = True
+
+class TradingSignal(SQLModel, table=True):
+    """A trading signal represents a suggestion to buy or sell. It is issued by a signal supplier (manually or algorithmically). It must have a correlating id to a trade."""
+    id: Optional[int] = Field(default=None, primary_key=True)
+    algo_id: str
+    """Provide the id of your algorithm entity (you might have more than one algorithm), which is sending this signal.  We have issued this id."""
+    provider_id: str
+    """The ID of the provider, who emitted the signal."""
+    market: str    
+    exchange: str
+    trade_correlation_id: str
+    """A signal without a trade intention makes has no value to us. Thus there always must be a trade id."""
+    direction: Direction
+    """The direction is actually provided by the trade, which is referenced by the correlation id. """
+    side: Side
+    price: float
+    tp: float
+    sl: float
+    datetime_of_creation: Optional[str]
+    """An ISO-UTC-compatible date and time of when the signal was created by the signal supplier."""
+    datetime_of_registration: Optional[str]
+    """The ISO-UTC-compatible date and time of when the signal was entering our interface."""
+    position_size_of_investment: float = 100
+    """Percentage of the investment position this algortihm is allowed to trade. Default is 100%, which is 1 position."""  
+    
