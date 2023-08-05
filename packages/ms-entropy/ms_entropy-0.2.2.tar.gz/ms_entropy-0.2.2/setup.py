7ff3782940505657f3b551efc6f5d0727aa72e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:11 +0000

    Add package ms-entropy/0.2.2/ms_entropy-0.2.2.tar.gz

---
+++ packages/ms-entropy/ms_entropy-0.2.2.tar.gz/ms_entropy-0.2.2/setup.py
@@ -0,0 +1,50 @@
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+from setuptools import setup, find_packages
+import numpy as np
+import os
+
+
+os.environ['CFLAGS'] = '-O3 -Wno-cpp -Wno-unused-function'
+
+
+setup(
+    name='ms_entropy',
+    version='0.2.2',
+    license='Apache License 2.0',
+    author='Yuanyue Li',
+    url='https://github.com/YuanyueLi/SpectralEntropy',
+    packages=['ms_entropy'],
+    install_requires=[
+        'Cython>=0.29.33',
+        "numpy>=1.23.5",
+        # "cupy>=11.6.0"
+    ],
+    keywords=[
+        "ms entropy",
+        "ms spectral entropy",
+        "spectral entropy",
+        "spectral similarity",
+        "entropy similarity",
+        "entropy",
+        "entropy search",
+        "flash entropy",
+        "flash entropy similarity",
+        "flash entropy search",
+    ],
+    package_dir={'': '.'},
+    ext_modules=cythonize([
+        Extension('ms_entropy.tools_cython',
+                  [r"ms_entropy/tools_cython.c"]),
+    ],
+        annotate=False,
+        compiler_directives={
+        'language_level': "3",
+        'cdivision': True,
+        'boundscheck': False,  # turn off bounds-checking for entire function
+        'wraparound': False  # turn off negative index wrapping for entire function
+    }),
+    include_dirs=[np.get_include()]
+)
+
+# python setup.py build_ext
