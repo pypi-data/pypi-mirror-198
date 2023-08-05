e09ef53fd32efa3e0d735fd2d419c981ad537a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:30 +0000

    Add package mols2grid/1.1.1/mols2grid-1.1.1-py3-none-any.whl

---
+++ packages/mols2grid/mols2grid-1.1.1-py3-none-any.whl/mols2grid/templates/js/molstorage.js
@@ -0,0 +1,33 @@
+class MolStorage extends Map {
+    multi_set(_id, _smiles) {
+        for (let i=0; i < _id.length; i++) {
+            this.set(_id[i], _smiles[i]);
+        }
+    }
+    multi_del(_id) {
+        for (let i=0; i < _id.length; i++) {
+            this.delete(_id[i]);
+        };
+    }
+    to_dict() {
+        var content = "{";
+        for (let [key, value] of this) {
+            content += key + ":" + JSON.stringify(value) + ",";
+        }
+        content = content.length > 1 ? content.slice(0, -1) : content;
+        content += "}";
+        return content
+    }
+    download_smi(fileName) {
+        var content = "";
+        for (let [key, value] of this) {
+            content += value + " " + key + "\n";
+        }
+        var a = document.createElement("a");
+        var file = new Blob([content], {type: "text/plain"});
+        a.href = URL.createObjectURL(file);
+        a.download = fileName;
+        a.click();
+        a.remove();
+    }
+}
