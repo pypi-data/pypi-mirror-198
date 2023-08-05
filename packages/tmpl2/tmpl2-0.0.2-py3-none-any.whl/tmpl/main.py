48685d191ab9db553905e7a2fa53f406db0c8162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:18 +0000

    Add package tmpl2/0.0.2/tmpl2-0.0.2-py3-none-any.whl

---
+++ packages/tmpl2/tmpl2-0.0.2-py3-none-any.whl/tmpl/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+from typing import Sequence
+
+import urwid
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from tmpl.ui import playerUI
+
+
+def parse_args(argv: Sequence[str] | None = None) -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "paths",
+        metavar="PATH",
+        type=str,
+        nargs="+",
+        help="Path(s) to video(s)/director(y)/(ies) to play.",
+    )
+    return parser.parse_args(argv)
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    args = parse_args(argv)
+    player = playerUI(args)
+    loop = urwid.MainLoop(
+        player.draw_ui(), player.palette, unhandled_input=player.handle_keys
+    )
+    loop.set_alarm_in(0.5, player.update_name)
+    loop.run()
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
