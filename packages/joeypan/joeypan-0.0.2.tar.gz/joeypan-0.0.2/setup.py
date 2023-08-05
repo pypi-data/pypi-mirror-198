57745ba4b6a27fa42251f3856a8f30e0a21a4f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:15 +0000

    Add package joeypan/0.0.2/joeypan-0.0.2.tar.gz

---
+++ packages/joeypan/joeypan-0.0.2.tar.gz/joeypan-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="joeypan",
+    version="0.0.2",
+    author="elijahxb",
+    author_email="elijahxb@outlook.com",
+    description="A small package for web framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/elijahxb/joey",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
