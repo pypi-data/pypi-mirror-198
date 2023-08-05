4efa88617eb3b93a58dddfea7c87511c3fffc048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:53 +0000

    Add package django-snowflake/4.2a1/django-snowflake-4.2a1.tar.gz

---
+++ packages/django-snowflake/django-snowflake-4.2a1.tar.gz/django-snowflake-4.2a1/django_snowflake/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '4.2a1'
+
+# Check Django compatibility before other imports which may fail if the
+# wrong version of Django is installed.
+from .utils import check_django_compatability
+
+check_django_compatability()
+
+from .functions import register_functions  # noqa
+
+register_functions()
