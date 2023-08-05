00ccf01bfc1717831bc770829d6648438501f4cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:21 +0000

    Add package tum-esm-utils/0.1.0/tum_esm_utils-0.1.0-py3-none-any.whl

---
+++ packages/tum-esm-utils/tum_esm_utils-0.1.0-py3-none-any.whl/tum_esm_utils/__init__.py
@@ -0,0 +1,16 @@
+"""Python utilities by the Professorship of Environmental Sensing and Modeling"""
+
+from . import (
+    datastructures,
+    decorators,
+    files,
+    github,
+    logging,
+    math,
+    processes,
+    shell,
+    string,
+    system,
+    testing,
+    validators,
+)
