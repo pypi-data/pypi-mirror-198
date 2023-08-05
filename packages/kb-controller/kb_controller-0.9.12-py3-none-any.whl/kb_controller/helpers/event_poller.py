a5de0e7fa4009e118bd976e3f840664769ab95eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:51 +0000

    Add package kb-controller/0.9.12/kb_controller-0.9.12-py3-none-any.whl

---
+++ packages/kb-controller/kb_controller-0.9.12-py3-none-any.whl/kb_controller/helpers/event_poller.py
@@ -0,0 +1,35 @@
+from threading import Thread
+from time import sleep
+from datetime import datetime
+from . import microbit
+from . import kb_config
+from . import kb_central
+from . import helper
+
+thread = None
+
+def kb_central_poller(current_track_id):
+    sleep(1) # Give controller time to initialize
+    while True:
+        print()
+        print('- - - - - - - - - - - - - - - - - - - - -')
+        print(f'{helper.getDateString()} POLLER CENTRAL...')
+
+        count = kb_central.get_number_of_events_for_track(current_track_id)
+        microbit.send_message(f'EVENT-TRACKS:{current_track_id}:{count}')
+
+        interval = kb_config.poll_interval_in_seconds
+        text_interval = f'{interval} sekunder' if interval <=60 else f'ca. {"%.0f" % (interval / 60)} minutter'
+
+        print(f'Kontakter kuglebane-centralen igen om {text_interval}...')
+        print('- - - - - - - - - - - - - - - - - - - - -')
+        print()
+        sleep(kb_config.poll_interval_in_seconds)
+
+def start_thread(current_track_id):
+    print()
+    print(f'Starter baggrunds-tråd der poller kb-centralen: {kb_config.kbc_host}')
+    global thread
+    if thread is None:
+        thread = Thread(group=None, daemon=True, target=kb_central_poller, args=(current_track_id,))
+        thread.start()
