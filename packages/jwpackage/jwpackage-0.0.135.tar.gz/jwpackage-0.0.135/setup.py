1b97070736c49980f340f675f6f8d98b192dfe3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:38 +0000

    Add package jwpackage/0.0.135/jwpackage-0.0.135.tar.gz

---
+++ packages/jwpackage/jwpackage-0.0.135.tar.gz/jwpackage-0.0.135/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.135' 
+DESCRIPTION = 'Justwicks package'
+LONG_DESCRIPTION = 'contains shared features and functionality'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="jwpackage", 
+        version=VERSION,
+        author="Preetham Puttaswamy",
+        author_email="<youremail@email.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=["PyJWT==2.6.0","fastapi==0.75.0","cryptography==39.0.2","pyOpenSSL==23.0.0"], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'jwpackage'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
