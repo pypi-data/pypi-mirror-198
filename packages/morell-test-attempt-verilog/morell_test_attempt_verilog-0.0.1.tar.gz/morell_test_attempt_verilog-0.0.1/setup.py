0a015bfdcae1425d9d40e3547ade2a50ab9cb5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:25 +0000

    Add package morell-test-attempt-verilog/0.0.1/morell_test_attempt_verilog-0.0.1.tar.gz

---
+++ packages/morell-test-attempt-verilog/morell_test_attempt_verilog-0.0.1.tar.gz/morell_test_attempt_verilog-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A Verilog Class'
+
+# Setting up
+setup(
+    name="morell_test_attempt_verilog",
+    version=VERSION,
+    author="Morteza Rezaalipour (MorellRAP)",
+    author_email="<rezaalipour.usi@gmail.com>",
+    description=DESCRIPTION,
+    packages=['libname', 'libname.config'],
+    install_requires=['numpy', 'scipy', 'pandas', 'matplotlib', 'networkx', 'python-dateutil', 'PyYAML'],
+    keywords=['python', 'verilog', 'circuits', 'synthesis'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+) 
