b7f1c61d98d93165755b8fbb2a041d28b2807ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:34 +0000

    Add package feather-api/1.0.0rc1/feather-api-1.0.0rc1.tar.gz

---
+++ packages/feather-api/feather-api-1.0.0rc1.tar.gz/feather-api-1.0.0rc1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='feather-api',
+      version='1.0.0-rc.1',
+      description='Python wrapper for Feather\'s API',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='http://github.com/Feather-Official/featherdbtools',
+      author='Feather Technology Inc.',
+      author_email='support@try-feather.com',
+      packages=['featherapi'],
+      install_requires=['setuptools', 'requests'],
+      setup_requires=['setuptools'],
+      zip_safe=False)
