468b645d79a27cbb86fc0de38e957bfe15808def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:12 +0000

    Add package nonumpy/0.1.1a1/nonumpy-0.1.1a1.tar.gz

---
+++ packages/nonumpy/nonumpy-0.1.1a1.tar.gz/nonumpy-0.1.1a1/nonumpy/factory.py
@@ -0,0 +1,70 @@
+from .filters import FilterNameEnum, FilterTypeEnum
+from .filters import Filter
+from .ema import EMA
+
+
+class Factory:
+    def __init__(self, verbose: bool = False, debug: bool = False, path=None):
+        # self._verbose = verbose
+        # self._debug = debug
+        # self._gen_folder = path
+        self._lib = Factory._build_lib(verbose=verbose, debug=debug, path=path)
+
+    # @classmethod
+    # def CreateFilterLowPassButterworth(cls, sample_rate_hz: float, order: int, cutoff_hz: float):
+    #     return Filter(
+    #         name=FilterNameEnum.Butterworth,
+    #         type=FilterTypeEnum.LowPass,
+    #         order=order,
+    #         sample_rate_hz=sample_rate_hz,
+    #         cutoff_low_hz=cutoff_hz)
+
+    # @classmethod
+    # def CreateFilterHighPassButterworth(cls, sample_rate_hz: float, order: int, cutoff_hz: float):
+    #     return Filter(
+    #         name=FilterNameEnum.Butterworth,
+    #         type=FilterTypeEnum.HighPass,
+    #         order=order,
+    #         sample_rate_hz=sample_rate_hz,
+    #         cutoff_low_hz=cutoff_hz)
+
+    # @classmethod
+    # def CreateEMA(cls, alpha: float):
+    #     return EMA(alpha=alpha)
+
+    def create_FilterLowPassButterworth(self, sample_rate_hz: float, order: int, cutoff_hz: float):
+        return Filter(self._lib,
+                      name=FilterNameEnum.Butterworth,
+                      type=FilterTypeEnum.LowPass,
+                      order=order,
+                      sample_rate_hz=sample_rate_hz,
+                      cutoff_low_hz=cutoff_hz)
+
+    def create_FilterHighPassButterworth(self, sample_rate_hz: float, order: int, cutoff_hz: float):
+        return Filter(self._lib,
+                      name=FilterNameEnum.Butterworth,
+                      type=FilterTypeEnum.HighPass,
+                      order=order,
+                      sample_rate_hz=sample_rate_hz,
+                      cutoff_low_hz=cutoff_hz)
+
+    def create_EMA(self, alpha: float):
+        return EMA(self._lib, alpha=alpha)
+
+    @staticmethod
+    def _build_lib(verbose: bool, debug: bool, path: str):
+        from crelm import Tube
+
+        if verbose:
+            print(f'building libnumpy, DEBUG={debug}, gen folder={path}')
+
+        paste = Tube('nonumpy') \
+            .verbose(verbose) \
+            .set_source_folder_from(__file__) \
+            .add_header_files(['nonumpy_ema.h', 'nonumpy_filters.h']) \
+            .add_source_files(['nonumpy_ema.c', 'nonumpy_filters.c']) \
+            .add_macro('NONUMPY_FLOAT_32') \
+            .add_macro_if(debug, 'DEBUG') \
+            .squeeze()
+
+        return paste
