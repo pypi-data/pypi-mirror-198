1c8f38a92a3fe5c7e73be0ed87636c4254f676a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:53 +0000

    Add package mrudtskiy/1.0.1/mrudtskiy-1.0.1.tar.gz

---
+++ packages/mrudtskiy/mrudtskiy-1.0.1.tar.gz/mrudtskiy-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'mrudtskiy',
+    version = '1.0.1',
+    author = 'Max',
+    author_email = '<mrudtskiy@live.ru>',
+    description = 'This module contains function which print nested lists with indents',
+    packages = find_packages(),
+)
