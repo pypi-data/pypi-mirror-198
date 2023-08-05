8e4e4bd7c1cc28c94f973953899d6f5bca28cf5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:46 +0000

    Add package onboardme/0.20.0/onboardme-0.20.0.tar.gz

---
+++ packages/onboardme/onboardme-0.20.0.tar.gz/onboardme-0.20.0/onboardme/env_config.py
@@ -0,0 +1,164 @@
+"""
+environment variable loading library for onboardme
+"""
+import logging as log
+# for system data, environment data, and checking/joining paths
+from os import path
+# rich helps pretty print everything
+from rich.prompt import Confirm
+import yaml
+
+# custom libs
+from .constants import OS, PWD, ONBOARDME_CONFIG_DIR
+from .console_logging import print_panel
+
+
+def load_cfg(config_file: str) -> dict:
+    """
+    load yaml config files for onboardme
+    """
+    # make sure the path is valid
+    if path.exists(config_file):
+        with open(config_file, 'r') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    # return empty dict if there was no file
+    return {}
+
+
+# defaults
+DEFAULTS = load_cfg(f"{PWD}/config/onboardme_config.yaml")
+USR_CONFIG_FILE = load_cfg(path.join(ONBOARDME_CONFIG_DIR, 'config.yaml'))
+
+
+def check_os_support():
+    """
+    verify we're on a supported OS and ask to quit if not.
+    """
+    if OS[0] not in ('Linux', 'Darwin'):
+        msg = (f"[ohno]{OS}[/ohno] isn't officially supported. We"
+               " have only tested Debian, Ubuntu, and macOS.")
+        print_panel(msg, "⚠️  [warn]WARNING")
+
+        quit_y = Confirm.ask("🌊 You're in uncharted waters. Wanna quit?")
+        if quit_y:
+            print_panel("That's probably safer. Have a safe day, friend.",
+                        "Safety Award ☆ ")
+            quit()
+        else:
+            print_panel("[red]Yeehaw, I guess.", "¯\\_(ツ)_/¯")
+    else:
+        print_panel("Operating System and Architechure [green]supported ♥",
+                    "[cornflower_blue]Compatibility Check")
+
+
+def process_steps(steps: list, firewall=False, browser=False):
+    """
+    process which steps to run for which OS, which steps the user passed in,
+    and then make sure dependent steps are always run.
+
+    Returns a list of str type steps to run.
+
+    # TODO: if 'capslock_to_control' in steps: map_caps_to_control()
+    """
+    if steps:
+        steps_list = list(steps)
+        # setting up vim is useless if we don't have a .vimrc
+        if 'vim_setup' in steps_list and 'dot_files' not in steps_list:
+            steps_list.append('dot_files')
+        if browser:
+            steps_list.append('browser_setup')
+        if firewall and 'Linux' in OS:
+            # currently don't have a great firewall on macOS, sans lulu
+            steps_list.append('firewall_setup')
+
+    removed_duplicates = set(steps_list)
+    steps = list(removed_duplicates)
+    default_order = DEFAULTS['steps'][OS[0]]
+
+    # Rearrange list by other list order Using list comprehension
+    result_steps = [ele for ele in default_order if ele in steps]
+
+    return result_steps
+
+
+def sort_pkgmngrs(package_managers_list: list) -> list:
+    """
+    make sure the package managers are in the right order 🤦
+    e.g. apt installs snap and flatpak, so niether can be run until apt is run
+
+    Takes list of package manager str and reorders them be (if they exist):
+       ['brew', 'pip3.11', 'apt', 'snap', 'flatpak']
+    Returns reordered_list
+    """
+    reordered_list = []
+    package_managers = ['brew', 'pip3.11', 'apt', 'snap', 'flatpak']
+
+    for mngr in package_managers:
+        if mngr in package_managers_list:
+            reordered_list.append(mngr)
+
+    return reordered_list
+
+
+def fill_in_defaults(defaults: dict, user_config: dict,
+                     always_prefer_default=False) -> dict:
+    """
+    Compares/Combines a default dict and another dict. Prefer default values
+    only if the value is empty in the second dict. Then return new dict.
+    """
+    for key, value in user_config.items():
+        # we have to iterate through the entire config file, and who knows how
+        # many levels there are, so we use recursion of this function
+        if type(value) is dict:
+            result_config = fill_in_defaults(defaults[key], user_config[key],
+                                             always_prefer_default)
+            user_config[key] = result_config
+
+        if not value or always_prefer_default:
+            user_config[key] = defaults[key]
+
+    return user_config
+
+
+def process_configs(overwrite=False, repo="", git_branch="", pkg_mngrs=[],
+                    pkg_groups=[], firewall=False, remote_host="", steps=[]):
+    """
+    process the config in ~/.config/onboardme/config.yaml if it exists,
+    then process the cli dict, and fill in defaults for anything not explicitly
+    defined. Returns full final config as dict for use in script.
+    """
+    if remote_host:
+        firewall = True
+        if type(remote_host) is str:
+            remote_host = [remote_host]
+
+    cli_dict = {'package': {'managers': {OS[0]: pkg_mngrs},
+                            'groups': pkg_groups},
+                'log': {'file': None, 'level': ""},
+                'remote_hosts': remote_host,
+                'firewall': firewall,
+                'steps': {OS[0]: steps},
+                'dot_files': {'overwrite': overwrite,
+                              'git_url': repo, 'git_branch': git_branch}}
+
+    log.debug(f"cli_dict is:\n{cli_dict}\n")
+    if USR_CONFIG_FILE:
+        log.debug(f"🗂 ⚙️  user_config_file: \n{USR_CONFIG_FILE}\n")
+
+        usr_cfgs = fill_in_defaults(DEFAULTS, USR_CONFIG_FILE)
+        log.debug(f"after usr config file filled in with defaults: {usr_cfgs}")
+
+        final_defaults = fill_in_defaults(cli_dict, usr_cfgs, True)
+    else:
+        final_defaults = fill_in_defaults(DEFAULTS, cli_dict)
+
+    log.debug(" final config after filling cli_dict in with defaults:\n"
+              f"{final_defaults}\n")
+
+    valid_steps = process_steps(final_defaults['steps'][OS[0]],
+                                final_defaults['remote_hosts'])
+    final_defaults['steps'][OS[0]] = valid_steps
+    sorted_mngrs = sort_pkgmngrs(final_defaults['package']['managers'][OS[0]])
+    final_defaults['package']['managers'][OS[0]] = sorted_mngrs
+
+    return final_defaults
---
+++ packages/onboardme/onboardme-0.20.0.tar.gz/onboardme-0.20.0/onboardme/ide_setup.py
@@ -0,0 +1,145 @@
+"""
+NAME:    Onboardme.ide_setup
+DESC:    install vim, neovim, and fonts
+AUTHOR:  Jesse Hitch
+LICENSE: GNU AFFERO GENERAL PUBLIC LICENSE
+"""
+
+import logging as log
+from git import Repo, RemoteProgress
+from os import path
+from pathlib import Path
+import wget
+from xdg_base_dirs import xdg_config_home
+
+# custom libs
+from .constants import HOME_DIR, OS
+from .console_logging import print_header, print_sub_header, print_msg
+from .subproc import subproc
+
+
+def vim_setup() -> None:
+    """
+    Installs vim-plug: does a wget on plug.vim
+    Installs vim plugins: calls vim with +Plug[Install,Upgrade,Upgrade]
+    Compiles youcompleteme if necessary
+    """
+    print_header('[b]vim-plug[/b] and [green][i]Vim[/i][/green] plugins '
+                 'installation [dim]and[/dim] upgrades')
+
+    # this is to make sure we have the correct plugin directory
+    vim_dir = path.join(xdg_config_home(), 'vim')
+    if not path.exists(vim_dir):
+        vim_dir = path.join(HOME_DIR, '.vim')
+        xdg = False
+    else:
+        xdg = True
+
+    # trick to not run youcompleteme init every single time
+    init_ycm = False
+    ycm_dir = path.join(vim_dir, 'plugged/YouCompleteMe')
+    if not path.exists(ycm_dir):
+        init_ycm = True
+
+    # this is for installing vim-plug
+    autoload_dir = path.join(vim_dir, 'autoload')
+    if not path.exists(autoload_dir):
+        print_msg('[i]Creating dir structure & downloading [b]vim-plug[/b]...')
+        Path(autoload_dir).mkdir(parents=True, exist_ok=True)
+        url = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/'
+        wget.download(url + 'plug.vim', autoload_dir)
+
+    # installs the vim plugins if not installed, updates vim-plug, and then
+    # updates all currently installed plugins
+    plug_cmds = "--not-a-term +PlugInstall +PlugUpgrade +PlugUpdate +qall!"
+    if xdg:
+        subproc([f'vim -u {vim_dir}/vimrc {plug_cmds}'], quiet=True)
+    else:
+        subproc([f'vim {plug_cmds}'], quiet=True)
+
+    print_sub_header('Vim Plugins installed.')
+
+    # if we need to install YouCompleteMe, run the compile script
+    if init_ycm:
+        if path.exists(ycm_dir):
+            print_sub_header("Compiling YouCompleteMe vim plugin.")
+            # This is for you complete me, which is a python completion module
+            subproc(["chmod +x install.py", "python3.11 install.py --all"],
+                    cwd=ycm_dir)
+
+
+def neovim_setup() -> None:
+    """
+    neovim plugins have a setup mostly already handled in your plugins.lua:
+    https://github.com/wbthomason/packer.nvim#bootstrapping
+    This is the command that works via the cli:
+    nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
+    uses special command (with packer bootstrapped) to have packer setup your
+    your configuration (or simply run updates) and close once all operations
+    are completed
+
+    This may switch to lazy soon
+    """
+    print_header('[b]packer[/b] and [green][i]NeoVim[/i][/green] plugins '
+                 'installation [dim]and[/dim] upgrades')
+
+    # updates all currently installed plugins (still need to do :PackerCompile)
+    commands = ["nvim --headless +PackerSync"]
+    subproc(commands)
+
+    print_sub_header('NeoVim Plugins installed.')
+
+
+def font_setup() -> None:
+    """
+    On Linux:
+      Clones nerd-fonts repo and does a sparse checkout on only mononoki and
+      hack fonts. Also removes 70-no-bitmaps.conf and links 70-yes-bitmaps.conf
+
+      Then runs install.sh from nerd-fonts repo
+    """
+    if 'Linux' in OS:
+        print_header('📝 [i]font[/i] installations')
+        # not sure if needed anymore
+        # mkdir -p ~/.local/share/fonts
+
+        fonts_dir = f'{HOME_DIR}/repos/nerd-fonts'
+
+        # do a shallow clone of the repo
+        if not path.exists(fonts_dir):
+            log.info('Nerdfonts require some setup on Linux...')
+            bitmap_conf = '/etc/fonts/conf.d/70-no-bitmaps.conf'
+            log.info(f'Going to remove {bitmap_conf} and link a yes map...')
+            # we do all of this with subprocess because I want the sudo prompt
+            if path.exists(bitmap_conf):
+                subproc([f'sudo rm {bitmap_conf}'], quiet=True, spinner=False)
+
+            cmd = ('sudo ln -s /etc/fonts/conf.avail/70-yes-bitmaps.conf '
+                   '/etc/fonts/conf.d/70-yes-bitmaps.conf')
+            subproc([cmd], error_ok=True, quiet=True, spinner=False)
+
+            print_msg('[i]Downloading installer and font sets... ')
+
+            Path(fonts_dir).mkdir(parents=True, exist_ok=True)
+            fonts_repo = 'https://github.com/ryanoasis/nerd-fonts.git'
+
+            class CloneProgress(RemoteProgress):
+                def update(self, op_code, cur_count, max_count=None,
+                           message=''):
+                    if message:
+                        log.info(message)
+
+            Repo.clone_from(fonts_repo, fonts_dir, progress=CloneProgress(),
+                            multi_options=['--sparse', '--filter=blob:none'])
+            cmds = ["git sparse-checkout add patched-fonts/Mononoki",
+                    "git sparse-checkout add patched-fonts/Hack"]
+            subproc(cmds, spinner=True, cwd=fonts_dir)
+        else:
+            subproc(["git pull"], spinner=True, cwd=fonts_dir)
+
+        subproc(['./install.sh Hack', './install.sh Mononoki'], quiet=True,
+                cwd=fonts_dir)
+
+        print_msg('[i][dim]The fonts should be installed, however, you have ' +
+                  'to set your terminal font to the new font. I rebooted too.')
