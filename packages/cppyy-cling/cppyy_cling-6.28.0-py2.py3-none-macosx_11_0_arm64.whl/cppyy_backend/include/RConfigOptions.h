7cb16fef2bd3b2347cfacf11ca8d46e723ed3098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:08 +0000

    Add package cppyy-cling/6.28.0/cppyy_cling-6.28.0-py2.py3-none-macosx_11_0_arm64.whl

---
+++ packages/cppyy-cling/cppyy_cling-6.28.0-py2.py3-none-macosx_11_0_arm64.whl/cppyy_backend/include/RConfigOptions.h
@@ -0,0 +1,7 @@
+#ifndef ROOT_RConfigOptions
+#define ROOT_RConfigOptions
+
+#define R__CONFIGUREOPTIONS   "PYTHON_INCLUDE_DIRS=/opt/local/Library/Frameworks/Python.framework/Versions/3.10/include/python3.10 PYTHON_LIBRARIES=/opt/local/Library/Frameworks/Python.framework/Versions/3.10/lib/libpython3.10.dylib PYTHON_LIBRARY_DIR=/opt/local/Library/Frameworks/Python.framework/Versions/3.10/lib PYTHON_VERSION_MAJOR=3 PYTHON_VERSION_MINOR=10 PYTHON_VERSION_STRING=3.10.9 ZLIB_INCLUDE_DIR=/Users/wlav/wheelie/cppyy-backend/cling/src/builtins/zlib ZLIB_INCLUDE_DIRS=/Users/wlav/wheelie/cppyy-backend/cling/src/builtins/zlib ZLIB_LIBRARIES=ZLIB::ZLIB ZLIB_VERSION=1.2.8 ZLIB_VERSION_STRING=1.2.8 "
+#define R__CONFIGUREFEATURES  "cxx20  builtin_clang builtin_llvm builtin_zlib libcxx shared"
+
+#endif
