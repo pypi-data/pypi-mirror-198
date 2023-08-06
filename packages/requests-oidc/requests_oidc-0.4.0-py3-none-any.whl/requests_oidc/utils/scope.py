348273a7ff34df37921e4954f19713b4e4beace0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:00 +0000

    Add package requests-oidc/0.4.0/requests_oidc-0.4.0-py3-none-any.whl

---
+++ packages/requests-oidc/requests_oidc-0.4.0-py3-none-any.whl/requests_oidc/utils/scope.py
@@ -0,0 +1,10 @@
+from typing import Optional, List
+
+def make_scope(scope: Optional[List[str]]) -> List[str]:
+    if scope is None:
+        return ["openid"]
+
+    if "openid" in scope:
+        return scope
+
+    return ["openid"] + scope
