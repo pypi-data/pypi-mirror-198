2e7ad2079a98a57d2233721e548dcbeb2ec86968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:04 +0000

    Add package mihir-helper-service/0.1.0/mihir-helper-service-0.1.0.tar.gz

---
+++ packages/mihir-helper-service/mihir-helper-service-0.1.0.tar.gz/mihir-helper-service-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="mihir-helper-service",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # List your package's dependencies here
+    ],
+    entry_points={
+        # Define any command-line scripts or console scripts here
+    },
+    # Other metadata about your package can be specified here
+)
