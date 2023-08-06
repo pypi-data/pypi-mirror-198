6922f62bfbd60a4b9701f40f3aab4d9929458d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:44 +0000

    Add package je-api-testka-dev/0.0.70/je_api_testka_dev-0.0.70-py3-none-any.whl

---
+++ packages/je-api-testka-dev/je_api_testka_dev-0.0.70-py3-none-any.whl/apitestka_driver/generate_apitestka_driver.py
@@ -0,0 +1,11 @@
+import sys
+
+from je_api_testka import start_apitestka_socket_server
+
+try:
+    server = start_apitestka_socket_server()
+    while not server.close_flag:
+        pass
+    sys.exit(0)
+except Exception as error:
+    print(repr(error))
