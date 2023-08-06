bdcaff034c2b00c13e632a1b780b353e45316e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:49 +0000

    Add package blackdark-nautobot-secrets-providers/1.8.0/blackdark_nautobot_secrets_providers-1.8.0.tar.gz

---
+++ packages/blackdark-nautobot-secrets-providers/blackdark_nautobot_secrets_providers-1.8.0.tar.gz/blackdark_nautobot_secrets_providers-1.8.0/blackdark_nautobot_secrets_providers/urls.py
@@ -0,0 +1,22 @@
+"""Django urlpatterns declaration for blackdark_nautobot_secrets_providers plugin."""
+from django.urls import path
+
+from blackdark_nautobot_secrets_providers import views
+from .views import IS_SECRET_CHECK_TAB_AVAILABLE
+
+
+
+app_name = "blackdark_nautobot_secrets_providers"
+
+urlpatterns = [
+    path("", views.SecretsProvidersHomeView.as_view(), name="home"),
+]
+if IS_SECRET_CHECK_TAB_AVAILABLE:
+    urlpatterns += [
+        path(
+            "secret/<uuid:pk>/check/",
+            views.SecretDetailNautobotSecretProvidersTabCheckView.as_view(),
+            name="check_secret_tab",
+        ),
+    ]
+
