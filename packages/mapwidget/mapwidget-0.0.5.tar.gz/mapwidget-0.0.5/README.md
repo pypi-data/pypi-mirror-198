c7c852819da844a93481f697410be8bc60c94a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:58 +0000

    Add package mapwidget/0.0.5/mapwidget-0.0.5.tar.gz

---
+++ packages/mapwidget/mapwidget-0.0.5.tar.gz/mapwidget-0.0.5/README.md
@@ -0,0 +1,19 @@
+# mapwidget
+
+[![image](https://img.shields.io/pypi/v/mapwidget.svg)](https://pypi.python.org/pypi/mapwidget)
+[![image](https://img.shields.io/conda/vn/conda-forge/mapwidget.svg)](https://anaconda.org/conda-forge/mapwidget)
+
+**Custom map widgets for creating interactive maps**
+
+The **mapwidget** package provides a set of custom map widgets for creating interactive maps in a Jupyter environment using different JavaScript mapping libraries, such as [cesium](https://cesium.com/), [leaflet](https://leafletjs.com/), [mapbox](https://www.mapbox.com/), [maplibre](https://maplibre.org/), and [openlayers](https://openlayers.org/). The widgets are built on top of the [anywidget](https://github.com/manzt/anywidget) Python package.
+
+-   Free software: MIT license
+-   Documentation: https://mapwidget.gishub.org
+
+## Features
+
+-   TODO
+
+## Credits
+
+This package was created with [Cookiecutter](https://github.com/cookiecutter/cookiecutter) and the [giswqs/pypackage](https://github.com/giswqs/pypackage) project template.
---
+++ packages/mapwidget/mapwidget-0.0.5.tar.gz/mapwidget-0.0.5/mapwidget/javascript/openlayers.js
@@ -0,0 +1,40 @@
+function loadScript(src) {
+    return new Promise((resolve, reject) => {
+        let script = Object.assign(document.createElement("script"), {
+            type: "text/javascript",
+            async: true,
+            src: src,
+        });
+        script.addEventListener("load", resolve);
+        script.addEventListener("error", reject);
+        document.body.appendChild(script);
+    });
+}
+
+await loadScript("https://cdn.jsdelivr.net/npm/ol@v7.3.0/dist/ol.js");
+
+export function render(view) {
+    let center = view.model.get("center");
+    center.reverse();
+    let zoom = view.model.get("zoom");
+    let width = view.model.get("width");
+    let height = view.model.get("height");
+
+    const div = document.createElement("div");
+    div.style.width = width;
+    div.style.height = height;
+
+    var map = new ol.Map({
+        target: div,
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM(),
+            }),
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat(center),
+            zoom: zoom,
+        }),
+    });
+    view.el.appendChild(div);
+}
