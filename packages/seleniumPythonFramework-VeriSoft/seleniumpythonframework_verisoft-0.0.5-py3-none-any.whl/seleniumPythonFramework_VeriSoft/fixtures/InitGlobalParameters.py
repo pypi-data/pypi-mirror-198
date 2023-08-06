980c3665a0c32147096639a54d6604038141e9d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:40:54 +0000

    Add package seleniumPythonFramework-VeriSoft/0.0.5/seleniumpythonframework_verisoft-0.0.5-py3-none-any.whl

---
+++ packages/seleniumPythonFramework-VeriSoft/seleniumpythonframework_verisoft-0.0.5-py3-none-any.whl/seleniumPythonFramework_VeriSoft/fixtures/InitGlobalParameters.py
@@ -0,0 +1,79 @@
+import datetime
+import logging.config
+import yaml
+import pytest
+import json
+
+"""
+Init global parameters fixture function to load all the project necessary global parameters before all the tests running.
+
+@Author: Efrat Cohen
+@Date: 11.2022
+"""
+
+
+def before_all():
+    """
+    Loads project global parameters and store them as pytest global variable, so the parameters will be accessible
+    everywhere
+    """
+
+    # Init Json properties
+    # Opening JSON file
+    f = open(pytest.project_dir + "/app-config.json")
+    # Returns JSON object as a dictionary
+    data = json.load(f)
+    # Store the properties in pytest global variables
+    pytest.properties = data
+    # Closing file
+    f.close()
+
+    # Init Tokens JSON data
+    # Opening token JSON file
+    f = open(pytest.project_dir + "/testsData/tokens.json")
+    # Returns JSON object as a dictionary
+    tokens = json.load(f)
+    # Store the data in pytest global variables
+    pytest.tokens = tokens
+    # Closing file
+    f.close()
+
+    # Init farms JSON data
+    # Opening token JSON file
+    f = open(pytest.project_dir + "/testsData/farms.json")
+    # Returns JSON object as a dictionary
+    farms_data = json.load(f)
+    # Store the data in pytest global variables
+    pytest.farms_data = farms_data
+    # Closing file
+    f.close()
+
+    # Init wallets JSON data
+    # Opening token JSON file
+    f = open(pytest.project_dir + "/testsData/wallets.json")
+    # Returns JSON object as a dictionary
+    wallets_data = json.load(f)
+    # Store the data in pytest global variables
+    pytest.wallets_data = wallets_data
+    # Closing file
+    f.close()
+
+    # Initialize the logger instance.
+    with open(pytest.project_dir + "/Tests/logging.yaml", 'rt') as f:
+        config = yaml.safe_load(f.read())
+    logging.config.dictConfig(config)
+
+    # Get an instance of the logger and use it to write a log
+    # Store the logger in pytest global variables
+    pytest.logger = logging.getLogger(__name__)
+
+
+def get_current_datetime():
+    """
+    init current datetime
+    @return date_time - current time
+    """
+    # Current date and time
+    current_time = datetime.datetime.now()
+    date_time = current_time.strftime("%d-%m-%Y %H-%M-%S")
+    return date_time
