0fd572a4cb68d880e0b833c076454c5d338ee519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:57 +0000

    Add package gmdh/1.0.1/gmdh-1.0.1.tar.gz

---
+++ packages/gmdh/gmdh-1.0.1.tar.gz/gmdh-1.0.1/external/indicators-master/build/CPackConfig.cmake
@@ -0,0 +1,62 @@
+# This file will be configured to contain variables for CPack. These variables
+# should be set in the CMake list file of the project before CPack module is
+# included. The list of available CPACK_xxx variables and their associated
+# documentation may be obtained using
+#  cpack --help-variable-list
+#
+# Some variables are common to all generators (e.g. CPACK_PACKAGE_NAME)
+# and some are specific to a generator
+# (e.g. CPACK_NSIS_EXTRA_INSTALL_COMMANDS). The generator specific variables
+# usually begin with CPACK_<GENNAME>_xxxx.
+
+
+set(CPACK_BUILD_SOURCE_DIRS "C:/Users/Mi/Documents/Diploma/indicators-master;C:/Users/Mi/Documents/Diploma/indicators-master/build")
+set(CPACK_CMAKE_GENERATOR "Visual Studio 16 2019")
+set(CPACK_COMPONENT_UNSPECIFIED_HIDDEN "TRUE")
+set(CPACK_COMPONENT_UNSPECIFIED_REQUIRED "TRUE")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_FILE "C:/Program Files (x86)/CMake/share/cmake-3.19/Templates/CPack.GenericDescription.txt")
+set(CPACK_DEFAULT_PACKAGE_DESCRIPTION_SUMMARY "indicators built using CMake")
+set(CPACK_GENERATOR "TGZ;TXZ")
+set(CPACK_INSTALL_CMAKE_PROJECTS "C:/Users/Mi/Documents/Diploma/indicators-master/build;indicators;ALL;/")
+set(CPACK_INSTALL_PREFIX "C:/Program Files (x86)/indicators")
+set(CPACK_MODULE_PATH "")
+set(CPACK_NSIS_DISPLAY_NAME "indicators 2.2.0")
+set(CPACK_NSIS_INSTALLER_ICON_CODE "")
+set(CPACK_NSIS_INSTALLER_MUI_ICON_CODE "")
+set(CPACK_NSIS_INSTALL_ROOT "$PROGRAMFILES64")
+set(CPACK_NSIS_PACKAGE_NAME "indicators 2.2.0")
+set(CPACK_NSIS_UNINSTALL_NAME "Uninstall")
+set(CPACK_OUTPUT_CONFIG_FILE "C:/Users/Mi/Documents/Diploma/indicators-master/build/CPackConfig.cmake")
+set(CPACK_PACKAGE_DEFAULT_LOCATION "/")
+set(CPACK_PACKAGE_DESCRIPTION_FILE "C:/Program Files (x86)/CMake/share/cmake-3.19/Templates/CPack.GenericDescription.txt")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Activity Indicators for Modern C++")
+set(CPACK_PACKAGE_FILE_NAME "indicators-2.2.0-win64")
+set(CPACK_PACKAGE_HOMEPAGE_URL "https://github.com/p-ranav/indicators")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "indicators 2.2.0")
+set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "indicators 2.2.0")
+set(CPACK_PACKAGE_NAME "indicators")
+set(CPACK_PACKAGE_RELOCATABLE "true")
+set(CPACK_PACKAGE_VENDOR "Humanity")
+set(CPACK_PACKAGE_VERSION "2.2.0")
+set(CPACK_PACKAGE_VERSION_MAJOR "2")
+set(CPACK_PACKAGE_VERSION_MINOR "2")
+set(CPACK_PACKAGE_VERSION_PATCH "0")
+set(CPACK_RESOURCE_FILE_LICENSE "C:/Program Files (x86)/CMake/share/cmake-3.19/Templates/CPack.GenericLicense.txt")
+set(CPACK_RESOURCE_FILE_README "C:/Program Files (x86)/CMake/share/cmake-3.19/Templates/CPack.GenericDescription.txt")
+set(CPACK_RESOURCE_FILE_WELCOME "C:/Program Files (x86)/CMake/share/cmake-3.19/Templates/CPack.GenericWelcome.txt")
+set(CPACK_SET_DESTDIR "OFF")
+set(CPACK_SOURCE_GENERATOR "TGZ;TXZ")
+set(CPACK_SOURCE_IGNORE_FILES ".*\\.suo;.*\\.user;.*\\.userosscache;.*\\.sln\\.docstates;.*\\.userprefs;[Dd]ebug/;[Dd]ebugPublic/;[Rr]elease/;[Rr]eleases/;x64/;x86/;bld/;[Bb]in/;[Oo]bj/;[Ll]og/;\\.vs/;\\.vscode/;[Tt]est[Rr]esult.*/;[Bb]uild[Ll]og\\..*;.*\\.VisualState\\.xml;TestResult\\.xml;[Dd]ebugPS/;[Rr]eleasePS/;dlldata\\.c;project\\.lock\\.json;project\\.fragment\\.lock\\.json;artifacts/;.*_i\\.c;.*_p\\.c;.*_i\\.h;.*\\.ilk;.*\\.meta;.*\\.obj;.*\\.pch;.*\\.pdb;.*\\.pgc;.*\\.pgd;.*\\.rsp;.*\\.sbr;.*\\.tlb;.*\\.tli;.*\\.tlh;.*\\.tmp;.*\\.tmp_proj;.*\\.log;.*\\.vspscc;.*\\.vssscc;\\.builds;.*\\.pidb;.*\\.svclog;.*\\.scc;_Chutzpah.*;ipch/;.*\\.aps;.*\\.ncb;.*\\.opendb;.*\\.opensdf;.*\\.sdf;.*\\.cachefile;.*\\.VC\\.db;.*\\.VC\\.VC\\.opendb;.*\\.psess;.*\\.vsp;.*\\.vspx;.*\\.sap;$tf/;.*\\.gpState;_ReSharper.*/;.*\\.[Rr]e[Ss]harper;.*\\.DotSettings\\.user;\\.JustCode;_TeamCity.*;.*\\.dotCover;_NCrunch_.*;\\..*crunch.*\\.local\\.xml;nCrunchTemp_.*;.*\\.mm\\..*;AutoTest\\.Net/;\\.sass-cache/;[Ee]xpress/;DocProject/buildhelp/;DocProject/Help/.*\\.HxT;DocProject/Help/.*\\.HxC;DocProject/Help/.*\\.hhc;DocProject/Help/.*\\.hhk;DocProject/Help/.*\\.hhp;DocProject/Help/Html2;DocProject/Help/html;publish/;.*\\.[Pp]ublish\\.xml;.*\\.azurePubxml;.*\\.publishproj;PublishScripts/;.*\\.nupkg;.*.*/packages/.*;!.*.*/packages/build/;.*\\.nuget\\.props;.*\\.nuget\\.targets;csx/;.*\\.build\\.csdef;ecf/;rcf/;AppPackages/;BundleArtifacts/;Package\\.StoreAssociation\\.xml;_pkginfo\\.txt;.*\\.[Cc]ache;!.*\\.[Cc]ache/;ClientBin/;~$.*;.*~;.*\\.dbmdl;.*\\.dbproj\\.schemaview;.*\\.jfm;.*\\.pfx;.*\\.publishsettings;node_modules/;orleans\\.codegen\\.cs;Generated_Code/;_UpgradeReport_Files/;Backup.*/;UpgradeLog.*\\.XML;UpgradeLog.*\\.htm;.*\\.mdf;.*\\.ldf;.*\\.rdl\\.data;.*\\.bim\\.layout;.*\\.bim_.*\\.settings;FakesAssemblies/;.*\\.GhostDoc\\.xml;\\.ntvs_analysis\\.dat;.*\\.plg;.*\\.opt;.*.*/.*\\.HTMLClient/GeneratedArtifacts;.*.*/.*\\.DesktopClient/GeneratedArtifacts;.*.*/.*\\.DesktopClient/ModelManifest\\.xml;.*.*/.*\\.Server/GeneratedArtifacts;.*.*/.*\\.Server/ModelManifest\\.xml;_Pvt_Extensions;\\.paket/paket\\.exe;paket-files/;\\.fake/;\\.idea/;.*\\.sln\\.iml;\\.cr/;__pycache__/;.*\\.pyc;build;analysis-cppcheck-build-dir;ideas;desktop\\.iniimages/;.editorconfig;.git;.gitignore;.travis.yml;.appveyor.yml")
+set(CPACK_SOURCE_OUTPUT_CONFIG_FILE "C:/Users/Mi/Documents/Diploma/indicators-master/build/CPackSourceConfig.cmake")
+set(CPACK_SOURCE_PACKAGE_FILE_NAME "indicators-2.2.0")
+set(CPACK_SYSTEM_NAME "win64")
+set(CPACK_TOPLEVEL_TAG "win64")
+set(CPACK_WIX_SIZEOF_VOID_P "8")
+
+if(NOT CPACK_PROPERTIES_FILE)
+  set(CPACK_PROPERTIES_FILE "C:/Users/Mi/Documents/Diploma/indicators-master/build/CPackProperties.cmake")
+endif()
+
+if(EXISTS ${CPACK_PROPERTIES_FILE})
+  include(${CPACK_PROPERTIES_FILE})
+endif()
---
+++ packages/gmdh/gmdh-1.0.1.tar.gz/gmdh-1.0.1/src/gmdh.h
@@ -0,0 +1,160 @@
+#pragma once
+#define BOOST_THREAD_PROVIDES_FUTURE_WHEN_ALL_WHEN_ANY
+#define EIGEN_MPL2_ONLY
+
+#include <vector>
+#include <cmath>
+#include <numeric>
+#include <cstdint>
+#include <utility>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <atomic>
+#include <set>
+
+#include <Eigen/Dense>
+
+#include <boost/asio.hpp>
+#include <boost/function.hpp>
+#include <boost/bind/bind.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/thread/future.hpp>
+#include <boost/type_index.hpp>
+#include <boost/chrono.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/json.hpp>
+
+#include <indicators/progress_bar.hpp>
+#include <indicators/cursor_control.hpp>
+#include <indicators/block_progress_bar.hpp>
+
+#include "gmdh_lib.h"
+#include "common.h"
+#include "gmdh_objects.h"
+#include "criterion.h"
+
+namespace GMDH {    
+
+SplittedData GMDH_API splitData(const MatrixXd& x, const VectorXd& y, double testSize = 0.2,
+    bool shuffle = false, int randomSeed = 0);
+
+class GMDH_API GmdhModel { 
+    //int calculateLeftTasksForVerbose(const std::vector<std::shared_ptr<std::vector<Combination>::iterator> > beginTasksVec, 
+    //const std::vector<std::shared_ptr<std::vector<Combination>::iterator> > endTasksVec) const; 
+protected:
+
+    int level;
+    int inputColsNumber;
+    double lastLevelEvaluation;
+    double currentLevelEvaluation;
+    std::vector<VectorC> bestCombinations;
+
+
+    /**
+     * @brief Get full class name
+     * 
+     * @return String of class name model
+     */
+    std::string getModelName() const;
+
+    VectorVu16 nChooseK(int n, int k) const;
+    double getMeanCriterionValue(const VectorC& sortedCombinations, int k) const;
+    std::string getPolynomialCoeffSign(double coeff, bool isFirstCoeff) const;
+    std::string getPolynomialCoeffValue(double coeff, bool isLastCoeff) const;
+    void polynomialsEvaluation(const SplittedData& data, const Criterion& criterion, IterC beginCoeffsVec, 
+                               IterC endCoeffsVec, std::atomic<int>* leftTasks, int verbose) const;
+    bool nextLevelCondition(int kBest, int pAverage, VectorC& combinations,
+                            const Criterion& criterion, SplittedData& data, double limit);
+    GmdhModel& gmdhFit(const MatrixXd& x, const VectorXd& y, const Criterion& criterion, int kBest, 
+                       double testSize, int pAverage, int threads, int verbose, double limit);
+
+    virtual VectorVu16 generateCombinations(int n_cols) const = 0;
+    virtual void removeExtraCombinations() = 0;
+    virtual bool preparations(SplittedData& data, VectorC&& _bestCombinations) = 0;
+    virtual MatrixXd xDataForCombination(const MatrixXd& x, const VectorU16& comb) const = 0;
+    virtual std::string getPolynomialPrefix(int levelIndex, int combIndex) const = 0;
+    virtual std::string getPolynomialVariable(int levelIndex, int coeffIndex, int coeffsNumber, 
+                                              const VectorU16& bestColsIndexes) const = 0;
+
+    /**
+     * @brief Transform model data to JSON format for saving
+     * 
+     * @return JSON value of model data
+     */
+    virtual boost::json::value toJSON() const;
+
+    /**
+     * @brief Set up model from JSON format model data
+     *
+     * @param jsonModel model data in JSON format
+     * 
+     * @return Method exit status
+     */
+    virtual int fromJSON(boost::json::value jsonModel);
+
+    static SplittedData internalSplitData(const MatrixXd& x, const VectorXd& y, double testSize, bool addOnesCol = false);
+
+    friend SplittedData splitData(const MatrixXd& x, const VectorXd& y, double testSize,
+                                           bool shuffle, int randomSeed);
+
+    void checkMatrixColsNumber(const MatrixXd& x) const;
+public:
+    GmdhModel() : level(1), lastLevelEvaluation(0) {}
+
+    /**
+     * @brief Save model data into regular file
+     *
+     * @param path path to regular file
+     * 
+     * @return Method exit status
+     */
+    int save(const std::string& path) const;
+
+    /**
+     * @brief Load model data from regular file
+     *
+     * @param path path to regular file
+     * 
+     * @warning if opening file has a large size then program falls without exceptions
+     * 
+     * @throw if the opening file JSON structure is broken can throw exceptions as std::invalid_argument or std::out_of_range
+     * 
+     * @return Method exit status
+     */
+    int load(const std::string& path);
+
+    VectorXd predict(const RowVectorXd& x, int lags) const;
+    virtual VectorXd predict(const MatrixXd& x) const = 0;
+    std::string getBestPolynomial() const;
+
+    virtual ~GmdhModel() {};
+};
+
+/**
+ * @brief Validate input params values and correct erroneous
+ *
+ * @param testSize value size of test selection
+ * @param pAverage value of ...
+ * @param threads value of using threads for fit
+ * @param kBest value of ...
+ * 
+ * Exit status stores info about erroneous values in bit format
+ * if testSize is erroneous then status = 0b1
+ * if pAverage is erroneous then status = 0b10
+ * if threads is erroneous then status = 0b100
+ * ...
+ * 
+ * @return Method exit status
+ */
+
+int GMDH_API validateInputData(double* testSize, int* pAverage = nullptr, int* threads = nullptr,
+                               int* verbose = nullptr, double* limit = nullptr, int* kBest = nullptr);
+
+std::string&& getVariableName(std::string&& pyName, std::string&& cppName);
+PairMVXd GMDH_API timeSeriesTransformation(const VectorXd& timeSeries, int lags);
+}
