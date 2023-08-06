6794583aa61274f2427a6e29f74ae60b889e6c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:19 +0000

    Add package snmp/0.5.0/snmp-0.5.0-cp310-cp310-win_amd64.whl

---
+++ packages/snmp/snmp-0.5.0-cp310-cp310-win_amd64.whl/snmp/dispatcher.py
@@ -0,0 +1,97 @@
+__all__ = ["Dispatcher"]
+
+import threading
+
+from abc import abstractmethod
+from snmp.ber import ParseError, decode
+from snmp.exception import *
+from snmp.message import *
+from snmp.security.levels import noAuthNoPriv
+from snmp.transport import *
+from snmp.types import SEQUENCE, Integer
+from snmp.utils import typename
+
+class Dispatcher(TransportListener):
+    def __init__(self, multiplexor):
+        self.lock = threading.Lock()
+        self.msgProcessors = {}
+
+        self.transports = {}
+        self.multiplexor = multiplexor
+        self.thread = None
+
+    def addMessageProcessor(self, mp):
+        with self.lock:
+            self.msgProcessors[mp.VERSION] = mp
+
+    def connectTransport(self, transport):
+        domain = transport.DOMAIN
+        if domain in self.transports:
+            if self.transports.get(domain) is transport:
+                return
+
+            errmsg = "This {} instance is already connected to {}"
+            raise ValueError(errmsg.format(typename(self), str(domain)))
+
+        if self.thread is not None:
+            self.multiplexor.stop()
+            self.thread.join()
+
+        self.multiplexor.register(transport)
+        self.thread = threading.Thread(
+            target=self.multiplexor.listen,
+            args=(self,)
+        )
+
+        self.thread.start()
+        self.transports[domain] = transport
+
+    def hear(self, transport, address, data):
+        try:
+            try:
+                msgVersion = MessageVersion.decode(data).version
+            except ParseError:
+                return
+
+            with self.lock:
+                try:
+                    mp = self.msgProcessors[msgVersion]
+                except KeyError:
+                    return
+
+            try:
+                message, handle = mp.prepareDataElements(data)
+                handle.push(message)
+            except IncomingMessageError:
+                return
+
+        except AssertionError:
+            pass
+        except Exception:
+            pass
+
+    def sendPdu(self, locator, mpm, pdu, handle, *args, **kwargs):
+        try:
+            transport = self.transports[locator.domain]
+        except KeyError as err:
+            domain = str(TransportDomain(locator.domain))
+            raise ValueError("{} is not enabled".format(domain)) from err
+
+        with self.lock:
+            try:
+                mp = self.msgProcessors[mpm]
+            except KeyError as err:
+                mpm = str(MessageProcessingModel(mpm))
+                raise ValueError("{} is not enabled".format(mpm)) from err
+
+        msg = mp.prepareOutgoingMessage(pdu, handle, *args, **kwargs)
+        transport.send(locator.address, msg)
+
+    def shutdown(self):
+        if self.thread is not None:
+            self.multiplexor.stop()
+            self.thread.join()
+
+        self.multiplexor.close()
+        self.transports.clear()
+        self.thread = None
---
+++ packages/snmp/snmp-0.5.0-cp310-cp310-win_amd64.whl/snmp/transport/__init__.py
@@ -0,0 +1,74 @@
+__all__ = ["TransportDomain", "TransportListener", "TransportMultiplexor"]
+
+from abc import abstractmethod
+from collections import namedtuple
+import enum
+import os
+import socket
+
+from snmp.typing import *
+
+class TransportDomain(enum.Enum):
+    def __init__(self, family: socket.AddressFamily, loopback: str):
+        self.address_family = family
+        self.loopback_address = loopback
+
+    UDP_IPv4 = socket.AF_INET, "127.0.0.1"
+    UDP_IPv6 = socket.AF_INET6, "::1"
+
+T = TypeVar("T")
+class TransportLocator(Generic[T]):
+    def __init__(self, domain: TransportDomain, address: T) -> None:
+        self.domain = domain
+        self.address = address
+
+class Transport(Generic[T]):
+    DOMAIN: ClassVar[TransportDomain]
+
+    @classmethod
+    def Locator(cls, address: T) -> TransportLocator[T]:
+        return TransportLocator(cls.DOMAIN, cls.normalizeAddress(address))
+
+    @classmethod
+    @abstractmethod
+    def normalizeAddress(cls, address: Any) -> T:
+        ...
+
+    @abstractmethod
+    def close(self) -> None:
+        ...
+
+    @abstractmethod
+    def send(self, address: T, data: bytes) -> None:
+        ...
+
+class TransportListener:
+    @abstractmethod
+    def hear(self, transport: Transport[T], address: T, data: bytes) -> None:
+        ...
+
+TransportType = TypeVar("TransportType", bound=Transport[Tuple[str, int]])
+class TransportMultiplexor(Generic[TransportType]):
+    @abstractmethod
+    def register(self, sock: TransportType) -> None:
+        ...
+
+    @abstractmethod
+    def listen(self, listener: TransportListener) -> None:
+        ...
+
+    @abstractmethod
+    def stop(self) -> None:
+        ...
+
+    @abstractmethod
+    def close(self) -> None:
+        ...
+
+supported = ("posix")
+if os.name in supported:
+    package = os.name
+else:
+    package = "generic"
+
+package = "{}.{}".format(__name__, package)
