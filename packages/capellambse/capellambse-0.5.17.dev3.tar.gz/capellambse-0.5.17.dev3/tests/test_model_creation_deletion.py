2b8c9ffdb09af5a6176dbefb19193387d05b0249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:37 +0000

    Add package capellambse/0.5.17.dev3/capellambse-0.5.17.dev3.tar.gz

---
+++ packages/capellambse/capellambse-0.5.17.dev3.tar.gz/capellambse-0.5.17.dev3/tests/test_model_creation_deletion.py
@@ -0,0 +1,104 @@
+# SPDX-FileCopyrightText: Copyright DB Netz AG and the capellambse contributors
+# SPDX-License-Identifier: Apache-2.0
+
+"""Tests for creating and deleting model elements."""
+# pylint: disable=missing-function-docstring, redefined-outer-name
+import pathlib
+
+import pytest
+
+import capellambse
+
+TEST_ROOT = pathlib.Path(__file__).parent / "data" / "writemodel"
+TEST_MODEL = "WriteTestModel.aird"
+
+XPATH_UUID = "//*[@id={!r}]"
+
+
+@pytest.fixture
+def model():
+    return capellambse.MelodyModel(TEST_ROOT / TEST_MODEL)
+
+
+def test_created_elements_can_be_accessed_in_model(
+    model: capellambse.MelodyModel,
+):
+    newobj = model.la.root_component.components.create(name="TestComponent")
+
+    assert newobj is not None
+    assert isinstance(newobj, capellambse.model.layers.la.LogicalComponent)
+    assert newobj in model.la.root_component.components
+
+
+def test_created_elements_show_up_in_xml_after_adding_them(
+    model: capellambse.MelodyModel,
+):
+    newobj = model.la.root_component.components.create(name="TestComponent")
+
+    try:
+        model._loader[newobj.uuid]
+    except KeyError as err:
+        raise AssertionError(
+            "Cannot find added element via subscripting"
+        ) from err
+
+    assert model._loader.xpath(
+        XPATH_UUID.format(newobj.uuid)
+    ), "Cannot find added element via XPath"
+
+
+@pytest.mark.parametrize(
+    "deletion_target",
+    [0, slice(None, 1)],
+)
+def test_deleted_elements_are_removed(
+    model: capellambse.MelodyModel, deletion_target
+):
+    comps = model.la.root_component.components
+    assert len(comps) == 2, "Precondition not met: Bad list length"
+
+    olduuid = comps[0].uuid
+    del comps[deletion_target]
+
+    assert len(comps) != 2, "List length did not change"
+
+    with pytest.raises(KeyError):
+        model._loader[olduuid]  # pylint: disable=pointless-statement
+
+    assert not model._loader.xpath(
+        XPATH_UUID.format(olduuid)
+    ), "Element is still present in tree after deleting"
+
+
+def test_delete_all_deletes_matching_objects(model: capellambse.MelodyModel):
+    comps = model.la.root_component.components
+    assert len(comps) == 2
+
+    comps.delete_all(name="Delete Me")
+    assert len(comps) == 1
+    assert comps[0].name == "Keep Me"
+
+
+def test_create_adds_missing_namespace_to_fragment(
+    model: capellambse.MelodyModel,
+) -> None:
+    assert "Requirements" not in model._element.nsmap, "Precondition failed"
+    module = model.by_uuid("85a31dd7-7755-486b-b803-1df8915e2cf9")
+
+    module.requirements.create(name="TestReq")
+
+    assert "Requirements" in model._element.nsmap
+
+
+def test_adding_a_namespace_preserves_the_capella_version_comment(
+    model: capellambse.MelodyModel,
+) -> None:
+    assert "Requirements" not in model._element.nsmap, "Precondition failed"
+    prev_elements = list(model._element.itersiblings(preceding=True))
+    assert len(prev_elements) == 1, "No version comment to preserve?"
+
+    model._loader.add_namespace(model._element, "Requirements")
+
+    prev_elements = list(model._element.itersiblings(preceding=True))
+    assert len(prev_elements) == 1
+    assert model.info.capella_version != "UNKNOWN"
