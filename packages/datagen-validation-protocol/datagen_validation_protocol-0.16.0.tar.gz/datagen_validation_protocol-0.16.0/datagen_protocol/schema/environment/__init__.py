4d688e93bb168ea86286bdf26eecb257b2b43e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:55 +0000

    Add package datagen-validation-protocol/0.16.0/datagen_validation_protocol-0.16.0.tar.gz

---
+++ packages/datagen-validation-protocol/datagen_validation_protocol-0.16.0.tar.gz/datagen_validation_protocol-0.16.0/datagen_protocol/schema/environment/__init__.py
@@ -0,0 +1,12 @@
+from .camera import (
+    Camera,
+    SequenceCamera,
+    CameraIntrinsicParams,
+    SequenceIntrinsicParams,
+    CameraExtrinsicParams,
+    Wavelength,
+    CameraProjection,
+    SequenceCameraProjection,
+)
+from .background import Background
+from .light import Light, LightType
