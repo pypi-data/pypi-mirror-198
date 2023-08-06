5564000e9a298e5461a1260ec33f346bc3059d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:52 +0000

    Add package nebullvm/0.9.1/nebullvm-0.9.1.tar.gz

---
+++ packages/nebullvm/nebullvm-0.9.1.tar.gz/nebullvm-0.9.1/nebullvm/optional_modules/torch.py
@@ -0,0 +1,44 @@
+from nebullvm.optional_modules.dummy import DummyClass
+
+try:
+    import torch  # noqa F401
+    from torch.nn import Module  # noqa F401
+    from torch.jit import ScriptModule  # noqa F401
+    from torch.fx import GraphModule
+    from torch.utils.data import DataLoader, Dataset  # noqa F401
+    from torch.quantization.quantize_fx import (  # noqa F401
+        prepare_fx,
+        convert_fx,
+    )
+
+    from torch.ao.quantization.stubs import QuantStub, DeQuantStub
+    from torch.fx import symbolic_trace
+    from torch.quantization import default_dynamic_qconfig
+except ImportError:
+
+    class nn:
+        Module = DummyClass
+
+    class torch:
+        float = half = int8 = DummyClass
+        float16 = float32 = int32 = int64 = DummyClass
+        Tensor = DummyClass
+        dtype = DummyClass
+        nn = nn
+
+        @staticmethod
+        def no_grad():
+            return lambda x: None
+
+        @staticmethod
+        def inference_mode():
+            return lambda x: None
+
+    Dataset = DummyClass
+    Module = DummyClass
+    ScriptModule = DummyClass
+    GraphModule = DummyClass
+    DataLoader = DummyClass
+    symbolic_trace = None
+    QuantStub = DeQuantStub = DummyClass
+    default_dynamic_qconfig = prepare_fx = convert_fx = None
