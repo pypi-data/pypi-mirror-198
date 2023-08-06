f4e98788580518b6f85c02e0310ebad2dc92937f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:09 +0000

    Add package cppyy-cling/6.28.0/cppyy_cling-6.28.0-py2.py3-none-manylinux2014_x86_64.whl

---
+++ packages/cppyy-cling/cppyy_cling-6.28.0-py2.py3-none-manylinux2014_x86_64.whl/cppyy_backend/include/RConfigOptions.h
@@ -0,0 +1,7 @@
+#ifndef ROOT_RConfigOptions
+#define ROOT_RConfigOptions
+
+#define R__CONFIGUREOPTIONS   "PYTHON_VERSION_MAJOR=3 PYTHON_VERSION_MINOR=8 PYTHON_VERSION_STRING=3.8.7 ZLIB_INCLUDE_DIR=/root/cppyy-backend/cling/src/builtins/zlib ZLIB_INCLUDE_DIRS=/root/cppyy-backend/cling/src/builtins/zlib ZLIB_LIBRARIES=ZLIB::ZLIB ZLIB_VERSION=1.2.8 ZLIB_VERSION_STRING=1.2.8 "
+#define R__CONFIGUREFEATURES  "cxx14  builtin_clang builtin_llvm builtin_zlib shared"
+
+#endif
