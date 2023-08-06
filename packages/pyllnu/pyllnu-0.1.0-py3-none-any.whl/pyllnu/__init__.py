ac4408c3a86a73a85c4d18d185571d82557c990a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:59 +0000

    Add package pyllnu/0.1.0/pyllnu-0.1.0-py3-none-any.whl

---
+++ packages/pyllnu/pyllnu-0.1.0-py3-none-any.whl/pyllnu/__init__.py
@@ -0,0 +1,10 @@
+"""PyLLNU, Python Low-Level Network Utils
+
+PyLLNU is a package that allows developers to send, receive, encode and decode
+binary network packets.
+"""
+
+__author__ = "Rafael de Bem"
+__license__ = "MIT"
+__maintainer__ = "Rafael de Bem"
+__email__ = "debemrafael@gmail.com"
