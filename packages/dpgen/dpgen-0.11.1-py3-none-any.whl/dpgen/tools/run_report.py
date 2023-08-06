627fe06f4a9030009aa5776fc728339d9b99a671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:33 +0000

    Add package dpgen/0.11.1/dpgen-0.11.1-py3-none-any.whl

---
+++ packages/dpgen/dpgen-0.11.1-py3-none-any.whl/dpgen/tools/run_report.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import argparse
+import glob
+import json
+import os
+import shutil
+import subprocess as sp
+import sys
+
+import numpy as np
+
+from dpgen.tools.stat_iter import stat_iter
+from dpgen.tools.stat_sys import stat_sys
+from dpgen.tools.stat_time import stat_time
+
+
+def run_report(args):
+    report_count = 0
+    if args.stat_sys:
+        stat_sys(args.JOB_DIR, args.param, args.verbose)
+        report_count += 1
+    # other stats added in the following
+    if args.stat_iter:
+        stat_iter(args.JOB_DIR, args.param, args.verbose)
+        report_count += 1
+    if args.stat_time:
+        stat_time(args.JOB_DIR, args.param, args.verbose)
+        report_count += 1
+    if report_count == 0:
+        print("nothing to report, rerun with -h for help")
+
+    return report_count
