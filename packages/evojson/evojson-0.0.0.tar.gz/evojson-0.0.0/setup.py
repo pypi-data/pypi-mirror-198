abd48e9a3d74956009cf6b288b41749f4b223da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:29 +0000

    Add package evojson/0.0.0/evojson-0.0.0.tar.gz

---
+++ packages/evojson/evojson-0.0.0.tar.gz/evojson-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="evojson",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+    ],
+)
