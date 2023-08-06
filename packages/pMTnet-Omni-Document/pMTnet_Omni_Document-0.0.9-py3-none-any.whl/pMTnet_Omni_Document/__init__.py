25a7b9f59686562db2f3e4948846c10580b74e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:59 +0000

    Add package pMTnet-Omni-Document/0.0.9/pMTnet_Omni_Document-0.0.9-py3-none-any.whl

---
+++ packages/pMTnet-Omni-Document/pMTnet_Omni_Document-0.0.9-py3-none-any.whl/pMTnet_Omni_Document/__init__.py
@@ -0,0 +1,15 @@
+"""
+pMTnet Omni Document
+
+Document for pMTnet Omni
+"""
+
+import os 
+
+__version__ = "0.0.9"
+__author__ = "Yi Han, Yuqiu Yang, Tao Wang"
+
+# location would be pMTnet_Omni_Document/pMTnet_Omni_Document
+location = os.path.dirname(os.path.realpath(__file__))
+
+validation_data_path = os.path.join(location, "validation_data")
