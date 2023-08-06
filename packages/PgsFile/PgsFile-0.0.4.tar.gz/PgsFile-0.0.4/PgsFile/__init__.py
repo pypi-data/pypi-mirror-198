7203117fb9de7660d06d6cd02339d34e0f178ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:23 +0000

    Add package PgsFile/0.0.4/PgsFile-0.0.4.tar.gz

---
+++ packages/PgsFile/PgsFile-0.0.4.tar.gz/PgsFile-0.0.4/PgsFile/__init__.py
@@ -0,0 +1,21 @@
+from .PgsFile import get_data_text
+from .PgsFile import get_data_lines
+from .PgsFile import get_data_excel
+from .PgsFile import get_data_json
+
+from .PgsFile import write_to_txt
+from .PgsFile import write_to_excel
+from .PgsFile import write_to_json
+
+from .PgsFile import FilePath
+from .PgsFile import FileName
+
+from .PgsFile import BigPunctuation
+from .PgsFile import StopTags
+from .PgsFile import Special
+from .PgsFile import StopWords
+from .PgsFile import EnPunctuation
+
+from .PgsFile import word_list
+
+name = "PgsFile"
