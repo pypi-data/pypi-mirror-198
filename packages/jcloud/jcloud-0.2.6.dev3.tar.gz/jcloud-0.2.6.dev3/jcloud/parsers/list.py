f9c13f79273854671ea0ba7282f331f52bee44bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:34 +0000

    Add package jcloud/0.2.6.dev3/jcloud-0.2.6.dev3.tar.gz

---
+++ packages/jcloud/jcloud-0.2.6.dev3.tar.gz/jcloud-0.2.6.dev3/jcloud/parsers/list.py
@@ -0,0 +1,23 @@
+from .base import set_base_parser
+
+
+def set_list_parser(parser=None):
+    from ..constants import Phase
+
+    if not parser:
+        parser = set_base_parser()
+
+    parser.add_argument(
+        '--phase',
+        type=str.title,
+        choices=[s.value for s in Phase] + ['All'],
+        help='Pass the phase of Flows to be listed.',
+    )
+
+    parser.add_argument(
+        '--name',
+        type=str,
+        default=None,
+        help='Pass the name of Flows to be listed.',
+    )
+    return parser
