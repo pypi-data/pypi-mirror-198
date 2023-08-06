c381c82d21b0967d0b6ede465d99a2cc5fa9d962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:44 +0000

    Add package feabas/0.1.1/feabas-0.1.1.tar.gz

---
+++ packages/feabas/feabas-0.1.1.tar.gz/feabas-0.1.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.1'
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='feabas',
+    version=VERSION,
+    description='A python library for stitching & alignment of connectome datasets using finite-element method.',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author='Yuelong Wu',
+    author_email='yuelong.wu.2017@gmail.com',
+    packages=find_packages(),
+    license='MIT',
+    install_requires=[
+        'h5py',
+        'matplotlib',
+        'opencv-python',
+        'pyyaml',
+        'rtree',
+        'scipy',
+        'shapely>=2.0.0',
+        'triangle'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3'
+    ]
+)
