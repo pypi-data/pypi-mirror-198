1baf3b8cc5b9760104611ba5abcead871614bd35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:42 +0000

    Add package shayanshamsi-distributions/0.1/shayanshamsi_distributions-0.1.tar.gz

---
+++ packages/shayanshamsi-distributions/shayanshamsi_distributions-0.1.tar.gz/shayanshamsi_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='shayanshamsi_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['shayanshamsi_distributions'],
+      zip_safe=False)
