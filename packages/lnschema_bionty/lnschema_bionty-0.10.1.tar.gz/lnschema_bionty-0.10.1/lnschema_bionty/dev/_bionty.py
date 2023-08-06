4105e3279aafbe7b8289dce722dcfb3a5f4de9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:33 +0000

    Add package lnschema_bionty/0.10.1/lnschema_bionty-0.10.1.tar.gz

---
+++ packages/lnschema_bionty/lnschema_bionty-0.10.1.tar.gz/lnschema_bionty-0.10.1/lnschema_bionty/dev/_bionty.py
@@ -0,0 +1,48 @@
+from bionty import Entity
+
+
+def fields_from_knowledge(
+    locals: dict,
+    knowledge_table: Entity,
+):
+    kwargs = {}
+    for k, v in locals.items():
+        df = knowledge_table(id=k).df
+        if v not in df.index:
+            continue
+        kwargs = df.loc[v].to_dict()
+        if "ontology_id" in kwargs:
+            # TODO: save to Readout.df like others
+            if knowledge_table.__class__.__name__ == "Readout":
+                kwargs = knowledge_table().get_term(kwargs["ontology_id"])
+            kwargs["id"] = kwargs["ontology_id"]
+    pydantic_attrs = kwargs
+    return pydantic_attrs
+
+
+def init_sqlmodel_parent(model, pydantic_attrs):
+    if len(pydantic_attrs) == 0:
+        return
+
+    for k, v in pydantic_attrs.items():
+        if k not in model.__fields__:
+            continue
+        model.__setattr__(k, v)
+
+
+def knowledge(knowledge_table: Entity):
+    def test(original_class):
+        orig_init = original_class.__init__
+
+        def __init__(self, **kwargs):
+            pydantic_attrs = fields_from_knowledge(locals=kwargs, knowledge_table=knowledge_table)
+
+            orig_init(self, **kwargs)
+
+            init_sqlmodel_parent(self, pydantic_attrs)
+
+        original_class.__init__ = __init__
+
+        return original_class
+
+    return test
