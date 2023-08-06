77e43ddccae241ed1411acc132ee12a3589274d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:56 +0000

    Add package acdh-cidoc-pyutils/0.16.1/acdh_cidoc_pyutils-0.16.1.tar.gz

---
+++ packages/acdh-cidoc-pyutils/acdh_cidoc_pyutils-0.16.1.tar.gz/acdh_cidoc_pyutils-0.16.1/acdh_cidoc_pyutils/namespaces.py
@@ -0,0 +1,22 @@
+from rdflib import Namespace
+
+CIDOC = Namespace("http://www.cidoc-crm.org/cidoc-crm/")
+FRBROO = Namespace("https://cidoc-crm.org/frbroo/sites/default/files/FRBR2.4-draft.rdfs#")
+
+NSMAP = {
+    "tei": "http://www.tei-c.org/ns/1.0",
+    "xml": "http://www.w3.org/XML/1998/namespace",
+}
+
+DATE_ATTRIBUTE_DICT = {
+    "notBefore": "start",
+    "notBefore-iso": "start",
+    "from": "start",
+    "from-iso": "start",
+    "notAfter": "end",
+    "notAfter-iso": "end",
+    "to": "end",
+    "to-iso": "end",
+    "when": "when",
+    "when-iso": "when"
+}
