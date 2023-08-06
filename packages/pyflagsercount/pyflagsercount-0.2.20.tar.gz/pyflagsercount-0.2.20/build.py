582f3a4933a5fa01a63e188db17492931bf548b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:14 +0000

    Add package pyflagsercount/0.2.20/pyflagsercount-0.2.20.tar.gz

---
+++ packages/pyflagsercount/pyflagsercount-0.2.20.tar.gz/pyflagsercount-0.2.20/build.py
@@ -0,0 +1,95 @@
+import os
+import re
+import sys
+import platform
+import subprocess
+
+from typing import Any, Dict
+
+from setuptools import Extension
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the "
+                               " following extensions: " +
+                               " , ".join(e.name for e in self.extensions))
+
+        if platform.system() == "Windows":
+            cmake_version = version.parse(re.search(r'version\s*([\d.]+)',
+                                                    out.decode()).group(1))
+            if cmake_version < version.parse("3.1.0"):
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+        self.install_dependencies()
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def install_dependencies(self):
+        dir_start = os.getcwd()
+        dir_pybind11 = os.path.join(dir_start, 'pybind11')
+        if os.path.exists(dir_pybind11):
+            return 0
+        os.mkdir(dir_pybind11)
+        subprocess.check_call(['git', 'clone',
+                               'https://github.com/pybind/pybind11.git',
+                               dir_pybind11])
+        #subprocess.check_call(['git', 'submodule', 'update', '--init', '--recursive'])
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.join(os.path.dirname(
+            self.get_ext_fullpath(ext.name)), 'pyflagsercount', 'modules'))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
+                cfg.upper(), extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
+
+        if sys.platform.startswith("darwin"):
+            # Cross-compile support for macOS - respect ARCHFLAGS if set
+            archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
+            if archs:
+                cmake_args += \
+                    ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get('CXXFLAGS', ''), self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
+                              cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args,
+                              cwd=self.build_temp)
+
+
+def build(setup_kwargs: Dict[str, Any]) -> None:
+    setup_kwargs.update(
+        {
+            "ext_modules": [CMakeExtension('pyflagsercount')],
+            "cmdclass": dict(build_ext=CMakeBuild),
+            "zip_safe": False,
+        }
+    )
