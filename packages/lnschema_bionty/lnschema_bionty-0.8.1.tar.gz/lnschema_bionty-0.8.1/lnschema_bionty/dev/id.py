a0aa113f881d5085d9256b7e099a2ab7e01c8778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:42 +0000

    Add package lnschema_bionty/0.8.1/lnschema_bionty-0.8.1.tar.gz

---
+++ packages/lnschema_bionty/lnschema_bionty-0.8.1.tar.gz/lnschema_bionty-0.8.1/lnschema_bionty/dev/id.py
@@ -0,0 +1,16 @@
+"""IDs.
+
+Entity-related generators:
+
+.. autosummary::
+   :toctree: .
+
+   gene
+   protein
+   species
+   cell_marker
+   disease
+   tissue
+   cell_type
+"""
+from ._id import cell_marker, cell_type, disease, gene, protein, species, tissue  # noqa
