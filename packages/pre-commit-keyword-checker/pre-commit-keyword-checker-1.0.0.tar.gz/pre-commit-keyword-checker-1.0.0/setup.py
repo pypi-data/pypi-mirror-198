3568e6ac0b4595b132cb2d6d8daffbe108890503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:27 +0000

    Add package pre-commit-keyword-checker/1.0.0/pre-commit-keyword-checker-1.0.0.tar.gz

---
+++ packages/pre-commit-keyword-checker/pre-commit-keyword-checker-1.0.0.tar.gz/pre-commit-keyword-checker-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pre-commit-keyword-checker",
+    version="1.0.0",
+    description="A pre-commit hook for checking specified keywords in the files being committed.",
+    author="Ali Yaman",
+    author_email="aliymn.db@gmail.com",
+    url="https://github.com/AliYmn/pre-commit-keyword-checker",
+    packages=find_packages(),
+    install_requires=["pre-commit"],
+    entry_points={
+        "console_scripts": ["check-keywords = check_keywords.main:main"],
+    },
+)
