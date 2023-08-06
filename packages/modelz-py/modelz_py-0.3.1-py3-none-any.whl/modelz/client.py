7c8446cba2b5b7ab16e1a0c14b31dc8a7e41292a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:26 +0000

    Add package modelz-py/0.3.1/modelz_py-0.3.1-py3-none-any.whl

---
+++ packages/modelz-py/modelz_py-0.3.1-py3-none-any.whl/modelz/client.py
@@ -0,0 +1,96 @@
+from __future__ import annotations
+from typing import Any, Generator
+import logging
+from http import HTTPStatus
+from urllib.parse import urljoin
+
+import httpx
+
+from .env import EnvConfig
+from .serde import Serde, SerdeEnum
+
+
+TIMEOUT = httpx.Timeout(5, read=300, write=300)
+logger = logging.getLogger(__name__)
+config = EnvConfig()
+
+
+class ModelzAuth(httpx.Auth):
+    def __init__(self, key: str | None = None) -> None:
+        self.key: str = key if key else config.api_key
+        if not self.key:
+            raise RuntimeError("cannot find the API key")
+
+    def auth_flow(
+        self, request: httpx.Request
+    ) -> Generator[httpx.Request, httpx.Response, None]:
+        request.headers["X-API-Key"] = self.key
+        yield request
+
+
+class InferenceResponse:
+    def __init__(self, resp: httpx.Response, serde: Serde):
+        if resp.status_code != HTTPStatus.OK:
+            logger.warn("err[%d]: %s", resp.status_code, resp.text)
+            raise ValueError(f"inference err: {resp.text}")
+        self.resp = resp
+        self.serde = serde
+        self._data = None
+
+    def _decode(self) -> Any:
+        self._data = self.serde.decode(self.resp.content)
+        return self._data
+
+    def save_to_file(self, file: str):
+        with open(file, "wb") as f:
+            f.write(self.data)
+
+    @property
+    def data(self) -> Any:
+        return self._decode()
+
+
+class ModelzClient:
+    def __init__(
+        self,
+        project: str,
+        key: str | None,
+        host: str | None = None,
+        serde: str = "json",
+    ) -> None:
+        """Create a Modelz Client.
+
+        Args:
+            project: Project ID
+            key: API key
+            host: Modelz host address
+            serde: serialize/deserialize method, choose from ("json", "msg", "raw")
+        """
+        self.host = host if host else config.host
+        auth = ModelzAuth(key)
+        self.project = project
+        self.client = httpx.Client(auth=auth)
+        self.serde: Serde = SerdeEnum[serde.lower()].value()
+
+    def inference(
+        self, params: Any, timeout: float | httpx.Timeout = TIMEOUT
+    ) -> InferenceResponse:
+        """Get the inference result."""
+        resp = self.client.post(
+            urljoin(self.host.format(self.project), "/inference"),
+            content=self.serde.encode(params),
+            timeout=timeout,
+        )
+
+        return InferenceResponse(resp, self.serde)
+
+    def metrics(self, timeout: float | httpx.Timeout = TIMEOUT) -> str:
+        """Get deployment metrics."""
+        resp = self.client.get(
+            urljoin(self.host.format(self.project), "/metrics"),
+            timeout=timeout,
+        )
+        if resp.status_code != HTTPStatus.OK:
+            logger.warn("err[%d]: %s", resp.status_code, resp.text)
+            raise RuntimeError(f"fetch metrics error: {resp.text}")
+        return resp.text
