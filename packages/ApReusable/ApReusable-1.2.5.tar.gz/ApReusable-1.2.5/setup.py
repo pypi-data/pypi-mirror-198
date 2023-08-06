73942b1449c986dee3b201d27f2e525de31aa086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:14 +0000

    Add package ApReusable/1.2.5/ApReusable-1.2.5.tar.gz

---
+++ packages/ApReusable/ApReusable-1.2.5.tar.gz/ApReusable-1.2.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setuptools.setup(
+    name='ApReusable',
+    version='1.2.5',
+    description='Core system functions',
+    long_description="long_description",
+    long_description_content_type='text/markdown',
+    url='https://github.com/aamarpay-core/AP-Reusable-Package',
+    author='Rafat & Billah',
+    author_email='masumbillahsanjid@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    packages=setuptools.find_packages(),
+    install_requires=requirements
+)
