fdc3530ef228b9836897a480a8df3865b880d11d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:21 +0000

    Add package kingback/1.0.1/kingback-1.0.1.tar.gz

---
+++ packages/kingback/kingback-1.0.1.tar.gz/kingback-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kingback",
+    version="1.0.1",
+    author="Kingson Wong",
+    author_email="kingson22.work@email.com",
+    description="Python Backtesting farmework for everyone",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/username/demopackage",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
