6a760f991865ca3a9343084e7852da047c291b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:51 +0000

    Add package cristianIordache/0.0.2/cristianIordache-0.0.2.tar.gz

---
+++ packages/cristianIordache/cristianIordache-0.0.2.tar.gz/cristianIordache-0.0.2/ruleaza/demisie.py
@@ -0,0 +1,5 @@
+def ruleaza():
+    print('Ruleaza, deci e bine')
+
+def nuRuleaza():
+    print('Ne dam demisia')
---
+++ packages/cristianIordache/cristianIordache-0.0.2.tar.gz/cristianIordache-0.0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools. setup(
+    include_package_data=True,
+    name= 'cristianIordache',
+    description='2cool 4school',
+    version='0.0.2',
+    author= 'Cristian Iordache',
+    author_email= 'cristian.iordache@ro.gt.com',
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3" ,
+    "Operating System :: OS Independent" ,]
+)
