895f920174dc0c802d46275bfc45178c4ca06f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:45 +0000

    Add package np4k/0.0.9/np4k-0.0.9-py3-none-any.whl

---
+++ packages/np4k/np4k-0.0.9-py3-none-any.whl/np4k/lib.py
@@ -0,0 +1,16 @@
+
+
+def is_paywalled(*, text: str) -> bool:
+    """
+    attempts to detect paywall by looking for certain strings
+    in the text body of the article
+    :param text: text body of article
+    :return: True if paywall detected, False if unknown
+    """
+    if "are you a robot" in text.lower():
+        return True
+    if "please verify you're not a robot" in text.lower():
+        return True
+    if "what is included in my trial?" in text.lower():
+        return True
+    return False
