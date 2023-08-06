8040d759a7d8749e0249411898b929c31254122b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:28 +0000

    Add package inductiva/0.0.1/inductiva-0.0.1.tar.gz

---
+++ packages/inductiva/inductiva-0.0.1.tar.gz/inductiva-0.0.1/setup.py
@@ -0,0 +1,30 @@
+"""Setup file"""
+
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = "inductiva"
+VERSION = "0.1"
+
+# pylint: disable=line-too-long
+
+REQUIRES = [
+    "setuptools >= 21.0.0",
+    "absl-py",
+    "numpy",
+    "scipy"
+]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="",
+    author="Inductiva Research Labs",
+    author_email="contact@inductiva.ai",
+    url="",
+    keywords=["Inductiva"],
+    python_requires=">=3.7",
+    install_requires=REQUIRES,
+    packages=find_packages(exclude=["test", "tests"]),
+    include_package_data=True,
+    long_description="",
+)
