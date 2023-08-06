ba188a38ac7994de0fe0a124b6f56d5314c70d12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:15 +0000

    Add package PlotLinearSolverResult/0.1.3/PlotLinearSolverResult-0.1.3.tar.gz

---
+++ packages/PlotLinearSolverResult/PlotLinearSolverResult-0.1.3.tar.gz/PlotLinearSolverResult-0.1.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='PlotLinearSolverResult',
+    packages=find_packages(),
+    version='0.1.3',
+    description='This project is for plot LinearMSTMM solver result that is write with hdf5.',
+    author='Zhengquan Liu',
+    license='MIT',
+    author_email="liuzq_96@163.com",
+    long_description_content_type="text/markdown",
+    long_description=open('README.md',encoding="UTF8").read(),
+    install_requires=['numpy'],
+    url="https://github.com/Qxxx1xx/PlotLinearSolverResult"
+)
