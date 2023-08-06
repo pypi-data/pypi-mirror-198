9aaaf983c7d564dc2f52f8d1f360b01e44f473b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:00 +0000

    Add package ostats/2023.3.23/ostats-2023.3.23-py3-none-any.whl

---
+++ packages/ostats/ostats-2023.3.23-py3-none-any.whl/ostats.py
@@ -0,0 +1,15 @@
+# author: Nicolas Tessore <n.tessore@ucl.ac.uk>
+# license: MIT
+'''online algorithm for mean, variance, and covariance'''
+
+__version__ = '2023.3.23'
+
+
+def add_sample(n, x, mu, *, var=None, cov=None):
+    '''add sample to online statistics'''
+    delta = x - mu
+    mu += delta/(n+1)
+    if var is not None:
+        var += (delta*(x - mu) - var)/(n+1)
+    if cov is not None:
+        cov += (delta[..., None]*(x - mu)[..., None, :] - cov)/(n+1)
