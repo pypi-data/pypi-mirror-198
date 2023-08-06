096f9a36138390fb0c3e1b182b07fce91cee39d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:26 +0000

    Add package opensesame-extension-language-server/0.2.1/opensesame_extension_language_server-0.2.1.tar.gz

---
+++ packages/opensesame-extension-language-server/opensesame_extension_language_server-0.2.1.tar.gz/opensesame_extension_language_server-0.2.1/opensesame_extensions/language_server/__init__.py
@@ -0,0 +1,5 @@
+# The name of the package to check for updates on conda and pip
+packages = ['opensesame-extension-language_server',
+            'opensesame-extension-language-server',
+            'pyqode-language_server',
+            'pyqode3-language_server']
