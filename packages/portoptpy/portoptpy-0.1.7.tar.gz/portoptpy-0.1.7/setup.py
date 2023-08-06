17873f2b41813a00e425d07dcb9dff90f242a835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:05 +0000

    Add package portoptpy/0.1.7/portoptpy-0.1.7.tar.gz

---
+++ packages/portoptpy/portoptpy-0.1.7.tar.gz/portoptpy-0.1.7/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="portoptpy",
+    version='0.1.7',
+    author="Maddox Southard",
+    author_email="maddoxsouthard@yahoo.com",
+    description="A Python library for interfacing with the Portfolio Optimizer API: https://docs.portfoliooptimizer.io/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/autsauce/portoptpy",
+    packages=setuptools.find_packages(),
+    install_requires=[
+            'yfinance',
+            'requests',
+            'aiohttp',
+            'asyncio',
+            'pandas',
+            'numpy',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
