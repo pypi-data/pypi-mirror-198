5f4332ee8ceeb1b00a452ca188f63d25be7b9039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:03 +0000

    Add package mth-3/1.0.5/mth-3-1.0.5.tar.gz

---
+++ packages/mth-3/mth-3-1.0.5.tar.gz/mth-3-1.0.5/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+
+#pypi-AgEIcHlwaS5vcmcCJDMzOGEyZDRmLWEyMjMtNDU3Yi1hNjFhLTE2Njk4Njg1NmYxZQACKlszLCJhZjg4MTI5Yi1kN2ZiLTQ0ZGEtOGUyMy0wZDk1ZDkyNTYzNDYiXQAABiB2q-fcRf5wvnrIZOONzSdhlZoQ-BhESBHUKslSFKPo0w
+
+setup(
+    name="mth-3",
+    version="1.0.5",
+    description="mth-3",
+    author="Python Powerhouse",
+    license="MIT",
+    url="https://github.com/NovykovDaniil/python-core-project",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    # include_dirs=True,
+    install_requires=["prettytable", "prompt-toolkit", 'pygame'],
+    data_files=[
+        (
+            r"test_version_powerhouse_helper\GameGooseKiller\font",
+            [r"test_version_powerhouse_helper\GameGooseKiller\font\UA Propisi.ttf"],
+        ),
+        (
+            r"test_version_powerhouse_helper\GameGooseKiller\image",
+            [
+                r"test_version_powerhouse_helper\GameGooseKiller\image\background.png",
+                r"test_version_powerhouse_helper\GameGooseKiller\image\bonus.png",
+                r"test_version_powerhouse_helper\GameGooseKiller\image\boom.png",
+                r"test_version_powerhouse_helper\GameGooseKiller\image\enemy.png",
+                r"test_version_powerhouse_helper\GameGooseKiller\image\Farm-Goose.ico",
+                r"test_version_powerhouse_helper\GameGooseKiller\image\gameover.png",
+            ],
+        ),
+        (
+            r"test_version_powerhouse_helper\GameGooseKiller\image\background",
+            [
+                r"test_version_powerhouse_helper\GameGooseKiller\image\background\1-1.png",
+                r"test_version_powerhouse_helper\GameGooseKiller\image\background\1-2.png",
+                r"test_version_powerhouse_helper\GameGooseKiller\image\background\1-3.png",
+            ],
+        ),
+        (
+            r"test_version_powerhouse_helper\GameGooseKiller\image\Goose",
+            [
+                r"test_version_powerhouse_helper\GameGooseKiller\image\Goose\1-1.png",
+                r"test_version_powerhouse_helper\GameGooseKiller\image\Goose\1-2.png",
+                r"test_version_powerhouse_helper\GameGooseKiller\image\Goose\1-3.png",
+                r"test_version_powerhouse_helper\GameGooseKiller\image\Goose\1-4.png",
+                r"test_version_powerhouse_helper\GameGooseKiller\image\Goose\1-5.png",
+            ],
+        ),
+        (r'test_version_powerhouse_helper\GameGooseKiller', [r'test_version_powerhouse_helper\GameGooseKiller\Game_goose.py'])
+    ],
+    entry_points={
+        "console_scripts": ["mth=test_version_powerhouse_helper.main:run"]
+    },
+)
