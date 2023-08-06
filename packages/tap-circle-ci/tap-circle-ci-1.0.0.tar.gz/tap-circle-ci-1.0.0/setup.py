96cf4ac0f07441494bc1309815638fd894600d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:12 +0000

    Add package tap-circle-ci/1.0.0/tap-circle-ci-1.0.0.tar.gz

---
+++ packages/tap-circle-ci/tap-circle-ci-1.0.0.tar.gz/tap-circle-ci-1.0.0/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+setup(
+    name="tap-circle-ci",
+    version="1.0.0",
+    description="Singer.io tap for extracting data from circle ci",
+    author="Sisu Data;Stitch",
+    url="https://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_circle_ci"],
+    install_requires=[
+        "singer-python==5.13.0",
+        "requests",
+    ],
+    extras_require={"dev": ["pylint"]},
+    entry_points="""
+    [console_scripts]
+    tap-circle-ci=tap_circle_ci:main
+    """,
+    packages=find_packages(exclude=["tests"]),
+    package_data={
+        "schemas": ["tap_circle_ci/schemas/*.json"]
+    },
+    include_package_data=True,
+)
