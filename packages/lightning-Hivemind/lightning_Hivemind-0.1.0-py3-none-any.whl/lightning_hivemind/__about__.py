1eebcec7033797e7c23ec31f3f016d66c5a43dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:13 +0000

    Add package lightning-Hivemind/0.1.0/lightning_Hivemind-0.1.0-py3-none-any.whl

---
+++ packages/lightning-Hivemind/lightning_Hivemind-0.1.0-py3-none-any.whl/lightning_hivemind/__about__.py
@@ -0,0 +1,17 @@
+__version__ = "0.1.0"
+__author__ = "Lightning-AI et al."
+__author_email__ = "name@lightning.ai"
+__license__ = "Apache-2.0"
+__copyright__ = f"Copyright (c) 2021-2023, {__author__}."
+__homepage__ = "https://github.com/Lightning-AI/lightning-Hivemind"
+__docs__ = "Lightning strategy extension for Hivemind."
+
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__copyright__",
+    "__docs__",
+    "__homepage__",
+    "__license__",
+    "__version__",
+]
