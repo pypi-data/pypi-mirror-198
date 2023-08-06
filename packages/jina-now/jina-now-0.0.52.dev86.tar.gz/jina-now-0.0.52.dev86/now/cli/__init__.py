cf916d0ce19165aae91fed62799a2d60e51a9c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:25 +0000

    Add package jina-now/0.0.52.dev86/jina-now-0.0.52.dev86.tar.gz

---
+++ packages/jina-now/jina-now-0.0.52.dev86.tar.gz/jina-now-0.0.52.dev86/now/cli/__init__.py
@@ -0,0 +1,86 @@
+import os
+import pathlib
+import sys
+import warnings
+from argparse import Namespace
+
+from now import __version__
+from now import __version__ as version
+from now.constants import SURVEY_LINK
+from now.run_all_k8s import compare_flows, fetch_logs_now, start_now, stop_now
+
+warnings.filterwarnings("ignore")
+
+cur_dir = pathlib.Path(__file__).parents[1].resolve()
+
+os.environ['JINA_CHECK_VERSION'] = 'False'
+os.environ['JCLOUD_LOGLEVEL'] = 'ERROR'
+# fix
+
+
+def _get_run_args():
+    from now.cli.parser import get_main_parser
+
+    parser = get_main_parser()
+    if len(sys.argv) == 1:
+        parser.print_help()
+        exit()
+    args, unknown = parser.parse_known_args()
+
+    # clean up the args with None values
+    args = {k: v for k, v in vars(args).items() if v is not None}
+    # Convert args back to Namespace
+    args = Namespace(**args)
+
+    if unknown:
+        raise Exception('unknown args: ', unknown)
+
+    return args
+
+
+def get_task(kwargs):
+    for x in ['cli', 'now']:
+        if x in kwargs:
+            return kwargs[x]
+    raise Exception('kwargs do not contain a task')
+
+
+def cli(args=None):
+    """The main entrypoint of the CLI"""
+    os.environ['JINA_LOG_LEVEL'] = 'CRITICAL'
+    print_version_line()
+    kwargs = parse_args(args)
+    task = get_task(kwargs)
+    if '--version' in sys.argv[1:]:
+        print(__version__)
+        exit(0)
+    if task == 'start':
+        return start_now(**kwargs)
+    elif task == 'stop':
+        stop_now(**kwargs)
+    elif task == 'logs':
+        fetch_logs_now(**kwargs)
+    elif task == 'compare':
+        compare_flows(**kwargs)
+    elif task == 'survey':
+        import webbrowser
+
+        webbrowser.open(SURVEY_LINK, new=0, autoraise=True)
+    else:
+        raise Exception(f'unknown task, {task}')
+
+
+def parse_args(args):
+    if not args:
+        args = _get_run_args()
+    args = vars(args)  # Make it a dict from Namespace
+    return args
+
+
+def print_version_line():
+    if len(sys.argv) != 1 and not ('-h' in sys.argv[1:] or '--help' in sys.argv[1:]):
+        print(f'Initialising Jina NOW v{version} ...')
+
+
+if __name__ == '__main__':
+    cli()
