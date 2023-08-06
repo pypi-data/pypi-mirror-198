c09ad7cda2c774a736cb2f803676e1d6af6f16b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:07 +0000

    Add package vtpython/1.1/vtpython-1.1.tar.gz

---
+++ packages/vtpython/vtpython-1.1.tar.gz/vtpython-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vtpython',
+    version='1.1',
+    description='A very simple API to communicate with the VirusTotal V3 API',
+    author='Mateo Mrvelj',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+)
