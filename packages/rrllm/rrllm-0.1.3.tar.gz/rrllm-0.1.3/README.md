de7da0efdbd27c26eb3802a4b4c3672fbb67dd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:49 +0000

    Add package rrllm/0.1.3/rrllm-0.1.3.tar.gz

---
+++ packages/rrllm/rrllm-0.1.3.tar.gz/rrllm-0.1.3/README.md
@@ -0,0 +1,26 @@
+# Rubin's Rules for LLMs
+This repository contains the code for applying Rubin's rules to account for performance instability of LLMs.
+
+## Setup
+
+To install using pip, run
+```
+pip install rrllm
+```
+
+To install from the source, first download the repository by running 
+
+```
+git clone https://github.com/mkcifci/RR_LLM.git
+```
+
+Then, install the dependencies for this repo and setup by running
+```
+cd rrllm
+pip install -r requirements.txt
+python setup.py install
+```
+
+## Using the package
+
+For further details, see `Tutorial.ipynb` in the [examples](https://github.com/mkcifci/RR_LLM/blob/main/examples) directory.
---
+++ packages/rrllm/rrllm-0.1.3.tar.gz/rrllm-0.1.3/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import os
+import sys
+
+if sys.version_info[0] != 3:
+    raise RuntimeError('Unsupported python version "{0}"'.format(
+        sys.version_info[0]))
+
+def _get_file_content(file_name):
+    with open(file_name, 'r') as file_handler:
+        return str(file_handler.read())
+      
+def get_long_description():
+    return _get_file_content('README.md')
+
+INSTALL_REQUIRES = [
+    'transformers',
+    'simpletransformers',
+    'pandas',
+    'numpy',
+    'torch',
+    'tensorflow',
+    'tensorflow_datasets',
+    'scikit-learn',
+    'matplotlib',
+    'linearmodels',
+    'scipy'
+]
+
+setup(
+    name="rrllm",
+    version='0.1.3',
+    author="Muhammed Cifci",
+    author_email="mkcifci@psu.edu",
+    description="Rubin's rules to account for performance instability of LLMs",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    keywords='nlp transformers classification rubinsrules',
+    url="https://github.com/mkcifci/RR_LLM.git",
+    packages=['rrllm'],
+    py_modules=['rrllm'],
+    license="MIT",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=INSTALL_REQUIRES
+)
