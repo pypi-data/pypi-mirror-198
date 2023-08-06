6c59c3dbcf93fe40ffba8bb13e99cf2ea49e01f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:08 +0000

    Add package pytest-homeassistant-custom-component/0.13.10/pytest_homeassistant_custom_component-0.13.10-py3-none-any.whl

---
+++ packages/pytest-homeassistant-custom-component/pytest_homeassistant_custom_component-0.13.10-py3-none-any.whl/generate_phacc/const.py
@@ -0,0 +1,36 @@
+"""Constants for phacc."""
+TMP_DIR = "tmp_dir"
+PACKAGE_DIR = "pytest_homeassistant_custom_component"
+REQUIREMENTS_FILE = "requirements_test.txt"
+CONST_FILE = "const.py"
+
+REQUIREMENTS_FILE_DEV = "requirements_dev.txt"
+
+path = "."
+clone = "git clone https://github.com/home-assistant/core.git tmp_dir"
+diff = "git diff --exit-code"
+
+files = [
+    "__init__.py",
+    "asyncio_legacy.py",  # remove when it is not longer needed
+    "common.py",
+    "conftest.py",
+    "ignore_uncaught_exceptions.py",
+    "components/recorder/common.py",
+    "syrupy.py",
+    "typing.py",
+]
+
+# remove requirements for development only, i.e not related to homeassistant tests
+requirements_remove = [
+    "codecov",
+    "mypy",
+    "pre-commit",
+    "pylint",
+    "astroid",
+]
+
+LICENSE_FILE_HA = "LICENSE.md"
+LICENSE_FILE_NEW = "LICENSE_HA_CORE.md"
+
+HA_VERSION_FILE = "ha_version"
