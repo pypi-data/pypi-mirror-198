fa9b005f6960f59ef6d2d8f992daaa075a376ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:00 +0000

    Add package onono/1.0.0/onono-1.0.0.tar.gz

---
+++ packages/onono/onono-1.0.0.tar.gz/onono-1.0.0/onono/image.py
@@ -0,0 +1,82 @@
+"""
+Module for loading images and turning them into game puzzles with variable threshold.
+"""
+
+from pathlib import Path
+
+import numpy as np
+from PIL import Image, ImageOps, UnidentifiedImageError
+
+
+def image_prepare(path: Path, dims: tuple) -> np.ndarray:
+    """
+    Opens the image and does various basic operations. Separated from `load_image()`
+    for testing purposes.
+    """
+    image = Image.open(path)
+
+    # cannot upscale image
+    assert image.size[0] >= dims[0] and image.size[1] >= dims[1]
+
+    image = ImageOps.grayscale(image)
+    image = ImageOps.fit(image, dims)
+    image = np.asarray(image)
+
+    return image
+
+
+def load_image(name: str, dims: tuple = (10, 10), percent_filled: float = 0.7):
+    """
+    Loads a PNG image from the `saves/images` directory, performs various operations
+    and returns it as an array. In case something goes wrong, returns None.
+    """
+    name += ".png"
+    path = Path(__file__).parent.parent
+    path = (path / 'saves' / 'images' / name).resolve()
+    try:
+        image = image_prepare(path, dims)
+        image = apply_threshold(image, percent_filled)
+        return image
+    except (FileNotFoundError, UnidentifiedImageError, ValueError, AssertionError):
+        return None
+
+
+def apply_threshold(image: np.ndarray, expected_filled: float = 0.7, return_threshold: bool = False):
+    """
+    Takes an image and an expected ratio of ones in a matrix. Finds the optimal threshold that puts the real ratio
+    closest to the expected one. Returns the matrix after threshold is applied.
+
+    if `return_threshold` is set to True, returns a tuple of the image and pixel value threshold.
+    """
+    total_px = image.shape[0] * image.shape[1]
+
+    prev_threshold = 128
+    threshold = 128
+    closest_match = 1.
+    while True:
+        mask = image > threshold
+        filled = mask.sum() / total_px
+        difference = filled - expected_filled
+
+        if threshold == 128 or abs(difference) <= closest_match:
+            closest_match = abs(difference)
+        else:
+            break
+
+        next_threshold = threshold - 1 if difference < 0 else threshold + 1
+        if next_threshold == prev_threshold or next_threshold < 0 or next_threshold > 255:
+            break
+
+        prev_threshold = threshold
+        threshold = next_threshold
+
+    return (mask, prev_threshold) if return_threshold else mask
+
+
+def get_images(subdir: str = ""):
+    """
+    Opens the `saves/images` directory and returns the list of all PNG files.
+    """
+    path = Path(__file__).parent.parent
+    path = (path / 'saves' / 'images' / subdir).resolve().glob("*.png")
+    return [x.stem for x in path if x.is_file()]
---
+++ packages/onono/onono-1.0.0.tar.gz/onono-1.0.0/tests/test_gamelogic.py
@@ -0,0 +1,86 @@
+import numpy as np
+
+import onono.savegame
+import onono.gamelogic
+
+
+def test_change_field():
+    save = onono.savegame.SaveGame()
+    pos = np.array((0, 0))  # does not really matter - can be anywhere
+
+    assert save.guesses[pos[0], pos[1]] == 0
+
+    onono.gamelogic.change_field(save, pos, 1)
+    assert save.guesses[pos[0], pos[1]] == 2
+
+    onono.gamelogic.change_field(save, pos, 1)
+    assert save.guesses[pos[0], pos[1]] == 0
+
+    onono.gamelogic.change_field(save, pos, 3)
+    assert save.guesses[pos[0], pos[1]] == 1
+
+    onono.gamelogic.change_field(save, pos, 1)
+    assert save.guesses[pos[0], pos[1]] == 2
+
+
+def test_validate_filled():
+    dims = (10, 10)
+    save = onono.savegame.SaveGame(dims)
+    save.randomize()
+    save.get_solution(True)  # overwrite the guesses to correct
+
+    # the game is surely correctly filled in
+    validate_asserts(save, True)
+
+
+def test_validate_incorrect():
+    dims = (1000, 1000)  # bigger board for less likely random match
+    save = onono.savegame.SaveGame(dims)
+    save.randomize(0.5)
+    save.get_solution(True)  # overwrite the guesses
+    save.randomize(0.5)
+
+    # the games cannot possibly be identical, not even have identical rows and columns
+    validate_asserts(save, False)
+
+
+def test_validate_empty():
+    dims = (1000, 1000)
+    save = onono.savegame.SaveGame(dims)
+    save.randomize(0.8)
+
+    # empty guesses... the game is surely has some non-empty fields in every row and column as well
+    validate_asserts(save, False)
+
+
+def test_validate_diagonal():
+    """
+    Testing puzzle which has MANY solutions.
+    """
+    name = "tests/diagonal"
+    save = onono.savegame.SaveGame()
+    assert save.load_game(name)
+
+    save.get_solution(True)
+
+    for _ in range(save.board.shape[0]):
+        validate_asserts(save, True)
+        save.guesses = np.roll(save.guesses, 1, axis=0)
+
+    for _ in range(save.board.shape[1]):
+        validate_asserts(save, True)
+        save.guesses = np.roll(save.guesses, 1, axis=1)
+
+
+def validate_asserts(save: onono.savegame.SaveGame, valid: bool):
+    """
+    Used as a part of validation tests. Checks both functions `validate_game` and `validate_row`.
+    """
+    assert onono.gamelogic.validate_game(save) == valid
+
+    dims = save.board.shape
+    for row, i in zip(save.guesses, range(dims[0])):
+        assert onono.gamelogic.validate_row(row, save.y[i]) == valid
+
+    for col, i in zip(save.guesses.T, range(dims[1])):
+        assert onono.gamelogic.validate_row(col, save.x[i]) == valid
