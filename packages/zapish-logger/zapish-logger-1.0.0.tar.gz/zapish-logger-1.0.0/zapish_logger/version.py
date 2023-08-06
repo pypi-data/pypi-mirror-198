a129c65978f43610b7807c58ea9e524bf8fd88f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:16 +0000

    Add package zapish-logger/1.0.0/zapish-logger-1.0.0.tar.gz

---
+++ packages/zapish-logger/zapish-logger-1.0.0.tar.gz/zapish-logger-1.0.0/zapish_logger/version.py
@@ -0,0 +1,16 @@
+"""
+Holds the version information for the package
+"""
+__title__ = 'zapish-logger'
+__description__ = 'A simple logger that logs in a JSON format inspired by go zap'
+__author__ = 'Benjamin P. Trachtenberg'
+__copyright__ = 'Copyright (c) 2023 Benjamin P. Trachtenberg'
+__credits__ = None
+__license__ = 'The MIT License (MIT)'
+__status__ = 'prod'
+
+__version_info__ = (1, 0, 0)
+__version__ = '.'.join(map(str, __version_info__))
+__maintainer__ = 'Benjamin P. Trachtenberg'
+__email__ = 'e_ben_75-python@yahoo.com'
+__url__ = 'https://github.com/btr1975/zapish-logger'
