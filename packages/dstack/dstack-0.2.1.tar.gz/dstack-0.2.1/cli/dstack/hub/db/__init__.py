880a629d8f59d09eab5e4010d04c297e2a17ffdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:13 +0000

    Add package dstack/0.2.1/dstack-0.2.1.tar.gz

---
+++ packages/dstack/dstack-0.2.1.tar.gz/dstack-0.2.1/cli/dstack/hub/db/__init__.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+data_path = os.getenv("DSTACK_HUB_DATA") or Path.home() / ".dstack" / "hub" / "data"
+if not data_path.exists():
+    data_path.mkdir(parents=True, exist_ok=True)
+
+
+class Database:
+    engine = create_async_engine(
+        f"sqlite+aiosqlite:///{str(data_path.absolute())}/sqlite.db", echo=False
+    )
+    Base = declarative_base()
+    Session = sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
