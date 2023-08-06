5e639154f06eb27ac7946013d0b904a22468a731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:32 +0000

    Add package venus-dashboard/2.0.0/venus-dashboard-2.0.0.tar.gz

---
+++ packages/venus-dashboard/venus-dashboard-2.0.0.tar.gz/venus-dashboard-2.0.0/venus_dashboard/static/dashboard/admin/services/venus.service.js
@@ -0,0 +1,47 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('horizon.dashboard.admin.venus')
+    .service('venusSrv', venusSrv);
+
+  venusSrv.$inject = ['$http', '$injector'];
+
+  function venusSrv($http, $injector) {
+    var venusAPI;
+
+    if ($injector.has('horizon.app.core.openstack-service-api.venus')) {
+      venusAPI = $injector.get('horizon.app.core.openstack-service-api.venus');
+    }
+
+    function getLogStorageDays() {
+      if (venusAPI) {
+        return venusAPI.getLogStorageDays()
+          .then(function (data) {
+            return data;
+          })
+          .catch(function (err) {
+            console.error(err);
+          });
+      }
+    }
+
+    function getLogs(config) {
+      config = {params: config};
+      if (venusAPI) {
+        return venusAPI.getLogs(config)
+          .then(function (data) {
+            return data;
+          })
+          .catch(function (err) {
+            console.error(err);
+          });
+      }
+    }
+
+    return {
+      getLogStorageDays: getLogStorageDays,
+      getLogs: getLogs
+    };
+  }
+})();
