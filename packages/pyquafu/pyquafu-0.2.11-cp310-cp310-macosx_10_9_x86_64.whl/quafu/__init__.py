076b693e254c0257a8d2c1f267b4ee8b9f7c5722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:36 +0000

    Add package pyquafu/0.2.11/pyquafu-0.2.11-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/pyquafu/pyquafu-0.2.11-cp310-cp310-macosx_10_9_x86_64.whl/quafu/__init__.py
@@ -0,0 +1,10 @@
+from .circuits.quantum_circuit import QuantumCircuit
+from .results.results import ExecResult, SimuResult 
+from .tasks.tasks import Task
+from .users.userapi import User
+from .simulators.simulator import simulate
+
+__all__ = ["QuantumCircuit", "ExecResult", "Task", "User", "SimuResult", "simulate"]
+
+def get_version():
+    return "0.2.11"
