3fdcb5fcf197cf475892ecebe7dd7d12abab12a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:25 +0000

    Add package mytools-wjs/0.0.8/mytools_wjs-0.0.8.tar.gz

---
+++ packages/mytools-wjs/mytools_wjs-0.0.8.tar.gz/mytools_wjs-0.0.8/mytools_wjs/__init__.py
@@ -0,0 +1,5 @@
+from mytools_wjs.mytools import expansion_up, expansion, mapping_yq, mapping_NumberToChinese
+__name__ = "mytools_wjs"
+__author__ = "Wujs (55340808@qq.com)"
+__version__ = "0.0.8"
+__all__ = ['expansion', 'expansion_up', 'mapping_yq', 'mapping_NumberToChinese']
