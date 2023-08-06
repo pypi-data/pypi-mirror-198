6fb31e1235563919b8470b26c2b76cbfbfdb9fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:41 +0000

    Add package jetson-adapter-pkg/0.11.3/jetson_adapter_pkg-0.11.3-py3-none-any.whl

---
+++ packages/jetson-adapter-pkg/jetson_adapter_pkg-0.11.3-py3-none-any.whl/frotech_adapter/__init__.py
@@ -0,0 +1,11 @@
+from .pantilt import PanTilt
+from .serialLed import SerialLed
+from .ledMatrix import LedMatrix
+from .powerDetect import PowerDetect
+from .car import Car
+from .claw import Claw
+from .lift import Lift
+from .dummyArm import DummyArm
+from .airPump import AirPump
+
+__version__ = '0.11.3'
