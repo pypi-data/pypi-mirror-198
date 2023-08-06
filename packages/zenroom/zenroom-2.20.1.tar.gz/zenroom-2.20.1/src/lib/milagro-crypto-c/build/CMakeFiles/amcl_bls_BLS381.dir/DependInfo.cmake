049842492b44589cc135539142e4d3def15bbe11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:46 +0000

    Add package zenroom/2.20.1/zenroom-2.20.1.tar.gz

---
+++ packages/zenroom/zenroom-2.20.1.tar.gz/zenroom-2.20.1/src/lib/milagro-crypto-c/build/CMakeFiles/amcl_bls_BLS381.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/runner/work/Zenroom/Zenroom/bindings/python3/src/lib/milagro-crypto-c/build/src/bls_BLS381.c" "CMakeFiles/amcl_bls_BLS381.dir/src/bls_BLS381.c.o" "gcc" "CMakeFiles/amcl_bls_BLS381.dir/src/bls_BLS381.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/runner/work/Zenroom/Zenroom/bindings/python3/src/lib/milagro-crypto-c/build/CMakeFiles/amcl_pairing_BLS381.dir/DependInfo.cmake"
+  "/home/runner/work/Zenroom/Zenroom/bindings/python3/src/lib/milagro-crypto-c/build/CMakeFiles/amcl_curve_BLS381.dir/DependInfo.cmake"
+  "/home/runner/work/Zenroom/Zenroom/bindings/python3/src/lib/milagro-crypto-c/build/CMakeFiles/amcl_core.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
