4a459b6581b31b43db73447173cd68700590c83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:31 +0000

    Add package Mensajes-AntonioY92/1.6/Mensajes_AntonioY92-1.6-py3-none-any.whl

---
+++ packages/Mensajes-AntonioY92/Mensajes_AntonioY92-1.6-py3-none-any.whl/Mensajes/Adios/despedida.py
@@ -0,0 +1,6 @@
+def despedir():
+    print("Adios me despido desde despedia.despedir")
+
+class Desdepedida:
+    def __init__(self):
+        print("Adiós, me despido desde Desdpedida.___init__()")
