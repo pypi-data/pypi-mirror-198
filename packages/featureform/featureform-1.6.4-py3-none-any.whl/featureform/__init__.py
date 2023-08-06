0732fda598a0d4912aff629701d3a700c7ea0f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:27 +0000

    Add package featureform/1.6.4/featureform-1.6.4-py3-none-any.whl

---
+++ packages/featureform/featureform-1.6.4-py3-none-any.whl/featureform/__init__.py
@@ -0,0 +1,19 @@
+from .register import *
+from .serving import ServingClient
+from .resources import DatabricksCredentials, EMRCredentials, AWSCredentials, GCPCredentials, SparkCredentials
+
+ServingClient = ServingClient
+ResourceClient = ResourceClient
+
+
+# Executor Credentials
+DatabricksCredentials = DatabricksCredentials
+EMRCredentials = EMRCredentials
+SparkCredentials = SparkCredentials
+
+# Cloud Provider Credentials
+AWSCredentials = AWSCredentials
+GCPCredentials = GCPCredentials
+
+local = register_local()
+register_user("default_user").make_default_owner()
