6812aa58eef63416d5dcd14ddf75a1dddf21994b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:31 +0000

    Add package pykinect-recorder/0.2.1/pykinect_recorder-0.2.1-py3-none-any.whl

---
+++ packages/pykinect-recorder/pykinect_recorder-0.2.1-py3-none-any.whl/src/main.py
@@ -0,0 +1,14 @@
+import qdarktheme
+from pykinect_recorder.renderer.main_window import MainWindow
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication, QSystemTrayIcon
+
+
+if __name__ == "__main__":
+    app = QApplication()
+    qdarktheme.setup_theme()
+    # tray_icon = QSystemTrayIcon(QIcon(os.path.abspath("./renderer/public/kinect-sensor.ico")))
+    # tray_icon.setToolTip("")
+    main_window = MainWindow()
+    main_window.show()
+    app.exec()
