9b491d507a0370024b9cefda04289845f208ae55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:27 +0000

    Add package icrypt/0.1.0.dev2/icrypt-0.1.0.dev2-py3-none-any.whl

---
+++ packages/icrypt/icrypt-0.1.0.dev2-py3-none-any.whl/icrypt/__init__.py
@@ -0,0 +1,10 @@
+# from icrypto.icrypto_old import icrypto as crypto
+
+# encrypt = crypto.encrypt
+# decrypt = crypto.decrypt
+
+from icrypt.icrypt import ICrypt as crypto
+
+generate_key = crypto.generate_key
+encrypt = crypto.encrypt
+decrypt = crypto.decrypt
---
+++ packages/icrypt/icrypt-0.1.0.dev2-py3-none-any.whl/icrypt/__version__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# icrypto
+#
+
+__title__ = 'icrypt'
+__license__ = 'MIT'
+__version__ = '0.1.0dev2'
+__author__ = 'Hans Park'
+__contact__ = 'rhcproc@gmail.com'
+__url__ = 'https://github.com/rhcproc/icrypt'
+__description__ = (
+    'A simple encryption and decryption module for Python 3.6+'
+)
