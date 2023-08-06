b144529f47bc3c62dc66cac12e2a8a8204591ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:16 +0000

    Add package reaktion/0.1.12/reaktion-0.1.12-py3-none-any.whl

---
+++ packages/reaktion/reaktion-0.1.12-py3-none-any.whl/reaktion/atoms/combination/withlatest.py
@@ -0,0 +1,73 @@
+import asyncio
+from typing import List, Optional, Tuple
+import uuid
+from reaktion.atoms.helpers import index_for_handle
+from rekuest.api.schema import AssignationLogLevel
+from rekuest.messages import Assignation
+from reaktion.atoms.combination.base import CombinationAtom
+from reaktion.events import EventType, OutEvent, Returns
+import logging
+import functools
+
+logger = logging.getLogger(__name__)
+
+
+class WithLatestAtom(CombinationAtom):
+    state: List[Optional[List[Returns]]] = [None, None]
+    complete: List[bool] = [None, None]
+
+    async def run(self):
+        self.state = list(map(lambda x: None, self.node.instream))
+        self.complete = list(map(lambda x: False, self.node.instream))
+
+        try:
+            while True:
+                event = await self.get()
+                print("WithLatestAtom", event)
+
+                if event.type == EventType.ERROR:
+                    await self.transport.put(
+                        OutEvent(
+                            handle="return_0",
+                            type=EventType.ERROR,
+                            value=event.value,
+                            source=self.node.id,
+                        )
+                    )
+                    break
+
+                streamIndex = index_for_handle(event.handle)
+                print(streamIndex)
+
+                if event.type == EventType.COMPLETE:
+                    self.complete[streamIndex] = True
+
+                    if streamIndex == 0:
+                        await self.transport.put(
+                            OutEvent(
+                                handle="return_0",
+                                type=EventType.COMPLETE,
+                                source=self.node.id,
+                            )
+                        )
+                        break
+
+                if event.type == EventType.NEXT:
+                    self.state[streamIndex] = event.value
+
+                    if self.state[0] is not None and self.state[1] is not None:
+                        await self.transport.put(
+                            OutEvent(
+                                handle="return_0",
+                                type=EventType.NEXT,
+                                value=functools.reduce(lambda a, b: a + b, self.state),
+                                source=self.node.id,
+                            )
+                        )
+
+        except asyncio.CancelledError as e:
+            logger.warning(f"Atom {self.node} is getting cancelled")
+            raise e
+
+        except Exception as e:
+            logger.exception(f"Atom {self.node} excepted")
