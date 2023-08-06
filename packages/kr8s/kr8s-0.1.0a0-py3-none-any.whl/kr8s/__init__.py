dfc30a21fa219000b68f9c52fff2d0161d8b1e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:58 +0000

    Add package kr8s/0.1.0a0/kr8s-0.1.0a0-py3-none-any.whl

---
+++ packages/kr8s/kr8s-0.1.0a0-py3-none-any.whl/kr8s/__init__.py
@@ -0,0 +1,6 @@
+"""An asyncio shim for pykube-ng."""
+# SPDX-FileCopyrightText: Copyright (c) 2023, Dask Developers, Yuvi Panda, Anaconda Inc, NVIDIA
+# SPDX-License-Identifier: BSD 3-Clause License
+from pykube import HTTPClient, KubeConfig, all  # noqa
+
+__version__ = "0.1.0-a.0"
