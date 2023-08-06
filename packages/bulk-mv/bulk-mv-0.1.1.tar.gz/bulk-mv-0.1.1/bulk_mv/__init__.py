bd93f65857d10319511c48320a7d2b70f91f1cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:01 +0000

    Add package bulk-mv/0.1.1/bulk-mv-0.1.1.tar.gz

---
+++ packages/bulk-mv/bulk-mv-0.1.1.tar.gz/bulk-mv-0.1.1/bulk_mv/__init__.py
@@ -0,0 +1,7 @@
+from .file_tree import FileTree
+from .file_tree_builder import build_from_directory
+from .bmv_parser import grammar, FileTreeVisitor
+from .helpers import get_last_index_of_substring
+from .operations import perform_adds, perform_deletes, perform_renames, perform_moves
+
+__version__ = "0.1.1"
