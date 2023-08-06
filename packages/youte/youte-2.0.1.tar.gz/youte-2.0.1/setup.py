850ad3e826da580ca45484788f19c1dd44ed7be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:17 +0000

    Add package youte/2.0.1/youte-2.0.1.tar.gz

---
+++ packages/youte/youte-2.0.1.tar.gz/youte-2.0.1/setup.py
@@ -0,0 +1,35 @@
+import pathlib
+
+from setuptools import find_packages, setup
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+if __name__ == "__main__":
+    setup(
+        name="youte",
+        author="Digital Observatory",
+        authhor_email="digitalobservatory@qut.edu.au",
+        version="2.0.1",
+        description="Command-line tool to collect video metadata and comments from "
+        "Youtube API",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Programming Language :: Python :: 3.8",
+        ],
+        packages=find_packages(where="src"),
+        install_requires=[
+            "click >= 8.0.3",
+            "requests >= 2.27.1",
+            "tqdm >= 4.64.0",
+            "python-dateutil >= 2.8.2",
+            "configobj >= 5.0.6",
+            "typing_extensions >= 4.5.0",
+        ],
+        python_requires=">=3.8",
+        entry_points={"console_scripts": ["youte=youte.cli:youte"]},
+    )
