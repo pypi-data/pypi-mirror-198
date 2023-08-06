b23c732faf8b39c28611ff1c63ac824a9421137d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:07 +0000

    Add package Newestearthquake-Indonesia-BMKG/0.2/Newestearthquake-Indonesia-BMKG-0.2.tar.gz

---
+++ packages/Newestearthquake-Indonesia-BMKG/Newestearthquake-Indonesia-BMKG-0.2.tar.gz/Newestearthquake-Indonesia-BMKG-0.2/setup.py
@@ -0,0 +1,26 @@
+#markdown guide : https://www.markdownguide.org/cheat-sheet/
+
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Newestearthquake-Indonesia-BMKG",
+    version="0.2",
+    author="Deni Rahmawan",
+    author_email="denirahmawan17@gmail.com",
+    description="This package will get the latest earthquake from BMKG | Meteorological, Climatological, "
+                "and Geophysical Agency.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Apatheia8/latest-Indonesia-earthquake",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    package=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
