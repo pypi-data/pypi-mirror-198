49e5d86a8a870e0844c6189fbf0da9322dcd9bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:30 +0000

    Add package abm-shape-collection/0.3.1/abm_shape_collection-0.3.1.tar.gz

---
+++ packages/abm-shape-collection/abm_shape_collection-0.3.1.tar.gz/abm_shape_collection-0.3.1/src/abm_shape_collection/calculate_shape_stats.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import ks_2samp
+from sklearn.decomposition import PCA
+
+
+def calculate_shape_stats(
+    pca: PCA, data: pd.DataFrame, ref_data: pd.DataFrame, components: int
+) -> pd.DataFrame:
+    all_stats = []
+
+    columns = ref_data.filter(like="shcoeffs").columns
+    data_transform = pca.transform(data[columns].values)
+    ref_data_transform = pca.transform(ref_data[columns].values)
+
+    for component in range(components):
+        ks_stats = get_ks_statistic(data_transform[:, component], ref_data_transform[:, component])
+        ks_stats.update({"FEATURE": f"PC_{component + 1}", "TICK": np.nan})
+        all_stats.append(ks_stats)
+
+    for tick, tick_data in data.groupby("TICK"):
+        tick_data_transform = pca.transform(tick_data[columns].values)
+
+        for component in range(components):
+            tick_ks_stats = get_ks_statistic(
+                tick_data_transform[:, component], ref_data_transform[:, component]
+            )
+            tick_ks_stats.update(
+                {
+                    "FEATURE": f"PC_{component + 1}",
+                    "TICK": tick,
+                }
+            )
+            all_stats.append(tick_ks_stats)
+
+    all_stats_df = pd.DataFrame(all_stats)
+
+    return all_stats_df
+
+
+def get_ks_statistic(population_a: np.ndarray, population_b: np.ndarray) -> dict:
+    ksresult = ks_2samp(population_a, population_b, mode="asymp")
+
+    return {
+        "KS_STATISTIC": ksresult.statistic,
+        "KS_PVALUE": ksresult.pvalue,
+    }
---
+++ packages/abm-shape-collection/abm_shape_collection-0.3.1.tar.gz/abm_shape_collection-0.3.1/src/abm_shape_collection/calculate_size_stats.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+
+from abm_shape_collection.calculate_shape_stats import get_ks_statistic
+
+
+def calculate_size_stats(
+    data: pd.DataFrame, ref_data: pd.DataFrame, regions: list[str]
+) -> pd.DataFrame:
+    all_stats = []
+
+    for region in regions:
+        for feature in ["volume", "height"]:
+            column_name = f"{feature}.{region}" if region != "DEFAULT" else feature
+            ref_values = ref_data[column_name].values
+            values = data[column_name].values
+
+            ks_stats = get_ks_statistic(values, ref_values)
+            ks_stats.update({"FEATURE": column_name, "TICK": np.nan})
+            all_stats.append(ks_stats)
+
+            for tick, tick_data in data.groupby("TICK"):
+                tick_values = tick_data[column_name].values
+
+                tick_ks_stats = get_ks_statistic(tick_values, ref_values)
+                tick_ks_stats.update(
+                    {
+                        "FEATURE": column_name,
+                        "TICK": tick,
+                    }
+                )
+                all_stats.append(tick_ks_stats)
+
+    all_stats_df = pd.DataFrame(all_stats)
+
+    return all_stats_df
