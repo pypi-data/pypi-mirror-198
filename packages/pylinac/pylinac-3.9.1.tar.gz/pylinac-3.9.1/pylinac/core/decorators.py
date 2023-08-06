8d4405e7fd11716d8b62776679a2aae10e3002e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:35 +0000

    Add package pylinac/3.9.1/pylinac-3.9.1.tar.gz

---
+++ packages/pylinac/pylinac-3.9.1.tar.gz/pylinac-3.9.1/pylinac/core/decorators.py
@@ -0,0 +1,25 @@
+import functools
+import weakref
+
+
+def lru_cache(*lru_args, **lru_kwargs):
+    """Method-safe LRU cache; https://stackoverflow.com/a/33672499"""
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapped_func(self, *args, **kwargs):
+            # We're storing the wrapped method inside the instance. If we had
+            # a strong reference to self the instance would never die.
+            self_weak = weakref.ref(self)
+
+            @functools.wraps(func)
+            @functools.lru_cache(*lru_args, **lru_kwargs)
+            def cached_method(*args, **kwargs):
+                return func(self_weak(), *args, **kwargs)
+
+            setattr(self, func.__name__, cached_method)
+            return cached_method(*args, **kwargs)
+
+        return wrapped_func
+
+    return decorator
