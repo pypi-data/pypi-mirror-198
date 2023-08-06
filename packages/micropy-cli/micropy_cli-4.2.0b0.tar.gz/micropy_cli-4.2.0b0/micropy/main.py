003f26b993723c334f90241e82fc54502aacc0de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:14 +0000

    Add package micropy-cli/4.2.0b0/micropy_cli-4.2.0b0.tar.gz

---
+++ packages/micropy-cli/micropy_cli-4.2.0b0.tar.gz/micropy_cli-4.2.0b0/micropy/main.py
@@ -0,0 +1,155 @@
+"""Main Module."""
+
+from __future__ import annotations
+
+import tempfile
+from pathlib import Path
+from typing import List, Literal, Optional, Union
+
+from micropy import data, utils
+from micropy.exceptions import PyDeviceError
+from micropy.logger import Log
+from micropy.project import Project, modules
+from micropy.pyd import DevicePath, MessageHandlers, ProgressStreamConsumer, PyDevice
+from micropy.pyd.backend_rshell import RShellPyDeviceBackend
+from micropy.pyd.backend_upydevice import UPyDeviceBackend
+from micropy.stubs import RepositoryInfo, StubManager, StubRepository
+from micropy.utils.stub import prepare_create_stubs
+from pydantic import parse_file_as
+
+
+class MicroPy:
+    """Handles App State Management."""
+
+    RUN_CHECKS = True
+    repo: StubRepository
+    _stubs: Optional[StubManager] = None
+
+    def __init__(self):
+        self.log = Log.get_logger("MicroPy")
+        self.verbose = True
+        self.log.debug("MicroPy Loaded")
+        repo_list = parse_file_as(List[RepositoryInfo], data.REPO_SOURCES)
+        self.repo = StubRepository()
+        for repo_info in repo_list:
+            self.repo = self.repo.add_repository(repo_info)
+        if not data.STUB_DIR.exists():
+            self.setup()
+
+    def setup(self):
+        """Creates necessary directories for micropy."""
+        self.log.debug("Running first time setup...")
+        self.log.debug(f"Creating .micropy directory @ {data.FILES}")
+        data.FILES.mkdir(exist_ok=True)
+        data.STUB_DIR.mkdir()
+
+    @property
+    def stubs(self) -> StubManager:
+        """Primary Stub Manager for MicroPy.
+
+        Returns:
+            StubManager: StubManager Instance
+
+        """
+        if not self._stubs:
+            self._stubs = StubManager(resource=data.STUB_DIR, repos=self.repo)
+        return self._stubs
+
+    @utils.lazy_property
+    def project(self):
+        """Current active project if available.
+
+        Returns:
+            Project: Instance of Current Project
+
+        """
+        proj = self.resolve_project(".", verbose=self.verbose)
+        return proj
+
+    def resolve_project(self, path, verbose=True):
+        """Returns project from path if it exists.
+
+        Args:
+            path (str): Path to test
+            verbose (bool): Log to stdout. Defaults to True.
+
+        Returns:
+            Project if it exists
+
+        """
+        path = Path(path).absolute()
+        proj = Project(path)
+        proj.add(modules.StubsModule, self.stubs)
+        proj.add(modules.PackagesModule, "requirements.txt")
+        proj.add(modules.DevPackagesModule, "dev-requirements.txt")
+        proj.add(modules.TemplatesModule, run_checks=self.RUN_CHECKS)
+        if proj.exists:
+            if verbose:
+                self.log.title("Loading Project")
+            proj.load()
+            if verbose:
+                self.log.success("Ready!")
+            return proj
+        return proj
+
+    def create_stubs(
+        self,
+        port,
+        verbose=False,
+        backend: Union[Literal["upydevice"], Literal["rshell"]] = "upydevice",
+    ):
+        """Create and add stubs from Pyboard.
+
+        Todo:
+            Extract and cleanup this mess.
+
+        Args:
+            port (str): Port of Pyboard
+
+        Returns:
+            Stub: generated stub
+
+        """
+        self.log.title(f"Connecting to Pyboard @ $[{port}]")
+        pyb_log = Log.add_logger("Pyboard", "bright_white")
+        backend = UPyDeviceBackend if backend == "upydevice" else RShellPyDeviceBackend
+
+        def _get_desc(name: str, cfg: dict):
+            desc = f"{pyb_log.get_service()} {name}"
+            return name, cfg | dict(desc=desc)
+
+        message_handler = MessageHandlers(
+            on_message=lambda x: isinstance(x, str) and pyb_log.info(x.strip())
+        )
+        try:
+            pyb = PyDevice(
+                port,
+                auto_connect=True,
+                stream_consumer=ProgressStreamConsumer(on_description=_get_desc),
+                message_consumer=message_handler,
+                backend=backend,
+            )
+        except (SystemExit, PyDeviceError):
+            self.log.error(f"Failed to connect, are you sure $[{port}] is correct?")
+            return None
+        self.log.success("Connected!")
+        create_stubs = prepare_create_stubs()
+        self.log.info("Executing stubber on pyboard...")
+        try:
+            pyb.run_script(create_stubs.getvalue(), DevicePath("createstubs.py"))
+        except Exception as e:
+            # TODO: Handle more usage cases
+            self.log.error(f"Failed to execute script: {str(e)}", exception=e)
+            raise
+        self.log.success("Done!")
+        self.log.info("Copying stubs...")
+        with tempfile.TemporaryDirectory() as tmpdir:
+            pyb.copy_from(DevicePath("/stubs"), tmpdir)
+            out_dir = Path(tmpdir)
+            stub_path = next(out_dir.iterdir())
+            self.log.info(f"Copied Stubs: $[{stub_path.name}]")
+            stub_path = self.stubs.from_stubber(stub_path, out_dir)
+            stub = self.stubs.add(str(stub_path))
+        pyb.disconnect()
+        self.log.success(f"Added {stub.name} to stubs!")
+        return stub
---
+++ packages/micropy-cli/micropy_cli-4.2.0b0.tar.gz/micropy_cli-4.2.0b0/tests/test_main.py
@@ -0,0 +1,73 @@
+from shutil import copytree, rmtree
+
+import micropy.exceptions as exc
+import pytest
+from micropy import data, main
+from micropy.stubs import StubManager, stubs
+from pytest_mock import MockFixture
+
+
+def test_setup(mock_micropy_path):
+    """Tests MicroPy Initial Setup"""
+    expect_mp_dir = mock_micropy_path
+    expect_stubs_dir = mock_micropy_path / "stubs"
+    mp = main.MicroPy()
+    assert expect_mp_dir.exists()
+    assert expect_stubs_dir.exists()
+    # Test after inital setup
+    mp_ = main.MicroPy()
+    assert len(mp_.stubs) == len(mp.stubs)
+
+
+def test_add_stub(mock_micropy, shared_datadir):
+    """Test Adding Valid Stub"""
+    fware_path = shared_datadir / "fware_test_stub"
+    stub_path = shared_datadir / "esp8266_test_stub"
+    stubs = mock_micropy.stubs
+    fware_stub = stubs.add(fware_path, data.STUB_DIR)
+    stub = stubs.add(stub_path, data.STUB_DIR)
+    assert stub in list(mock_micropy.stubs)
+    assert stub.path in data.STUB_DIR.iterdir()
+    assert stub.path.exists()
+    assert fware_stub in list(mock_micropy.stubs._firmware)
+
+
+def test_create_stub__connect_error(mock_micropy, mocker, shared_datadir, tmp_path):
+    mock_pyb = mocker.patch("micropy.main.PyDevice")
+    mock_pyb.side_effect = [SystemExit, exc.PyDeviceError]
+    mp = mock_micropy
+    assert mp.create_stubs("/dev/PORT") is None
+    assert mp.create_stubs("/dev/PORT") is None
+
+
+def test_create_stub(mock_micropy, mocker: MockFixture, shared_datadir, tmp_path):
+    """should create and add stubs"""
+    mock_micropy.stubs.add(shared_datadir / "fware_test_stub")
+    tmp_stub_path = tmp_path / "createtest"
+    tmp_stub_path.mkdir()
+    copytree(str(shared_datadir / "stubber_test_stub"), str(tmp_stub_path / "stubber_test_stub"))
+    mock_tmpdir = mocker.patch("micropy.main.tempfile.TemporaryDirectory")
+    mock_tmpdir.return_value.__enter__.return_value = tmp_stub_path
+    mock_pyb = mocker.patch("micropy.main.PyDevice")
+    mp = mock_micropy
+    mocker.spy(StubManager, "add")
+    mock_pyb.return_value.run_script.side_effect = [Exception, mocker.ANY, mocker.ANY]
+    with pytest.raises(Exception):
+        stub = mp.create_stubs("/dev/PORT")
+    stub = mp.create_stubs("/dev/PORT")
+    mp.stubs.add.assert_any_call(mocker.ANY, str(tmp_stub_path / "esp32-1.11.0"))
+    rmtree(tmp_stub_path / "esp32-1.11.0")
+    assert isinstance(stub, stubs.DeviceStub)
+
+
+def test_stub_error():
+    with pytest.raises(exc.StubError):
+        raise exc.StubError(None)
+
+
+def test_resolve_project(mocker, mock_micropy):
+    mock_proj = mocker.patch.object(main, "Project").return_value
+    mock_proj.exists = False
+    assert not mock_micropy.resolve_project(".").exists
+    mock_proj.exists = True
+    assert mock_micropy.resolve_project(".")
