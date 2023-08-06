bdda03e6003cf72ac0f7653c91c0f75f817eb972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:00 +0000

    Add package python-paypal-api/0.1.2/python_paypal_api-0.1.2-py3-none-any.whl

---
+++ packages/python-paypal-api/python_paypal_api-0.1.2-py3-none-any.whl/python_paypal_api/api/__init__.py
@@ -0,0 +1,18 @@
+from .orders import Orders
+from .disputes import Disputes
+from .products import Products
+from .tracking import Tracking
+from .identity import Identity
+from .invoices import Invoices
+from .partner_referrals import PartnerReferrals
+from .transactions import Transactions
+
+__all__ = [
+    "Orders",
+    "Disputes",
+    "Products",
+    "Identity",
+    "Invoices",
+    "PartnerReferrals",
+    "Transactions"
+]
