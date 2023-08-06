afc14391b7476f5f025a3fa2947c9fefc577bb20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:21 +0000

    Add package pyo3-tonic-greeter-example/0.1.0/pyo3_tonic_greeter_example-0.1.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyo3-tonic-greeter-example/pyo3_tonic_greeter_example-0.1.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyo3_tonic_greeter_example/__init__.py
@@ -0,0 +1,5 @@
+from .pyo3_tonic_greeter_example import *
+
+__doc__ = pyo3_tonic_greeter_example.__doc__
+if hasattr(pyo3_tonic_greeter_example, "__all__"):
+    __all__ = pyo3_tonic_greeter_example.__all__
