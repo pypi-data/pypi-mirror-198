5317707dd17dee81ca340da986d0740fb19e1398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:12 +0000

    Add package modeldocumentation/0.0.2/modeldocumentation-0.0.2.tar.gz

---
+++ packages/modeldocumentation/modeldocumentation-0.0.2.tar.gz/modeldocumentation-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 11',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='modeldocumentation',
+  version='0.0.2',
+  description='A model documneter for power bi files',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Ayush Jain',
+  author_email='jainaayush051@outlook.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='', 
+  packages=find_packages(),
+  install_requires=['openpyxl','openai'] 
+)
