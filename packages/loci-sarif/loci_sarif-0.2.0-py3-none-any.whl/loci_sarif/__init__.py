1ac92e88008eaaedddc18a3706f1d50a8d6380c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:17 +0000

    Add package loci-sarif/0.2.0/loci_sarif-0.2.0-py3-none-any.whl

---
+++ packages/loci-sarif/loci_sarif-0.2.0-py3-none-any.whl/loci_sarif/__init__.py
@@ -0,0 +1,8 @@
+# Application global vars
+__version__ = "0.2.0"
+PROG_NAME = "loci-sarif"
+PROG_DESC = "The official Loci Notes SARIF output processor."
+PROG_EPILOG = "Written by TheTwitchy"
+DEBUG = True
+SUPPORTED_SARIF_VERSIONS = ["2.1.0"]
+SUPPORTED_SARIF_TOOLS = ["semgrep"]
