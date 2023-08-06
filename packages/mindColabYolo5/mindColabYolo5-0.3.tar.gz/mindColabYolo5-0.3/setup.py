d981bfd195cbcd02ce2e5ee6042a25fddeef8fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:53 +0000

    Add package mindColabYolo5/0.3/mindColabYolo5-0.3.tar.gz

---
+++ packages/mindColabYolo5/mindColabYolo5-0.3.tar.gz/mindColabYolo5-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mindColabYolo5',
+    version='0.3',
+    author='MindColab',
+    author_email='ramsesgiralald@gmail.com',
+    description='Descripción Demo',
+    packages=["mindColabYolo5"]
+)
