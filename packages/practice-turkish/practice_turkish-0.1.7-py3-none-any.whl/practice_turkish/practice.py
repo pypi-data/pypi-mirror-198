baceba2ff7d6cfb4a6780c9de21600fa30c6d917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:16 +0000

    Add package practice-turkish/0.1.7/practice_turkish-0.1.7-py3-none-any.whl

---
+++ packages/practice-turkish/practice_turkish-0.1.7-py3-none-any.whl/practice_turkish/practice.py
@@ -0,0 +1,25 @@
+import typer
+
+from .translation import translation
+from .make_csv import make_dictionary as make_csv
+from .number import numbers
+
+
+def license() -> str:
+    "Returns the content of `LICENSE` file."
+    with open("LICENSE") as f:
+        print(f.read())
+
+
+def main() -> None:
+    "Create Typer application and run it."
+    app = typer.Typer()
+    translation_ = app.command(help="Practice translation")(translation)
+    make_csv_ = app.command(help="Create a new CSV dictionary")(make_csv)
+    numbers_ = app.command(help="Practice numbers")(numbers)
+    license_ = app.command(help="Show license")(license)
+    app()
+
+
+if __name__ == "__main__":
+    main()
