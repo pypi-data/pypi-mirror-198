7bfae9b3de6556137deb053ab80f75744a5ccebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:08 +0000

    Add package yente/3.3.0/yente-3.3.0.tar.gz

---
+++ packages/yente/yente-3.3.0.tar.gz/yente-3.3.0/setup.py
@@ -0,0 +1,64 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+
+setup(
+    name="yente",
+    version="3.3.0",
+    url="https://opensanctions.org/docs/api/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    author="OpenSanctions",
+    author_email="info@opensanctions.org",
+    packages=find_packages(exclude=["examples", "tests"]),
+    namespace_packages=[],
+    install_requires=[
+        "followthemoney==3.3.0",
+        "nomenklatura==2.8.1",
+        "asyncstdlib==3.10.5",
+        "aiocron==1.8",
+        "aiocsv==1.2.3",
+        "aiofiles==23.1.0",
+        "types-aiofiles==23.1.0.0",
+        "aiohttp[speedups]==3.8.4",
+        "elasticsearch[async]==8.6.2",
+        "fastapi==0.95.0",
+        "uvicorn[standard]==0.21.1",
+        "python-multipart==0.0.6",
+        "email-validator==1.3.1",
+        "structlog==22.3.0",
+        "pyicu==2.10.2",
+        "orjson==3.8.7",
+        "text-unidecode==1.3",
+        "click==8.0.4",
+        "normality==2.4.0",
+        "languagecodes==1.1.1",
+        "countrynames==1.14.3",
+        "fingerprints==1.1.0",
+        "pantomime==0.6.0",
+    ],
+    extras_require={
+        "dev": [
+            "pip>=10.0.0",
+            "bump2version",
+            "wheel>=0.29.0",
+            "twine",
+            "mypy",
+            "httpx",
+            "pytest",
+            "pytest-cov",
+            "pytest-asyncio",
+            "flake8>=2.6.0",
+            "black",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "yente = yente.cli:cli",
+        ],
+    },
+    zip_safe=False,
+)
