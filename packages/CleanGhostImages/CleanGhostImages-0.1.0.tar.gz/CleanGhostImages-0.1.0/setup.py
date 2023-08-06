9551845665004700c2dc08aeae896e37d0189467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:18 +0000

    Add package CleanGhostImages/0.1.0/CleanGhostImages-0.1.0.tar.gz

---
+++ packages/CleanGhostImages/CleanGhostImages-0.1.0.tar.gz/CleanGhostImages-0.1.0/setup.py
@@ -0,0 +1,27 @@
+"""Install packages as defined in this file into the Python environment."""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="CleanGhostImages",
+    author="Ixonae",
+    author_email="contact@ixonae.com",
+    description="A tool to find unused images in the Ghost blog content directory",
+    version="0.1.0",
+    packages=find_packages(where=".", exclude=["tests"]),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+    install_requires=[
+        "setuptools>=45.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "cleanghostimages=cleanghostimages.__main__:main",
+        ]
+    },
+    license="GNU GENERAL PUBLIC LICENSE v2"
+)
