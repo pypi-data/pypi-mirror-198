11d8bf773883bdb1c9fbed77f64d15a3e9a58693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:36 +0000

    Add package webai/0.0.1/webai-0.0.1.tar.gz

---
+++ packages/webai/webai-0.0.1.tar.gz/webai-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='webai',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://people.eecs.berkeley.edu/~shishirpatil/',
+    author='Shishir Patil',
+    author_email='shishirpatil@berkeley.edu',
+    packages=[],
+    classifiers=['Development Status :: 2 - Pre-Alpha'],
+)
