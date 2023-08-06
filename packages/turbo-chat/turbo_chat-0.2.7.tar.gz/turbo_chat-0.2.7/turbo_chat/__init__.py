3e498a7ab80b78fea09cddfa2e8bac36cd72d104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:27 +0000

    Add package turbo-chat/0.2.7/turbo_chat-0.2.7.tar.gz

---
+++ packages/turbo-chat/turbo_chat-0.2.7.tar.gz/turbo_chat-0.2.7/turbo_chat/__init__.py
@@ -0,0 +1,47 @@
+# flake8: noqa
+
+from .bots import *
+from .cache import *
+from .chat import *
+from .config import *
+from .errors import *
+from .memory import *
+from .runner import *
+from .structs import *
+from .turbo import *
+from .types import *
+from .utils import *
+
+__all__ = [
+    "System",
+    "User",
+    "Assistant",
+    "ExampleUser",
+    "ExampleAssistant",
+    "Generate",
+    "GetInput",
+    "InvalidValueYieldedError",
+    "GeneratorAlreadyExhaustedError",
+    "TurboGen",
+    "MessageRole",
+    "Message",
+    "BaseMessageCollection",
+    "BaseMemory",
+    "LocalMemory",
+    "LocalSummarizeMemory",
+    "LocalTruncatedMemory",
+    "Example",
+    "BaseCache",
+    "SimpleCache",
+    "Scratchpad",
+    "Result",
+    "Tool",
+    "turbo",
+    "run",
+    "summarize_bot",
+    "subqueries_bot",
+    "tool_bot",
+    "self_ask_bot",
+    "qa_bot",
+    "available_models",
+]
---
+++ packages/turbo-chat/turbo_chat-0.2.7.tar.gz/turbo_chat-0.2.7/turbo_chat/types/memory.py
@@ -0,0 +1,50 @@
+from abc import abstractmethod
+from typing import List
+
+import pydantic
+
+from ..config import TurboModel
+from .messages import (
+    BaseMessageCollection,
+    MessageDict,
+    Message,
+)
+
+__all__ = [
+    "BaseMemory",
+]
+
+
+# Abstract classes
+class BaseMemory(BaseMessageCollection, pydantic.BaseModel):
+    """Base class for persisting conversation history and state."""
+
+    model: TurboModel
+
+    async def init(self, context={}) -> None:
+        ...
+
+    @abstractmethod
+    async def extend(self, items: List[Message]) -> None:
+        ...
+
+    @abstractmethod
+    async def get_state(self) -> dict:
+        ...
+
+    @abstractmethod
+    async def set_state(self, new_state: dict, merge: bool = False) -> None:
+        ...
+
+    async def append(self, item: Message) -> None:
+        await self.extend([item])
+
+    async def prepare_prompt(
+        self,
+        max_tokens: int = 0,
+    ) -> List[MessageDict]:
+        """Turn message history into a prompt for openai."""
+
+        # Noop: Override to add filtering
+        messages: List[MessageDict] = await self.get_dicts()
+        return messages
