df720ca513da427820f30c0cb3a6174fd75ba8c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:46 +0000

    Add package tiledbsoma/1.0.1/tiledbsoma-1.0.1.tar.gz

---
+++ packages/tiledbsoma/tiledbsoma-1.0.1.tar.gz/tiledbsoma-1.0.1/dist_links/libtiledbsoma/cmake/Modules/FindTileDB_EP.cmake
@@ -0,0 +1,122 @@
+#
+# FindTileDB_EP.cmake
+#
+#
+# The MIT License
+#
+# Copyright (c) 2018 TileDB, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# Finds the TileDB library, installing with an ExternalProject as necessary.
+
+# If TileDB was installed as an EP, need to search the EP install path also.
+set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} "${EP_INSTALL_PREFIX}")
+
+if (FORCE_BUILD_TILEDB)
+  find_package(TileDB CONFIG PATHS ${EP_INSTALL_PREFIX} NO_DEFAULT_PATH)
+else()
+  find_package(TileDB CONFIG)
+endif()
+
+if (TILEDB_FOUND)
+  get_target_property(TILEDB_LIB TileDB::tiledb_shared IMPORTED_LOCATION_RELEASE)
+  # If release build location not found, check for debug build location
+  if (TILEDB_LIB MATCHES "NOTFOUND")
+    get_target_property(TILEDB_LIB TileDB::tiledb_shared IMPORTED_LOCATION_DEBUG)
+  endif()
+  message(STATUS "Found TileDB: ${TILEDB_LIB}")
+else()
+  if (SUPERBUILD)
+    message(STATUS "Adding TileDB as an external project")
+    if (TILEDB_S3 STREQUAL "OFF")
+      message(STATUS "TileDB will be built WITHOUT S3 support")
+    endif()
+
+    # Try to download prebuilt artifacts unless the user specifies to build from source
+    # NB When updating the pinned URLs here, please also update in file apis/r/tools/get_tarball.R
+    if(DOWNLOAD_TILEDB_PREBUILT)
+        if (WIN32) # Windows
+          SET(DOWNLOAD_URL "https://github.com/TileDB-Inc/TileDB/releases/download/2.14.2/tiledb-windows-x86_64-2.14.2-42147ab.zip")
+          SET(DOWNLOAD_SHA1 "46369893e1eaf296d46549d0c105cd284689da84")
+        elseif(APPLE) # OSX
+
+          if (CMAKE_OSX_ARCHITECTURES STREQUAL x86_64 OR CMAKE_SYSTEM_PROCESSOR MATCHES "(x86_64)|(AMD64|amd64)|(^i.86$)")
+            SET(DOWNLOAD_URL "https://github.com/TileDB-Inc/TileDB/releases/download/2.14.2/tiledb-macos-x86_64-2.14.2-42147ab.tar.gz")
+            SET(DOWNLOAD_SHA1 "1e62d71e0d672dadc629d8c7631fb927d57acba7")
+          elseif (CMAKE_OSX_ARCHITECTURES STREQUAL arm64 OR CMAKE_SYSTEM_PROCESSOR MATCHES "^aarch64" OR CMAKE_SYSTEM_PROCESSOR MATCHES "^arm")
+            SET(DOWNLOAD_URL "https://github.com/TileDB-Inc/TileDB/releases/download/2.14.2/tiledb-macos-arm64-2.14.2-42147ab.tar.gz")
+            SET(DOWNLOAD_SHA1 "a39f98fdd602cdffe430ce4c6a8cf70a82623f8b")
+          endif()
+        else() # Linux
+          SET(DOWNLOAD_URL "https://github.com/TileDB-Inc/TileDB/releases/download/2.14.2/tiledb-linux-x86_64-2.14.2-42147ab.tar.gz")
+          SET(DOWNLOAD_SHA1 "84731641b42990f7eeea9d0d06dec39411254b9b")
+        endif()
+
+        ExternalProject_Add(ep_tiledb
+                PREFIX "externals"
+                URL ${DOWNLOAD_URL}
+                URL_HASH SHA1=${DOWNLOAD_SHA1}
+                CONFIGURE_COMMAND ""
+                BUILD_COMMAND ""
+                UPDATE_COMMAND ""
+                PATCH_COMMAND ""
+                TEST_COMMAND ""
+                INSTALL_COMMAND
+                    ${CMAKE_COMMAND} -E copy_directory ${EP_BASE}/src/ep_tiledb ${EP_INSTALL_PREFIX}
+                LOG_DOWNLOAD TRUE
+                LOG_CONFIGURE FALSE
+                LOG_BUILD FALSE
+                LOG_INSTALL FALSE
+                )
+    else() # Build from source
+        ExternalProject_Add(ep_tiledb
+          PREFIX "externals"
+          URL "https://github.com/TileDB-Inc/TileDB/archive/2.14.2.zip"
+          URL_HASH SHA1=badc010633f6089706bc8322108e09de2ee5bda1
+          DOWNLOAD_NAME "tiledb.zip"
+          CMAKE_ARGS
+            -DCMAKE_INSTALL_PREFIX=${EP_INSTALL_PREFIX}
+            -DCMAKE_PREFIX_PATH=${EP_INSTALL_PREFIX}
+            -DTILEDB_S3=${TILEDB_S3}
+            -DTILEDB_VERBOSE=ON
+            -DTILEDB_SERIALIZATION=ON
+            -DTILEDB_TESTS=OFF
+            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+          UPDATE_COMMAND ""
+          INSTALL_COMMAND
+            ${CMAKE_COMMAND} --build . --target install-tiledb
+          LOG_DOWNLOAD TRUE
+          LOG_CONFIGURE TRUE
+          LOG_BUILD TRUE
+          LOG_INSTALL TRUE
+        )
+    endif()
+
+    list(APPEND FORWARD_EP_CMAKE_ARGS -DEP_TILEDB_BUILT=TRUE)
+    list(APPEND EXTERNAL_PROJECTS ep_tiledb)
+  else()
+    message(FATAL_ERROR "Unable to find TileDB library.")
+  endif()
+endif()
+
+if (EP_TILEDB_BUILT AND TARGET TileDB::tiledb_shared AND NOT TILEDBSOMA_BUILD_R)
+  include(TileDBCommon)
+  install_target_libs(TileDB::tiledb_shared)
+endif()
---
+++ packages/tiledbsoma/tiledbsoma-1.0.1.tar.gz/tiledbsoma-1.0.1/setup.py
@@ -0,0 +1,229 @@
+# This file is enables building the libtiledbsoma external package as part of the
+# tiledbsoma module install process.
+#
+# Local non-editable install:
+#   `pip install .`
+#
+# Local editable install:
+#   `pip install -e .`
+#
+# Install from PyPI
+#   `pip install tiledbsoma`
+#
+# Based on ideas from https://github.com/pybind/cmake_example
+# The `bld` script here is reused for pip install, CI, and local builds.
+
+# type: ignore
+import ctypes
+import os
+import pathlib
+import shutil
+import subprocess
+import sys
+
+import setuptools
+import setuptools.command.build_ext
+import wheel.bdist_wheel
+
+this_dir = pathlib.Path(__file__).parent.absolute()
+sys.path.insert(0, str(this_dir))
+import version  # noqa E402
+
+
+def get_libtiledbsoma_library_name():
+    """
+    :return: List of TileDB shared library names.
+    """
+    if os.name == "posix":
+        if sys.platform == "darwin":
+            return "libtiledbsoma.dylib"
+        else:
+            return "libtiledbsoma.so"
+    elif os.name == "nt":
+        return "tiledbsoma.dll"
+    else:
+        raise RuntimeError(f"Unsupported OS name {os.name}")
+
+
+def find_libtiledbsoma_full_path_on_linux(lib_name):
+    # https://stackoverflow.com/questions/35682600/get-absolute-path-of-shared-library-in-python
+    class LINKMAP(ctypes.Structure):
+        _fields_ = [("l_addr", ctypes.c_void_p), ("l_name", ctypes.c_char_p)]
+
+    libdl = ctypes.CDLL(lib_name)
+    dlinfo = libdl.dlinfo
+    dlinfo.argtypes = ctypes.c_void_p, ctypes.c_int, ctypes.c_void_p
+    dlinfo.restype = ctypes.c_int
+
+    libc = ctypes.CDLL(lib_name)
+    lmptr = ctypes.c_void_p()
+    dlinfo(libc._handle, 2, ctypes.byref(lmptr))
+
+    return ctypes.cast(lmptr, ctypes.POINTER(LINKMAP)).contents.l_name.decode()
+
+
+def libtiledbsoma_global_exists():
+    """
+    Returns the path to the globally installed TileDB-SOMA library, if it exists.
+    :return: The path to the TileDB-SOMA library, or None.
+    """
+    # Check to see if TileDB-SOMA is globally installed.
+    lib_name = get_libtiledbsoma_library_name()
+
+    try:
+        # Note: This is a relative path on Linux
+        # https://bugs.python.org/issue21042
+        if os.name == "posix" and sys.platform != "darwin":
+            path = find_libtiledbsoma_full_path_on_linux(lib_name)
+        else:
+            path = ctypes.CDLL(lib_name)
+        print(f"Found globally installed {path}")
+        return pathlib.Path(path).parents[0]
+    except Exception as e:
+        print(e)
+        return None
+
+
+def libtiledbsoma_dist_exists():
+    """
+    Returns the path to the TileDB-SOMA library installed in dist, if it exists.
+    :return: The path to the TileDB-SOMA library, or None.
+    """
+    dist_dirs = [libtiledbsoma_dir / "dist" / "lib"]
+    if sys.platform.startswith("linux"):
+        dist_dirs.append(libtiledbsoma_dir / "dist" / "lib64")
+        dist_dirs.append(libtiledbsoma_dir / "dist" / "lib" / "x86_64-linux-gnu")
+    elif os.name == "nt":
+        dist_dirs.append(libtiledbsoma_dir / "dist" / "bin")
+
+    for lib_dir in dist_dirs:
+        full_lib_path = lib_dir / get_libtiledbsoma_library_name()
+        print(f"Checking: {full_lib_path} exists: {full_lib_path.exists()}")
+        if full_lib_path.exists():
+            return lib_dir
+    return None
+
+
+def find_or_build_package_data(setuptools_cmd):
+    global libtiledbsoma_dir
+
+    # Set up paths
+    scripts_dir = this_dir / "dist_links" / "scripts"
+
+    if scripts_dir.is_symlink():
+        # in git source tree
+        libtiledbsoma_dir = this_dir.parent.parent
+    else:
+        # in extracted sdist, with libtiledbsoma copied into dist_links/
+        libtiledbsoma_dir = this_dir / "dist_links"
+
+    # check if libtiledbsoma is installed in dist
+    lib_dir = libtiledbsoma_dist_exists()
+
+    # check if libtilesoma is globally installed
+    if lib_dir is None:
+        lib_dir = libtiledbsoma_global_exists()
+
+    # if not then build from source
+    if lib_dir is None:
+        # Note: The GitHub build process uses the contents of `bld` as a key
+        # to cache the native binaries. Using non-default options here will
+        # cause that cache to fall out of sync.
+        #
+        # See `.github/workflows/python-ci-single.yml` for configuration.
+        subprocess.run(["./bld"], cwd=scripts_dir)
+        lib_dir = libtiledbsoma_dist_exists()
+
+    # Copy native libs into the package dir so they can be found by package_data
+    package_data = []
+    src_dir = this_dir / "src" / "tiledbsoma"
+    for f in lib_dir.glob("*tiledbsoma*"):
+        if f.suffix != ".a":  # skip static library
+            print(f"  copying file {f} to {src_dir}")
+            shutil.copy(f, src_dir)
+            package_data.append(f.name)
+    assert package_data, f"libtiledbsoma artifacts absent from {lib_dir}"
+
+    # Install shared libraries inside the Python module via package_data.
+    print(f"  adding to package_data: {package_data}")
+    setuptools_cmd.distribution.package_data["tiledbsoma"] = package_data
+
+
+class build_ext(setuptools.command.build_ext.build_ext):
+    def run(self):
+        find_or_build_package_data(self)
+
+
+class bdist_wheel(wheel.bdist_wheel.bdist_wheel):
+    def run(self):
+        find_or_build_package_data(self)
+        super().run()
+
+
+# ----------------------------------------------------------------
+# Don't use `if __name__ == "__main__":` as the `python_requires` must
+# be at top level, outside any if-block
+# https://github.com/pypa/cibuildwheel/blob/7c4bbf8cb31d856a0fe547faf8edf165cd48ce74/cibuildwheel/projectfiles.py#L41-L46
+setuptools.setup(
+    name="tiledbsoma",
+    description="Python API for efficient storage and retrieval of single-cell data using TileDB",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="TileDB, Inc.",
+    author_email="help@tiledb.io",
+    maintainer="TileDB, Inc.",
+    maintainer_email="help@tiledb.io",
+    url="https://github.com/single-cell-data/TileDB-SOMA/tree/main/apis/python",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Operating System :: Unix",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages("src"),
+    # This next is necessary to avoid cibuildwheel thinking we want a python-only wheel:
+    ext_modules=[setuptools.Extension("tiledbsoma.libtiledbsoma", sources=[])],
+    zip_safe=False,
+    install_requires=[
+        "anndata",
+        "attrs>=22.2",
+        # Pinning numba & its particular numpy constraints:
+        # The old pip solver (<=2020) doesn't deal with the transitive
+        # requirements (scanpy -> numba -> numpy) properly resulting in broken
+        # installation of incompatible numpy>=1.24. Issue #1051
+        # These pins can be removed either when there's a new numba release
+        # with less-particular numpy version constraints, or if we decide we no
+        # longer need to support the old pip solver (default on ubuntu 20.04).
+        "numba==0.56.4",
+        "numpy>=1.18,<1.24",
+        "pandas",
+        "pyarrow>=9.0.0",
+        "scanpy>=1.9.2",
+        "scipy",
+        "somacore==1.0.0",
+        "tiledb==0.20.*",
+        "typing-extensions",  # Note "-" even though `import typing_extensions`
+    ],
+    extras_require={
+        "dev": [
+            "black",
+            "ruff",
+            "pytest",
+            "typeguard<3.0",
+        ]
+    },
+    python_requires=">=3.7",
+    cmdclass={"build_ext": build_ext, "bdist_wheel": bdist_wheel},
+    version=version.getVersion(),
+)
