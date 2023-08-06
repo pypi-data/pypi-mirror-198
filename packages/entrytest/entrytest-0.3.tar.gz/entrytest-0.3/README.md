1af6043f0c9ccbb042ab26c2c7f5c0cb0b1cf743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:06 +0000

    Add package entrytest/0.3/entrytest-0.3.tar.gz

---
+++ packages/entrytest/entrytest-0.3.tar.gz/entrytest-0.3/README.md
@@ -0,0 +1,1 @@
+## pip entry test
---
+++ packages/entrytest/entrytest-0.3.tar.gz/entrytest-0.3/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+        name="entrytest",
+    version="0.3",
+    license='MIT',
+    author="FacerAin",
+    author_email="syw5141@gmail.com",
+    description="test pip entry point",
+    long_description=open('README.md').read(),
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    entry_points={"console_scripts": ["entrytest = entrytest.app:main"]},
+    scripts=["bin/entrytest.cmd"]
+)
