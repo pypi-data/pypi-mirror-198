ec31123b391fe6a1ae2b8520f555ee02f74e206a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:16 +0000

    Add package timetoc/0.1.2/timetoc-0.1.2.tar.gz

---
+++ packages/timetoc/timetoc-0.1.2.tar.gz/timetoc-0.1.2/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['timetoc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['holidays>=0.21.13,<0.22.0',
+ 'pandas>=1.5.3,<2.0.0',
+ 'python-dateutil>=2.8.2,<3.0.0',
+ 'requests>=2.28.2,<3.0.0',
+ 'selenium-wire>=5.1.0,<6.0.0',
+ 'typer[all]>=0.7.0,<0.8.0',
+ 'webdriver-manager>=3.8.5,<4.0.0']
+
+entry_points = \
+{'console_scripts': ['timetoc = timetoc.cli:app']}
+
+setup_kwargs = {
+    'name': 'timetoc',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': '',
+    'author': 'Guilherme Prokisch',
+    'author_email': 'guilherme.prokisch@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/timetoc/timetoc-0.1.2.tar.gz/timetoc-0.1.2/timetoc/cli.py
@@ -0,0 +1,78 @@
+# from os import environ
+import os
+from os import environ
+from pathlib import Path
+
+import typer
+from rich.console import Console
+from rich.prompt import Prompt
+
+from timetoc.login import get_acess_token
+from timetoc.timeparser import parse_date_range
+from timetoc.timetracking import add_work_day
+
+app = typer.Typer()
+console = Console()
+
+
+@app.command()
+def main():
+    token_file = Path("./token_file")
+    if token_file.is_file():
+        access_token = open(token_file).readlines(0)[0]
+    else:
+        console.print("token file not found!", style="red")
+        access_token = access_token_by_login()
+
+    day_str = Prompt.ask("Day", default="yesterday")
+    days = parse_date_range(day_str)
+    for day in days:
+        console.print("Day: ", day, style="bold red")
+        start = Prompt.ask("Start Time", default="09:00")
+        finish = Prompt.ask("Finish Time", default="17:00")
+        break_start = Prompt.ask("Break Start", default="12:00")
+        break_finish = Prompt.ask("Break Finish", default="13:00")
+        is_home_office = typer.confirm("Is Home Office?", default=True)
+
+        try:
+            add_work_day(
+                day=day,
+                start=start,
+                finish=finish,
+                break_start=break_start,
+                break_finish=break_finish,
+                is_home_office=is_home_office,
+                token=access_token,
+            )
+        except:
+            console.print("Token expired please login again")
+            access_token = access_token_by_login()
+            add_work_day(
+                day=day,
+                start=start,
+                finish=finish,
+                break_start=break_start,
+                break_finish=break_finish,
+                is_home_office=is_home_office,
+                token=access_token,
+            )
+
+
+def access_token_by_login():
+    time_track_login = typer.confirm("Headless login?")
+    if time_track_login:
+        if not environ.get("TIME_TRACK_EMAIL") or environ.get("TIME_TRACK_PASSWORD"):
+            email = Prompt.ask("Email")
+            password = Prompt.ask("Password", password=True)
+        else:
+            email = environ["TIME_TRACK_EMAIL"]
+            password = environ["TIME_TRACK_PASSWORD"]
+        access_token = get_acess_token(email=email, password=password, headless=False)
+    else:
+        access_token = get_acess_token()
+
+    return access_token
+
+
+if __name__ == "__main__":
+    app()
