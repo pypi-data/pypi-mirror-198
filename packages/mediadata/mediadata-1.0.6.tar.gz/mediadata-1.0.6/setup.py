5cea42e53db0a50224eeaec728cec350128dcfb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:47 +0000

    Add package mediadata/1.0.6/mediadata-1.0.6.tar.gz

---
+++ packages/mediadata/mediadata-1.0.6.tar.gz/mediadata-1.0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("mediadata/requirements.txt", "r") as f:
+    required = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mediadata",
+    version="1.0.6",
+    author="Praveen",
+    author_email="mspraveenkumar77@gmail.com",
+    description="A wrapper tool for mediainfo tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=required,
+    url="https://git.selfmade.ninja/mspraveenkumar77/mediadata_pkg",
+    entry_points={
+        "console_scripts": ["mediadata = mediadata.mediadata:main"],
+    },
+)
