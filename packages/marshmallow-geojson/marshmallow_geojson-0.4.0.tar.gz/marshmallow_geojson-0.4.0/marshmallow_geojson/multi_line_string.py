01e6b31072dc183b937f6a7b94b12e2218d34393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:00 +0000

    Add package marshmallow-geojson/0.4.0/marshmallow_geojson-0.4.0.tar.gz

---
+++ packages/marshmallow-geojson/marshmallow_geojson-0.4.0.tar.gz/marshmallow_geojson-0.4.0/marshmallow_geojson/multi_line_string.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from marshmallow.fields import List, Str, Tuple
+from marshmallow.validate import OneOf
+
+from ._base import BaseSchema, lat, lon
+from .object_type import MULTI_LINE_STRING
+
+
+class MultiLineStringSchema(BaseSchema):
+    type = Str(
+        required=True,
+        validate=OneOf(
+            [MULTI_LINE_STRING],
+            error='Invalid multi line string string type',
+        )
+    )
+
+    coordinates = List(
+        List(
+            Tuple([lon, lat], required=True),
+            required=True
+        ),
+        required=True,
+    )
+
---
+++ packages/marshmallow-geojson/marshmallow_geojson-0.4.0.tar.gz/marshmallow_geojson-0.4.0/marshmallow_geojson/multi_polygon.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from marshmallow.fields import List, Str, Tuple
+from marshmallow.validate import OneOf
+
+from ._base import BaseSchema, lat, lon
+from .object_type import MULTI_POLYGON
+
+
+class MultiPolygonSchema(BaseSchema):
+    type = Str(
+        required=True,
+        validate=OneOf(
+            [MULTI_POLYGON],
+            error='Invalid multi polygon type',
+        )
+    )
+
+    coordinates = List(
+        List(
+            List(
+                Tuple([lon, lat], required=True),
+                required=True,
+            ),
+            required=True,
+        ),
+        required=True,
+    )
+
