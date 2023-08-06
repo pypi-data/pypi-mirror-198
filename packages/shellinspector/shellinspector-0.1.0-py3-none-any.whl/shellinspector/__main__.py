d279d6e08dd796487e5418ef712cda035358f846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:53 +0000

    Add package shellinspector/0.1.0/shellinspector-0.1.0-py3-none-any.whl

---
+++ packages/shellinspector/shellinspector-0.1.0-py3-none-any.whl/shellinspector/__main__.py
@@ -0,0 +1,78 @@
+import argparse
+import re
+import sys
+from pathlib import Path
+
+from shellinspector.parser import parse
+from shellinspector.runner import ShellRunner
+
+
+def get_vagrant_sshport():
+    inventory_file = (
+        Path(__file__).parent
+        / "../.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory"
+    )
+
+    if not inventory_file.exists():
+        return None
+
+    content = inventory_file.read_text()
+    return int(re.search("ansible_port=([0-9]+)", content)[1])
+
+
+def run_spec_file(runner, path, sshconfig):
+    print(f"running {path}")
+
+    spec_file = Path(path).resolve()
+
+    if not spec_file.exists():
+        print(f"file {spec_file} does not exist")
+        return False
+
+    lines = spec_file.read_text().splitlines()
+    commands = parse(spec_file, lines)
+
+    if commands is False:
+        print("parsing failed")
+        return False
+
+    return runner.run(commands, sshconfig)
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--target-host",
+        default="vagrant",
+    )
+    parser.add_argument(
+        "test_files",
+        nargs="+",
+    )
+
+    args = parser.parse_args()
+
+    if args.target_host == "vagrant":
+        sshconfig = {
+            "server": "127.0.0.1",
+            "username": "root",
+            "port": get_vagrant_sshport(),
+        }
+    else:
+        sshconfig = {
+            "server": args.target_host,
+            "username": "root",
+            "port": 22,
+        }
+
+    success = True
+    runner = ShellRunner()
+
+    for spec_file in args.test_files:
+        success &= run_spec_file(runner, spec_file, sshconfig)
+
+    return success
+
+if __name__ == "__main__":
+    success = main()
+    sys.exit(0 if success else 1)
