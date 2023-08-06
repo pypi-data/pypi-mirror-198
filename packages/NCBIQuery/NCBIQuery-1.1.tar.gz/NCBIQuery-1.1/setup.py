609c5dd969076baae9e5aeb83b7f10bde470e9c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:27 +0000

    Add package NCBIQuery/1.1/NCBIQuery-1.1.tar.gz

---
+++ packages/NCBIQuery/NCBIQuery-1.1.tar.gz/NCBIQuery-1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+exec(open("NCBIQuery/version.py").read())
+
+setup(
+    name='NCBIQuery',
+    version=__version__,
+    license='Delt4',
+    author="Leo Sun",
+    author_email='zidaneandmessi@gmail.com',
+    packages=['NCBIQuery'],
+    url='https://github.com/nachovy/NCBIQuery',
+    keywords='NCBI',
+    install_requires=[
+          'html',
+          'requests',
+          'tokenizers',
+      ],
+
+)
