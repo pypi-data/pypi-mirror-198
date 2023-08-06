9a919903edf9bc016a437cba9156f28aaee53ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:38 +0000

    Add package cliai/0.2.8/cliai-0.2.8-py3-none-any.whl

---
+++ packages/cliai/cliai-0.2.8-py3-none-any.whl/cliai/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+TODO:
+    - Save Conversations
+    - Fast response
+        - `cliai ask <message>`
+    - Model
+        - `cliai chat --model <model>`
+        - `cliai audio` with Whisper!
+    - Configuration manager
+        - `cliai config`
+        - set default model, system etc.
+    - Preset prompts
+        - `cliai git`: Generate Git commit message
+    - Provide context
+        - `cliai chat -i <file>`
+    - Python module (and use in Vim)
+"""
+__version__ = '0.2.8'
+__all__ = ['core', 'config', 'convo', 'cli', 'util']
+
+if __name__ == '__main__':
+    for module in __all__:
+        exec("import {}".format(module))
+    cli.cli()
