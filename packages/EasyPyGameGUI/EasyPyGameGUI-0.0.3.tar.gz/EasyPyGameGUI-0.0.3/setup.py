fe9b8eb9e888d96bd001341c58a61c9ef9251822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:13 +0000

    Add package EasyPyGameGUI/0.0.3/EasyPyGameGUI-0.0.3.tar.gz

---
+++ packages/EasyPyGameGUI/EasyPyGameGUI-0.0.3.tar.gz/EasyPyGameGUI-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='EasyPyGameGUI',
+    version='0.0.3',    
+    description='PyGame based module allowing for easy in-Pygame GUI',
+    url='',
+    author='WerronPL',
+    author_email='paligaaleksander@gmail.com',
+    license='MIT License',
+    packages=['EasyPyGameGUI'],
+    install_requires=['mpi4py>=3.1.4',
+                      'pygame',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3.10',        
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
