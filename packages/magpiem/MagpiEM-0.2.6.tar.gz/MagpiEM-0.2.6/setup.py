83998592a25529cc0b24fc9da734dc5b270a2eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:32 +0000

    Add package magpiem/0.2.6/MagpiEM-0.2.6.tar.gz

---
+++ packages/magpiem/MagpiEM-0.2.6.tar.gz/MagpiEM-0.2.6/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 30 12:05:54 2023
+
+@author: Frank
+"""
+
+from setuptools import setup, find_packages
+
+requirements = [
+    "numpy",
+    "pandas",
+    "prettytable",
+    "scipy",
+    "plotly",
+    "pathlib",
+    "dash==2.8.0",
+    "dash_bootstrap_components",
+    "dash_bootstrap_templates",
+    "dash_extensions",
+    "dash_daq",
+    "flask",
+    "imodmodel",
+    "emfile",
+    "pyyaml",
+    "zipfile",
+]
+
+setup(
+    name="MagpiEM",
+    version="0.2.6",
+    description="Automated cleaning of sub-tomogram particle picking",
+    author="Frank Nightingale",
+    author_email="frank.nightingale@linacre.ox.ac.uk",
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={"console_scripts": ["magpiem = MagpiEM.dash_ui:main"]},
+    python_requires=">3.8.0",
+)
