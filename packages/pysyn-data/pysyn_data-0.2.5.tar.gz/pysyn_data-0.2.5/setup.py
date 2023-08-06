812a1c92ff80faa6d842657b4f6cdc0fd11c1b9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:28 +0000

    Add package pysyn-data/0.2.5/pysyn_data-0.2.5.tar.gz

---
+++ packages/pysyn-data/pysyn_data-0.2.5.tar.gz/pysyn_data-0.2.5/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+setuptools.setup(
+ name='pysyn_data',
+ version='0.2.5',
+ author="Raghav_Ritesh",
+ author_email="raghav.20.rb@gmail.com",
+ description="This package is for generating synthetic data using 4 models i.e Conditional Genrative Adveserial Networks(CTGAN), Gaussian Mixture Model (GMM), Prinicipal Component Analysis (PCA) and Bayesian Network (BN). It also informs the user which model will work best based on the input data characterisitics."
+ ,
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+ readme = "README.md",
+ )
