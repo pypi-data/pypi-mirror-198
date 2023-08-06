f0301ae535696db770b9f4a4fc4d16e870bacf0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:44 +0000

    Add package letloop.py/0.1.1/letloop.py-0.1.1.tar.gz

---
+++ packages/letloop.py/letloop.py-0.1.1.tar.gz/letloop.py-0.1.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['letloopit']
+install_requires = \
+['requests>=2.28.2,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['letloop.py = letloopit:main']}
+
+setup_kwargs = {
+    'name': 'letloop.py',
+    'version': '0.1.1',
+    'description': 'A cloud for the parenthetical leaning doers',
+    'long_description': None,
+    'author': 'Amir',
+    'author_email': 'amirouche@hyper.dev',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
