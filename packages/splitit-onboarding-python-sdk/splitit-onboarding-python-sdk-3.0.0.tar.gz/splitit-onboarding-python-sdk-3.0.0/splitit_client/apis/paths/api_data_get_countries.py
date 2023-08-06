7b272ca0bf91b262490cc1c4f6d32e46ae713d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:20 +0000

    Add package splitit-onboarding-python-sdk/3.0.0/splitit-onboarding-python-sdk-3.0.0.tar.gz

---
+++ packages/splitit-onboarding-python-sdk/splitit-onboarding-python-sdk-3.0.0.tar.gz/splitit-onboarding-python-sdk-3.0.0/splitit_client/apis/paths/api_data_get_countries.py
@@ -0,0 +1,7 @@
+from splitit_client.paths.api_data_get_countries.get import ApiForget
+
+
+class ApiDataGetCountries(
+    ApiForget,
+):
+    pass
---
+++ packages/splitit-onboarding-python-sdk/splitit-onboarding-python-sdk-3.0.0.tar.gz/splitit-onboarding-python-sdk-3.0.0/splitit_client/apis/paths/api_merchants_get_details.py
@@ -0,0 +1,7 @@
+from splitit_client.paths.api_merchants_get_details.get import ApiForget
+
+
+class ApiMerchantsGetDetails(
+    ApiForget,
+):
+    pass
