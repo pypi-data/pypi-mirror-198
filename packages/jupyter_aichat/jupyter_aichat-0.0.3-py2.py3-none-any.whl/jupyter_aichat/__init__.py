50bbc36ba3300e2d4d2e5950eb13a916fc1e2c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:26 +0000

    Add package jupyter_aichat/0.0.3/jupyter_aichat-0.0.3-py2.py3-none-any.whl

---
+++ packages/jupyter_aichat/jupyter_aichat-0.0.3-py2.py3-none-any.whl/jupyter_aichat/__init__.py
@@ -0,0 +1,13 @@
+"""Jupyter magic command for having AI chat conversations in a notebook"""
+
+__version__ = "0.0.3"
+
+from IPython.core.interactiveshell import InteractiveShell
+
+from .aichat_magic import ConversationMagic
+from .output import output, TemplateLoader
+
+
+def load_ipython_extension(ipython: InteractiveShell) -> None:
+    ipython.register_magics(ConversationMagic)  # type: ignore[no-untyped-call]
+    output(TemplateLoader()["load_ipython_extension"])
