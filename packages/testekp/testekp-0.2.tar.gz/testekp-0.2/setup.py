60cfa621dbcf831d2a8e4a3af94e201b70978e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:42 +0000

    Add package testekp/0.2/testekp-0.2.tar.gz

---
+++ packages/testekp/testekp-0.2.tar.gz/testekp-0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+python_version = '>=3.7'
+
+install_requires = [
+    'numpy',
+    'scipy',
+    'sympy',
+    'tabulate',
+    'irrep',
+    'qsymm'
+]
+
+# uses README.md as the package long description
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="testekp",
+    description="...",
+    long_description_content_type="text/markdown",
+    version=0.2,
+    packages=find_packages('.'),
+    install_requires=install_requires,
+    python_requires=python_version
+    #licence=
+    #classifiers=
+)
