12d5479bf39621a642643c18ed53844b12013944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:34 +0000

    Add package mytools-wjs/0.0.5/mytools_wjs-0.0.5.tar.gz

---
+++ packages/mytools-wjs/mytools_wjs-0.0.5.tar.gz/mytools_wjs-0.0.5/mytools_wjs/__init__.py
@@ -0,0 +1,5 @@
+from mytools_wjs.mytools import expansion_up, expansion
+__name__ = "mytools_wjs"
+__author__ = "Wujs (55340808@qq.com)"
+__version__ = "0.0.5"
+__all__ = ['expansion', 'expansion_up']
