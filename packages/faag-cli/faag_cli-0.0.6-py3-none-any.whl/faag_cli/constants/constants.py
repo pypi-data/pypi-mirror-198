7f882af41ec8f6be1d19f17c044d9f0cf10af196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:09 +0000

    Add package faag-cli/0.0.6/faag_cli-0.0.6-py3-none-any.whl

---
+++ packages/faag-cli/faag_cli-0.0.6-py3-none-any.whl/faag_cli/constants/constants.py
@@ -0,0 +1,47 @@
+"""
+Description: This file contains the constants that will be used in the project.
+"""
+
+"""
+FOLDERS_FILES is a dictionary that contains the folders and files that will be created.
+Acts as a template for the project structure.
+"""
+
+FOLDERS_FILES = {
+    "config": [
+        "__init__",
+        "constants",
+        "config",
+    ],
+    "connection": [
+        "__init__",
+    ],
+    "controller": [
+        "__init__",
+        "sample_controller",
+    ],
+    "schemas": [
+        "__init__",
+    ],
+    "request": [],
+    "response": [
+        "common_response_schema",
+        "user_response_schema",
+    ],
+    "utils": [
+        "__init__",
+        "logger",
+    ],
+    "model": [
+        "__init__",
+        "sample_model",
+    ],
+    "service": [
+        "__init__",
+        "sample_service",
+    ],
+    "dao": [
+        "__init__",
+        "sample_dao",
+    ],
+}
