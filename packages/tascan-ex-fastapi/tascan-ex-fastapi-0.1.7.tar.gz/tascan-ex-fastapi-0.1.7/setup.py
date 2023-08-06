37885a9aaeac0d233d628166761721ba9a81cf05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:00 +0000

    Add package tascan-ex-fastapi/0.1.7/tascan-ex-fastapi-0.1.7.tar.gz

---
+++ packages/tascan-ex-fastapi/tascan-ex-fastapi-0.1.7.tar.gz/tascan-ex-fastapi-0.1.7/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+version = '0.1.7'
+
+
+setup(
+    name='tascan-ex-fastapi',
+    version=version,
+
+    author='alta7700',
+    author_email='alta7700@mail.ru',
+
+    packages=find_packages(),
+    package_dir={'ex_fastapi': 'ex_fastapi'},
+    package_data={'ex_fastapi': ['templates/*.html']},
+
+    install_requires=[
+        'fastapi[all]==0.94.1',
+        'phonenumbers==8.13.7',
+        'passlib==1.7.4',
+        'PyJWT==2.6.0',
+        'cryptography==39.0.2',
+    ],
+
+    python_requires='>=3.11',
+)
