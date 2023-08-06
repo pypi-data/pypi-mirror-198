0e3aad3e681228bb6723173feb67d8f243ed107a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:26 +0000

    Add package AutomaPy/1.3.4/AutomaPy-1.3.4.tar.gz

---
+++ packages/AutomaPy/AutomaPy-1.3.4.tar.gz/AutomaPy-1.3.4/AutomaPy/__init__.py
@@ -0,0 +1,2 @@
+from AutomaPy.main import DFA, TuringMachine, Tokenizer
+from AutomaPy.examples import EndingWithOneZeroOne, DecimalNumberDivisibleByTwo, ThreeConsecutiveOne, EqualNumberOfOneZero, CountNumberOfOneZero, TuringMachineEvenOnes
