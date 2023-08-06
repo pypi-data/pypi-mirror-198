05d089d75fa7d683688a0c7dcd14aa8a842415b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:02 +0000

    Add package cdk-lex-zip-import/0.0.86/cdk-lex-zip-import-0.0.86.tar.gz

---
+++ packages/cdk-lex-zip-import/cdk-lex-zip-import-0.0.86.tar.gz/cdk-lex-zip-import-0.0.86/src/cdk_lex_zip_import/_jsii/__init__.py
@@ -0,0 +1,25 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-lex-zip-import",
+    "0.0.86",
+    __name__[0:-6],
+    "cdk-lex-zip-import@0.0.86.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
