e6070e56751bf02e4579d0bffc554587e0667f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:29 +0000

    Add package code2chart/0.1.0/code2chart-0.1.0.tar.gz

---
+++ packages/code2chart/code2chart-0.1.0.tar.gz/code2chart-0.1.0/code2flowchart/generators.py
@@ -0,0 +1,15 @@
+from langchain.llms import OpenAI
+
+from utils.mermaid import generate_flowchart
+from templates.prompt import flowchart_template
+
+
+def generate_output(code):
+    llm = OpenAI(temperature=0.9)
+
+    prompt = flowchart_template(
+        f"""
+        {code}
+        """)
+    res = llm(prompt)
+    return generate_flowchart(res.replace('```', ''))
---
+++ packages/code2chart/code2chart-0.1.0.tar.gz/code2chart-0.1.0/code2flowchart/tools/FlowchartGenTool.py
@@ -0,0 +1,23 @@
+from langchain.tools import BaseTool
+from code2flowchart.utils.mermaid import generate_flowchart
+from langchain.llms import OpenAI
+from code2flowchart.templates.flowchart_prompt import flowchart_template
+
+
+class FlowchartGenTool(BaseTool):
+    name = "Generate flowchart"
+    description = (
+        "A flowchart generator, which generates a flowchart out of some text explaining a code process."
+        "Input should be a valid a detailed text explaining a process."
+        ""
+    )
+    llm = OpenAI(temperature=0.9)
+
+    def _run(self, query: str) -> str:
+        """Use the tool."""
+        code = self.llm(flowchart_template(query))
+        return generate_flowchart(code.replace('`', ''))
+
+    async def _arun(self, query: str) -> str:
+        """Use the tool asynchronously."""
+        raise NotImplementedError("BingSearchRun does not support async")
