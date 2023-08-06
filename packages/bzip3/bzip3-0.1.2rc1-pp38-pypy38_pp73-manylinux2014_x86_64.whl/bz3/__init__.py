72523eabffc7ee9801e62c58b384662d2149168e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:09 +0000

    Add package bzip3/0.1.2rc1/bzip3-0.1.2rc1-pp38-pypy38_pp73-manylinux2014_x86_64.whl

---
+++ packages/bzip3/bzip3-0.1.2rc1-pp38-pypy38_pp73-manylinux2014_x86_64.whl/bz3/__init__.py
@@ -0,0 +1,15 @@
+"""
+Copyright (c) 2008-2021 synodriver <synodriver@gmail.com>
+"""
+__version__ = "0.1.2rc1"
+
+from bz3.backends import (
+    bound,
+    compress_file,
+    compress_into,
+    decompress_file,
+    decompress_into,
+    libversion,
+    test_file,
+)
+from bz3.bz3 import BZ3File, compress, decompress, open
