77aaa8a1ab4a13f2e2a8c3a37bd47d00d34b2d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:59 +0000

    Add package argcomplete/3.0.1/argcomplete-3.0.1-py3-none-any.whl

---
+++ packages/argcomplete/argcomplete-3.0.1-py3-none-any.whl/argcomplete/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2012-2023, Andrey Kislyuk and argcomplete contributors.
+# Licensed under the Apache License. See https://github.com/kislyuk/argcomplete for more info.
+
+from . import completers
+from .completers import ChoicesCompleter, DirectoriesCompleter, EnvironCompleter, FilesCompleter, SuppressCompleter
+from .exceptions import ArgcompleteException
+from .finders import CompletionFinder, ExclusiveCompletionFinder, safe_actions
+from .io import debug, mute_stderr, warn
+from .shell_integration import shellcode
+
+autocomplete = CompletionFinder()
+autocomplete.__doc__ = """ Use this to access argcomplete. See :meth:`argcomplete.CompletionFinder.__call__()`. """
