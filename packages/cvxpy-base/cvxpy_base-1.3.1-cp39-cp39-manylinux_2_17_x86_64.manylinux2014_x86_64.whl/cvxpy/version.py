b729398808425d5600c7719cf6a0f576f4035583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:41 +0000

    Add package cvxpy-base/1.3.1/cvxpy_base-1.3.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cvxpy-base/cvxpy_base-1.3.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/cvxpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CVXPY SETUP.PY
+short_version = '1.3.1'
+version = '1.3.1'
+full_version = '1.3.1'
+git_revision = 'a335b0c'
+commit_count = '0'
+release = True
+if not release:
+    version = full_version
