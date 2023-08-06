337011705cd30f613cf9d97378833d3d9af2e5ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:27 +0000

    Add package dtsnejedi/0.1.2/dtsnejedi-0.1.2.tar.gz

---
+++ packages/dtsnejedi/dtsnejedi-0.1.2.tar.gz/dtsnejedi-0.1.2/setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup, find_packages
+ 
+setup(name = "dtsnejedi",
+      author="Aleksandr Odnakov",
+      author_email="me@dnakov.ooo",
+      packages = find_packages(),
+      install_requires = ['matplotlib','numpy','scipy','tqdm'],
+      version='0.1.2')
