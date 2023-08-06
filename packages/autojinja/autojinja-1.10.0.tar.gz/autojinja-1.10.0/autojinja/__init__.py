4068d39b324924cc4414a6c7e42741b01a578b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:17 +0000

    Add package autojinja/1.10.0/autojinja-1.10.0.tar.gz

---
+++ packages/autojinja/autojinja-1.10.0.tar.gz/autojinja-1.10.0/autojinja/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "1.10.0"
+
+from . import defaults
+from . import exceptions
+from . import main
+from . import path
+from .path import DirPath, Path
+from . import parser
+from .parser import ParserSettings
+from . import templates
+from .templates import AutoLoader, CogTemplate, JinjaTemplate, RawTemplate
+from . import utils
+
+import os
