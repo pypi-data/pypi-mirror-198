50c009f21d6b101260e33e9259225a33c1fc6f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:28 +0000

    Add package linpde-gp/0.0.1a1/linpde_gp-0.0.1a1-py3-none-any.whl

---
+++ packages/linpde-gp/linpde_gp-0.0.1a1-py3-none-any.whl/linpde_gp/__init__.py
@@ -0,0 +1,17 @@
+# isort: off
+from . import (
+    linops,
+    randvars,
+    domains,
+    functions,
+    linfuncops,
+    linfunctls,
+    randprocs,
+    problems,
+)
+
+# isort: on
+
+from . import linalg, typing, utils
+
+__version__ = "0.0.1a1"
