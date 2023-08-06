fb0b2daad3d77316ed2c8d60fc83842dbc7e2b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:00 +0000

    Add package hypersquirrel/0.22/hypersquirrel-0.22-py3-none-any.whl

---
+++ packages/hypersquirrel/hypersquirrel-0.22-py3-none-any.whl/hypersquirrel/entry.py
@@ -0,0 +1,30 @@
+import re
+from typing import Iterator
+
+from hypersquirrel.core import Watchlist
+from hypersquirrel.scraperfactory import get_scraper
+
+PAGED_PATTERN = re.compile("<\d+-\d+>")
+
+
+def create_watchlist(url: str, max_items: int):
+    match = PAGED_PATTERN.findall(url)
+    if len(match) == 1:
+        pages = match[0].replace("<", "").replace(">", "").split("-")
+        return Watchlist(
+            url=url.replace(match[0], "${page}"),
+            max_items=max_items,
+            page_min=int(pages[0]),
+            page_max=int(pages[1])
+        )
+    return Watchlist(
+        url=url,
+        max_items=max_items
+    )
+
+
+def scrape(w: Watchlist, body: str = None) -> Iterator[dict]:
+    if body:
+        scraper = get_scraper(w.url)
+        yield from scraper(w.url, body)
+    return w.scrape(get_scraper(w))
