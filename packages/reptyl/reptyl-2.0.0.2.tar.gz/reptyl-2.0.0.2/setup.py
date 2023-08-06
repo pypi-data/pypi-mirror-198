7d1d58c0beea23b96954782b35634e31cc754fb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:51 +0000

    Add package reptyl/2.0.0.2/reptyl-2.0.0.2.tar.gz

---
+++ packages/reptyl/reptyl-2.0.0.2.tar.gz/reptyl-2.0.0.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+setuptools.setup(name='Reptyl',
+    version='2.0.0.2',
+    author='0ut0flin3',
+    description='Reptyl is a free-to-use and cross-platform command line shell that supports execution of commands in natural language',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        
+                ],
+    install_requires=['outdated==0.2.2'],
+    python_requires='>=3'
+        )
