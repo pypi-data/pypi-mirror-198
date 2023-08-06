5fc94771ee91e950e08c5af761f2ad3744a6299d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:24 +0000

    Add package trill/0.0.6/trill-0.0.6-py3-none-any.whl

---
+++ packages/trill/trill-0.0.6-py3-none-any.whl/trill/ast/base.py
@@ -0,0 +1,30 @@
+"""Base AST classes and types."""
+from dataclasses import dataclass
+from typing import Any, TypeVar, Generic
+
+T = TypeVar('T')
+
+class UnknownNodeType(Exception):
+    """Unknown node type."""
+
+
+class Visitor(Generic[T]):
+    """Visitor base class."""
+
+    def visit_generic(self, node: 'Node'):
+        """Visit a generic node."""
+        raise UnknownNodeType(f"No visit_{type(node).__name__} method")
+
+
+@dataclass
+class Node:
+    """Node base class."""
+
+    def accept(self, visitor: Visitor[Any]):
+        """Accept visitor."""
+        method_name = 'visit_' + type(self).__name__ + '_' + type(self).__bases__[0].__name__
+        visitor_function = getattr(visitor, method_name)
+        return visitor_function(self)
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} />"
