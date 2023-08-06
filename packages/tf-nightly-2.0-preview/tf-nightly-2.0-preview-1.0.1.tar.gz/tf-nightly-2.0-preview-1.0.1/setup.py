bf14937d7e2ffaab9796f8ae273e2fbeb2d4d20d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:09 +0000

    Add package tf-nightly-2.0-preview/1.0.1/tf-nightly-2.0-preview-1.0.1.tar.gz

---
+++ packages/tf-nightly-2.0-preview/tf-nightly-2.0-preview-1.0.1.tar.gz/tf-nightly-2.0-preview-1.0.1/setup.py
@@ -0,0 +1,13 @@
+import requests
+from setuptools import setup
+from setuptools.command.install import install
+
+print("test anupamas01 ")
+
+
+setup(name='tf-nightly-2.0-preview',
+      version='1.0.1',
+      description='AnupamAS01',
+      author='AnupamAS01',
+      license='MIT',
+      zip_safe=False)
