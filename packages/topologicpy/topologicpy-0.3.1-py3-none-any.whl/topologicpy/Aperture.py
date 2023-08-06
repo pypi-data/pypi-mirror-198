786af85d760700dd8885e0cd685ef4a2c2e2d8e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:55 +0000

    Add package topologicpy/0.3.1/topologicpy-0.3.1-py3-none-any.whl

---
+++ packages/topologicpy/topologicpy-0.3.1-py3-none-any.whl/topologicpy/Aperture.py
@@ -0,0 +1,46 @@
+import topologicpy
+import topologic
+
+class Aperture(topologic.Aperture):
+    @staticmethod
+    def ByTopologyContext(topology, context):
+        """
+        Creates an aperture.
+
+        Parameters
+        ----------
+        topology : topologic.Topology
+            The input topology to which the aperture will belong
+        context : topologic.Context
+            The context of the aperture.
+
+        Returns
+        -------
+        topologic.Aperture
+            The created aperture.
+
+        """
+        aperture = None
+        try:
+            aperture = topologic.Aperture.ByTopologyContext(topology, context)
+        except:
+            aperture = None
+        return aperture
+
+    @staticmethod
+    def ApertureTopology(aperture):
+        """
+        Returns the topology of the input aperture.
+        
+        Parameters
+        ----------
+        aperture : topologic.Aperture
+            The input aperture.
+
+        Returns
+        -------
+        topologic.Topology
+            The topology of the input aperture.
+
+        """
+        return topologic.Aperture.Topology(aperture)
