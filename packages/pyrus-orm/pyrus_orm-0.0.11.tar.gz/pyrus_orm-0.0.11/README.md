ff9b184451d9cf209da4b670e3f5eed997bab673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:06 +0000

    Add package pyrus-orm/0.0.11/pyrus_orm-0.0.11.tar.gz

---
+++ packages/pyrus-orm/pyrus_orm-0.0.11.tar.gz/pyrus_orm-0.0.11/README.md
@@ -0,0 +1,210 @@
+pyrus-orm
+=========
+
+Radically simple, django/peewee-like, easy and incomplete ORM for [Pyrus](https://pyrus.com).
+
+With pyrus-orm, you can read, create and modify [tasks](https://pyrus.com/en/help/api/models#form-registry-task).
+
+Works with [pyrus-api](https://github.com/simplygoodsoftware/pyrusapi-python) under the hood.
+
+### This is an early development version
+
+### Features:
+
+- Define models with:
+    - [x] simple fields (text, number, dates, checkmark, flag, ...)
+    - [x] catalog fields, single item
+    - [ ] catalog fields, multiple items
+    - [ ] "title" fields (pyrus-orm ignores the nested structure of 'title' fields, all its contents are treated as usual root-level fields)
+    - [x] multiple choice fields (without nested fields at this moment)
+- Operations with models:
+    - [x] Create and save
+    - [x] Read from registry by ID
+    - [x] Modify and save changes
+- Filtering:
+    - [x] by include_archived and steps fields
+    - [x] by value of simple or catalog fields
+    - [ ] less than, greater than
+    - [ ] value in a list
+    - [ ] ranges
+
+Installation
+-----------
+
+```shell
+pip install pyrus-orm
+```
+
+Examples
+-------
+
+
+### Define model and initialize
+
+```python
+
+class Book(PyrusModel):
+    title = TextField(1)  # 1 is a field ID in pyrus's form
+    time = TimeField(2)
+    date = DateField(3)
+    number = NumericField(4)
+    round_number = IntegerField(5)
+    author = CatalogField(6, catalog=<catalog id>)
+
+    class Meta:
+        form_id = <form_id>
+
+
+pyrus_api = PyrusAPI(...)
+session = PyrusORMSession(pyrus_api)
+
+set_session_global(session)
+```
+
+
+### Create item
+
+```python
+book = Book(
+    title='Don Quixote',
+    date='1605-01-01',
+    author=Book.author.find({'Name': 'Alonso Fernández de Avellaneda'})
+)
+
+book.save()
+
+book.id
+>>> <task_id>
+```
+
+
+### Read and modify item
+
+```python
+book = Book.objects.get(id=...)
+
+# simple field
+book.title
+>>> 'Don Quixote'
+book.title = 'Don Quixote, Part Two'
+book.save('title changed')
+
+# catalog field
+book.author
+>>> CatalogItem(item_id=..., values={'Name': 'Alonso Fernández de Avellaneda'})  # values comes from the catalog definition
+
+book.author.find_and_set({'Name': 'Miguel de Cervantes'})  # may raise ValueError if no value found
+book.save('changed an author to the real one')
+```
+
+### Catalog Enum fields
+
+Enums can be mapped to catalog items by ID or by custom property name.
+
+#### Enums mapped to specific catalog items ID
+
+No catalog lookups are preformed on reading or writing of such fields.
+
+```python
+class Genre(Enum):
+    fiction = 100001
+    nonfiction = 100002
+
+
+class Book(PyrusModel):
+    genre = CatalogEnumField(<field_id>, catalog_id=<catalog_id>, enum=Genre, id_field='item_id')
+
+book = Book.objects.get(id=...)
+
+book.genre
+>>> Genre.fiction
+
+book.genre = Genre.nonfiction
+book.save()
+
+book.genre
+>>> Genre.nonfiction
+```
+
+
+#### Enums mapped to catalog item properties
+
+(imagine book has a property 'media' with field 'Name')
+
+```python
+class Media(Enum):
+    paper = 'paper'
+    papirus = 'papirus'
+    pdf = 'pdf'
+
+class Book(PyrusModel):
+    media = CatalogEnumField(<field_id>, catalog_id=<catalog_id>, enum=Genre, id_field='Name')
+```
+
+### Filtering
+
+Only basic filtering is supported:
+
+```python
+
+Book.objects.get_filtered(
+    title='Don Quixote',
+)
+>>> [Book(...), ...]
+
+
+Book.objects.get_filtered(
+    genre=Book.genre.find({'Name': 'Fiction'})
+)
+>>> [Book(...), ...]
+
+Book.objects.get_filtered(
+    ...
+    include_archived=True,
+    steps=[1, 2],
+)
+>>> [Book(...), ...]
+```
+
+
+### Catalog fields, all the API
+```python
+# Read values
+
+# Non-empty value
+book.author
+>>> CatalogItem(item_id=..., values={<your custom values here>})
+
+assert bool(book.author) == True
+
+# Empty value
+book.author
+>>> CatalogEmptyValue()
+
+assert bool(book.author) == False
+
+
+# Get all possible values (works for empty fields as well)
+book.author.catalog()
+>>> [CatalogItem(...), CatalogItem(...), ...]
+
+
+# Find a value in a catalog
+new_author = book.author.catalog().find({'Name': 'Miguel de Cervantes'})
+new_author
+>>> CatalogItem(item_id=..., values={'Name': 'Miguel de Cervantes'})  # or None
+
+book.author = new_author
+book.save()
+
+
+# Find and set shortcut
+book.author.catalog().find_and_set({'Name': 'William Shakespeare'})
+
+book.author.find_and_set({'Name': 'NonExistent'})
+>>> ValueError raised
+
+
+# Set value to a specific item_id
+book.author = CatalogItem(item_id=123456)
+```
---
+++ packages/pyrus-orm/pyrus_orm-0.0.11.tar.gz/pyrus_orm-0.0.11/pyrus_orm/model.py
@@ -0,0 +1,164 @@
+import copy
+import sys
+from datetime import datetime
+from typing import Any, TypeVar, Type, Optional, Generic
+
+from pyrus_orm.catalog import CatalogItem, CatalogEmptyValue
+from pyrus_orm.fields import BaseField
+from pyrus_orm.manager import Manager
+from pyrus_orm.session import get_session
+from pyrus_orm.utils import classproperty
+
+T = TypeVar('T', bound='PyrusModel')
+
+
+class PyrusModel(Generic[T]):
+    id: Optional[int]
+    create_date: datetime
+    last_modified_date: datetime
+
+    _data = None
+    _field_values: dict[int, Any] = {}
+    _changed_fields: set[int]
+
+    class Meta:
+        form_id: int
+        fields: dict[str, BaseField]
+
+    @classproperty
+    def objects(cls: Type[T]) -> Manager[T]:
+        return Manager(cls)
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+
+        try:
+            int(cls.Meta.form_id)
+        except (ValueError, AttributeError) as e:
+            raise Exception('Model.Meta.form_id is not set') from e
+
+    def __init__(self, **kwargs):
+        self._field_values = {}
+        self._changed_fields = set()
+        self.id = None
+
+        for field_name, field in self.Meta.fields.items():
+            if field.id not in self._field_values:
+                self._field_values[field.id] = {
+                    'id': field.id,
+                }
+
+        for k, v in kwargs.items():
+            if v is not None:
+                setattr(self, k, v)
+
+    @classmethod
+    def from_pyrus_data(
+        cls: Type[T],
+        data: dict[str, Any],
+    ) -> T:
+        data = copy.deepcopy(data)
+
+        def fix_datetime(v: str) -> str:
+            # makes datetime compatible with python's 3.9 fromisoformat() function
+            # 3.10+ does not require this
+            return v.replace('Z', '+00:00')
+
+        create_date = datetime.fromisoformat(fix_datetime(data['create_date']))
+        last_modified_date = datetime.fromisoformat(fix_datetime(data['last_modified_date']))
+
+        fields = data.pop('fields')
+
+        # Flatten 'title' fields. Title is a field type which groups another fields inside.
+        flatten_fields = {}
+        for field in fields:
+            if field['type'] == 'title':  # TODO: check if title fields can be nested
+                flatten_fields.update({
+                    f['id']: f
+                    for f in field['value']['fields']
+                })
+            else:
+                flatten_fields[field['id']] = field
+
+        # Convert pyrus types to python
+        for name, field in cls.Meta.fields.items():
+            if field.id not in flatten_fields:
+                continue
+
+            flatten_field = flatten_fields[field.id]
+
+            if flatten_field.get('value') is not None:
+                try:
+                    flatten_field['value'] = field.deserialize_from_pyrus(flatten_field['value'])
+                except Exception as e:
+                    if sys.version_info >= (3, 11):
+                        e.add_note(f"Task id: {data.get('id')}, field struct: {flatten_field}")
+                    raise e
+
+        obj = cls(
+            id=data['id'],
+            _data=data,
+            _field_values=flatten_fields,
+            create_date=create_date,
+            last_modified_date=last_modified_date,
+        )
+        return obj
+
+    def as_pyrus_data(self):
+        return {
+            **(self._data or {}),
+            'fields': self.get_pyrus_fields_data(),
+            'form_id': self.Meta.form_id,
+        }
+
+    def get_pyrus_fields_data(self, changed_only: bool = False) -> list[Any]:
+        # serialize values to pyrus format
+        values = []
+        for name, field in self.Meta.fields.items():
+            if 'value' not in self._field_values[field.id]:
+                continue
+            value = self._field_values[field.id]['value']
+            try:
+                values.append({
+                    'id': field.id,
+                    'value': field.serialize_to_pyrus(value),
+                })
+            except Exception as e:
+                if sys.version_info >= (3, 11):
+                    e.add_note(f"Task id: {self.id}, field struct: {self._field_values[field.id]}")
+                raise e
+
+        if changed_only:
+            values = [x for x in values if x['id'] in self._changed_fields]
+
+        return sorted(values, key=lambda f: f['id'])
+
+    def as_dict(self) -> dict[str, Any]:
+        values = {}
+        for field_name in self.Meta.fields.keys():
+            value = getattr(self, field_name)
+            if isinstance(value, CatalogItem):
+                value = value.values
+            elif isinstance(value, CatalogEmptyValue):
+                value = None
+            values[field_name] = value
+
+        return values
+
+    def save(self, comment: Optional[str] = None):
+        if self.id:
+            get_session().update_task(self.id, self.get_pyrus_fields_data(changed_only=True), comment)
+        else:
+            data = get_session().create_task(self.as_pyrus_data())
+            new_item = type(self).from_pyrus_data(data)
+            self.__dict__ = new_item.__dict__
+
+    def comment(self, comment: str) -> None:
+        assert self.id
+        get_session().comment_task(self.id, comment)
+
+    def get_url(self) -> str:
+        return f'https://pyrus.com/t#id{self.id}'
+
+    def __repr__(self) -> str:
+        return f'Task {self.get_url()}'
