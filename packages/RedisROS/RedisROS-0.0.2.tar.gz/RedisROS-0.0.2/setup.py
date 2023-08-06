fd1cafee234f1923c8036061b5a506fb4dc06a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:19 +0000

    Add package RedisROS/0.0.2/RedisROS-0.0.2.tar.gz

---
+++ packages/RedisROS/RedisROS-0.0.2.tar.gz/RedisROS-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# -> Fetch documentation from readme
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="RedisROS",
+    version="0.0.2",
+    license="GNU GPLv3",
+    description="A redis-based event-based ROS2-like library for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vguillet/RedisROS",
+    packages=find_packages(),
+    install_requires=["redis-py", "python-redis-lock"],
+    keywords=["ROS", "ROS2", "event-based", "python", "redis", "robotics"],
+    python_requires=">= 3.4",
+    author="Victor Guillet",
+    author_email="victor.guillet@protonmail.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ]
+    )
