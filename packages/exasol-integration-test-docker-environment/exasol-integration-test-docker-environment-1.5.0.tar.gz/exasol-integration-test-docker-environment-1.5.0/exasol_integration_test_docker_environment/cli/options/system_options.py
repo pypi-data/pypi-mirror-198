2313fc85022f2fc35b5fc0c9eb2ef7b3d7c5774e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:32 +0000

    Add package exasol-integration-test-docker-environment/1.5.0/exasol-integration-test-docker-environment-1.5.0.tar.gz

---
+++ packages/exasol-integration-test-docker-environment/exasol-integration-test-docker-environment-1.5.0.tar.gz/exasol-integration-test-docker-environment-1.5.0/exasol_integration_test_docker_environment/cli/options/system_options.py
@@ -0,0 +1,23 @@
+import click
+
+DEFAULT_OUTPUT_DIRECTORY = ".build_output"
+
+output_directory_option = click.option('--output-directory', type=click.Path(file_okay=False, dir_okay=True),
+                                       default=DEFAULT_OUTPUT_DIRECTORY,
+                                       show_default=True,
+                                       help="Output directory where the system stores all output and log files.")
+
+tempory_base_directory_option = click.option('--temporary-base-directory',
+                                             type=click.Path(file_okay=False,
+                                                      dir_okay=True),
+                                             default="/tmp",
+                                             show_default=True,
+                                             help="Directory where the system creates temporary directories.")
+
+system_options = [
+    click.option('--workers', type=int,
+                 default=5, show_default=True,
+                 help="Number of parallel workers"),
+    click.option('--task-dependencies-dot-file', type=click.Path(file_okay=True),
+                 default=None, help="Path where to store the Task Dependency Graph as dot file")
+]
