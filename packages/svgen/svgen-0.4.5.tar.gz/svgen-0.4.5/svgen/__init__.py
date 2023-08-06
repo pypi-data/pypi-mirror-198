f59f616e489c35dd404a3d665d206a17826278a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:46 +0000

    Add package svgen/0.4.5/svgen-0.4.5.tar.gz

---
+++ packages/svgen/svgen-0.4.5.tar.gz/svgen-0.4.5/svgen/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.0
+# hash=ae681c7c2c1d6ce64d6b0e0799e8ff67
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A tool for working with scalable vector graphics."
+PKG_NAME = "svgen"
+VERSION = "0.4.5"
