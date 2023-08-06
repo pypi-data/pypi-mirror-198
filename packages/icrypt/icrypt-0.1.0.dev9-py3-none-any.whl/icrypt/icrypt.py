ce406436f83537eaa3023b18c7ac9b35642d5f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:27 +0000

    Add package icrypt/0.1.0.dev9/icrypt-0.1.0.dev9-py3-none-any.whl

---
+++ packages/icrypt/icrypt-0.1.0.dev9-py3-none-any.whl/icrypt/icrypt.py
@@ -0,0 +1,43 @@
+from .pgp_handler import PGPHandler
+from pgpy import PGPKey, PGPUID, PGPMessage
+
+
+class ICrypt:
+
+    @staticmethod
+    def generate_key(**kwargs):
+        # key = self.pgp_handler.generate_key(**kwargs)
+        key = PGPHandler.generate_key(**kwargs)
+        return key
+
+    @staticmethod
+    def encrypt(pubkey_path, message):
+        pubkey, _ = PGPKey.from_file(pubkey_path)
+        return PGPHandler.encrypt(pubkey=pubkey, message=message)
+    
+    @staticmethod
+    def decrypt(privkey_path, encrypted):
+        privkey, _ = PGPKey.from_file(privkey_path)
+        return PGPHandler.decrypt(privkey=privkey, encrypted=encrypted)
+
+
+if __name__ == '__main__':
+    ic = ICrypt()
+    # key = ic.generate_key(name="Test", email="test.com")
+    # print(key.pubkey)
+    # print(key)
+  
+    en_msg = ic.encrypt(pubkey_path="sample_pubkey.txt", message="Hello World")
+    print(en_msg)
+
+    f = open("sample_encrypted.txt", "w")
+    f.write(str(en_msg))
+    f.close()
+
+    encrypted= PGPMessage.from_file("sample_encrypted.txt")
+    # print(encrypted)
+    msg = ic.decrypt(privkey_path="sample_privkey.txt", encrypted=encrypted)
+    print(msg)
+
+    # msg = ic.decrypt(privkey_path="privkey.txt", encrypted=en_msg)
+    # print(msg)
