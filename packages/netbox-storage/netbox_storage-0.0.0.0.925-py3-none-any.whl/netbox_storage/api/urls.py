e929c2b8d186d8822ccf597f0e93c1ce2e445352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:42 +0000

    Add package netbox-storage/0.0.0.0.925/netbox_storage-0.0.0.0.925-py3-none-any.whl

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.925-py3-none-any.whl/netbox_storage/api/urls.py
@@ -0,0 +1,27 @@
+from netbox.api.routers import NetBoxRouter
+
+from netbox_storage.api.views import (
+    NetboxStorageRootView,
+    DriveViewSet,
+    FilesystemViewSet,
+    PartitionViewSet,
+    MountedVolumeViewSet,
+    LinuxDeviceViewSet,
+    LogicalVolumeViewSet,
+    PhysicalVolumeViewSet,
+    VolumeGroupViewSet
+)
+
+router = NetBoxRouter()
+router.APIRootView = NetboxStorageRootView
+
+router.register("drive", DriveViewSet)
+router.register("filesystem", FilesystemViewSet)
+router.register("partition", PartitionViewSet)
+router.register("mountedvolume", MountedVolumeViewSet)
+router.register("linuxdevice", LinuxDeviceViewSet)
+router.register("logicalvolume", LogicalVolumeViewSet)
+router.register("physicalvolume", PhysicalVolumeViewSet)
+router.register("volumegroup", VolumeGroupViewSet)
+
+urlpatterns = router.urls
