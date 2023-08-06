a775a18eeb2e439d1345353c097218dc240fa9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:14 +0000

    Add package akfraction/0.0.2/akfraction-0.0.2.tar.gz

---
+++ packages/akfraction/akfraction-0.0.2.tar.gz/akfraction-0.0.2/akfraction/__init__.py
@@ -0,0 +1,1 @@
+from akfraction.module_a import Fraction
---
+++ packages/akfraction/akfraction-0.0.2.tar.gz/akfraction-0.0.2/setup.py
@@ -0,0 +1,38 @@
+import io
+from os.path import abspath, dirname, join
+from setuptools import find_packages, setup
+
+
+HERE = dirname(abspath(__file__))
+LOAD_TEXT = lambda name: io.open(join(HERE, name), encoding='UTF-8').read()
+DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
+    'README.rst'
+])
+
+setup(
+  name = 'akfraction',      
+  packages = ['akfraction'], 
+  version = '0.0.2',
+  license='MIT', 
+  description = 'Fraction by Mr.Adisak',
+  long_description=DESCRIPTION,
+  author = 'Adisak Karnbanjong',                 
+  author_email = 'kpadisak@gmail.com',     
+  url = 'https://github.com/kadisak/akfraction',  
+  download_url = 'https://github.com/kadisak/akfraction/archive/v0.0.2.zip',  
+  keywords = ['Fraction', 'Mathematics'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Education',     
+    'Topic :: Utilities',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+  ],
+)
