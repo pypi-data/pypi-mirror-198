6cbe421d855be7dc425019be3480a3b5564350b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:47 +0000

    Add package bx-django-utils/53/bx_django_utils-53-py3-none-any.whl

---
+++ packages/bx-django-utils/bx_django_utils-53-py3-none-any.whl/bx_django_utils_tests/test_app/admin.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+
+from bx_django_utils.admin_utils.filters import ExistingCountedListFilter
+from bx_django_utils.translation import TranslationFieldAdmin
+from bx_django_utils_tests.test_app.admin_filters_demo import NotAllSimpleListFilterDemo
+from bx_django_utils_tests.test_app.models import (
+    ColorFieldTestModel,
+    CreateOrUpdateTestModel,
+    TranslatedModel,
+    TranslatedSlugTestModel,
+)
+
+
+class NameListFilter(ExistingCountedListFilter):
+    title = 'name'
+    parameter_name = 'name'
+    model_field_name = 'name'
+
+
+class SlugListFilter(ExistingCountedListFilter):
+    title = 'slug'
+    parameter_name = 'slug'
+    model_field_name = 'slug'
+
+
+@admin.register(CreateOrUpdateTestModel)
+class CreateOrUpdateTestModelAdmin(admin.ModelAdmin):
+    list_display = ('human_create_dt', 'human_update_dt', 'name', 'slug')
+    list_display_links = ('name', 'slug')
+    readonly_fields = ('create_dt', 'update_dt')
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = (NotAllSimpleListFilterDemo, NameListFilter, SlugListFilter)
+
+
+@admin.register(ColorFieldTestModel)
+class ColorFieldTestModelAdmin(admin.ModelAdmin):
+    list_display = ('required_color', 'optional_color')
+
+
+@admin.register(TranslatedModel)
+class TranslatedModelAdmin(TranslationFieldAdmin):
+    list_display = ['translated', 'translated_multiline', 'not_translated']
+    list_display_links = ['translated', 'translated_multiline']
+
+
+@admin.register(TranslatedSlugTestModel)
+class TranslatedSlugTestModelAdmin(TranslationFieldAdmin):
+    list_display = ['translated', 'translated_slug']
