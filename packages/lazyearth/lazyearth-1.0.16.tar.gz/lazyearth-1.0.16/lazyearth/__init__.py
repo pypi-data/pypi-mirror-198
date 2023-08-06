a3a2d5cae00858c0e6c845e10023e73cbe98b536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:20 +0000

    Add package lazyearth/1.0.16/lazyearth-1.0.16.tar.gz

---
+++ packages/lazyearth/lazyearth-1.0.16.tar.gz/lazyearth-1.0.16/lazyearth/__init__.py
@@ -0,0 +1,13 @@
+# If there are this code in orther local have to use 
+# from lazyearth.mainfile import objearth
+# from lazyearth.mainfile import water
+
+from lazyearth.mainfile import objearth
+from lazyearth.mainfile import water
+from lazyearth.mainfile import imagedataset
+
+deving_by = 'tul kedsaro'
+
+__version__ = '1.0.16'
+print(f'Lazyearth version : {__version__} Krub')
+
