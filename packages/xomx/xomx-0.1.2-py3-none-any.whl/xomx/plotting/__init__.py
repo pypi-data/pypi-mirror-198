5c472522b37496aa72250f23c53d69be74ef1c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:36 +0000

    Add package xomx/0.1.2/xomx-0.1.2-py3-none-any.whl

---
+++ packages/xomx/xomx-0.1.2-py3-none-any.whl/xomx/plotting/__init__.py
@@ -0,0 +1,19 @@
+from .basic_plot import (
+    extension,
+    colormap,
+    scatter,
+    plot,
+    plot_scores,
+    plot_var,
+    plot_2d_obsm,
+    plot_2d_embedding,
+)
+
+import matplotlib.pyplot as plt
+
+# custom modification to the nipy_spectral colormap
+nipy_spectral_colormap = plt.get_cmap("nipy_spectral")
+nipy_spectral_colormap._segmentdata["red"][-1] = (1.0, 0.9, 0.9)
+nipy_spectral_colormap._segmentdata["green"][-1] = (1.0, 0.5, 0.5)
+nipy_spectral_colormap._segmentdata["blue"][-1] = (1.0, 0.5, 0.5)
+nipy_spectral_colormap._init()
