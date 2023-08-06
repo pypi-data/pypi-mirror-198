1d6e233622399564e58ccbae8b28303b1c0eba23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:10 +0000

    Add package igogo/1.0.1/igogo-1.0.1-py3-none-any.whl

---
+++ packages/igogo/igogo-1.0.1-py3-none-any.whl/igogo/context.py
@@ -0,0 +1,40 @@
+#  Copyright (c) 2023.
+#  Aleksandr Dremov
+
+import asyncio
+import contextvars
+
+from .output import OutputStreamsSetter, OutputObject, AdditionalOutputs
+from .exceptions import IgogoInvalidContext
+
+
+class IgogoContext(object):
+    """
+    This class represents the context for running a function decorated by igogo job.
+    """
+    out_stream: OutputStreamsSetter
+    task: asyncio.Task
+    additional_outputs: AdditionalOutputs
+
+    def __init__(self, task: asyncio.Task, out_stream: OutputStreamsSetter, additional_outputs: AdditionalOutputs):
+        self.out_stream = out_stream
+        self.task = task
+        self.additional_outputs = additional_outputs
+
+
+_context: contextvars.ContextVar[IgogoContext | None] = contextvars.ContextVar("igogo_context", default=None)
+
+
+def get_context_or_none() -> IgogoContext:
+    return _context.get()
+
+
+def get_context_or_fail() -> IgogoContext:
+    value = get_context_or_none()
+    if value is None:
+        raise IgogoInvalidContext()
+    return value
+
+
+def set_context(context: IgogoContext):
+    _context.set(context)
