3d169197d6decb08f89c4b288d62fca15a845e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:58 +0000

    Add package pyflagsercount/0.2.33/pyflagsercount-0.2.33.tar.gz

---
+++ packages/pyflagsercount/pyflagsercount-0.2.33.tar.gz/pyflagsercount-0.2.33/setup.py
@@ -0,0 +1,100 @@
+#! /usr/bin/env python
+"""Python bindings for the flagser C++ library."""
+
+import os
+import codecs
+import re
+import sys
+import platform
+import subprocess
+
+from pkg_resources.extern.packaging import version
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the "
+                               " following extensions: " +
+                               " , ".join(e.name for e in self.extensions))
+
+        if platform.system() == "Windows":
+            cmake_version = version.parse(re.search(r'version\s*([\d.]+)',
+                                                    out.decode()).group(1))
+            if cmake_version < version.parse("3.1.0"):
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+        self.install_dependencies()
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def install_dependencies(self):
+        dir_start = os.getcwd()
+        dir_pybind11 = os.path.join(dir_start, 'pybind11')
+        if os.path.exists(dir_pybind11):
+            return 0
+        os.mkdir(dir_pybind11)
+        subprocess.check_call(['git', 'clone',
+                               'https://github.com/pybind/pybind11.git',
+                               dir_pybind11])
+        #subprocess.check_call(['git', 'submodule', 'update', '--init', '--recursive'])
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.join(os.path.dirname(
+            self.get_ext_fullpath(ext.name)), 'pyflagsercount', 'modules'))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
+                cfg.upper(), extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
+
+        if sys.platform.startswith("darwin"):
+            # Cross-compile support for macOS - respect ARCHFLAGS if set
+            archs = re.findall(r"-arch (\S+)", os.environ.get("ARCHFLAGS", ""))
+            if archs:
+                cmake_args += \
+                    ["-DCMAKE_OSX_ARCHITECTURES={}".format(";".join(archs))]
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get('CXXFLAGS', ''), self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
+                              cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args,
+                              cwd=self.build_temp)
+
+
+setup(
+    name='pyflagsercount',
+    version='0.2.33',
+    author='Jason P. Smith',
+    description='A pybind11 wrapper for a version of flagser-count',
+    ext_modules=[CMakeExtension('pyflagsercount')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    packages=["pyflagsercount"],
+    zip_safe=False,
+)
