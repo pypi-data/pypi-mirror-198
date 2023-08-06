c8ef87ff6f994c766a132d8465d1079290328091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:29 +0000

    Add package sklearnpy/0.0.6/sklearnpy-0.0.6-py3-none-any.whl

---
+++ packages/sklearnpy/sklearnpy-0.0.6-py3-none-any.whl/src/duoyuantongji/__init__.py
@@ -0,0 +1,11 @@
+
+from . import example
+from . import vals_vectors
+from . import classify
+from . import corrcoef
+from . import k_means
+from . import labels
+from . import linear_predict
+from . import pca
+from . import help_h
+
