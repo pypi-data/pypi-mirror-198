99a3c583aae8c7cc0cea2b6193196201c5831790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:26 +0000

    Add package diffusion-webui/1.2.2/diffusion-webui-1.2.2.tar.gz

---
+++ packages/diffusion-webui/diffusion-webui-1.2.2.tar.gz/diffusion-webui-1.2.2/diffusion_webui/helpers.py
@@ -0,0 +1,12 @@
+from diffusion_webui.diffusion_models.controlnet.controlnet_canny import (
+    StableDiffusionControlNetCannyGenerator,
+)
+from diffusion_webui.diffusion_models.stable_diffusion.img2img_app import (
+    StableDiffusionImage2ImageGenerator,
+)
+from diffusion_webui.diffusion_models.stable_diffusion.inpaint_app import (
+    StableDiffusionInpaintGenerator,
+)
+from diffusion_webui.diffusion_models.stable_diffusion.text2img_app import (
+    StableDiffusionText2ImageGenerator,
+)
