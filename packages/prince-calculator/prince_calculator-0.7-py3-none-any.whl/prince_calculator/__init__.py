5562b006b9b03081c6494d9cf9ecc173ab6a151d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:45 +0000

    Add package prince-calculator/0.7/prince_calculator-0.7-py3-none-any.whl

---
+++ packages/prince-calculator/prince_calculator-0.7-py3-none-any.whl/prince_calculator/__init__.py
@@ -0,0 +1,32 @@
+def add_numbers(a,b):
+    return a+b
+
+class enter_key:
+    def __init__(self):
+        print("Please enter a key below")
+        self.key = input()
+        print('Key entered is : ',self.key)
+    def show(self):    
+        return self.key
+
+class set_parameters:
+    def __init__(self):
+        self.model_engine = 'davinci'
+        self.temp = 0.2
+        print("Please enter a model engine:")
+        self.model_engine = input()
+        print("Please set the accuracytemperature :")
+        self.temp = float(input())
+    def show_parameters(self):
+        return self.model_engine,self.temp
+class qna(enter_key,set_parameters):
+    def __init__(self):
+        print('iniside qna')   
+    def question(self):
+        print(self.key)
+        if self.key == "":
+            print("You have not entered the API key!")
+        print("Enter question below : ",self.key)
+        self.question = input()
+        self.answer = 'Not configured yet'
+        print(self.answer)
