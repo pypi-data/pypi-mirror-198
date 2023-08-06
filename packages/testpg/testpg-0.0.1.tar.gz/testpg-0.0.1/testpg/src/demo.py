affc9581e336b01c27bed6dc7e23586efe8c6441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:02 +0000

    Add package testpg/0.0.1/testpg-0.0.1.tar.gz

---
+++ packages/testpg/testpg-0.0.1.tar.gz/testpg-0.0.1/testpg/src/demo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/3/22 15:34
+# @Author  : unclear
+# @File    : demo.py
+# @Software: PyCharm
+def hello():
+    return 'hello world!'
