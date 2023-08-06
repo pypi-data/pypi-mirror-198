5117b6c31e51c7e8242ac876f6dca2580a6b215b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:43 +0000

    Add package qlik-test-etay/0.1.0/qlik_test_etay-0.1.0-py3-none-any.whl

---
+++ packages/qlik-test-etay/qlik_test_etay-0.1.0-py3-none-any.whl/qlik_test_etay/__init__.py
@@ -0,0 +1,18 @@
+print('import')
+import pandas as pd
+def answer_to_life():
+    return 42
+
+
+
+def add21(x):
+    return x+21
+
+
+def multi2(x,y):
+    return x*y
+
+def get_data():
+    return pd.series(["a","b","c"])
+
+
