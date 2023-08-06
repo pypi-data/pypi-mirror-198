de581f8fefcb2be0cc0082eb114c21ad3d73fde6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:43 +0000

    Add package django-request-track/0.0.1/django-request-track-0.0.1.tar.gz

---
+++ packages/django-request-track/django-request-track-0.0.1.tar.gz/django-request-track-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='django-request-track',
+    version='0.0.1',
+    packages=['request_track'],
+    url='https://github.com/PooyaRezaee/django-request-logger',
+    license='MIT',
+    author='PooyaRezaee',
+    author_email='pooya.rezaee.official@gmail.com',
+    description="Save log for requests with detail's"
+)
