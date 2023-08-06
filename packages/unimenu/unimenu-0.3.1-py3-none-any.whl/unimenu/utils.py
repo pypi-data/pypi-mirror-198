bd4700395bda4cd1b5681fa6f4f5a40664dff7f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:04 +0000

    Add package unimenu/0.3.1/unimenu-0.3.1-py3-none-any.whl

---
+++ packages/unimenu/unimenu-0.3.1-py3-none-any.whl/unimenu/utils.py
@@ -0,0 +1,41 @@
+import json
+
+
+def load_json(config_path):
+    """get json data from a file path, return None if not json"""
+    path = str(config_path)
+    if not path.lower().endswith(".json"):
+        return
+
+    with open(config_path) as file:
+        data = json.load(file)
+    return data
+
+
+def load_yaml(config_path):
+    """get yaml data from a file path, return None if not yaml"""
+    path = str(config_path)
+    if not path.lower().endswith(".yaml"):
+        return
+
+    import yaml
+
+    with open(config_path) as file:
+        data = yaml.load(file, Loader=yaml.SafeLoader)
+    return data
+
+
+def load_config(config_path):
+    """get data from a JSON or YAML config"""
+    return load_json(config_path) or load_yaml(config_path)
+
+
+def getattr_recursive(obj, attr: str):
+    """
+    getattr but recursive, supports nested attributes
+    attr: provide either 1 attribute, or multiple separated by a dot
+    """
+    attributes = attr.split(".")
+    for attribute in attributes:
+        obj = getattr(obj, attribute)
+    return obj
