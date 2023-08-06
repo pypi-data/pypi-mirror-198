3fef59bfae6673b07c58f1f3fa3fa3ff97d9b02d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:02 +0000

    Add package nsface-cpu-python/1.1.50/nsface_cpu_python-1.1.50-py3-none-any.whl

---
+++ packages/nsface-cpu-python/nsface_cpu_python-1.1.50-py3-none-any.whl/nsface_cpu/model_zoo/model_detection/__init__.py
@@ -0,0 +1,5 @@
+from .retinaface_torch import RetinaFace
+from .retinaface_insightface import RetinaFace
+from .scrfd import SCRFD_CV
+from .blazeface import BlazeFace
+from .mask_detector import MaskDetector
