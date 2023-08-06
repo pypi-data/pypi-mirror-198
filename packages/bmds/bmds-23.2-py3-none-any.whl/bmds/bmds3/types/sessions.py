f184b75b7429e04c107499715396c7c34b9fcc87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:15 +0000

    Add package bmds/23.2/bmds-23.2-py3-none-any.whl

---
+++ packages/bmds/bmds-23.2-py3-none-any.whl/bmds/bmds3/types/sessions.py
@@ -0,0 +1,25 @@
+from typing import Optional, Tuple
+
+from pydantic import BaseModel
+
+from ...datasets.base import DatasetSchemaBase
+from ..models.base import BmdModelAveragingSchema, BmdModelSchema
+from ..recommender import RecommenderSchema
+from ..selected import SelectedModelSchema
+
+
+class VersionSchema(BaseModel):
+    string: str
+    pretty: str
+    numeric: Tuple[int, ...]
+    python: str
+    dll: str
+
+
+class SessionSchemaBase(BaseModel):
+    version: VersionSchema
+    dataset: DatasetSchemaBase
+    models: list[BmdModelSchema]
+    model_average: Optional[BmdModelAveragingSchema]
+    recommender: Optional[RecommenderSchema]
+    selected: SelectedModelSchema
