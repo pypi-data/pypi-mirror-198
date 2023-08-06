169e314b734ee7166f168877f7afa718d2dececc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:29 +0000

    Add package sklearnpy/0.0.6/sklearnpy-0.0.6.tar.gz

---
+++ packages/sklearnpy/sklearnpy-0.0.6.tar.gz/sklearnpy-0.0.6/src/duoyuantongji/labels.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+from sklearn.cluster import AgglomerativeClustering
+
+arr = np.array([float(i) for i in input().split(' ')]).reshape(
+    *[int(i) for i in input().split(' ')])
+
+res = AgglomerativeClustering(int(input())).fit(arr.T).labels_
+print("香气和酸质{val}属于一类。".format(val='' if res[0] == res[2] else '不'), end='')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def println():
+    print(''' arr = np.array([float(i) for i in input().split(' ')]).reshape(*[int(i) for i in input().split(' ')])
+res = AgglomerativeClustering(int(input())).fit(arr.T).labels_
+("".format(val = '' if res[0]==res[2] else '不'),end='')''')
