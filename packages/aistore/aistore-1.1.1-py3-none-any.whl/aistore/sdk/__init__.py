bdb8a222792dc7753e351f743e4104ff8d6e68c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:51 +0000

    Add package aistore/1.1.1/aistore-1.1.1-py3-none-any.whl

---
+++ packages/aistore/aistore-1.1.1-py3-none-any.whl/aistore/sdk/__init__.py
@@ -0,0 +1,2 @@
+from aistore.sdk.client import Client
+from aistore.sdk.list_object_flag import ListObjectFlag
---
+++ packages/aistore/aistore-1.1.1-py3-none-any.whl/aistore/sdk/cluster.py
@@ -0,0 +1,105 @@
+#
+# Copyright (c) 2022-2023, NVIDIA CORPORATION. All rights reserved.
+#
+
+from __future__ import annotations  # pylint: disable=unused-variable
+from typing import List
+
+from aistore.sdk.const import (
+    HTTP_METHOD_GET,
+    ACT_LIST,
+    PROVIDER_AIS,
+    QPARAM_WHAT,
+    QPARAM_PRIMARY_READY_REB,
+    QPARAM_PROVIDER,
+    WHAT_SMAP,
+    URL_PATH_BUCKETS,
+    URL_PATH_HEALTH,
+    URL_PATH_DAEMON,
+)
+
+from aistore.sdk.types import BucketModel
+from aistore.sdk.request_client import RequestClient
+from aistore.sdk.types import ActionMsg, Smap
+
+
+# pylint: disable=unused-variable
+class Cluster:
+    """
+    A class representing a cluster bound to an AIS client.
+    """
+
+    # pylint: disable=duplicate-code
+    def __init__(self, client: RequestClient):
+        self._client = client
+
+    @property
+    def client(self):
+        """Client this cluster uses to make requests"""
+        return self._client
+
+    def get_info(self) -> Smap:
+        """
+        Returns state of AIS cluster, including the detailed information about its nodes.
+
+        Returns:
+            aistore.sdk.types.Smap: Smap containing cluster information
+
+        Raises:
+            requests.RequestException: "There was an ambiguous exception that occurred while handling..."
+            requests.ConnectionError: Connection error
+            requests.ConnectionTimeout: Timed out connecting to AIStore
+            requests.ReadTimeout: Timed out waiting response from AIStore
+        """
+        return self.client.request_deserialize(
+            HTTP_METHOD_GET,
+            path=URL_PATH_DAEMON,
+            res_model=Smap,
+            params={QPARAM_WHAT: WHAT_SMAP},
+        )
+
+    def list_buckets(self, provider: str = PROVIDER_AIS):
+        """
+        Returns list of buckets in AIStore cluster.
+
+        Args:
+            provider (str, optional): Name of bucket provider, one of "ais", "aws", "gcp", "az", "hdfs" or "ht".
+            Defaults to "ais". Empty provider returns buckets of all providers.
+
+        Returns:
+            List[BucketModel]: A list of buckets
+
+        Raises:
+            requests.RequestException: "There was an ambiguous exception that occurred while handling..."
+            requests.ConnectionError: Connection error
+            requests.ConnectionTimeout: Timed out connecting to AIStore
+            requests.ReadTimeout: Timed out waiting response from AIStore
+        """
+        params = {QPARAM_PROVIDER: provider}
+        action = ActionMsg(action=ACT_LIST).dict()
+
+        return self.client.request_deserialize(
+            HTTP_METHOD_GET,
+            path=URL_PATH_BUCKETS,
+            res_model=List[BucketModel],
+            json=action,
+            params=params,
+        )
+
+    def is_aistore_running(self) -> bool:
+        """
+        Checks if cluster is ready or still setting up.
+
+        Returns:
+            bool: True if cluster is ready, or false if cluster is still setting up
+        """
+
+        # compare with AIS Go API (api/cluster.go) for additional supported options
+        params = {QPARAM_PRIMARY_READY_REB: "true"}
+        try:
+            resp = self.client.request(
+                HTTP_METHOD_GET, path=URL_PATH_HEALTH, params=params
+            )
+            return resp.ok
+        except Exception:
+            return False
