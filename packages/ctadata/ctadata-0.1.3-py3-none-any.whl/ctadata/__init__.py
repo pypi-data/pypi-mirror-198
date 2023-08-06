621d515c9098cb70920ca7556e5a4b46f275af4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:04 +0000

    Add package ctadata/0.1.3/ctadata-0.1.3-py3-none-any.whl

---
+++ packages/ctadata/ctadata-0.1.3-py3-none-any.whl/ctadata/__init__.py
@@ -0,0 +1,28 @@
+import os
+import logging
+from urllib.parse import urljoin
+
+import requests
+
+logger = logging.getLogger(__name__)
+
+# https://daskhub.dev.ctaodc.ch/services/downloadservice/fetch/pnfs/cta.cscs.ch/lst/DL1/20230226/v0.9_calib12100/tailcut84
+
+
+def fetch_and_save_file(url, fn):
+    total_wrote = 0
+
+    with open(fn, "w") as outf:
+        with requests.get(
+            urljoin("http://hub:5000/services/downloadservice/fetch/pnfs/cta.cscs.ch/", url),
+            params = {'token': os.getenv("JUPYTERHUB_API_TOKEN")},
+            stream=True
+        ) as f:          
+            f.raise_for_status()
+            for r in f.iter_content(chunk_size=1024*1024):
+                outf.write(r)                
+                total_wrote += len(r)
+
+    return total_wrote
+        
+    
