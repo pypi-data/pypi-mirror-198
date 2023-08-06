46e8084ab3fbc8b15d6a9aafc6f055f9b78c938b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:33 +0000

    Add package nonebot-plugin-quote/0.2.0/nonebot_plugin_quote-0.2.0-py3-none-any.whl

---
+++ packages/nonebot-plugin-quote/nonebot_plugin_quote-0.2.0-py3-none-any.whl/nonebot_plugin_quote/config.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Extra
+from typing import List, Dict
+
+
+class Config(BaseModel, extra=Extra.ignore):
+    record_path: str = ''
+    inverted_index_path: str = ''
+    quote_superuser: Dict[str, List[str]] = {}
+
+
+
+
+
+
