9f23f3524f55d663f36b737c0c702b8d2df6d2db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:03 +0000

    Add package databend-sqlalchemy/0.2.4/databend_sqlalchemy-0.2.4-py3-none-any.whl

---
+++ packages/databend-sqlalchemy/databend_sqlalchemy-0.2.4-py3-none-any.whl/databend_sqlalchemy/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+
+from databend_sqlalchemy.entry_points import validate_entrypoints
+
+VERSION = (0, 2, 4)
+__version__ = '.'.join(str(x) for x in VERSION)
---
+++ packages/databend-sqlalchemy/databend_sqlalchemy-0.2.4-py3-none-any.whl/databend_sqlalchemy/test.py
@@ -0,0 +1,16 @@
+from databend_sqlalchemy import connector
+
+
+def test():
+    cursor = connector.connect('databend://root:@localhost:8000').cursor()
+    cursor.execute("select null as db, name as name, database as schema, if(engine = 'VIEW', 'view', 'table') as type from system.tables where database = 'default';")
+    # print(cursor.fetchone())
+    print(cursor.fetchall())
+    print(cursor.description)
+
+    # for i in cursor.next():
+    #     print(i)
+
+
+if __name__ == '__main__':
+    test()
