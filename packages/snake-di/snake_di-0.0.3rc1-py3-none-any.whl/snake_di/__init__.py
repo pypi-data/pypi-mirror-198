4be223dccf003fc1a3d8489ac2c4007bede541b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:29 +0000

    Add package snake-di/0.0.3rc1/snake_di-0.0.3rc1-py3-none-any.whl

---
+++ packages/snake-di/snake_di-0.0.3rc1-py3-none-any.whl/snake_di/__init__.py
@@ -0,0 +1,7 @@
+"""Simple yet powerful dependency injection framework!"""
+
+from snake_di._container import Container
+from snake_di._provider import AsyncProvider, Provider
+
+__version__ = "0.0.3rc1"
+__all__ = ["Provider", "AsyncProvider", "Container", "__version__"]
