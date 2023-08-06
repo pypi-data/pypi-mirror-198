7e237bc57afabcd81c6fdd47877ca7f51bd80ea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:01 +0000

    Add package example-pyLegend/0.1/example_pyLegend-0.1.tar.gz

---
+++ packages/example-pyLegend/example_pyLegend-0.1.tar.gz/example_pyLegend-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="example_pyLegend",
+    author="LegendBoy",
+    author_email="krishna045jaiswal@gmail.com",
+    version="0.1",
+    description="A short description",
+    long_description = "A Long Description",
+    url="https://github.com/LEGEND-AI/example_pyLegend",
+    packages=find_packages(),
+    license="GNU General Public License v3.0",
+    include_package_data=True,
+    keywords=["example_pyLegend"],
+    install_requires=[]
+)
