ce9117754a15febe60180705827c2a6a9a1fe533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:27 +0000

    Add package kfsdutils/0.0.54/kfsdutils-0.0.54.tar.gz

---
+++ packages/kfsdutils/kfsdutils-0.0.54.tar.gz/kfsdutils-0.0.54/kfsdutils/apps/endpoints/exceptions/response.py
@@ -0,0 +1,11 @@
+from rest_framework.exceptions import ValidationError
+
+class KubefacetsException(APIException):
+    default_detail = "Unexpected Error"
+    status_code = 500
+    default_code = "unexpected_error"
+
+    def __init__(self, detail, defaultCode, statusCode):
+        self.detail = detail
+        self.status_code = statusCode
+        self.default_code = defaultCode
