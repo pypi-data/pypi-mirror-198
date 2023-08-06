c2ff7e60661fe6a7de96c8081dc686f0e02a25b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:12 +0000

    Add package makemake/0.1/makemake-0.1.tar.gz

---
+++ packages/makemake/makemake-0.1.tar.gz/makemake-0.1/setup.py
@@ -0,0 +1,31 @@
+# makemake - a simple Makefile generator
+# (c) 2023 by Andreas Schwenk <contact@compiler-construction.com>
+# License: GPLv3
+
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='makemake',
+      version='0.1',
+      description='A simple makefile generator for C projects',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3.9',
+      ],
+      keywords='make makefile C build',
+      url='https://github.com/andreas-schwenk/makemake',
+      author='Andreas Schwenk',
+      author_email='contact@compiler-construction.com',
+      license='GPLv3',
+      packages=['makemake'],
+      scripts=['bin/makemake'],
+      include_package_data=True,
+      zip_safe=False)
