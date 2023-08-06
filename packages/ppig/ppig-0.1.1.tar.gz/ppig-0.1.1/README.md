0d71ffb2a189b15eebbbc45d29ffff56805a84c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:29 +0000

    Add package ppig/0.1.1/ppig-0.1.1.tar.gz

---
+++ packages/ppig/ppig-0.1.1.tar.gz/ppig-0.1.1/README.md
@@ -0,0 +1,21 @@
+# PPIG: A simple AI terminal assistant
+
+Example:
+
+* Input:
+
+```bash
+➜ ppig list only directories        
+
+      ls -d */
+The command has been copied to the clipboard
+
+➜ 
+```
+
+Make sure your device has some sort of clipboard system such as `xclip`. If it doesn't you can install it executing: 
+
+```bash
+➜ sudo apt-get install xclip
+```
+
---
+++ packages/ppig/ppig-0.1.1.tar.gz/ppig-0.1.1/ppig/main.py
@@ -0,0 +1,91 @@
+import yaml
+import openai
+import sys    
+import pyperclip
+import os 
+from os.path import expanduser
+
+def load_cfg(file_path='.pig/pig_cfg.yaml'):
+    home = expanduser("~")
+    file_path = f'{home}/.pig/pig_cfg.yaml'
+    # If file does not exist, search in local directory
+    if not os.path.exists(file_path):
+        file_path = './pig_cfg.yaml'
+        if not os.path.exists(file_path):
+            raise Exception('Config file not found')
+
+    # Load the yaml config file
+    cfg = yaml.load(open(file_path, 'r'), Loader=yaml.FullLoader)
+    return cfg
+
+def get_api():
+    # Load the config file
+    cfg = load_cfg()
+    # Set the API key
+    openai.api_key = cfg['api_key']
+    return openai
+
+api = get_api()
+
+def process_prompt(prompt):
+    _prompt = f"""
+      Question: {prompt}
+      I will only ansert with the command, which is:
+    """
+
+    result = api.Completion.create(
+        engine="text-davinci-003",
+        prompt=_prompt,
+        temperature=0.9,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,        
+    )
+
+    return result.choices
+
+def main():
+    # Parse arguments from the command line
+    args = sys.argv[1:]
+
+    # Check if the user provided a prompt
+    if len(args) == 0:
+        print("Please provide a prompt")
+        return
+    
+    # Join the prompt 
+    prompt = ' '.join(args)
+
+    # Process the prompt
+    choices = process_prompt(prompt)
+
+    # Print the result
+    command = choices[0].text
+    
+    # COpy the result to the clipboard
+    pyperclip.copy(command.strip())
+    print(f"{command}")
+    print("The command has been copied to the clipboard")
+
+
+
+    
+    
+        
+
+    
+
+
+if __name__ == "__main__":
+    main()
+
+    
+    
+
+
+
+
+
+
+
