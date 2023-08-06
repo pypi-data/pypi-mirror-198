bdcd4b5eabee506a1080d8ec3b19184b7a2babc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:42:38 +0000

    Add package ai-dashboard/0.2.18/ai_dashboard-0.2.18-py3-none-any.whl

---
+++ packages/ai-dashboard/ai_dashboard-0.2.18-py3-none-any.whl/ai_dashboard/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.18"
+
+try:
+    from ai_dashboard.api.client import Client
+except ModuleNotFoundError:
+    pass
+except Exception as e:
+    import traceback
+
+    traceback.print_exc()
