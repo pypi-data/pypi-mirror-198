311f92e20c980fed98a7ac4d9030d8bcf1f1f880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:58 +0000

    Add package aipn-guass/0.1/aipn_guass-0.1.tar.gz

---
+++ packages/aipn-guass/aipn_guass-0.1.tar.gz/aipn_guass-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='aipn_guass',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['aipn_guass'],
+      author='Siva Shankar',
+      author_email= 'sivarvs11@gmail.com',
+      zip_safe=False
+)
