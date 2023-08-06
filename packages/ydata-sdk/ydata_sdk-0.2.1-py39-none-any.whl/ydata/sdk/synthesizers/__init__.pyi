736a3fa5d753ca524248e027afeabd356edc240a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:01 +0000

    Add package ydata-sdk/0.2.1/ydata_sdk-0.2.1-py39-none-any.whl

---
+++ packages/ydata-sdk/ydata_sdk-0.2.1-py39-none-any.whl/ydata/sdk/synthesizers/__init__.pyi
@@ -0,0 +1,4 @@
+from ydata.sdk.synthesizers._models.synthesizers_list import SynthesizersList as SynthesizersList
+from ydata.sdk.synthesizers.regular import RegularSynthesizer as RegularSynthesizer
+from ydata.sdk.synthesizers.synthesizer import BaseSynthesizer as Synthesizer
+from ydata.sdk.synthesizers.timeseries import TimeSeriesSynthesizer as TimeSeriesSynthesizer
