9f5cbbf09c093baf0d6b6311f770404530bf4260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:02 +0000

    Add package astakosa/0.1.6/astakosa-0.1.6-py3-none-any.whl

---
+++ packages/astakosa/astakosa-0.1.6-py3-none-any.whl/astakosa/file.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine, Column, Integer, String, text, func
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.sql import select
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+
+class Database:
+    def __init__(self, connection_string):
+        self.engine = create_engine(connection_string)
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
+
+    def test_connection(self):
+        try:
+            connection = self.engine.connect()
+            connection.close()
+            return True
+        except Exception as e:
+            print(f"Error: {e}")
+            return False
+
+def try_this():
+	print('yes sir')
