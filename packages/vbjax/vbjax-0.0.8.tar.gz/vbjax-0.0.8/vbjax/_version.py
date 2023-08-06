b6ffbb95e33e13a192235d4097d9406d93e20084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:35 +0000

    Add package vbjax/0.0.8/vbjax-0.0.8.tar.gz

---
+++ packages/vbjax/vbjax-0.0.8.tar.gz/vbjax-0.0.8/vbjax/_version.py
@@ -0,0 +1,10 @@
+__version__ = 'v0.0.8'
+
+if __name__ == '__main__':
+    print(__version__)
+    import os, sys
+    _, cmd, *args = sys.argv
+    print(cmd, args)
+    if cmd == 'tag':
+        os.system(f'git tag {__version__}')
+        os.system(f'git push -u origin {__version__}')
