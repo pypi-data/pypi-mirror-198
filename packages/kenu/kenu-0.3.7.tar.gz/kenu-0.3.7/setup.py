104db28f4deaf39a2f4521b56ce60178d2716850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:05 +0000

    Add package kenu/0.3.7/kenu-0.3.7.tar.gz

---
+++ packages/kenu/kenu-0.3.7.tar.gz/kenu-0.3.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+	name="kenu",
+	version="0.3.7",
+	description="Python kenu",
+	author="r4isy",
+	author_email="r4isy@kenucorp.com",
+	packages=["kenu"],
+	install_requires=[
+	'requests'
+	]
+	)
