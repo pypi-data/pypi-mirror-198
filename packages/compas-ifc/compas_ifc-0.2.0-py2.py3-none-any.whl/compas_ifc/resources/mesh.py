ada4a34160f7bc668472faa5f4fd9ccbf3652274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:24 +0000

    Add package compas-ifc/0.2.0/compas_ifc-0.2.0-py2.py3-none-any.whl

---
+++ packages/compas-ifc/compas_ifc-0.2.0-py2.py3-none-any.whl/compas_ifc/resources/mesh.py
@@ -0,0 +1,22 @@
+import ifcopenshell
+
+from compas.datastructures import Mesh
+
+
+def mesh_to_IfcPolygonalFaceSet(file: ifcopenshell.file, mesh: Mesh) -> ifcopenshell.entity_instance:
+    """
+    Convert a COMPAS mesh to an IFC PolygonalFaceSet.
+    """
+    vertices = []
+    for _, attr in mesh.vertices(True):
+        vertices.append((attr["x"], attr["y"], attr["z"]))
+    faces = []
+    for fkey in mesh.faces():
+        indexes = [i + 1 for i in mesh.face_vertices(fkey)]
+        faces.append(file.createIfcIndexedPolygonalFace(indexes))
+
+    return file.create_entity(
+        "IfcPolygonalFaceSet",
+        Coordinates=file.createIfcCartesianPointList3D(vertices),
+        Faces=faces,
+    )
