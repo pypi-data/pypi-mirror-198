f847488c2e4e03c8eb3921111247e247c3cd0fe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:00 +0000

    Add package pylontech/0.1.3/pylontech-0.1.3.tar.gz

---
+++ packages/pylontech/pylontech-0.1.3.tar.gz/pylontech-0.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="pylontech",
+    version="0.1.3",
+    packages=find_packages(include=['pylontech', 'pylontech.*']),
+    url="https://github.com/Tomcat42/pylontech-python/",
+    author="Bernd Singer",
+    author_email="singer@nefkom.net",
+    description=("Communicate to Pylontech batteries using a RS485 interface"),
+    license="MIT",
+    keywords="pylontech pylon RS485 lithium battery US2000 US2000C US3000",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=["pyserial"],
+)
