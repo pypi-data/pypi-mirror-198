d8fccb6881637eef991d07a4ac06665a405b92ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:59 +0000

    Add package physicsLab/1.0.2/physicsLab-1.0.2-py3-none-any.whl

---
+++ packages/physicsLab/physicsLab-1.0.2-py3-none-any.whl/physicsLab/electricity/__init__.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+# 操作元件
+from electricity.element import *
+# 操作导线
+from electricity.wire import *
+# 元件类
+from electricity.elementsClass import *
+# 元件坐标系
+from electricity._elementPosition import set_elementXYZ
