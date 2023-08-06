9969327beee09834e29667103073d6ff14c6eb4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:23 +0000

    Add package weather-ch/0.0.2/weather-ch-0.0.2.tar.gz

---
+++ packages/weather-ch/weather-ch-0.0.2.tar.gz/weather-ch-0.0.2/src/weather/__init__.py
@@ -0,0 +1,4 @@
+from weather.functions import *
+from weather.config import API
+
+__all__ = ['API']
---
+++ packages/weather-ch/weather-ch-0.0.2.tar.gz/weather-ch-0.0.2/src/weather/config.py
@@ -0,0 +1,5 @@
+weather_api_key = ""
+
+class API:
+    def __init__(self, api_key):
+        weather_api_key = api_key
