8dafcd12a29d4881ba579d2fa21140e100a7a966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:23 +0000

    Add package pokemon-card-recognizer/0.0.1.3.5/pokemon-card-recognizer-0.0.1.3.5.tar.gz

---
+++ packages/pokemon-card-recognizer/pokemon-card-recognizer-0.0.1.3.5.tar.gz/pokemon-card-recognizer-0.0.1.3.5/README.md
@@ -0,0 +1,21 @@
+# Pokemon Card Recognizer
+
+Recognize a Pokemon Card in an image or video.
+
+```python
+from card_recognizer.api.card_recognizer import CardRecognizer, Mode 
+recognizer = CardRecognizer(
+    mode=Mode.PULLS_VIDEO
+)
+pulls = recognizer.exec("/path/to/video")
+```
+
+Example analysis of a booster pack opening video:
+
+![](https://github.com/prateekt/pokemon-card-recognizer/blob/75409e8ecdc32256dfc4a0a8243782152fdd406b/example2.png?raw=true)
+![](https://github.com/prateekt/pokemon-card-recognizer/blob/75409e8ecdc32256dfc4a0a8243782152fdd406b/example.png?raw=true)
+
+Benchmarks: https://docs.google.com/presentation/d/1Q6PzJpqyyLFvtkLGoCFeXaHYOauOzeF_umx8V3w4fV8/edit?usp=sharing
+
+
+    
---
+++ packages/pokemon-card-recognizer/pokemon-card-recognizer-0.0.1.3.5.tar.gz/pokemon-card-recognizer-0.0.1.3.5/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="pokemon-card-recognizer",
+    version="0.0.1.3.5",
+    author="Prateek Tandon",
+    author_email="prateek1.tandon@gmail.com",
+    description="Pokemon TCG Card Recognizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/prateekt/pokemon-card-recognizer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9.7",
+    install_requires=required,
+)
