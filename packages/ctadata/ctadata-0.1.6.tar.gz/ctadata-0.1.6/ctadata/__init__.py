b9cb3d98e36958da1e51b5a0c00d2d543df140f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:29 +0000

    Add package ctadata/0.1.6/ctadata-0.1.6.tar.gz

---
+++ packages/ctadata/ctadata-0.1.6.tar.gz/ctadata-0.1.6/ctadata/__init__.py
@@ -0,0 +1,41 @@
+import os
+import logging
+from urllib.parse import urljoin
+
+import requests
+
+logger = logging.getLogger(__name__)
+
+# https://daskhub.dev.ctaodc.ch/services/downloadservice/fetch/pnfs/cta.cscs.ch/lst/DL1/20230226/v0.9_calib12100/tailcut84
+
+
+hubbase = "http://hub:5000/services/downloadservice/fetch/"
+
+def list_dir(url):
+    return [u.replace(hubbase, "") for u in requests.get(
+            urljoin(hubbase, "pnfs/cta.cscs.ch/", url),
+            params = {'token': os.getenv("JUPYTERHUB_API_TOKEN")},
+            stream=True
+        ).json()['urls']]
+
+
+def fetch_and_save_file(url, fn=None):
+    total_wrote = 0
+
+    if fn is None:
+        fn = url.split("/")[-1]
+
+    with open(fn, "wb") as outf:
+        with requests.get(
+            urljoin(hubbase, "pnfs/cta.cscs.ch/", url),
+            params = {'token': os.getenv("JUPYTERHUB_API_TOKEN")},
+            stream=True
+        ) as f:          
+            f.raise_for_status()
+            for r in f.iter_content(chunk_size=1024*1024):
+                outf.write(r)                
+                total_wrote += len(r)
+
+    return total_wrote
+        
+    
