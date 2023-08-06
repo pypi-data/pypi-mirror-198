52711092af2f015eccac9cd4551d03b7c3ff2ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:05 +0000

    Add package duqtools/1.6.1/duqtools-1.6.1-py3-none-any.whl

---
+++ packages/duqtools/duqtools-1.6.1-py3-none-any.whl/duqtools/models/__init__.py
@@ -0,0 +1,10 @@
+from ._job import Job, JobStatus
+from ._locations import Locations
+from ._system import AbstractSystem
+
+__all__ = [
+    'Locations',
+    'AbstractSystem',
+    'Job',
+    'JobStatus',
+]
