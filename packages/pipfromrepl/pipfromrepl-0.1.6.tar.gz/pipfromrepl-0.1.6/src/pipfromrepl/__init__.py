db57d57152f5d8fb494c456cc4da987f11e04194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:54 +0000

    Add package pipfromrepl/0.1.6/pipfromrepl-0.1.6.tar.gz

---
+++ packages/pipfromrepl/pipfromrepl-0.1.6.tar.gz/pipfromrepl-0.1.6/src/pipfromrepl/__init__.py
@@ -0,0 +1,45 @@
+"""Pip From REPL
+By Al Sweigart al@inventwithpython.com
+
+Run pip to install packages from the Python interactive shell aka REPL."""
+
+__version__ = '0.1.6'
+
+import sys, subprocess
+
+
+def user_install(module_name):
+    subprocess.run([sys.executable, '-m', 'pip', 'install', '--user', module_name])
+
+
+def install(module_name):
+    subprocess.run([sys.executable, '-m', 'pip', 'install', module_name])
+
+
+def uninstall(module_name, confirm=False):
+    if confirm:
+        subprocess.run(['echo', 'y', '|', sys.executable, '-m', 'pip', 'uninstall', module_name])
+    else:
+        echoProcess = subprocess.Popen(['echo', 'y'], stdout=subprocess.PIPE)
+        pipProcess = subprocess.Popen([sys.executable, '-m', 'pip', 'uninstall', module_name], stdin=echoProcess.stdout)
+        pipProcess.communicate()
+
+
+def list():
+    subprocess.run([sys.executable, '-m', 'pip', 'list'])
+
+
+def pip(*args):
+    if len(args) == 1:
+        args = tuple(args[0].split())
+    subprocess.run([sys.executable, '-m', 'pip', *args])
+
+
+"""
+REMOVED FOR NOW:
+def pipenv(*args):
+    if len(args) == 1:
+        args = tuple(args[0].split())
+    subprocess.run([sys.executable, '-m', 'pipenv', *args])
+"""
+
