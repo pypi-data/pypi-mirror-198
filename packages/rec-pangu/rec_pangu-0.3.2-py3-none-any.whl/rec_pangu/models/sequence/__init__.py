37943cf1650ab32184616bd2c33837ea8c8413bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:23 +0000

    Add package rec-pangu/0.3.2/rec_pangu-0.3.2-py3-none-any.whl

---
+++ packages/rec-pangu/rec_pangu-0.3.2-py3-none-any.whl/rec_pangu/models/sequence/__init__.py
@@ -0,0 +1,12 @@
+# -*- ecoding: utf-8 -*-
+# @ModuleName: __init__
+# @Author: wk
+# @Email: 306178200@qq.com
+# @Time: 2023/3/5 15:08
+from .comirec import ComirecDR, ComirecSA
+from .mind import MIND
+from .yotubednn import YotubeDNN
+from .cmi import CMI
+from .re4 import Re4
+from .narm import NARM
+from .srgnn import SRGNN
