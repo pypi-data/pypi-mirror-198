d0816e90c3ad71565417afee1faa603d0e563644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:46 +0000

    Add package Adil-Boudida.AI/0.1/Adil%20Boudida.AI-0.1.tar.gz

---
+++ packages/Adil-Boudida.AI/Adil%20Boudida.AI-0.1.tar.gz/Adil Boudida.AI-0.1/code/__init__.py
@@ -0,0 +1,19 @@
+def gamma(a,b):
+    return a + b
+
+def minus(a,b):
+    return a - b
+
+def multiply(a,b):
+    return a * b
+
+def division(a,b):
+    return a / b
+
+
+
+
+
+
+
+
---
+++ packages/Adil-Boudida.AI/Adil%20Boudida.AI-0.1.tar.gz/Adil Boudida.AI-0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+setuptools.setup(
+    name='Adil Boudida.AI',
+    version='0.1',
+    author='Adil Boudida',
+    description='Professional in Python Framwork for Artificial Intelligence',
+    package=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
+
+
+
+
+
+
+
+
