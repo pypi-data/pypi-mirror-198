7c6204277149d7bae845c9b64085ba241721294d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:31 +0000

    Add package pyflodotcom/0.0.0/pyflodotcom-0.0.0.tar.gz

---
+++ packages/pyflodotcom/pyflodotcom-0.0.0.tar.gz/pyflodotcom-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyflodotcom",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+    ],
+)
