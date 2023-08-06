70b59aac945fdb427df8d9da1fdf1d63fdfc866e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:44 +0000

    Add package datapatch/1.0.2/datapatch-1.0.2-py2.py3-none-any.whl

---
+++ packages/datapatch/datapatch-1.0.2-py2.py3-none-any.whl/datapatch/__init__.py
@@ -0,0 +1,24 @@
+from os import read
+from typing import Any, Dict
+from datapatch.lookup import Lookup
+from datapatch.result import Result
+from datapatch.util import read_yaml_file, PathLike
+from datapatch.exc import LookupException
+
+
+__version__ = "1.0.2"
+__all__ = ["Lookup", "Result", "LookupException", "get_lookups", "read_lookups"]
+
+
+def get_lookups(data: Dict[str, Dict[str, Any]]) -> Dict[str, Lookup]:
+    """Turn a mapping of configs into a dict of Lookups."""
+    lookups = {}
+    for name, config in data.items():
+        lookups[name] = Lookup(name, config)
+    return lookups
+
+
+def read_lookups(path: PathLike) -> Dict[str, Lookup]:
+    """Read a set of named lookups from a JSON/YAML file."""
+    data = read_yaml_file(path)
+    return get_lookups(data)
---
+++ packages/datapatch/datapatch-1.0.2-py2.py3-none-any.whl/datapatch/util.py
@@ -0,0 +1,34 @@
+import yaml
+import pathlib
+from banal import is_listish
+from typing import List, Optional, Union, Any
+from functools import lru_cache
+from normality import stringify, normalize
+
+PathLike = Union[str, pathlib.Path]
+
+
+def read_yaml_file(path: PathLike) -> Any:
+    with open(path, "r") as fh:
+        return yaml.load(fh, Loader=yaml.SafeLoader)
+
+
+def str_list(obj: Any) -> List[Optional[str]]:
+    if not is_listish(obj):
+        obj = [obj]
+    return [stringify(o) for o in obj]
+
+
+@lru_cache(maxsize=20000)
+def normalize_value(
+    value: Any, normalize_: bool = False, lowercase: bool = False
+) -> Optional[str]:
+    if normalize_:
+        return normalize(value, ascii=True, lowercase=lowercase)
+
+    text = stringify(value)
+    if text is None:
+        return None
+    if lowercase:
+        text = text.lower()
+    return text.strip()
