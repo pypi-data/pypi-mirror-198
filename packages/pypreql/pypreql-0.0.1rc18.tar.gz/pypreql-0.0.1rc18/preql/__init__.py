268219d8de3f9dcf7b684b5485a96552225252a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:10 +0000

    Add package pypreql/0.0.1rc18/pypreql-0.0.1rc18.tar.gz

---
+++ packages/pypreql/pypreql-0.0.1rc18.tar.gz/pypreql-0.0.1rc18/preql/__init__.py
@@ -0,0 +1,8 @@
+from preql.executor import Executor, Dialects
+from preql.parser import parse
+from preql.core.models import Environment
+
+
+__version__ = "0.0.1-rc.18"
+
+__all__ = ["parse", "Executor", "Dialects", "Environment"]
---
+++ packages/pypreql/pypreql-0.0.1rc18.tar.gz/pypreql-0.0.1rc18/preql/utility.py
@@ -0,0 +1,29 @@
+import hashlib
+from typing import List, Any, Union, Callable
+
+INT_HASH_SIZE = 16
+
+
+def string_to_hash(input: str) -> int:
+    return (
+        int(hashlib.sha256(input.encode("utf-8")).hexdigest(), 16) % 10 ** INT_HASH_SIZE
+    )
+
+
+def unique(inputs: List, property: Union[str, Callable]) -> List[Any]:
+    final = []
+    dedupe = set()
+    if isinstance(property, str):
+
+        def getter(x):
+            return getattr(x, property, "default")
+
+    else:
+        getter = property
+    for input in inputs:
+        key = getter(input)
+        if key in dedupe:
+            continue
+        dedupe.add(key)
+        final.append(input)
+    return final
