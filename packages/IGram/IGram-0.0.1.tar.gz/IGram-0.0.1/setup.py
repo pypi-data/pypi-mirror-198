72cf311c11566d663d2a13ab5a3a6a9fdb237c02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:35 +0000

    Add package IGram/0.0.1/IGram-0.0.1.tar.gz

---
+++ packages/IGram/IGram-0.0.1.tar.gz/IGram-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup,find_packages
+import os
+import codecs
+
+
+setup(
+    name="IGram",
+    version="0.0.1",
+    author="Vaibhav Pandey",
+    author_email="pandey.vaibhav3110@gmail.com",
+    description="InstaGram Scrapper",
+    long_description_content_type="text/markdown",
+    long_description="A webscrapper to Scrap some details from INSTAGRAM handle\nThis would basically gives the \nPosts(),Followers(),Following(),ProfileImage(),Name(),Description()",
+    packages=find_packages(),
+    install_requires=['requests','json','bs4'],
+    keywords=['Instagram info','webscrapping','instagram id name','webscrapper','beautifulsoup','vaibhavpandey module','vaibhav pandey python module','Instagram by pyhton'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
