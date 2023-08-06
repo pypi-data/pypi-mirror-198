16d6b0c1afa453e1963017b3b29f96098a21f034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:28 +0000

    Add package elevenlabslib/0.3.1/elevenlabslib-0.3.1.tar.gz

---
+++ packages/elevenlabslib/elevenlabslib-0.3.1.tar.gz/elevenlabslib-0.3.1/elevenlabslib/__init__.py
@@ -0,0 +1,7 @@
+from .ElevenLabsUser import ElevenLabsUser
+from .ElevenLabsVoice import ElevenLabsVoice
+from .ElevenLabsVoice import ElevenLabsGeneratedVoice
+from .ElevenLabsVoice import ElevenLabsClonedVoice
+from .ElevenLabsSample import ElevenLabsSample
+from .ElevenLabsHistoryItem import ElevenLabsHistoryItem
+__all__ = ["ElevenLabsUser", "ElevenLabsVoice","ElevenLabsClonedVoice","ElevenLabsGeneratedVoice", "ElevenLabsSample", "ElevenLabsHistoryItem"]
