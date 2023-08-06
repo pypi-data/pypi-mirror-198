a05c66ada9127487071d54b8227be8dc51eb47c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:01 +0000

    Add package shopcloud-microservice/0.14.0/shopcloud-microservice-0.14.0.tar.gz

---
+++ packages/shopcloud-microservice/shopcloud-microservice-0.14.0.tar.gz/shopcloud-microservice-0.14.0/shopcloud_microservice/cli.py
@@ -0,0 +1,26 @@
+from . import audit, helpers, security, backup
+from .configs import Config
+
+
+def main(args) -> int:
+    if not hasattr(args, 'which'):
+        print(
+            helpers.bcolors.FAIL
+            + 'Can not parse action use --help'
+            + helpers.bcolors.ENDC
+        )
+        return 1
+
+    if hasattr(args, 'debug') and args.debug:
+        print(args)
+
+    config = Config()
+
+    if args.which == 'security':
+        return security.cli_main(args, config)
+    elif args.which == 'audit':
+        return audit.cli_main(args, config)
+    elif args.which == 'backup':
+        return backup.cli_main(args, config)
+
+    return 0
