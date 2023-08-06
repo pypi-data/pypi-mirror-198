c6c47902d796f691d9ba34a0ac8db5de0e27cfb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:18 +0000

    Add package langful/0.18/langful-0.18-py2.py3-none-any.whl

---
+++ packages/langful/langful-0.18-py2.py3-none-any.whl/langful/__init__.py
@@ -0,0 +1,70 @@
+"""
+# langful
+
+Help developers to localize
+
+# example
+
+- test.py
+- lang
+    - zh_cn.json
+    - en_us.json
+
+## zh_cn.json
+
+```json
+{
+    "hi" : "你好" ,
+    "welcome" : "欢迎"
+}
+```
+
+## en_us.json
+
+```json
+{
+    "hi" : "Hi" ,
+    "welcome" : "Welcome"
+}
+```
+
+## tset.py
+
+```python
+import langful
+
+Text = langful.lang()
+
+print( Text.language_dict )
+
+print( Text.replace( "%hi%\n%welcome%\n%%" ) )
+```
+
+## Output
+
+```python
+{'en_us': {'welcome': 'Welcom', 'hi': 'Hi'}, 'zh_cn': {'welcome': '欢迎', 'hi': '你好'}}
+Hi
+Welcome
+%
+```
+
+or
+
+```python
+{'en_us': {'welcome': 'Welcom', 'hi': 'Hi'}, 'zh_cn': {'welcome': '欢迎', 'hi': '你好'}}
+你好
+欢迎
+%
+```
+
+# About
+
+github: https://github.com/cueavyqwp/langful
+
+pypi: https://pypi.org/project/langful
+
+issues: https://github.com/cueavyqwp/langful/issues
+"""
+
+from langful.lang import *
