ea66bc9a3aca66233e8934e6ccb33040e61073cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:02 +0000

    Add package forestyle/2.9/forestyle-2.9.tar.gz

---
+++ packages/forestyle/forestyle-2.9.tar.gz/forestyle-2.9/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name="forestyle",
+    version='2.9',
+    license='Eclipse Public License 2.0',
+    authors=["sudinedy"],
+    author_email="<billydev356@gmail.com>",
+    description="forked from sudinedy",
+    long_description='Documentation: https://github.com/sudinedy/forestyle',
+    packages=['forestyle']
+)
+
+
