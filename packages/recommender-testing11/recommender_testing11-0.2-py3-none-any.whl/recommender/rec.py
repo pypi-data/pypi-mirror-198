46c2d555af0e3e5156ed98c9b4a56d8f770a74f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:26 +0000

    Add package recommender-testing11/0.2/recommender_testing11-0.2-py3-none-any.whl

---
+++ packages/recommender-testing11/recommender_testing11-0.2-py3-none-any.whl/recommender/rec.py
@@ -0,0 +1,18 @@
+# %%
+import numpy as np
+from pathlib import Path
+
+# print(Path('/Users/arjunharidas/Documents/tahini-tensor-encounter-notes/week11/python_packaging/randomrecommender/recommender/data/movies.txt').parent.parent)
+movies =[]
+with open(Path(__file__).parent+'/data/movies.txt') as file:
+    movies.append(file.readlines())
+
+def random_recommeder(movies=movies,k=5):
+    return np.random.choice(movies,k)
+
+
+# from recommender import rec
+# rec.random_recommeder(['1','2','dcad','acasd','acsc','afq','q'],k=3)
+
+
+# %%
