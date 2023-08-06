a088d420351adfae0b78c33355eca8242d5c408b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:44:06 +0000

    Add package cdk-webapp-skeleton/0.2.dev16/cdk_webapp_skeleton-0.2.dev16-py3-none-any.whl

---
+++ packages/cdk-webapp-skeleton/cdk_webapp_skeleton-0.2.dev16-py3-none-any.whl/cdk_webapp_skeleton/__init__.py
@@ -0,0 +1,6 @@
+from .auth_stack import AuthStack
+from .branch_cicd_pipeline import BranchCICDPipeline
+from .branch_config import BranchConfig
+from .webapp_lambda import WebappLambda
+
+__all__ = [BranchCICDPipeline, BranchConfig, AuthStack, WebappLambda]
