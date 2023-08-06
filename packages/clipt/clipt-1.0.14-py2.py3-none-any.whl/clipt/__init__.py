c2009a1cb3f91a8351b683a1a09c01c9d8878572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:04 +0000

    Add package clipt/1.0.14/clipt-1.0.14-py2.py3-none-any.whl

---
+++ packages/clipt/clipt-1.0.14-py2.py3-none-any.whl/clipt/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for clipt."""
+
+__author__ = """Stephen Wasilewski"""
+__email__ = 'stephanwaz@gmail.com'
+__version__ = '1.0.14'
+__all__ = ['plot', 'cl_plot']
+
+
+import matplotlib
+backend = matplotlib.get_backend()
+import clipt.plot as mplt
+
+try:
+    matplotlib.use(backend)
+except ModuleNotFoundError:
+    pass
+
+
+def get_root(inside=True, pyenv=False):
+    """return root directory of clipt install"""
+
+    import sys
+
+    if pyenv:
+        return sys.executable.rsplit("/bin", 1)[0]
+    elif inside:
+        return __file__.rsplit("clipt", 1)[0] + "clipt"
+    else:
+        return __file__.rsplit("/clipt", 1)[0]
