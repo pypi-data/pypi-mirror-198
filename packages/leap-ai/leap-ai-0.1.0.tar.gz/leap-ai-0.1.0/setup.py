59522864311acb662277e16742a1c30decb98282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:47 +0000

    Add package leap-ai/0.1.0/leap-ai-0.1.0.tar.gz

---
+++ packages/leap-ai/leap-ai-0.1.0.tar.gz/leap-ai-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='leap-ai',
+    version='0.1.0',
+    description='Leap API',
+    author='Leap',
+    author_email='hello@tryleap.ai',
+    packages=find_packages(),
+    install_requires=[
+        # List of package dependencies
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
