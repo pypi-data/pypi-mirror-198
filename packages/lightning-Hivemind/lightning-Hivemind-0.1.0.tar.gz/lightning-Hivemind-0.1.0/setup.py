8aa6b0c2719b0b673abc0d8c14856084526c8722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:13 +0000

    Add package lightning-Hivemind/0.1.0/lightning-Hivemind-0.1.0.tar.gz

---
+++ packages/lightning-Hivemind/lightning-Hivemind-0.1.0.tar.gz/lightning-Hivemind-0.1.0/setup.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import os
+from importlib.util import module_from_spec, spec_from_file_location
+
+from pkg_resources import parse_requirements
+from setuptools import find_packages, setup
+
+_PATH_ROOT = os.path.dirname(__file__)
+_PATH_SOURCE = os.path.join(_PATH_ROOT, "src")
+_PATH_README = os.path.join(_PATH_ROOT, "README.md")
+
+
+def _load_py_module(fname, pkg="lightning_hivemind"):
+    spec = spec_from_file_location(os.path.join(pkg, fname), os.path.join(_PATH_SOURCE, pkg, fname))
+    py = module_from_spec(spec)
+    spec.loader.exec_module(py)
+    return py
+
+
+def _load_requirements(path_dir: str, file_name: str = "requirements.txt") -> list:
+    reqs = parse_requirements(open(os.path.join(path_dir, file_name)).readlines())
+    return list(map(str, reqs))
+
+
+def _load_readme(path_readme: str = _PATH_README) -> str:
+    with open(path_readme) as fp:
+        return fp.read()
+
+
+about = _load_py_module("__about__.py")
+
+# https://packaging.python.org/discussions/install-requires-vs-requirements /
+# keep the meta-data here for simplicity in reading this file... it's not obvious
+# what happens and to non-engineers they won't know to look in init ...
+# the goal of the project is simplicity for researchers, don't want to add too much
+# engineer specific practices
+setup(
+    name="lightning-Hivemind",
+    version=about.__version__,
+    description=about.__docs__,
+    author=about.__author__,
+    author_email=about.__author_email__,
+    url=about.__homepage__,
+    download_url="https://github.com/Lightning-AI/lightning-Hivemind",
+    license=about.__license__,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    long_description=_load_readme(),
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    zip_safe=False,
+    keywords=["deep learning", "pytorch", "AI"],
+    python_requires=">=3.8",
+    setup_requires=["wheel"],
+    install_requires=_load_requirements(_PATH_ROOT),
+    project_urls={
+        "Bug Tracker": "https://github.com/Lightning-AI/lightning-Hivemind/issues",
+        "Documentation": "https://lightning-Hivemind.rtfd.io/en/latest/",
+        "Source Code": "https://github.com/Lightning-AI/lightning-Hivemind",
+    },
+    classifiers=[
+        "Environment :: Console",
+        "Natural Language :: English",
+        # How mature is this project? Common values are
+        #   3 - Alpha, 4 - Beta, 5 - Production/Stable
+        "Development Status :: 4 - Beta",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        # Pick your license as you wish
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
