6a0b32b934a3143627d1cdda18a5cabf596357ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:48 +0000

    Add package arbitrum-huy/0.0/arbitrum_huy-0.0.tar.gz

---
+++ packages/arbitrum-huy/arbitrum_huy-0.0.tar.gz/arbitrum_huy-0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+requirements = ["requests"]
+
+setup(name='arbitrum_huy',
+      version='0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['arbitrum_huy'],
+      install_requires=requirements,
+      author_email='qwelijdqw@rambler.ru',
+      zip_safe=False)
