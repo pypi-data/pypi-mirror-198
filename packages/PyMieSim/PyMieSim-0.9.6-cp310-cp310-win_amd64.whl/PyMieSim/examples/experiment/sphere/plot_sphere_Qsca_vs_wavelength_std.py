63c89d06196a1da86b1290798814ccd15b717e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:03 +0000

    Add package PyMieSim/0.9.6/PyMieSim-0.9.6-cp310-cp310-win_amd64.whl

---
+++ packages/PyMieSim/PyMieSim-0.9.6-cp310-cp310-win_amd64.whl/PyMieSim/examples/experiment/sphere/plot_sphere_Qsca_vs_wavelength_std.py
@@ -0,0 +1,42 @@
+"""
+=======================
+Qsca vs wavelength STD
+=======================
+
+"""
+
+
+import numpy as np
+from PyMieSim.experiment import SphereSet, SourceSet, Setup
+from PyMieSim.materials import Silver
+from PyMieSim import measure
+
+scatterer_set = SphereSet(
+    diameter=np.linspace(400e-9, 1400e-9, 10),
+    material=Silver,
+    n_medium=1
+)
+
+source_set = SourceSet(
+    wavelength=np.linspace(200e-9, 1800e-9, 300),
+    polarization=[0],
+    amplitude=1
+)
+
+experiment = Setup(
+    scatterer_set=scatterer_set,
+    source_set=source_set
+)
+
+data = experiment.Get(measure.Qsca)
+
+figure = data.plot(
+    y=measure.Qsca,
+    x=source_set.wavelength,
+    y_scale='log',
+    std=scatterer_set.diameter
+)
+
+figure.show()
+
+# -
