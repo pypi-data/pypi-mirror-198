9fc103492acae7f70179edc75a5e9e532e6cffee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:55 +0000

    Add package lstm-preprocess/0.2.0/lstm_preprocess-0.2.0.tar.gz

---
+++ packages/lstm-preprocess/lstm_preprocess-0.2.0.tar.gz/lstm_preprocess-0.2.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setuptools.setup(
+    name="lstm_preprocess",
+    version="0.2.0",
+    author="song111",
+    author_email="clarksoyoung@sina.com",
+    description="This package provides fundamental operation on reshaping dataframes for Keras LSTM input",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url="https://github.com/ysong126/lstm_reshaper",
+    project_urls={
+        "Bug Tracker": "https://github.com/ysong126/lstm_reshaper",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
