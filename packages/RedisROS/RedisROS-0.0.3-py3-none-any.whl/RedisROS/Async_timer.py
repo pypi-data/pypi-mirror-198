d5723b3e845c731c8d4c0b4de0b8730ce02d7566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:50 +0000

    Add package RedisROS/0.0.3/RedisROS-0.0.3-py3-none-any.whl

---
+++ packages/RedisROS/RedisROS-0.0.3-py3-none-any.whl/RedisROS/Async_timer.py
@@ -0,0 +1,36 @@
+import time
+from threading import Timer as ThreadTimer
+import random
+import string
+
+
+class Async_timer(ThreadTimer):
+    def __init__(self,
+                 timer_period,
+                 callback,
+                 ref: str = None
+                 ):
+        super().__init__(interval=timer_period, function=callback)
+
+        # -> Create a unique ID for the timer
+        if ref is None: 
+            self.ref = ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(8)])
+        else:
+            self.ref = ref
+
+        # -> Initialise the timer properties
+        self.timer_period = timer_period
+        self.callback = callback
+
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
+
+
+if __name__ == "__main__":
+    def callback():
+        print("Hello World!")
+
+    timer = Timer(timer_period=0.1, callback=callback)
+    timer.run()
+
