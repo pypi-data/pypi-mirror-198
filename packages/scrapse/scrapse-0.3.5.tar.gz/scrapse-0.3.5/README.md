74ffedc20b3b68fc1abfd674f6ef164dce61f415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:20:10 +0000

    Add package scrapse/0.3.5/scrapse-0.3.5.tar.gz

---
+++ packages/scrapse/scrapse-0.3.5.tar.gz/scrapse-0.3.5/README.md
@@ -0,0 +1,45 @@
+# ScrapSE
+
+## Package description
+
+ScrapSE downloads the judgments in the desired format.  
+
+Currently supported platforms: LEGGI D'ITALIA PA.
+
+### Install scrapse
+```
+pip install scrapse
+```
+
+### How to use
+
+#### Saving cookies - important!
+```
+scrapse leggitalia save-cookie 'your_cokies'
+```
+This command creates a `.txt` file containing `your_cookie`.
+
+#### Show filter values
+```
+scrapse leggitalia show-filters
+```
+This command shows the possible values to be assigned to sentence search filters.
+
+#### Download the judgments
+Make sure you have **saved** platform-related cookies before downloading the judgments!.
+```
+scrapse leggitalia scrap-judgments -l torino -s 'Sez. lavoro, Sez. V'
+```
+This command creates a folder in `Users/your_username/scrapse` named `sez.lavoro&sez.v_torino` containing the judgments.
+
+#### Dump judgments to json format
+```
+scrapse leggitalia dump-judgments -d 'folder_path'
+```
+This command creates the json files by saving them in the judgments_dump folder.
+
+#### For more help
+For more information for each command.
+```
+command-name --help
+```
---
+++ packages/scrapse/scrapse-0.3.5.tar.gz/scrapse-0.3.5/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['scrapse', 'scrapse.leggitalia', 'scrapse.leggitalia.commands']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['beautifulsoup4>=4.11.2,<5.0.0',
+ 'numpy>=1.24.2,<2.0.0',
+ 'requests>=2.28.2,<3.0.0',
+ 'typer[all]>=0.7.0,<0.8.0']
+
+entry_points = \
+{'console_scripts': ['scrapse = scrapse.main:app']}
+
+setup_kwargs = {
+    'name': 'scrapse',
+    'version': '0.3.5',
+    'description': 'Package to download and manage judgments',
+    'long_description': "# ScrapSE\n\n## Package description\n\nScrapSE downloads the judgments in the desired format.  \n\nCurrently supported platforms: LEGGI D'ITALIA PA.\n\n### Install scrapse\n```\npip install scrapse\n```\n\n### How to use\n\n#### Saving cookies - important!\n```\nscrapse leggitalia save-cookie 'your_cokies'\n```\nThis command creates a `.txt` file containing `your_cookie`.\n\n#### Show filter values\n```\nscrapse leggitalia show-filters\n```\nThis command shows the possible values to be assigned to sentence search filters.\n\n#### Download the judgments\nMake sure you have **saved** platform-related cookies before downloading the judgments!.\n```\nscrapse leggitalia scrap-judgments -l torino -s 'Sez. lavoro, Sez. V'\n```\nThis command creates a folder in `Users/your_username/scrapse` named `sez.lavoro&sez.v_torino` containing the judgments.\n\n#### Dump judgments to json format\n```\nscrapse leggitalia dump-judgments -d 'folder_path'\n```\nThis command creates the json files by saving them in the judgments_dump folder.\n\n#### For more help\nFor more information for each command.\n```\ncommand-name --help\n```\n",
+    'author': 'zaharia laurentiu jr marius',
+    'author_email': 'zaharialorenzo@gmail.com',
+    'maintainer': 'zaharia laurentiu jr marius',
+    'maintainer_email': 'zaharialorenzo@gmail.com',
+    'url': 'https://gitlab.di.unito.it/ngupp/ngupp-scrapse',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
