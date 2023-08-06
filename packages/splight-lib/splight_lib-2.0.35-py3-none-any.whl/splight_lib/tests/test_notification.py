94dd49108ca34f68067fb380942343546733e4fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:15 +0000

    Add package splight-lib/2.0.35/splight_lib-2.0.35-py3-none-any.whl

---
+++ packages/splight-lib/splight_lib-2.0.35-py3-none-any.whl/splight_lib/tests/test_notification.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestNotification(TestCase):
+    def test_import_notification(self):
+        from splight_abstract.notification import AbstractNotificationClient
---
+++ packages/splight-lib/splight_lib-2.0.35-py3-none-any.whl/splight_lib/tests/test_shortcut.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+
+class TestShortcut(TestCase):
+    def test_import_shortcut(self):
+        from splight_lib.shortcut import (
+            OrganizationHandler,
+        )
