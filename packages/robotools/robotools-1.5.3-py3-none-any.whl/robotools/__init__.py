ed63b3a9f5dc91404f4b232773df9b14186839b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:08:17 +0000

    Add package robotools/1.5.3/robotools-1.5.3-py3-none-any.whl

---
+++ packages/robotools/robotools-1.5.3-py3-none-any.whl/robotools/__init__.py
@@ -0,0 +1,7 @@
+from . import evotools, liquidhandling
+from .evotools import InvalidOperationError, Labwares, Tip, Worklist
+from .liquidhandling import Labware, Trough, VolumeOverflowError, VolumeUnderflowError
+from .transform import WellRotator, WellShifter, make_well_array, make_well_index_dict
+from .utils import DilutionPlan, get_trough_wells
+
+__version__ = "1.5.3"
