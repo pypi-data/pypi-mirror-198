39a3103d9249d808454d58a4484a768cb1f50529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:17 +0000

    Add package test-automation/0.1.6/test_automation-0.1.6-py3-none-any.whl

---
+++ packages/test-automation/test_automation-0.1.6-py3-none-any.whl/test_automation/__init__.py
@@ -0,0 +1,6 @@
+from .api import *
+
+
+__API__ = ['login_user', 'restaurant', 'login_admin', 'send_message_salck', 'update_message_slack', 'send_message_salck_fulfilment', 'update_message_slack_fulfilment', 'cart', 'cart_submit', 'login_vms', 'complete_order']
+
+__all__ = __API__
