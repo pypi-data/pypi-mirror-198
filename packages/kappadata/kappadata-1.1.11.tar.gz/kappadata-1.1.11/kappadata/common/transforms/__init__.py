959c10f0e21226c9aa1b44a5c4508cff72b6136c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:12 +0000

    Add package kappadata/1.1.11/kappadata-1.1.11.tar.gz

---
+++ packages/kappadata/kappadata-1.1.11.tar.gz/kappadata-1.1.11/kappadata/common/transforms/__init__.py
@@ -0,0 +1,7 @@
+# transforms
+from .byol_transforms import BYOLTransform0, BYOLTransform1
+from .mae_finetune_transform import MAEFinetuneTransform
+# norm
+from .norm.kd_cifar10_norm import KDCifar10Norm
+from .norm.kd_cifar100_norm import KDCifar100Norm
+from .norm.kd_image_net_norm import KDImageNetNorm
