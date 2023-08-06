7b4a65966564448ee656ff09052b2af8634413ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:01 +0000

    Add package mr-graph/0.2.3/mr_graph-0.2.3.tar.gz

---
+++ packages/mr-graph/mr_graph-0.2.3.tar.gz/mr_graph-0.2.3/tests/test_fan_out_graphs.py
@@ -0,0 +1,19 @@
+import pytest
+from mr_graph.graph import Graph
+from functions import sub_1, add_1, mult_2
+
+
+@pytest.mark.asyncio
+async def test_fan_out():
+    # fan out example
+    g = Graph()
+    g.add_nodes([sub_1, add_1, mult_2])
+
+    i_0 = g.input(name="n")
+    o_1 = g.add_1(i_0)
+    g.outputs = g.mult_2(o_1)  # q
+    g.outputs += g.sub_1(o_1)  # p
+
+    v = await g(n=1)
+    assert v.q == 4
+    assert v.p == 1
