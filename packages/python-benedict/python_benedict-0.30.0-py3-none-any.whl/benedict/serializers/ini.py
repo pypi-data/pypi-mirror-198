a69a9ca04d5772e57c4dd19e3f969fe796378d90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:20 +0000

    Add package python-benedict/0.30.0/python_benedict-0.30.0-py3-none-any.whl

---
+++ packages/python-benedict/python_benedict-0.30.0-py3-none-any.whl/benedict/serializers/ini.py
@@ -0,0 +1,69 @@
+from configparser import DEFAULTSECT as default_section
+from configparser import RawConfigParser
+from io import StringIO
+
+from benedict.serializers.abstract import AbstractSerializer
+from benedict.utils import type_util
+
+
+class INISerializer(AbstractSerializer):
+    """
+    This class describes an ini serializer.
+    """
+
+    def __init__(self):
+        super().__init__(
+            extensions=[
+                "ini",
+            ],
+        )
+
+    @staticmethod
+    def _get_parser(options):
+        optionxform = options.pop("optionxform", lambda key: key)
+        parser = RawConfigParser(**options)
+        if optionxform and callable(optionxform):
+            parser.optionxform = optionxform
+        return parser
+
+    @staticmethod
+    def _get_section_option_value(parser, section, option):
+        value = None
+        funcs = [parser.getint, parser.getfloat, parser.getboolean, parser.get]
+        for func in funcs:
+            try:
+                value = func(section, option)
+                break
+            except ValueError:
+                continue
+        return value
+
+    def decode(self, s, **kwargs):
+        parser = self._get_parser(options=kwargs)
+        parser.read_string(s)
+        data = {}
+        for option, _ in parser.defaults().items():
+            data[option] = self._get_section_option_value(
+                parser, default_section, option
+            )
+        for section in parser.sections():
+            data[section] = {}
+            for option, _ in parser.items(section):
+                data[section][option] = self._get_section_option_value(
+                    parser, section, option
+                )
+        return data
+
+    def encode(self, d, **kwargs):
+        parser = self._get_parser(options=kwargs)
+        for key, value in d.items():
+            if not type_util.is_dict(value):
+                parser.set(default_section, key, f"{value}")
+                continue
+            section = key
+            parser.add_section(section)
+            for option_key, option_value in value.items():
+                parser.set(section, option_key, f"{option_value}")
+        str_data = StringIO()
+        parser.write(str_data)
+        return str_data.getvalue()
