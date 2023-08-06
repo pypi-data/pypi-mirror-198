7763ff886d07f690b4d5e5fb763d97941ba1f7a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:51 +0000

    Add package GTTD/0.1.15/GTTD-0.1.15-py3-none-any.whl

---
+++ packages/GTTD/GTTD-0.1.15-py3-none-any.whl/bot/sublayers/handler.py
@@ -0,0 +1,32 @@
+import difflib
+
+
+class Handler:
+    def __init__(self, help, commands, database=None):
+        self.database = database
+        self.commands = commands
+        self.help = help
+
+    def get_command_suggestion(self, query):
+        return difflib.get_close_matches(query, self.commands.keys(), n=1, cutoff=0.6)
+    
+    def execute_command(self, query):
+        if self.database:
+            self.commands[query](self.database)
+        self.commands[query]()
+    
+    def run(self):
+        print(self.help)
+        while True:
+            query = input('> ')
+            if query == 'back':
+                break
+            try:
+                self.execute_command(query)
+            except KeyError:
+                suggestion = self.get_command_suggestion(query)
+                if suggestion:
+                    print(f'Did you mean {suggestion[0]}?')
+                else:
+                    print('Invalid command')
+                    print(self.help)
