4390e7daa2f114cba93100f8ac35e5fc8697eb64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:03 +0000

    Add package pii-transform/0.4.1/pii-transform-0.4.1.tar.gz

---
+++ packages/pii-transform/pii-transform-0.4.1.tar.gz/pii-transform-0.4.1/src/pii_transform/app/multi.py
@@ -0,0 +1,24 @@
+import logging
+import sys
+import json
+from pii_transform.api.e2e import MultiPiiTextProcessor
+
+configfile = sys.argv[1]
+infile = sys.argv[2]
+outfile = sys.argv[3]
+
+logging.basicConfig(level="INFO")
+proc = MultiPiiTextProcessor(lang=["en", "es"], config=configfile)
+
+
+with open(outfile, "w", encoding="utf-8") as fout:
+    with open(infile, encoding="utf-8") as fin:
+        for line in fin:
+            doc = json.loads(line)
+            doc["text"] = proc(doc["text"], lang=doc["lang"])
+            json.dump(doc, fout, indent=None, ensure_ascii=False)
+            print(file=fout)
+
+stats = proc.stats()
+print(stats)
+
