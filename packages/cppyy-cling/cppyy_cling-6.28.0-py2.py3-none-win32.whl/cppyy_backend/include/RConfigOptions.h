e291d69fde3de09358be0774ac4b98e5843e0e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:12 +0000

    Add package cppyy-cling/6.28.0/cppyy_cling-6.28.0-py2.py3-none-win32.whl

---
+++ packages/cppyy-cling/cppyy_cling-6.28.0-py2.py3-none-win32.whl/cppyy_backend/include/RConfigOptions.h
@@ -0,0 +1,7 @@
+#ifndef ROOT_RConfigOptions
+#define ROOT_RConfigOptions
+
+#define R__CONFIGUREOPTIONS   "CMAKE_CXX_STANDARD_LIBRARIES=kernel32.lib user32.lib gdi32.lib winspool.lib shell32.lib ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib CMAKE_C_STANDARD_LIBRARIES=kernel32.lib user32.lib gdi32.lib winspool.lib shell32.lib ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib NUMPY_INCLUDE_DIRS=C:/Users/Code/WORK32/Lib/site-packages/numpy/core/include PYTHON_INCLUDE_DIRS=C:/Users/Wim Lavrijsen/AppData/Local/Programs/Python/Python311-32/include PYTHON_LIBRARIES=C:/Users/Wim Lavrijsen/AppData/Local/Programs/Python/Python311-32/libs/python311.lib PYTHON_LIBRARY_DIR=C:/Users/Wim Lavrijsen/AppData/Local/Programs/Python/Python311-32/libs PYTHON_VERSION_MAJOR=3 PYTHON_VERSION_MINOR=11 PYTHON_VERSION_STRING=3.11.2 ZLIB_INCLUDE_DIR=C:/Users/Code/cppyy-backend/cling/src/builtins/zlib ZLIB_INCLUDE_DIRS=C:/Users/Code/cppyy-backend/cling/src/builtins/zlib ZLIB_LIBRARIES=ZLIB::ZLIB ZLIB_VERSION=1.2.8 ZLIB_VERSION_STRING=1.2.8 "
+#define R__CONFIGUREFEATURES  "cxx20  builtin_clang builtin_llvm builtin_zlib shared"
+
+#endif
