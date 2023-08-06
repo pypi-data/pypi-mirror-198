0513380a528458a3704e7e1a790e0e57fbdf2ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:51 +0000

    Add package ilikepaintings/0.0.2/ilikepaintings-0.0.2.tar.gz

---
+++ packages/ilikepaintings/ilikepaintings-0.0.2.tar.gz/ilikepaintings-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name='ilikepaintings',
+    version='0.0.2',
+    author='Nicolas BIZZOZZERO, Ihab BENDIDI, Olivier RISSER-MAROIX',
+    author_email='orissermaroix@gmail.com',
+    description='A package for predicting painting appreciation from images using a linear regressor on top of a frozen CLIP model',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/VieVie31/i-like-painings',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'numpy'
+    ],
+    package_data={
+        "ilikepaintings": ["weights/*/*.pth"]
+    },
+    license_files=["LICENSE"]
+)
