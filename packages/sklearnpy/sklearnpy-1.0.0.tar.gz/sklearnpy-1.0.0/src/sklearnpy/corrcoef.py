9754b557f72df122aa9e2baec8bd19f7caff5a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:34 +0000

    Add package sklearnpy/1.0.0/sklearnpy-1.0.0.tar.gz

---
+++ packages/sklearnpy/sklearnpy-1.0.0.tar.gz/sklearnpy-1.0.0/src/sklearnpy/corrcoef.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+
+print(np.corrcoef(np.mat(np.array([float(i)
+      for i in input().split()]).reshape(-1, 31))))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def println():
+    print('''
+    np.corrcoef(np.mat(np.array([float(i)
+                 for i in input().split()]).reshape(-1, 31)))
+    ''')
