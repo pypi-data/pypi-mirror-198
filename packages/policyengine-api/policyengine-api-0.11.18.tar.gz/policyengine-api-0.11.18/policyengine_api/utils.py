300f44d4f527325cf8c636eef5b5066757465a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:45 +0000

    Add package policyengine-api/0.11.18/policyengine-api-0.11.18.tar.gz

---
+++ packages/policyengine-api/policyengine-api-0.11.18.tar.gz/policyengine-api-0.11.18/policyengine_api/utils.py
@@ -0,0 +1,59 @@
+import hashlib
+import base64
+import dpath
+import numpy as np
+from flask import Response
+from policyengine_core.parameters import ParameterNode
+from policyengine_core.reforms import Reform
+from policyengine_core.periods import instant
+import json
+
+
+def make_hashable(o):
+    if isinstance(o, (tuple, list)):
+        return tuple((make_hashable(e) for e in o))
+
+    if isinstance(o, dict):
+        return tuple(sorted((k, make_hashable(v)) for k, v in o.items()))
+
+    if isinstance(o, (set, frozenset)):
+        return tuple(sorted(make_hashable(e) for e in o))
+
+    return o
+
+
+def hash_object(o):
+    hasher = hashlib.sha256()
+    hasher.update(repr(make_hashable(o)).encode())
+    return base64.b64encode(hasher.digest()).decode()
+
+
+def get_safe_json(value):
+    if isinstance(value, (int, float)):
+        if value == np.inf:
+            return ".inf"
+        elif value == -np.inf:
+            return "-.inf"
+        return value
+    if isinstance(value, str):
+        return value
+    if isinstance(value, dict):
+        return {k: get_safe_json(v) for k, v in value.items()}
+    if isinstance(value, list):
+        return [get_safe_json(v) for v in value]
+    return None
+
+
+def safe_endpoint(f):
+    def wrapper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except Exception as e:
+            return Response(
+                response=json.dumps(dict(status="error", message=str(e))),
+                status=500,
+            )
+
+    wrapper.__name__ = f.__name__
+
+    return wrapper
---
+++ packages/policyengine-api/policyengine-api-0.11.18.tar.gz/policyengine-api-0.11.18/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from policyengine_api.constants import __version__
+
+setup(
+    name="policyengine-api",
+    version=__version__,
+    author="PolicyEngine",
+    author_email="hello@policyengine.org",
+    description="PolicyEngine API",
+    packages=find_packages(),
+    install_requires=[
+        "click>=8",
+        "flask>=1",
+        "flask-cors>=3",
+        "PolicyEngine-Core>=1.12.1,<2",
+        "policyengine_uk==0.43.0",
+        "policyengine_us==0.251.1",
+        "policyengine_canada==0.42.2",
+        "policyengine-ng==0.4.0",
+        "gunicorn",
+        "cloud-sql-python-connector",
+        "google-cloud-logging",
+        "pymysql",
+        "sqlalchemy>=1.4,<2",
+        "streamlit",
+        "markupsafe==2.0.1",
+    ],
+)
