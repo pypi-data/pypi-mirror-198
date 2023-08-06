d046edb97afa5e97bfb0b93a26c9e1610818d63a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:38 +0000

    Add package stepcount/2.1.0/stepcount-2.1.0.tar.gz

---
+++ packages/stepcount/stepcount-2.1.0.tar.gz/stepcount-2.1.0/stepcount/__init__.py
@@ -0,0 +1,13 @@
+name = "stepcount"
+__version__ = "2.1.0"
+__author__ = "Shing Chan, Scott Small, Gert Mertes, Aiden Doherty"
+__email__ = "shing.chan@ndph.ox.ac.uk, scott.small@ndph.ox.ac.uk, gert.mertes@ndph.ox.ac.uk, aiden.doherty@ndph.ox.ac.uk"
+__license__ = "2-Clause BSD"
+__model_version__ = {
+    "rf": "20230210",
+    "ssl": "ssl-20230208"
+}
+__model_md5__ = {
+    "rf": "8d803d175b792fff12f93ca37fbc9271",
+    "ssl": "eea6179f079b554d5e2c8c98ccea8423"
+}
