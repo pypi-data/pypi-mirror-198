6407d1e0d557c378219ced94270ce550b7563565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:58 +0000

    Add package chime-frb-api/3.0.1/chime_frb_api-3.0.1.tar.gz

---
+++ packages/chime-frb-api/chime_frb_api-3.0.1.tar.gz/chime_frb_api-3.0.1/chime_frb_api/configs/__init__.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+import yaml
+
+CONFIG = yaml.safe_load(open(Path(__file__).parent / "workflow.yaml"))
+
+MOUNTS = CONFIG["mounts"]
+LOKI_URLS = CONFIG["loki_urls"]
+
+TEST_CONFIG = yaml.safe_load(open(Path(__file__).parent / "test_workflow.yaml"))
+
+TEST_MOUNTS = TEST_CONFIG["mounts"]
