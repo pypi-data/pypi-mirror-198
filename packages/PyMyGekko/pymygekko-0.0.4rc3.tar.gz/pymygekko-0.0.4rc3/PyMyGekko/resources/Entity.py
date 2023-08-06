91e4366a2748f4246f253ad75707a25ec996f996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:18 +0000

    Add package PyMyGekko/0.0.4rc3/pymygekko-0.0.4rc3.tar.gz

---
+++ packages/PyMyGekko/pymygekko-0.0.4rc3.tar.gz/pymygekko-0.0.4rc3/PyMyGekko/resources/Entity.py
@@ -0,0 +1,4 @@
+class Entity:
+    def __init__(self, id: str, name: str) -> None:
+        self._id = id
+        self._name = name
---
+++ packages/PyMyGekko/pymygekko-0.0.4rc3.tar.gz/pymygekko-0.0.4rc3/PyMyGekko/resources/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2023-present Stephan Uhle <stephanu@gmx.net>
+#
+# SPDX-License-Identifier: MIT
+
+from .Entity import Entity as Entity
+from .Blind import Blind as Blind
