4b3e7399f28eec12cf90104292b8d8d8716fcb25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:28 +0000

    Add package cdpi/0.0.7/cdpi-0.0.7.tar.gz

---
+++ packages/cdpi/cdpi-0.0.7.tar.gz/cdpi-0.0.7/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="cdpi", 
+    version="0.0.7",
+    author="Technology_Robster",
+    author_email="metube0113@gmail.com",
+    description="Python Implementation of Causal Discovery",
+    url="https://github.com/Ho-Jun-Moon/picd",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires = ['numpy', 'scipy', 'pandas', 'matplotlib'],
+)   
