d2b59354944c6db9e495598c2e535714db7eb59e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:25 +0000

    Add package qn/0.2.1/qn-0.2.1.tar.gz

---
+++ packages/qn/qn-0.2.1.tar.gz/qn-0.2.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='qn',
+      version='0.2.1',
+      description='Handy functions I use everyday.',
+      url='https://github.com/frlender/qn',
+      author='Qiaonan Duan',
+      author_email='geonann@gmail.com',
+      license='MIT',
+      packages=['qn'],
+      # install_requires=[
+      #       'matplotlib',
+      #       'seaborn',
+      #       'numpy',
+      #       'scipy',
+      #       'pandas',
+      #       'PyYAML',
+      #       'matplotlib-venn',
+      #       'scikit-learn'
+      # ],
+      zip_safe=False)
