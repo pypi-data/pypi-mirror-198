bdb676cee5e016c759f276cae796f831a4d9f491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:16 +0000

    Add package macrogpt/0.1.0/macrogpt-0.1.0.tar.gz

---
+++ packages/macrogpt/macrogpt-0.1.0.tar.gz/macrogpt-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='macrogpt',
+    version='0.1.0',
+    description='For fun',
+    author='Xuhu Wan',
+    author_email='xuhu.wan@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+)
