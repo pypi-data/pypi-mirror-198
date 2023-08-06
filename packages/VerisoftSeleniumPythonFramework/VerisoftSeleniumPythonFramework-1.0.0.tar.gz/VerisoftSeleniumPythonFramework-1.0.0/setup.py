7e06ecff64de2f03db8cfd1c1b43009b3d5652ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:12 +0000

    Add package VerisoftSeleniumPythonFramework/1.0.0/VerisoftSeleniumPythonFramework-1.0.0.tar.gz

---
+++ packages/VerisoftSeleniumPythonFramework/VerisoftSeleniumPythonFramework-1.0.0.tar.gz/VerisoftSeleniumPythonFramework-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='VerisoftSeleniumPythonFramework',
+    version='1.0.0',
+    packages=['automation-qa-infra'],
+    url='https://bitbucket.org/efrat93367/selenium_python_framework/',
+    license='Apache 2.0',
+    author='Efrat Cohen',
+    author_email='efrat.cohen@verisoft.co',
+    description='a project of selenium python framework,includes whatever you need for your automation tests framework features.'
+)
