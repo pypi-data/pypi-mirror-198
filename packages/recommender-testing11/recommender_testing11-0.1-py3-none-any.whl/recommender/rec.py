bfca180ded9e720294ab8f5af974c2acb284179e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:00 +0000

    Add package recommender-testing11/0.1/recommender_testing11-0.1-py3-none-any.whl

---
+++ packages/recommender-testing11/recommender_testing11-0.1-py3-none-any.whl/recommender/rec.py
@@ -0,0 +1,8 @@
+import numpy as np
+def random_recommeder(movies,k=5):
+    return np.random.choice(movies,k)
+
+
+# from recommender import rec
+# rec.random_recommeder(['1','2','dcad','acasd','acsc','afq','q'],k=3)
+
