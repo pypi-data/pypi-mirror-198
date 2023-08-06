63de50d805a198bdc14680635cd38d344a9cff95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:52 +0000

    Add package scrapy-settings-log/1.3.2/scrapy_settings_log-1.3.2-py3-none-any.whl

---
+++ packages/scrapy-settings-log/scrapy_settings_log-1.3.2-py3-none-any.whl/scrapy_settings_log/__init__.py
@@ -0,0 +1,37 @@
+import logging
+import json
+import re
+
+import scrapy
+from scrapy import signals
+from scrapy.settings import BaseSettings
+
+logger = logging.getLogger(__name__)
+
+
+class SpiderSettingsLogging:
+    @classmethod
+    def from_crawler(cls, crawler):
+        ext = cls()
+        crawler.signals.connect(ext.spider_closed, signal=signals.spider_closed)
+        return ext
+
+    def spider_closed(self, spider):
+        settings = spider.settings
+        if not settings.getbool("SETTINGS_LOGGING_ENABLED"):
+            return
+
+        regex = settings.get("SETTINGS_LOGGING_REGEX")
+        if regex is not None:
+            settings = {k: v for k, v in settings.items() if re.search(regex, k)}
+
+        # nested settings will be BaseSettings objects that are not JSON seriable
+        settings = {
+            k: dict(v) if type(v) is BaseSettings else v for k, v in settings.items()
+        }
+        self.output_settings(settings, spider)
+
+    def output_settings(self, settings: dict, spider: scrapy.Spider):
+        # this can be overwritten in a subclass if you want to send this data elsewhere
+        indent = spider.settings.get("SETTINGS_LOGGING_INDENT")
+        logger.info(json.dumps(settings, indent=indent))
