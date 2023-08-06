e4bab12ec388950cae88fbb7e887f5eab66e70ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:40 +0000

    Add package uuid8/0.0.0/uuid8-0.0.0.tar.gz

---
+++ packages/uuid8/uuid8-0.0.0.tar.gz/uuid8-0.0.0/src/lib.rs
@@ -0,0 +1,6 @@
+use pyo3::prelude::*;
+
+#[pymodule]
+fn _uuid8(_py: Python, m: &PyModule) -> PyResult<()> {
+    Ok(())
+}
---
+++ packages/uuid8/uuid8-0.0.0.tar.gz/uuid8-0.0.0/uuid8/__init__.py
@@ -0,0 +1,6 @@
+# from ._uuid8 import UUID, uuid1, uuid3, uuid4, uuid5, uuid6, uuid7, uuid8
+from ._uuid8 import UUID
+
+__all__ = [
+    "UUID",
+]
