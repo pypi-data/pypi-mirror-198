7e4ff15a456fe9e277a7f046b7d35d6cfbf918ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:47 +0000

    Add package streamlit-molstar/0.2.0/streamlit-molstar-0.2.0.tar.gz

---
+++ packages/streamlit-molstar/streamlit-molstar-0.2.0.tar.gz/streamlit-molstar-0.2.0/streamlit_molstar/docking/frontend/build/precache-manifest.7f81767eccaf3723c735c23c0b904760.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dabd2a665d47ff4bc89abe8188b7a4e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "915b57d9414d0921b170",
+    "url": "./static/css/2.42fe329c.chunk.css"
+  },
+  {
+    "revision": "65cc7432839442917ad7",
+    "url": "./static/css/main.f48d1622.chunk.css"
+  },
+  {
+    "revision": "915b57d9414d0921b170",
+    "url": "./static/js/2.16c31245.chunk.js"
+  },
+  {
+    "revision": "4c1d72cf69bc803e3bdd68d663a8f18b",
+    "url": "./static/js/2.16c31245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65cc7432839442917ad7",
+    "url": "./static/js/main.27c3074f.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.27c3074f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54847fc9bcbb4da411ae",
+    "url": "./static/js/runtime-main.bad65001.js"
+  },
+  {
+    "revision": "6ace9f620783964143445908f7f4be11",
+    "url": "./static/media/cells.6ace9f62.jpg"
+  },
+  {
+    "revision": "1d38789c19edcd4c7609f9d3cb648e12",
+    "url": "./static/media/nebula_back6.1d38789c.jpg"
+  },
+  {
+    "revision": "4066cc73b1773f200d361445dd82437c",
+    "url": "./static/media/nebula_bottom4.4066cc73.jpg"
+  },
+  {
+    "revision": "60fa77931912b7ccc9503b54147dbb22",
+    "url": "./static/media/nebula_front5.60fa7793.jpg"
+  },
+  {
+    "revision": "2ee6e703264acb327b59903a3c67dd35",
+    "url": "./static/media/nebula_left2.2ee6e703.jpg"
+  },
+  {
+    "revision": "cae753c3668b217aa110e583ddb5bc22",
+    "url": "./static/media/nebula_right1.cae753c3.jpg"
+  },
+  {
+    "revision": "9aff707c882ead329acfad85a3db83ef",
+    "url": "./static/media/nebula_top3.9aff707c.jpg"
+  }
+]);
---
+++ packages/streamlit-molstar/streamlit-molstar-0.2.0.tar.gz/streamlit-molstar-0.2.0/streamlit_molstar/frontend/build/precache-manifest.dd565c618a324639f3458c360028e9ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ebb03ef579fa6f6ef8eb7115c6df69",
+    "url": "./index.html"
+  },
+  {
+    "revision": "859d658395f9c3d8a168",
+    "url": "./static/css/2.907d6a00.chunk.css"
+  },
+  {
+    "revision": "9a446778da1960dadd34",
+    "url": "./static/css/main.2a41cbb3.chunk.css"
+  },
+  {
+    "revision": "859d658395f9c3d8a168",
+    "url": "./static/js/2.c63d1c15.chunk.js"
+  },
+  {
+    "revision": "df087e3b51cab167e93df344458ea8f1",
+    "url": "./static/js/2.c63d1c15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a446778da1960dadd34",
+    "url": "./static/js/main.bbae2d37.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.bbae2d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54847fc9bcbb4da411ae",
+    "url": "./static/js/runtime-main.bad65001.js"
+  }
+]);
