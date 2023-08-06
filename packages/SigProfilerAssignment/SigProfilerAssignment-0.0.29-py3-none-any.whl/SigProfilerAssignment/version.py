7384c7f28a86582e778e8e3aca28a88d41d9ca0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:03 +0000

    Add package SigProfilerAssignment/0.0.29/SigProfilerAssignment-0.0.29-py3-none-any.whl

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.0.29-py3-none-any.whl/SigProfilerAssignment/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.0.29'
+version = '0.0.29'
+Update = 'v0.0.29: Stop decomposition plotting with custom signature database and update parameters for sample reconstruction plots'
+
+    
+    
