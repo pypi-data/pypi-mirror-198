fd96ef41af4b5dc380f924f14bdaddfc961035a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:01 +0000

    Add package testpg/0.0.1/testpg-0.0.1-py3-none-any.whl

---
+++ packages/testpg/testpg-0.0.1-py3-none-any.whl/testpg/src/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2023/3/22 15:34
+# @Author  : unclear
+# @File    : __init__.py.py
+# @Software: PyCharm
+from package_name.src import demo
+
+__all__ = [
+    "demo"
+]
