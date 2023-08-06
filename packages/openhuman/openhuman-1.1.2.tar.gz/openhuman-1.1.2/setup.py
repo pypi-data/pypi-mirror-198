1e5ed4168a77c06d209d57985de673266fb02317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:57 +0000

    Add package openhuman/1.1.2/openhuman-1.1.2.tar.gz

---
+++ packages/openhuman/openhuman-1.1.2.tar.gz/openhuman-1.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='openhuman',
+    version='1.1.2',
+    author='OpenHuman',
+    author_email='openhuman.ai@gmail.com',
+    summary="Virtual human assistant",
+    description='OpenHuman: Virtual human assistant',
+    home_page="openhuman.ai",
+    license_file="LICENSE",
+    install_requires=[
+         # List your package's dependencies here
+        "numpy"
+    ],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/OpenHuman-ai/python-openhuman",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    license="MIT",
+    platform="linux",
+    packages=find_packages('src/reader', exclude=['test'])
+)
