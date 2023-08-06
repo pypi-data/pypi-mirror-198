5f08f19ca1b194f4bc63aac8d94088df1b278d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:25 +0000

    Add package ai-transform/0.29.2/ai_transform-0.29.2.tar.gz

---
+++ packages/ai-transform/ai_transform-0.29.2.tar.gz/ai_transform-0.29.2/ai_transform/api/wrappers.py
@@ -0,0 +1,45 @@
+import time
+import logging
+import requests
+
+from ai_transform.logger import format_logging_info
+from requests.models import Response
+
+from typing import Union, Sequence, Mapping, Any
+
+logger = logging.getLogger(__file__)
+logging.basicConfig()
+
+
+def request_wrapper(
+    fn: Union[requests.get, requests.post],
+    args: Sequence = None,
+    kwargs: Mapping[str, Any] = None,
+    num_retries: int = 3,
+    timeout: int = 30,
+    raise_errors: bool = False,
+) -> Response:
+
+    if args is None:
+        args = ()
+
+    if kwargs is None:
+        kwargs = {}
+
+    for _ in range(num_retries):
+        try:
+            result = fn(*args, **kwargs)
+            assert result.status_code == 200, format_logging_info(
+                result.content.decode()
+            )
+        except Exception as e:
+            logger.exception(e)
+            if raise_errors:
+                raise e
+            time.sleep(timeout)
+        else:
+            return result
+
+    raise ValueError(
+        f"Request was not able to be completed within {num_retries} retries"
+    )
