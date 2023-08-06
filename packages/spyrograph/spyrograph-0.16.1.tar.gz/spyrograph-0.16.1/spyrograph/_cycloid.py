e4c977523c2c317c8ec9317e2ecc204e5caf7a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:08 +0000

    Add package spyrograph/0.16.1/spyrograph-0.16.1.tar.gz

---
+++ packages/spyrograph/spyrograph-0.16.1.tar.gz/spyrograph-0.16.1/spyrograph/_cycloid.py
@@ -0,0 +1,26 @@
+"""Abstract base class for generalizing the hypocycloid and epicycloid
+shape's methods i.e. tracing, calculating, etc.
+"""
+
+from abc import ABC
+from numbers import Number
+from typing import List, Tuple
+
+class _Cycloid(ABC):
+    # pylint: disable=too-few-public-methods
+    @classmethod
+    def n_cusps(
+            cls, R: Number, n: int, thetas: List[Number] = None,
+            theta_start: Number = None, theta_stop: Number = None,
+            theta_step: Number = None, origin: Tuple[Number, Number] = (0, 0)
+        ) -> "Cycloid":
+        """Return a cycloid with n number of cusps"""
+        return cls(
+            R=R,
+            r=R/n,
+            thetas=thetas,
+            theta_start=theta_start,
+            theta_stop=theta_stop,
+            theta_step=theta_step,
+            origin=origin
+        )
