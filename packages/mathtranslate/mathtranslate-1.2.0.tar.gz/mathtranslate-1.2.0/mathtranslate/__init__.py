637449ad07340c560eefe6e60f12911a4fd9c773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:10 +0000

    Add package mathtranslate/1.2.0/mathtranslate-1.2.0.tar.gz

---
+++ packages/mathtranslate/mathtranslate-1.2.0.tar.gz/mathtranslate-1.2.0/mathtranslate/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "1.2.0"
+__author__ = "Jiace Sun"
+
+import os
+ROOT = os.path.dirname(os.path.abspath(__file__))
+from . import config
+from . import translation
+from .translation import translate
