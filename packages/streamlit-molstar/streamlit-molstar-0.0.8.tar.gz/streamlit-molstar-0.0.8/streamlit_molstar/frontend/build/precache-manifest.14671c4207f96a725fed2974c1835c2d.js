b1a723de9c837d6598881059b0222cacc33406d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:53 +0000

    Add package streamlit-molstar/0.0.8/streamlit-molstar-0.0.8.tar.gz

---
+++ packages/streamlit-molstar/streamlit-molstar-0.0.8.tar.gz/streamlit-molstar-0.0.8/streamlit_molstar/frontend/build/precache-manifest.14671c4207f96a725fed2974c1835c2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3dc7c0c8df91a835b832e8c9de8ead",
+    "url": "./index.html"
+  },
+  {
+    "revision": "35e645df1db26d9a284b",
+    "url": "./static/css/2.4df151a9.chunk.css"
+  },
+  {
+    "revision": "5ab08741213a5127f2f2",
+    "url": "./static/css/main.7f88cef9.chunk.css"
+  },
+  {
+    "revision": "35e645df1db26d9a284b",
+    "url": "./static/js/2.8367132a.chunk.js"
+  },
+  {
+    "revision": "3f0fcbcaf056a497f1da70120f110cf6",
+    "url": "./static/js/2.8367132a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ab08741213a5127f2f2",
+    "url": "./static/js/main.fe5f1cf6.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.fe5f1cf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54847fc9bcbb4da411ae",
+    "url": "./static/js/runtime-main.bad65001.js"
+  }
+]);
