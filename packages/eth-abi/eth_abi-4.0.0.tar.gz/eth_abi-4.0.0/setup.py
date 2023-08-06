85696c559241bae1d35a9916893da0473c6f4b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:24 +0000

    Add package eth-abi/4.0.0/eth_abi-4.0.0.tar.gz

---
+++ packages/eth-abi/eth_abi-4.0.0.tar.gz/eth_abi-4.0.0/setup.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+HYPOTHESIS_REQUIREMENT = "hypothesis>=4.18.2,<5.0.0"
+
+extras_require = {
+    "tools": [
+        HYPOTHESIS_REQUIREMENT,
+    ],
+    "test": [
+        "pytest>=6.2.5,<7",
+        "pytest-pythonpath>=0.7.1",
+        "pytest-xdist>=2.5.0,<3",
+        "tox>=2.9.1,<3",
+        "eth-hash[pycryptodome]",
+        HYPOTHESIS_REQUIREMENT,
+    ],
+    "lint": [
+        "flake8",
+        "isort>=4.2.15,<5",
+        "mypy==0.910",
+        "pydocstyle>=6.0.0,<7",
+        "black",
+    ],
+    "doc": [
+        "sphinx>=4.5.0,<5",
+        "jinja2>=3.0.0,<3.1.0",  # jinja2<3.0 or >=3.1.0 cause doc build failures.
+        "sphinx_rtd_theme>=1.0.0",
+        "towncrier==18.5.0",  # towncrier doesn't follow semver
+    ],
+    "dev": [
+        "bumpversion>=0.5.3,<1",
+        "pytest-watch>=4.1.0,<5",
+        "wheel",
+        "twine",
+        "ipython",
+    ],
+}
+
+extras_require["dev"] = (
+    extras_require["dev"]
+    + extras_require["test"]
+    + extras_require["lint"]
+    + extras_require["doc"]
+)
+
+
+with open("./README.md") as readme:
+    long_description = readme.read()
+
+
+setup(
+    name="eth_abi",
+    # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
+    version="4.0.0",
+    description="""eth_abi: Python utilities for working with Ethereum ABI definitions, especially encoding and decoding""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="The Ethereum Foundation",
+    author_email="snakecharmers@ethereum.org",
+    url="https://github.com/ethereum/eth-abi",
+    include_package_data=True,
+    install_requires=[
+        "eth-utils>=2.0.0",
+        "eth-typing>=3.0.0",
+        "parsimonious>=0.9.0,<0.10.0",
+    ],
+    python_requires=">=3.7, <4",
+    extras_require=extras_require,
+    py_modules=["eth_abi"],
+    license="MIT",
+    zip_safe=False,
+    keywords="ethereum",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={"eth_abi": ["py.typed"]},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
