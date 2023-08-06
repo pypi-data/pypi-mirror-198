1f2a5d4d4bc1082f14b7b024f71be38518f585e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:26 +0000

    Add package cgpt/0.1.20/cgpt-0.1.20.tar.gz

---
+++ packages/cgpt/cgpt-0.1.20.tar.gz/cgpt-0.1.20/README.md
@@ -0,0 +1,42 @@
+![](https://visitor-badge.glitch.me/badge?page_id=Aina15-DT.cli-gpt)
+
+## Use openai chat-gpt on your cli
+cgpt is a Python module that allows you to use Chat-GPT directly in your Terminal.
+
+### ❓ REQUIREMENTS
+
+- python >=3.7
+- openai API KEY : 
+You need to register on openai to receive your own api key , here : [api_key](https://platform.openai.com/account/api-keys).
+
+### 💻 SETUP
+
+```
+pip install -r requirements.txt
+```
+
+### 🔨 BUILD
+
+Create a build.sh file , copy - paste the content of build.sh.example 
+
+If you are on Linux , launch:
+
+```
+sudo chmod +x build.sh
+```
+Then , :
+
+```
+./build.sh
+```
+
+
+### 🚀 RUN
+
+```
+cgpt tellme
+```
+
+### 💚 Feedback
+
+Please feel free to leave feedback in issues/PRs.
---
+++ packages/cgpt/cgpt-0.1.20.tar.gz/cgpt-0.1.20/app/plugin.py
@@ -0,0 +1,41 @@
+import os, click
+from typing import Union
+import openai
+from app.file_service import file_prompt
+from dotenv import load_dotenv
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def davinci(what : str) -> Union[str, None]:
+
+    try:
+        response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"The following is a conversation with an AI. The AI is helpful, creative, clever, and very friendly.\n\nHuman:{what}",
+        temperature=0.9,
+        max_tokens=200,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
+        )
+        
+        res = response["choices"][0]["text"].replace("AI:","")
+
+    
+    except openai.error.AuthenticationError:
+
+        modify_apikey = input("Votre API KEY est incorrect, tapez `m` pour modifer le key , ou `q` pour quitter > ")
+
+        if modify_apikey == "m" :
+
+            file_prompt()
+            res = None
+            
+        else:
+
+            res = None
+            
+
+    return res
