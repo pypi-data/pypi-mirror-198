87d005fe65fef1e3ae3163931c54b8799c1b53a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:33 +0000

    Add package ansys-motorcad-core/0.1.3/ansys-motorcad-core-0.1.3.tar.gz

---
+++ packages/ansys-motorcad-core/ansys-motorcad-core-0.1.3.tar.gz/ansys-motorcad-core-0.1.3/src/ansys/motorcad/core/__init__.py
@@ -0,0 +1,12 @@
+"""PyMotorCAD is a Python RPC-JSON interface for Motor-CAD."""
+
+__version__ = "0.1.3"
+
+import ansys.motorcad.core.geometry
+from ansys.motorcad.core.motorcad_methods import MotorCAD, MotorCADCompatibility
+from ansys.motorcad.core.rpc_client_core import (
+    MotorCADError,
+    set_default_instance,
+    set_motorcad_exe,
+    set_server_ip,
+)
