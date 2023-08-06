d51abd493985d479e371a23a04f6f82105691eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:15 +0000

    Add package bangla-python/1.1/bangla_python-1.1-py3-none-any.whl

---
+++ packages/bangla-python/bangla_python-1.1-py3-none-any.whl/bn/__init__.py
@@ -0,0 +1,7 @@
+from bn.bangla_python import tokenizer
+from bn.bangla_python import remove_punctuation
+from bn.bangla_python import remove_foreign
+from bn.bangla_python import find_numbers
+from bn.bangla_python import ban_to_eng_num
+from bn.bangla_python import eng_to_ban_num
+from bn.bangla_python import number_to_word
