39b66d583af4ce23fbfda46342947b8d24d2b7fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:47 +0000

    Add package vodesfunc/1.2.2/vodesfunc-1.2.2-py3-none-any.whl

---
+++ packages/vodesfunc/vodesfunc-1.2.2-py3-none-any.whl/vodesfunc/__init__.py
@@ -0,0 +1,15 @@
+"""
+    Oh god no
+"""
+
+# flake8: noqa
+
+from . import aa, automation, misc, noise, scale, descale, util
+from .aa import *
+from .automation import *
+from .auto.convert import *
+from .misc import *
+from .noise import *
+from .scale import *
+from .descale import *
+from .util import *
