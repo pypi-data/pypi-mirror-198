9dbeaa40887edc045d0e82b2bf1ab3d723ad7e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:04 +0000

    Add package newsy/0.1.0/newsy-0.1.0.tar.gz

---
+++ packages/newsy/newsy-0.1.0.tar.gz/newsy-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='newsy',
+    version='0.1.0',
+    description='Summarize news articles on a given topic',
+    author='Gowner Jones',
+    packages=['newsy'],
+    install_requires=[
+        'newspaper3k',
+        'transformers',
+        'GoogleNews',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+
+)
