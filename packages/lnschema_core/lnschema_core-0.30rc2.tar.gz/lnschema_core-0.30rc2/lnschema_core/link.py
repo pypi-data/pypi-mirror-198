a51e4c6e39338f67a77c233c59e9f9e7d6c9b51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:41 +0000

    Add package lnschema_core/0.30rc2/lnschema_core-0.30rc2.tar.gz

---
+++ packages/lnschema_core/lnschema_core-0.30rc2.tar.gz/lnschema_core-0.30rc2/lnschema_core/link.py
@@ -0,0 +1,12 @@
+"""Link tables.
+
+.. autosummary::
+   :toctree: .
+
+   RunIn
+   DObjectFeatures
+   ProjectDFolder
+   DFolderDObject
+
+"""
+from ._link import DFolderDObject, DObjectFeatures, ProjectDFolder, RunIn  # noqa
