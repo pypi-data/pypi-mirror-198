cafb69708c5f79698d42fe8c96d5f8333d739459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:51 +0000

    Add package BeardbAPI/0.0.3/BeardbAPI-0.0.3.tar.gz

---
+++ packages/BeardbAPI/BeardbAPI-0.0.3.tar.gz/BeardbAPI-0.0.3/ BeardbAPI/BeardbAPI.py
@@ -0,0 +1,72 @@
+from .app import create_app
+import os
+import json
+from werkzeug.routing import BaseConverter
+import subprocess
+from flask_cors import CORS,cross_origin
+import datetime
+class ListConverter(BaseConverter):
+    def to_python(self, value):
+        return value.split(',')
+    def to_url(self, values):
+        return ','.join(super(ListConverter, self).to_url(value)
+                        for value in values)
+class BeardbAPI:
+    def __init__(self):
+        self.app = create_app('config.development')
+        self.cors = CORS(self.app, resources={r"/foo": {"origins": "*"}})
+        self.app.config['CORS_HEADERS'] = 'Content-Type'
+        self.host = 'localhost'
+        self.port= 9000
+        print(self.app.url_map)
+ 
+        
+
+
+    #  
+    def storage(self,app_name:str, dir:str=''):     
+            # if not os.path.isdir(dir):
+            #     os.mkdir(dir)
+            app_dir = os.path.join(dir, app_name)
+            if not os.path.exists(app_dir):
+                os.makedirs(app_dir)
+                with open(os.path.join(app_dir, ".bdb"), "w") as f:
+                    f.write(datetime.datetime.now().strftime("%Y-%m-%d"))
+                    f.write("\n")
+                    f.write(app_name)
+                open(os.path.join(app_dir, "ips.json"), "w").close()
+              
+            else:
+                if not os.path.exists(os.path.join(app_dir, ".bdb")):
+                    raise Exception(".bdb file does not exist in the directory")
+            os.system(f"cd {app_dir}")
+            os.chdir(app_dir)
+            print(os.getcwd())
+            if not os.path.exists('users.json'):
+                with open('users.json', 'w') as f:
+                    json.dump([], f)
+
+    def service(self):
+        return self.app                
+    def host(self):
+        return self.app
+    def run(self,host:str,port:int):
+        
+        # print('BearDB API started at http://{}:{}'.format(self.host, self.port))
+
+        self.app.run(host=self.host, port=self.port, debug=True)
+    # def run_gunicorn(host, port):
+    #     try:
+    
+    #         subprocess.check_output(['gunicorn', '--version'])
+    #     except OSError:
+    
+    #         print('Gunicorn is not installed. Please install Gunicorn to run the app.')
+    #         return
+
+    
+    #     subprocess.run(['gunicorn', '-b', f'{host}:{port}', 'run:api'])
+        
+     
+
+
---
+++ packages/BeardbAPI/BeardbAPI-0.0.3.tar.gz/BeardbAPI-0.0.3/setup.py
@@ -0,0 +1,40 @@
+
+# -*- coding=utf-8 -*-
+# Name: teddy oweh
+# Email: teddy@teddyoweh.com
+# Message: Feel Free To Contact Me on Enquires or Question, il Reply :)mport pathlib
+
+from setuptools import setup, find_packages
+import pathlib
+ 
+HERE = pathlib.Path(__file__).parent
+
+ 
+README = (HERE / "README.md").read_text()
+ 
+setup(
+    name="BeardbAPI",
+    version="0.0.3",
+    description="Microservice to deploy Beardb Secured Databases Remotely.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/teddyoweh/beardb-api",
+    author="Teddy Oweh",
+    author_email="teddy@teddyoweh.net",
+    packages=find_packages(),
+ 
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+  
+    include_package_data=True,
+    install_requires=['cryptography','requests','flask_cors','flask','werkzeug','gunicorn',''],
+    entry_points={
+        "console_scripts": [
+            "Beardb=reader.__main__:main",
+        ]
+    },
+)
