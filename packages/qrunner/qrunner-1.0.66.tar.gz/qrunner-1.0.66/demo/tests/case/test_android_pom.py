1e79c6412875bade37b0b1555cb9dc5810c7824a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:02 +0000

    Add package qrunner/1.0.66/qrunner-1.0.66.tar.gz

---
+++ packages/qrunner/qrunner-1.0.66.tar.gz/qrunner-1.0.66/demo/tests/case/test_android_pom.py
@@ -0,0 +1,26 @@
+import qrunner
+
+from tests.page.adr_page import HomePage
+
+
+class TestSearch(qrunner.TestCase):
+    """进入我的页"""
+
+    def start(self):
+        self.page = HomePage(self.driver)
+
+    def test_pom(self):
+        self.page.my_entry.click()
+        self.page.setting_entry.click()
+        self.assert_in_page('设置')
+
+
+if __name__ == '__main__':
+    # 连接本地设备
+    qrunner.main(
+        platform='android',
+        device_id='UJK0220521066836',
+        pkg_name='com.qizhidao.clientapp',
+        errors=[{"resourceId": "com.qizhidao.clientapp:id/bottom_btn"}]
+    )
+
