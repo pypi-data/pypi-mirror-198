512acaa52bcd84590e66abc070e0f8b3be165a73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:06 +0000

    Add package junifer/0.0.2.dev294/junifer-0.0.2.dev294.tar.gz

---
+++ packages/junifer/junifer-0.0.2.dev294.tar.gz/junifer-0.0.2.dev294/junifer/external/h5io/h5io/__init__.py
@@ -0,0 +1,14 @@
+"""Python Objects Onto HDF5
+"""
+
+
+from ._h5io import (
+    has_hdf5,  # noqa, analysis:ignore
+    read_hdf5,  # noqa, analysis:ignore
+    write_hdf5,  # noqa, analysis:ignore
+    _TempDir,  # noqa, analysis:ignore
+    object_diff,  # noqa, analysis:ignore
+    list_file_contents,  # noqa, analysis:ignore
+)
+from ._version import __version__
+from .chunked_array import ChunkedArray  # noqa, analysis:ignore
