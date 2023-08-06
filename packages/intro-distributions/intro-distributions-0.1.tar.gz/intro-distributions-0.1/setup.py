c4d309d49a56562b186fb95c56239209936444d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:40 +0000

    Add package intro-distributions/0.1/intro-distributions-0.1.tar.gz

---
+++ packages/intro-distributions/intro-distributions-0.1.tar.gz/intro-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='intro-distributions',
+      version='0.1',
+      description='Binomial and Gaussian distribution handling',
+      packages=['distributions'],
+      zip_safe=False)
