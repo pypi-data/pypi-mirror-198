6878a528cf4c810c6f42c1c6fccde07178fbf97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:32 +0000

    Add package gptutil/0.0.8/gptutil-0.0.8.tar.gz

---
+++ packages/gptutil/gptutil-0.0.8.tar.gz/gptutil-0.0.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gptutil",
+    version="0.0.8",
+    packages=find_packages(),
+    install_requires=[
+        "retry",
+        "click",
+        "openai",
+        "tiktoken",
+        "prompt_toolkit"
+    ],
+    package_data={"gptutil": ["example/*.yaml"]},
+    entry_points={
+        "console_scripts": [
+            "gpt-ask=gptutil.ask:main",
+            "gpt-image=gptutil.image:main",
+            "gpt-interact=gptutil.interact:main",
+        ],
+    },
+)
