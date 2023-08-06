c1c4cc56342d7b7beb8c49b697057cb115ea2bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:02 +0000

    Add package astakosa/0.1.6/astakosa-0.1.6.tar.gz

---
+++ packages/astakosa/astakosa-0.1.6.tar.gz/astakosa-0.1.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="astakosa",
+    version="0.1.6",
+    packages=find_packages(),
+    install_requires=[
+        "sqlalchemy",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
