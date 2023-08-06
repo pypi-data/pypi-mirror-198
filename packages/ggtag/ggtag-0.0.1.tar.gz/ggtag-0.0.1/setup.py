218256d1165afd764bb35f36a72c154190ef51aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:17 +0000

    Add package ggtag/0.0.1/ggtag-0.0.1.tar.gz

---
+++ packages/ggtag/ggtag-0.0.1.tar.gz/ggtag-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup, Extension
+
+def main():
+    setup(name="ggtag",
+          version="0.0.1",
+          description="Python interface for ggtag",
+          author="Radoslav Gerganov",
+          author_email="rgerganov@gmail.com",
+          include_dirs = ['../host/include', '../shared/include'],
+          ext_modules=[Extension("ggtag", [
+                "ggtagmodule.c",
+                "../host/src/ggtag.cpp",
+                "../host/src/utils.cpp",
+                "../host/src/rfid.cpp",
+                "../shared/src/protocol.cpp",
+                "../shared/src/GUI_Paint.c",
+                "../shared/src/fa.c",
+                "../shared/src/font8.c",
+                "../shared/src/font12.c",
+                "../shared/src/font16.c",
+                "../shared/src/font20.c",
+                "../shared/src/font24.c",
+                "../shared/src/qrcodegen.c",
+                ])])
+
+if __name__ == "__main__":
+    main()
