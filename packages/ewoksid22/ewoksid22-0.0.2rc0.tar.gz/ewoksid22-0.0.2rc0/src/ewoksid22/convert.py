0dfbd9d67d47a64830fb3b3160c1c6c670461554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:15 +0000

    Add package ewoksid22/0.0.2rc0/ewoksid22-0.0.2rc0.tar.gz

---
+++ packages/ewoksid22/ewoksid22-0.0.2rc0.tar.gz/ewoksid22-0.0.2rc0/src/ewoksid22/convert.py
@@ -0,0 +1,33 @@
+from ewokscore import Task
+from .h5_to_spec import convert_h5
+
+
+class ID22H5ToSpec(
+    Task,
+    input_names=["filename"],
+    optional_input_names=[
+        "entries",
+        "outdirs",
+        "primary_outdir",
+        "outprefix",
+        "retry_timeout",
+        "rebin_filename",
+    ],
+    output_names=["outfile"],
+):
+    def run(self):
+        entries = self.get_input_value("entries", None)
+        outdirs = self.get_input_value("outdirs", None)
+        primary_outdir = self.get_input_value("primary_outdir", None)
+        retry_timeout = self.get_input_value("retry_timeout", 10)
+        rebin_filename = self.get_input_value("rebin_filename", None)
+
+        self.outputs.outfile = convert_h5(
+            self.inputs.filename,
+            self.inputs.outprefix,
+            entries=entries,
+            outdirs=outdirs,
+            primary_outdir=primary_outdir,
+            retry_timeout=retry_timeout,
+            rebin_filename=rebin_filename,
+        )
