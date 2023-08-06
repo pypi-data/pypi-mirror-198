4f9a034bb8c3bb732b94dc7c92c311e878258479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:13 +0000

    Add package tkintertools-dev/2.5.12/tkintertools_dev-2.5.12.tar.gz

---
+++ packages/tkintertools-dev/tkintertools_dev-2.5.12.tar.gz/tkintertools_dev-2.5.12/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+from tkintertools import __version__
+
+mode = 1
+
+name = ('tkintertools', 'tkintertools_dev', 'tkintertools_test')[mode]
+version = __version__ if mode == 0 else input(
+    '输入%s版本号: ' % ('开发' if mode == 1 else '测试'))
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author='Xiaokang2022',
+    author_email='2951256653@qq.com',
+    description='An auxiliary module of the tkinder module',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url='https://gitcode.net/weixin_62651706/tkintertools',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Mulan Permissive Software License v2 (MulanPSL-2.0)',
+        'Operating System :: OS Independent',
+    ],
+)
+
+# python -m pip install --user --upgrade setuptools wheel [检查更新]
+# python setup.py sdist bdist_wheel [打包]
+# twine upload dist/* [上传]
