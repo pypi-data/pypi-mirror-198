0b5db698c4734625c0f4fb6f0510eecb328e609a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:44 +0000

    Add package devi-cli/0.2.1/devi-cli-0.2.1.tar.gz

---
+++ packages/devi-cli/devi-cli-0.2.1.tar.gz/devi-cli-0.2.1/devi/commands/add_template.py
@@ -0,0 +1,116 @@
+from os import rename
+from sys import exit
+from shutil import rmtree
+from pathlib import Path
+from typing import Optional
+from subprocess import CalledProcessError
+
+from devi.utils import (
+    copy_dir_content,
+    read_config_file,
+    ask_user_input,
+    edit_file,
+    DeviErrorEditorNotFound,
+    DeviErrorInvalidConfigFile
+)
+import devi.config as config
+from devi.cli.log import deviprint, DEVI_COLORS
+
+# TODO: convert this to interactive input instead of file editing
+# reasons:
+# - input validation: currently, we can only validate the input after file was edited
+#   - we need to validate if template name already exists
+# - better feeling: we are an interactive cli application
+def add_template(src_path: Path, template_name: Optional[str]):
+    """Main functionality for the devi's `add` command.
+    """
+    # Default template name is the dir name
+    if not template_name:
+        template_name = src_path.resolve().name
+
+    if not src_path.exists():
+        deviprint.err(f'path \'{src_path}\' does not exist')
+        exit(1)
+
+    if not src_path.is_dir():
+        deviprint.err(
+            f"'{src_path.resolve()}' is not a directory, that's not how templates work"
+        )
+        exit(1)
+
+    template_name = ask_user_input(f'template name', default=template_name)
+
+    template_path = Path(config.DEVI_TEMPLATES, template_name)
+
+    # We need to handle the case where the template already exists
+    if template_path.exists():
+        deviprint.err(f"template '{template_name}' already exists")
+        exit(1)
+
+    # Copy to $DEVI_TEMPLATES
+    copy_dir_to_template(src_path, template_name)
+
+    template_config_path = Path(config.DEVI_TEMPLATES, template_name, config.DEVI_TEMPLATE_TOML_FILENAME)
+
+    # The user may have a pre-defined template config, so we need to check that
+    if not template_config_path.exists():
+        template_config_path.touch()
+        dump_default_template_config(template_config_path, template_name)
+
+    # Edit the new config file
+    editor = config.DEVI_DEFAULT_EDITOR
+    try:
+        deviprint.info('opening config in editor...')
+        edit_file(template_config_path)
+    except CalledProcessError as e:
+        deviprint.err(f'{editor} exited with code {e.returncode}')
+        exit_cleanup(template_path)
+    except DeviErrorEditorNotFound as e:
+        deviprint.warn(f"couldn't find editor: '{editor}', do you defined it properly?")
+
+    # try to parse the newly created configfile
+    try:
+        read_config_file(template_config_path)
+    except DeviErrorInvalidConfigFile as e:
+        deviprint.err('failed to parse your config file:')
+        deviprint(e)
+        exit_cleanup(template_path)
+    except FileNotFoundError as e:
+        # This shouldn't happen, config file was created previously
+        e.add_note('This may happened because the config file that previously existed was deleted')
+        exit_cleanup(template_path, only_cleanup=True)
+        raise
+
+    deviprint.info(f"new template {DEVI_COLORS['primary'](template_name)} created succesfully")
+
+def copy_dir_to_template(src_path: Path, template_name: str):
+    template_path = Path(config.DEVI_TEMPLATES, template_name)
+    try:
+        # This function handles
+        copy_dir_content(src_path, template_path)
+    except (Exception, KeyboardInterrupt) as e:
+        deviprint()
+        deviprint.err(f"error copying recusively to '{template_path}': {e}")
+        exit_cleanup(template_path)
+
+# TODO: update this information, because currently there is no "devi config file"
+def dump_default_template_config(template_file: Path, template_name: str):
+    """Dumps a default config file for your new templates.
+
+    The default content is defined in the DEVI_TEMPLATE_TOML_CONTENT constant.
+    The default name for this config file should be 'default.template.devi.toml',
+    and is defined in the DEVI_TEMPLATE_TOML_FILENAME constant.
+    """
+    conf_str = config.DEVI_TEMPLATE_TOML_CONTENT(template_name)
+    try:
+        with open(template_file, 'w+') as t_conf:
+            t_conf.write(conf_str)
+    except (IOError, OSError):
+        raise
+
+def exit_cleanup(dir_to_delete: Optional[Path] = None, only_cleanup=False):
+    if dir_to_delete:
+        rmtree(dir_to_delete, ignore_errors=True)
+    deviprint.err('template creation aborted')
+    if not only_cleanup:
+        exit(1)
---
+++ packages/devi-cli/devi-cli-0.2.1.tar.gz/devi-cli-0.2.1/devi/config.py
@@ -0,0 +1,103 @@
+import os
+import platform
+from pathlib import Path
+from devi.cli.log import deviprint, DEVI_COLORS
+from devi.toml_parser import tomllib
+from typing import Callable
+
+is_windows = platform.system() == 'Windows'
+
+DEVI_TEMPLATES: Path
+DEVI_CONFIG: Path
+DEVI_TEMPLATE_TOML_CONTENT: Callable[[str], str]
+DEVI_DEFAULT_EDITOR: str
+DEVI_COLORS: dict[str, Callable[[str], str]]
+
+def init():
+    """
+    Initialize the config variables for devi.
+    """
+    global DEVI_TEMPLATES
+    global DEVI_CONFIG
+
+    global DEVI_TEMPLATE_TOML_FILENAME
+    global DEVI_TEMPLATE_TOML_CONTENT
+    global DEVI_DEFAULT_EDITOR
+
+    global DEVI_COLORS # defined in log.py
+
+    is_custom_user_dir = False
+    if (devi_templates_dir := os.getenv('DEVI_TEMPLATES')):
+        DEVI_TEMPLATES = Path(devi_templates_dir)
+        is_custom_user_dir = True
+    else:
+        DEVI_TEMPLATES = Path.home() / '.templates'
+
+    try:
+        # exists ok
+        if not DEVI_TEMPLATES.exists():
+            DEVI_TEMPLATES.mkdir()
+            deviprint.success(
+                f'we just setup \'{DEVI_TEMPLATES.resolve().as_posix()}\' just for you!'
+            )
+            if not is_custom_user_dir:
+                deviprint.info(f"if you want to override it, define {DEVI_COLORS['info']('$DEVI_TEMPLATES')}\n")
+    except Exception as e:
+        deviprint.err(f'couldn\'t create $DEVI_TEMPLATES at \'{DEVI_TEMPLATES}\'. exiting')
+        deviprint.err(f'error received: {e}')
+        exit(1)
+
+    # Config file for devi is ~/.devi.toml
+    # (TODO: put this in a better cross-platform place)
+    DEVI_CONFIG = Path.home() / '.devi.toml'
+
+    try:
+        with open(DEVI_CONFIG, 'rb') as f:
+            conf = tomllib.load(f)
+            # loading config options
+            DEVI_DEFAULT_EDITOR = conf.get('editor', get_default_text_editor())
+    except IOError:
+        # deviprint.warn('couldn\'t found devi config')
+        DEVI_DEFAULT_EDITOR = get_default_text_editor()
+        pass
+
+    DEVI_TEMPLATE_TOML_FILENAME = 'template.devi.toml'
+    # TODO: make this a customizable file
+    DEVI_TEMPLATE_TOML_CONTENT = lambda template_name: (
+f"""# configuration for template '{template_name}'
+
+# this description will be shown on 'devi list'
+description = ''
+
+# this name will be used as default name for 'devi create "{template_name}"'
+default_name = '{template_name}'
+
+# destination to be used when for 'devi create "{template_name}"'
+# (can be relative or absolute)
+destination = '.'
+
+# command to execute after template creation
+oncreate = ''
+
+# wheter you want to 'cd' to the newly created template or not
+change_dir = true
+""")
+
+def get_default_text_editor() -> str:
+    """Try to get the default text editor from the environment variables.
+
+    It falls back for notepad on Windows and
+    [sensible-editor, editor, nano, vim, vi] on *nix.
+    """
+    # prefer VIEWER as sensible-editor does
+    for editor in [os.getenv('VIEWER'), os.getenv('EDITOR')]:
+        if editor: return editor
+
+    find_cmd = 'where' if is_windows else 'which'
+    null = 'NUL' if is_windows else '/dev/null'
+
+    for editor in ['sensible-editor', 'editor', 'nano', 'vim']:
+        if os.system(f'{find_cmd} {editor} >{null} 2>&1') == 0:
+            return editor
+
+    return 'notepad' if is_windows else 'vi'
