b6ad43a961664ee91928010200a3f58f15d23866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:40 +0000

    Add package foundationlive/0.0.48/foundationlive-0.0.48.tar.gz

---
+++ packages/foundationlive/foundationlive-0.0.48.tar.gz/foundationlive-0.0.48/src/foundationlive/menu.py
@@ -0,0 +1,44 @@
+import logging
+import pathlib
+
+import simple_term_menu
+
+from . import config as configmod
+
+_logger = logging.getLogger(__name__)
+
+base_dir = pathlib.Path(
+    configmod.config["FOUNDATIONLIVE_TEMPLATES_OUTPUT_DIRECTORY"]
+).expanduser()
+
+_logger.debug(f"{base_dir=}")
+
+lst = [
+    "view_csv.txt",
+    "view_hours_per_task.txt",
+    "view_hours_worked_per_day.txt",
+    "view_hours_worked_per_day_summary.txt",
+    "view_invoices.txt",
+]
+
+
+def main():
+
+    for file in lst:
+        path = base_dir / file
+        _logger.debug(f"{path=}")
+
+        if not path.exists():
+            lst.remove(file)
+
+    options = lst
+    terminal_menu = simple_term_menu.TerminalMenu(options)
+    menu_entry_index = terminal_menu.show()
+
+    if menu_entry_index:
+        selected = base_dir / options[menu_entry_index]
+        print(selected.read_text())
+
+
+if __name__ == "__main__":
+    main()
