5360988e67035025741ba8fbef19aacd90ab34e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:03 +0000

    Add package mrudtskiy/1.0.4/mrudtskiy-1.0.4.tar.gz

---
+++ packages/mrudtskiy/mrudtskiy-1.0.4.tar.gz/mrudtskiy-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'mrudtskiy',
+    packages=find_packages(),
+    version = '1.0.4',
+    py_modules = ['mrudtskiy'],
+    author = 'Max',
+    author_email = 'max@mail.ru',
+    description = 'A simple printer of nested lists',
+)
