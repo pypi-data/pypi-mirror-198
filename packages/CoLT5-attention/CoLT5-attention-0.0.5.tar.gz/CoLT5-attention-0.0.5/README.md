754ab89df3a64b87cb804e4b69ef444879a8b269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:49 +0000

    Add package CoLT5-attention/0.0.5/CoLT5-attention-0.0.5.tar.gz

---
+++ packages/CoLT5-attention/CoLT5-attention-0.0.5.tar.gz/CoLT5-attention-0.0.5/README.md
@@ -0,0 +1,90 @@
+<img src="./colt5.png" width="400px"></img>
+
+## CoLT5 Attention - Pytorch (wip)
+
+Implementation of the conditionally routed efficient attention in the proposed <a href="https://arxiv.org/abs/2303.09752">CoLT5</a> architecture, in Pytorch. Besides their routing, which is based on normalizing the scores and weighting the outputs of the "heavy" modules based on <a href="https://arxiv.org/abs/2211.01267">this paper</a>, will also try using sinkhorn for differentible topk, as I've seen in some mixture of experts papers.
+
+
+## Appreciation
+
+- <a href="https://stability.ai/">Stability.ai</a> for the generous sponsorship to work on cutting edge artificial intelligence research
+
+## Install
+
+```bash
+$ pip install colt5-attention
+```
+
+## Usage
+
+```python
+import torch
+
+from colt5_attention import (
+    ConditionalRoutedFeedForward,
+    ConditionalRoutedAttention,
+    ConditionalRoutedTransformerBlock
+)
+
+# mock input, say it is 32768 length
+
+tokens = torch.randn(2, 32768, 512)
+mask = torch.ones(2, 32768).bool()  # can handle variable lengthed sequences
+
+# feedforward
+
+ff = ConditionalRoutedFeedForward(
+    dim = 512,
+    light_ff_mult = 0.5,      # hidden dimension ratio of light branch
+    heavy_ff_mult = 4,        # hidden dimension ratio of heavy branch
+    num_heavy_tokens = 1024   # heavy branch receives only 1024 routed tokens of 32768
+)
+
+ff_out = ff(tokens, mask = mask)  # (2, 32768, 512) - light and heavy branch summed
+
+# attention
+
+attn = ConditionalRoutedAttention(
+    dim = 512,
+    light_dim_head = 64,       # attention head dimension of light branch
+    light_heads = 8,           # number of attention heads for light branch
+    light_window_size = 128,   # local attention receptive field for light
+    heavy_dim_head = 64,       # attention head dimension of heavy branch
+    heavy_heads = 8,           # number of attention heads for heavy branch
+    num_heavy_tokens_q = 1024, # heavy branch receives only 1024 routed tokens of 32768
+    num_heavy_tokens_kv = 1024 # heavy branch receives only 1024 routed tokens of 32768
+)
+
+attn_out = attn(tokens, mask = mask) # (2, 32768, 512) - light and heavy branch summed
+
+# both attention and feedforward with residual
+# the complete transformer block
+# a stack of these would constitute the encoder of CoLT5
+
+block = ConditionalRoutedTransformerBlock(
+    dim = 512,
+    light_dim_head = 64,
+    light_heads = 8,
+    light_window_size = 128,
+    heavy_dim_head = 64,
+    heavy_heads = 8,
+    light_ff_mult = 0.5,
+    heavy_ff_mult = 4,
+    num_heavy_ff_tokens = 1024,
+    num_heavy_attn_tokens_q = 1024,
+    num_heavy_attn_tokens_kv = 1024
+)
+
+block_out = block(tokens, mask = mask) # (2, 32768, 512)
+```
+
+
+## Citations
+
+```bibtex
+@inproceedings{Ainslie2023CoLT5FL,
+    title   = {CoLT5: Faster Long-Range Transformers with Conditional Computation},
+    author  = {Joshua Ainslie and Tao Lei and Michiel de Jong and Santiago Ontan'on and Siddhartha Brahma and Yury Zemlyanskiy and David Uthus and Mandy Guo and James Lee-Thorp and Yi Tay and Yun-Hsuan Sung and Sumit Sanghai},
+    year    = {2023}
+}
+```
---
+++ packages/CoLT5-attention/CoLT5-attention-0.0.5.tar.gz/CoLT5-attention-0.0.5/colt5_attention/transformer_block.py
@@ -0,0 +1,361 @@
+import torch
+import torch.nn.functional as F
+from torch import nn, einsum
+
+from local_attention import LocalMHA
+from einops import rearrange
+
+# helper functions
+
+def exists(val):
+    return val is not None
+
+def default(val, d):
+    return val if exists(val) else d
+
+# tensor helpers
+
+def create_batch_range(t):
+    b, device = t.shape[0], t.device
+    batch_range = torch.arange(b, device = device)
+    return rearrange(batch_range, 'b -> b 1')
+
+# normalization
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.scale = dim ** 0.5
+        self.gamma = nn.Parameter(torch.ones(dim))
+
+    def forward(self, x):
+        normed = F.normalize(x, dim = -1)
+        return normed * self.scale * self.gamma
+
+# modules
+
+def FeedForward(dim, mult = 4):
+    dim_hidden = int(dim * mult)
+    return nn.Sequential(
+        RMSNorm(dim),
+        nn.Linear(dim, dim_hidden),
+        nn.GELU(),
+        nn.Linear(dim_hidden, dim)
+    )
+
+class Attention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        dim_head = 64,
+        heads = 8
+    ):
+        super().__init__()
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        dim_hidden = dim_head * heads
+
+        self.norm = RMSNorm(dim)
+        self.to_q = nn.Linear(dim, dim_hidden, bias = False)
+        self.to_kv = nn.Linear(dim, dim_hidden * 2, bias = False)
+        self.to_out = nn.Linear(dim_hidden, dim, bias = False)
+
+    def forward(
+        self,
+        x,
+        context = None,
+        mask = None,
+        normalized_scores_kv = None
+    ):
+        h = self.heads
+
+        x = self.norm(x)
+
+        if exists(context):
+            context = self.norm(context)
+
+        context = default(context, x)
+
+        q, k, v = (self.to_q(x), *self.to_kv(context).chunk(2, dim = -1))
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), (q, k, v))
+
+        if exists(normalized_scores_kv):
+            # in paper, not sure how they passed back the signal from heavy attention to normalized scores for key/values. just multiply the values by the normalized kv scores for now
+            v = v * rearrange(normalized_scores_kv, 'b n -> b 1 n 1')
+
+        q = q * self.scale
+        sim = einsum('b h i d, b h j d -> b h i j', q, k)
+
+        if exists(mask):
+            mask = rearrange(mask, 'b j -> b 1 1 j')
+            sim = sim.masked_fill(~mask, -torch.finfo(sim.dtype).max)
+
+        attn = sim.softmax(dim = -1)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+# routing related logic
+
+class DifferentiableTopKRouter(nn.Module):
+    """ differentiable topk using cumulative softmax """
+
+    def __init__(
+        self,
+        dim,
+        straight_through = True
+    ):
+        super().__init__()
+        self.routing_token = nn.Parameter(torch.randn(dim))
+        self.straight_through = straight_through
+
+    def forward(
+        self,
+        x,
+        *,
+        num_tokens,
+        mask = None,
+        temperature = 1.
+    ):
+        assert temperature > 0
+
+        scores = einsum('b n d, d -> b n', x, self.routing_token)
+
+        scores = scores / temperature
+
+        if exists(mask):
+            mask_value = -torch.finfo(scores.dtype).max
+            scores = scores.masked_fill(~mask, mask_value / 2)
+
+        scores, indices = scores.sort(dim = -1)
+
+        scores = scores - scores.amax(dim = -1, keepdim = True).detach()
+
+        exp_scores = scores.exp()
+
+        cum_softmax = exp_scores / exp_scores.cumsum(dim = -1).clamp(min = 1e-6)
+
+        selected_scores, selected_indices = map(lambda t: t[:, -num_tokens:], (cum_softmax, indices))
+
+        if self.straight_through:
+            # this would make sure all normalized scores returned are 1., but still differentiable using straight-through trick
+            selected_scores = selected_scores + (1. - selected_scores).detach()
+
+            if exists(mask):
+                batch_range = create_batch_range(x)
+                selected_mask = mask[batch_range, selected_indices]
+                selected_scores = selected_scores.masked_fill(~selected_mask, 0.)
+
+        return selected_scores, selected_indices
+
+# main classes
+
+class ConditionalRoutedFeedForward(nn.Module):
+    def __init__(
+        self,
+        dim,
+        *,
+        num_heavy_tokens,
+        light_ff_mult = 0.5,
+        heavy_ff_mult = 4,
+        router_straight_through = True # would make sure all normalized scores are 1., still differentiable
+    ):
+        super().__init__()
+        self.num_heavy_tokens = num_heavy_tokens
+
+        self.router = DifferentiableTopKRouter(
+            dim = dim,
+            straight_through = router_straight_through
+        )
+
+        self.light_ff = FeedForward(dim, light_ff_mult)
+        self.heavy_ff = FeedForward(dim, heavy_ff_mult)
+
+    def forward(
+        self,
+        x,
+        mask = None,
+        num_heavy_tokens = None
+    ):
+        device, num_heavy_tokens = x.device, default(num_heavy_tokens, self.num_heavy_tokens)
+
+        batch_range = create_batch_range(x)
+
+        # light feedforward sees all the tokens (hidden dimension is only 1/2 of model dimensions)
+
+        light_out = self.light_ff(x)
+
+        # route tokens appropriately for heavy branch
+
+        normalized_scores, indices = self.router(x, num_tokens = num_heavy_tokens, mask = mask)
+
+        # select the tokens to be routed to heavier feedforward (hidden dimension is 4 times model dimensions)
+
+        routed_tokens = x[batch_range, indices]
+
+        # do the heavier branch with only routed tokens
+
+        routed_tokens_out = self.heavy_ff(routed_tokens) * rearrange(normalized_scores, '... -> ... 1')
+
+        # scatter back the output of the heavy feedforward branch
+
+        heavy_out = torch.zeros_like(x)
+        heavy_out[batch_range, indices] = routed_tokens_out
+
+        # sum light and heavy branches
+
+        return light_out + heavy_out
+
+class ConditionalRoutedAttention(nn.Module):
+    def __init__(
+        self,
+        dim,
+        *,
+        num_heavy_tokens_q,
+        num_heavy_tokens_kv,
+        light_dim_head = 64,
+        light_heads = 8,
+        light_window_size = 128,       # each token would see ~ 64 tokens either way to left or right
+        heavy_dim_head = 64,
+        heavy_heads = 8,
+        router_straight_through = True # would make sure all normalized scores are 1., still differentiable
+    ):
+        super().__init__()
+        self.num_heavy_tokens_q = num_heavy_tokens_q
+        self.num_heavy_tokens_kv = num_heavy_tokens_kv
+
+        self.light_attn = LocalMHA(
+            dim = dim,
+            dim_head = light_dim_head,
+            heads = light_heads,
+            window_size = light_window_size // 2,
+            prenorm = True,
+            causal = False,
+            use_rotary_pos_emb = False,
+            look_backward = 1,
+            look_forward = 1
+        )
+
+        # for now, just do qkv for starters, need to separate to q and kv
+
+        self.q_router = DifferentiableTopKRouter(
+            dim = dim,
+            straight_through = router_straight_through
+        )
+
+        self.kv_router = DifferentiableTopKRouter(
+            dim = dim,
+            straight_through = router_straight_through
+        )
+
+        self.heavy_attn = Attention(
+            dim = dim,
+            dim_head = heavy_dim_head,
+            heads = heavy_heads
+        )
+
+    def forward(
+        self,
+        x,
+        *,
+        num_heavy_tokens_q = None,
+        num_heavy_tokens_kv = None,
+        mask = None
+    ):
+        batch, device = x.shape[0], x.device
+
+        num_heavy_tokens_q = default(num_heavy_tokens_q, self.num_heavy_tokens_q)
+        num_heavy_tokens_kv = default(num_heavy_tokens_kv, self.num_heavy_tokens_kv)
+
+        batch_range = torch.arange(batch, device = device)
+        batch_range = rearrange(batch_range, 'b -> b 1')
+
+        # light local attention sees all tokens
+
+        light_out = self.light_attn(x, mask = mask)
+
+        # route tokens appropriately for heavy branch
+
+        normalized_scores_q, indices_q = self.q_router(x, num_tokens = num_heavy_tokens_q, mask = mask)
+        normalized_scores_kv, indices_kv = self.kv_router(x, num_tokens = num_heavy_tokens_kv, mask = mask)
+
+        # select the tokens to be routed to heavier feedforward (hidden dimension is 4 times model dimensions)
+
+        routed_tokens_q = x[batch_range, indices_q]
+        routed_tokens_kv = x[batch_range, indices_kv]
+
+        # calculate key padding mask
+
+        routed_tokens_kv_mask = None
+        if exists(mask):
+            routed_tokens_kv_mask = mask[batch_range, indices_kv]
+
+        # do the heavier branch with only routed tokens
+
+        routed_tokens_out = self.heavy_attn(
+            routed_tokens_q,
+            mask = routed_tokens_kv_mask,
+            context = routed_tokens_kv,
+            normalized_scores_kv = normalized_scores_kv
+        )
+
+        routed_tokens_out = routed_tokens_out * rearrange(normalized_scores_q, '... -> ... 1')
+
+        # scatter back the output of the heavy feedforward branch
+
+        heavy_out = torch.zeros_like(x)
+        heavy_out[batch_range, indices_q] = routed_tokens_out
+
+        # sum light and heavy branches
+
+        return light_out + heavy_out
+
+# block
+
+class ConditionalRoutedTransformerBlock(nn.Module):
+    def __init__(
+        self,
+        dim,
+        *,
+        num_heavy_attn_tokens_q,
+        num_heavy_attn_tokens_kv,
+        num_heavy_ff_tokens,
+        light_dim_head = 64,
+        light_heads = 8,
+        light_window_size = 128,
+        heavy_dim_head = 64,
+        heavy_heads = 8,
+        light_ff_mult = 0.5,
+        heavy_ff_mult = 4,
+    ):
+        super().__init__()
+        self.conditional_ff = ConditionalRoutedFeedForward(
+            dim,
+            num_heavy_tokens = num_heavy_ff_tokens,
+            light_ff_mult = light_ff_mult,
+            heavy_ff_mult = heavy_ff_mult
+        )
+
+        self.conditional_attn = ConditionalRoutedAttention(
+            dim,
+            light_dim_head = light_dim_head,
+            light_heads = light_heads,
+            light_window_size = light_window_size,
+            heavy_dim_head = heavy_dim_head,
+            heavy_heads = heavy_heads,
+            num_heavy_tokens_q = num_heavy_attn_tokens_q,
+            num_heavy_tokens_kv = num_heavy_attn_tokens_kv,
+        )
+
+    def forward(
+        self,
+        x,
+        mask = None,
+        num_heavy_attn_tokens_q = None,
+        num_heavy_attn_tokens_kv = None,
+        num_heavy_ff_tokens = None
+    ):
+        x = self.conditional_attn(x, mask = mask, num_heavy_tokens_q = num_heavy_attn_tokens_q, num_heavy_tokens_kv = num_heavy_attn_tokens_kv) + x
+        x = self.conditional_ff(x, mask = mask, num_heavy_tokens = num_heavy_ff_tokens) + x
+        return x
