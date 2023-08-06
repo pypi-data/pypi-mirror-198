08f318fc998f1afe61852e29d221071cf6ddd39c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:07 +0000

    Add package simplebeam/0.0.6/simplebeam-0.0.6-py3-none-any.whl

---
+++ packages/simplebeam/simplebeam-0.0.6-py3-none-any.whl/simplebeam/exceptions.py
@@ -0,0 +1,57 @@
+"""
+File contains a custom exception hierarchy.
+"""
+
+
+class SimpleBeamError(Exception):
+    """
+    Parent class in an exception hierarchy for the simplebeam project.
+    """
+
+
+class BeamError(SimpleBeamError):
+    """
+    Parent class for errors in the Beam classes.
+    """
+
+
+class BeamNotSolvedError(BeamError):
+    """
+    Error to raise if the beam has to be solved before requesting a property.
+    """
+
+
+class PointNotOnBeamError(BeamError):
+    """
+    Error to raise if a result is requested at a point that is not on the beam.
+    """
+
+
+class LoadError(SimpleBeamError):
+    """
+    Parent class for errors in the Load classes.
+    """
+
+
+class LoadPositionError(LoadError):
+    """
+    Class for errors related to the position of the load.
+    """
+
+
+class RestraintError(SimpleBeamError):
+    """
+    Parent class for errors in the Restraint classes.
+    """
+
+
+class RestraintPositionError(RestraintError):
+    """
+    Class for errors related to the position of the restraint.
+    """
+
+
+class ResultError(SimpleBeamError):
+    """
+    Class for errors in the results.
+    """
