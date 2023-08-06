37bf00d9fde43607f0a1470f207b8503ce72d29a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:34 +0000

    Add package practice-turkish/0.1.4/practice_turkish-0.1.4.tar.gz

---
+++ packages/practice-turkish/practice_turkish-0.1.4.tar.gz/practice_turkish-0.1.4/practice_turkish/practice.py
@@ -0,0 +1,23 @@
+import typer
+
+from .translation import translation
+from .number import numbers
+
+
+def license() -> str:
+    "Returns the content of `LICENSE` file."
+    with open("LICENSE") as f:
+        print(f.read())
+
+
+def main() -> None:
+    "Create Typer application and run it."
+    app = typer.Typer()
+    translation = app.command(help="Practice translation")(translation)
+    numbers = app.command(help="Practice numbers")(numbers)
+    license = app.command(help="Show license")(license)
+    app()
+
+
+if __name__ == "__main__":
+    main()
