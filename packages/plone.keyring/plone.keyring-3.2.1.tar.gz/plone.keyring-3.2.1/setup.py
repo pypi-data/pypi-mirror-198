29d0a0a23179bfe4e2d9400bf5eff3aad62f833c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:07 +0000

    Add package plone.keyring/3.2.1/plone.keyring-3.2.1.tar.gz

---
+++ packages/plone.keyring/plone.keyring-3.2.1.tar.gz/plone.keyring-3.2.1/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+version = '3.2.1'
+
+setup(
+    name='plone.keyring',
+    version=version,
+    description="Manage secrets",
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.rst").read()),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.2",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope :: 4",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords='secret key keyring',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://pypi.org/project/plone.keyring',
+    license='BSD',
+    packages=find_packages(),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
+    install_requires=[
+        'setuptools',
+        'ZODB',
+        'zope.container',
+        'zope.interface',
+        'zope.location',
+    ],
+    )
