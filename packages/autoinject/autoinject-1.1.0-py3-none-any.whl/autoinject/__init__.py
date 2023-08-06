aac356b0071bbcca3c93cff09ccc5441d3a012df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:13 +0000

    Add package autoinject/1.1.0/autoinject-1.1.0-py3-none-any.whl

---
+++ packages/autoinject/autoinject-1.1.0-py3-none-any.whl/autoinject/__init__.py
@@ -0,0 +1,15 @@
+"""
+
+
+.. moduleauthor:: Erin Turnbull <erin.a.turnbull@gmail.com>
+
+"""
+from .injection import InjectionManager, MissingArgumentError, ExtraKeywordArgumentsError, ExtraPositionalArgumentsError
+from .class_registry import ClassRegistry, ClassNotFoundException, CacheStrategy
+from .context_manager import ContextManager
+
+__version__ = '1.1.0'
+
+injector = InjectionManager()
+
+from .informants import ContextInformant, NamedContextInformant
