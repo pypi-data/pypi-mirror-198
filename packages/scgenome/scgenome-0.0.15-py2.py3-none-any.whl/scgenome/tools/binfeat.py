8b14bbd91d6c74f042053e51df2676b5c5e70a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:49 +0000

    Add package scgenome/0.0.15/scgenome-0.0.15-py2.py3-none-any.whl

---
+++ packages/scgenome/scgenome-0.0.15-py2.py3-none-any.whl/scgenome/tools/binfeat.py
@@ -0,0 +1,112 @@
+import pyfaidx
+import pyBigWig
+import numba
+import numpy as np
+
+import scgenome.refgenome
+
+
+def _chromosome_count_gc(data, **kwargs):
+    genome_fasta = kwargs['genome_fasta']
+    chromosome = kwargs['chromosome']
+    column_name = kwargs['column_name']
+
+    genome = pyfaidx.Fasta(genome_fasta)
+    chrom_sequence = np.fromiter(str(genome[chromosome]).upper(), dtype='<U1')
+
+    gc_indicator = ((chrom_sequence == 'G') | (chrom_sequence == 'C')) * 1
+    gc_cumsum = gc_indicator.cumsum()
+
+    data[column_name] = gc_cumsum[data['End'].values - 1] - gc_cumsum[data['Start'].values]
+
+    return data
+
+
+def count_gc(bins, genome_fasta, column_name='gc', proportion=False):
+    """ Count gc in each bin
+
+    Parameters
+    ----------
+    bins : pyranges.PyRanges
+        ranges for which to count gc
+    genome_fasta : str
+        reference genome fasta
+    column_name : str, optional
+        column to add to `bins`, by default 'gc'
+    proportion : bool, optional
+        proportion of length, by default False
+
+    Returns
+    -------
+    pyranges.PyRanges
+        output ranges with additional column for gc count
+    """    
+
+    data = bins.apply(_chromosome_count_gc, genome_fasta=genome_fasta, column_name=column_name)
+
+    if proportion:
+        data = data.assign(column_name, lambda df: df[column_name] / (df['End'] - df['Start']))
+
+    return data
+
+
+@numba.jit(nopython=True)
+def mean_ranges(values, sequence, starts, ends):
+    for i in range(starts.shape[0]):
+        values[i] = np.nanmean(sequence[starts[i]:ends[i]])
+
+
+def _chromosome_mean_bigwig(data, **kwargs):
+    bigwig_file = kwargs['bigwig_file']
+    chromosome = kwargs['chromosome']
+    column_name = kwargs['column_name']
+    chr_prefix = kwargs['chr_prefix']
+
+    chromosome = chr_prefix + chromosome
+    
+    bw = pyBigWig.open(bigwig_file, 'r')
+
+    if chromosome not in bw.chroms():
+        data[column_name] = None
+        return data
+
+    chromosome_length = bw.chroms()[chromosome]
+    sequence = np.array(bw.values(chromosome, start=0, end=chromosome_length))
+
+    x = np.zeros(data.shape[0])
+    mean_ranges(x, sequence, data['Start'].values, data['End'].values)
+
+    data[column_name] = x
+
+    return data
+
+
+def mean_from_bigwig(bins, bigwig_file, column_name, chr_prefix=''):
+    """ Count gc in each bin
+
+    Parameters
+    ----------
+    bins : pyranges.PyRanges
+        ranges for which to count gc
+    bigwig_file : str
+        bigwig filename
+    column_name : str
+        column to add to `bins`
+    chr_prefix : str
+        prefix for chromosome names, default ''
+
+    Returns
+    -------
+    pyranges.PyRanges
+        output ranges with additional column for mean bigwig value per bin
+    """    
+
+    data = bins.apply(
+        _chromosome_mean_bigwig,
+        bigwig_file=bigwig_file,
+        column_name=column_name,
+        chr_prefix=chr_prefix,
+    )
+
+    return data
+
---
+++ packages/scgenome/scgenome-0.0.15-py2.py3-none-any.whl/scgenome/tools/sorting.py
@@ -0,0 +1,77 @@
+import numpy as np
+import scipy.cluster.hierarchy as sch
+import scipy.spatial.distance as dst
+import sklearn.preprocessing
+import pandas as pd
+
+from anndata import AnnData
+from collections.abc import Iterable
+from typing import Union
+
+import scgenome.preprocessing.transform
+
+
+def sort_cells(
+        adata: AnnData,
+        layer_name: Union[None, str, Iterable[Union[None,str]]]='copy',
+        cell_ids: Iterable[str]=None,
+        bin_ids: Iterable[str]=None,
+        standarize: bool=False,
+    ) -> AnnData:
+    """ Sort cells by hierarchical clustering on copy number values.
+
+    Parameters
+    ----------
+    adata : AnnData
+        copy number data
+    layer_name : str, optional
+        layer with copy number data to use for sorting, None for X, by default 'copy'
+    cell_ids : str, optional
+        subset of cells to cluster, by default None
+    bin_ids : str, optional
+        subset of bins to cluster, by default None
+    standarize : bool
+        standardize the data prior to sorting, by default False
+
+    Returns
+    -------
+    AnnData
+        copy number data with cell_order column added to obs
+    """
+    if cell_ids is None:
+        cell_ids = adata.obs.index
+
+    if bin_ids is None:
+        bin_ids = adata.var.index
+
+    def __get_layer(layer_name):
+        if layer_name is not None:
+            return np.array(adata[cell_ids, bin_ids].layers[layer_name])
+        else:
+            return np.array(adata[cell_ids, bin_ids].X)
+
+    if isinstance(layer_name, (str, type(None))):
+        X = __get_layer(layer_name)
+    elif isinstance(layer_name, Iterable):
+        X = np.concatenate([__get_layer(l) for l in layer_name], axis=1)
+    else:
+        raise ValueError(f'layer_name was {layer_name}')
+
+    X = scgenome.preprocessing.transform.fill_missing(X)
+
+    if standarize:
+        X = sklearn.preprocessing.StandardScaler().fit_transform(X)
+
+    D = dst.squareform(dst.pdist(X, 'cityblock'))
+    Y = sch.linkage(D, method='complete')
+    Z = sch.dendrogram(Y, color_threshold=-1, no_plot=True)
+    idx = np.array(Z['leaves'])
+
+    ordering = np.zeros(idx.shape[0], dtype=int)
+    ordering[idx] = np.arange(idx.shape[0])
+
+    adata.obs['cell_order'] = '-1'
+    adata.obs.loc[cell_ids, 'cell_order'] = pd.Series(ordering, index=adata.obs.loc[cell_ids].index)
+
+    return adata
+
