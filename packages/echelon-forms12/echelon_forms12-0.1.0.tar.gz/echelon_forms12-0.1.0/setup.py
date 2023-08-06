8b7025d1a0326b7856ba66037900627e045311a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:06 +0000

    Add package echelon-forms12/0.1.0/echelon_forms12-0.1.0.tar.gz

---
+++ packages/echelon-forms12/echelon_forms12-0.1.0.tar.gz/echelon_forms12-0.1.0/setup.py
@@ -0,0 +1,38 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# This call to setup() does all the work
+setup(
+    name="echelon_forms12",
+    version="0.1.0",
+    description="Demo library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akiva123/echelon",
+    author="Akiva Neuman",
+    author_email="neuman1@umbc.edu",
+    license="Akiva Neuman",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["numpy"]
+)
