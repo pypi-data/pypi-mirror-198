69b573847f145aefa92abd9d0e809ce2f0b54e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:11 +0000

    Add package handyinfer/0.0.5/handyinfer-0.0.5.tar.gz

---
+++ packages/handyinfer/handyinfer-0.0.5.tar.gz/handyinfer-0.0.5/handyinfer/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+from .depth_estimation import *
+from .face_alignment import *
+from .saliency_detection import *
+from .utils import *
+from .visualization import *
---
+++ packages/handyinfer/handyinfer-0.0.5.tar.gz/handyinfer-0.0.5/handyinfer/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Mon Mar 20 09:19:11 2023
+__version__ = '0.0.5'
+__gitsha__ = 'a524868'
+version_info = (0, 0, 5)
