a6cef7d1e1e0459697059de544e392f726ef8b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:49 +0000

    Add package aioaria2/1.3.5rc3/aioaria2-1.3.5rc3.tar.gz

---
+++ packages/aioaria2/aioaria2-1.3.5rc3.tar.gz/aioaria2-1.3.5rc3/aioaria2/typing.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+支持类型注释
+"""
+from typing import TYPE_CHECKING, Any, Awaitable, Callable, Dict, TypeVar, Union
+
+Aria2WebsocketTrigger = TypeVar("Aria2WebsocketTrigger", bound="Aria2WebsocketTrigger")
+
+if TYPE_CHECKING:
+    from aioaria2 import Aria2WebsocketTrigger
+
+"""
+Websocket事件的回调函数
+"""
+CallBack = Callable[
+    [Aria2WebsocketTrigger, Dict[str, Any]], Union[Awaitable[Any], Awaitable[None]]
+]
+
+"""
+产生随机id的工厂函数 如果一定要参数可以用functools.partial
+"""
+IdFactory = Callable[[], Union[int, Awaitable[int]]]
