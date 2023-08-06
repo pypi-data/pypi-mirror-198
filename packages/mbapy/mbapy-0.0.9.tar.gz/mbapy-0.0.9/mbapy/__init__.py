bb8006ecaf9423c73cbccb726d5f34301a9b1779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:35 +0000

    Add package mbapy/0.0.9/mbapy-0.0.9.tar.gz

---
+++ packages/mbapy/mbapy-0.0.9.tar.gz/mbapy-0.0.9/mbapy/__init__.py
@@ -0,0 +1,20 @@
+'''
+Author: BHM-Bob 2262029386@qq.com
+Date: 2022-11-01 22:16:49
+LastEditors: BHM-Bob
+LastEditTime: 2023-03-21 00:40:02
+Description: 
+'''
+# from . import __version__, base, file, plot, web
+
+# def main():
+#     pass
+"""
+var naming:
+
+constant : CONSTANT_VAR_NAME
+variable : var_name
+func : func_name
+global var: globalVarName
+class : ClassName
+"""
---
+++ packages/mbapy/mbapy-0.0.9.tar.gz/mbapy-0.0.9/mbapy/stats/__init__.py
@@ -0,0 +1,20 @@
+'''
+Author: BHM-Bob 2262029386@qq.com
+Date: 2022-12-09 17:24:18
+LastEditors: BHM-Bob
+LastEditTime: 2022-12-09 17:24:37
+Description: 
+'''
+# import geography
+
+# def main():
+#     pass
+"""
+var naming:
+
+constant : CONSTANT_VAR_NAME
+variable : var_name
+func : func_name
+global var: globalVarName
+class : ClassName
+"""
