7aa02265234deb286ba575b1c99a53bbf074ab1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:04 +0000

    Add package art-daq/0.1.1/art-daq-0.1.1.tar.gz

---
+++ packages/art-daq/art-daq-0.1.1.tar.gz/art-daq-0.1.1/Script/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 18 16:05:36 2023
+
+@author: julu
+
+__init__.py para que python sepa que es un paquete.
+TIENE QUE ESTAR VACÍO PARA QUE NO HAYA DEPENDENCIA CÍCLICA
+Para usar la libreria tiene que tener ser import Script.prueba
+"""
+
