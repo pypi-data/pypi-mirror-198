13cecdd453ba9ad2311bbb889f2f6b0ef9b901fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:45 +0000

    Add package qlik-test-aviad/0.1.0/qlik_test_aviad-0.1.0.tar.gz

---
+++ packages/qlik-test-aviad/qlik_test_aviad-0.1.0.tar.gz/qlik_test_aviad-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['qlik_test_aviad']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas>=1.5.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 'qlik-test-aviad',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '# qlik-test-aviad\n \nthis amazing project does something!',
+    'author': 'Aviad Rozenhek',
+    'author_email': 'aviadr1@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
