278d947cf9aa1095f861929eb6a67226d56e1c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:14 +0000

    Add package streamlit-molstar/0.2.1/streamlit-molstar-0.2.1.tar.gz

---
+++ packages/streamlit-molstar/streamlit-molstar-0.2.1.tar.gz/streamlit-molstar-0.2.1/streamlit_molstar/docking/frontend/build/precache-manifest.03d97adeb3aeb2ed823c3da1e116e3d9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a6f4cba2b1c2a90ede3f821ee25f360",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ea3730fec727e987b903",
+    "url": "./static/css/2.42fe329c.chunk.css"
+  },
+  {
+    "revision": "52c03d5baaa11fa8034f",
+    "url": "./static/css/main.f48d1622.chunk.css"
+  },
+  {
+    "revision": "ea3730fec727e987b903",
+    "url": "./static/js/2.92425638.chunk.js"
+  },
+  {
+    "revision": "4c1d72cf69bc803e3bdd68d663a8f18b",
+    "url": "./static/js/2.92425638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52c03d5baaa11fa8034f",
+    "url": "./static/js/main.5447f75b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.5447f75b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54847fc9bcbb4da411ae",
+    "url": "./static/js/runtime-main.bad65001.js"
+  },
+  {
+    "revision": "6ace9f620783964143445908f7f4be11",
+    "url": "./static/media/cells.6ace9f62.jpg"
+  },
+  {
+    "revision": "1d38789c19edcd4c7609f9d3cb648e12",
+    "url": "./static/media/nebula_back6.1d38789c.jpg"
+  },
+  {
+    "revision": "4066cc73b1773f200d361445dd82437c",
+    "url": "./static/media/nebula_bottom4.4066cc73.jpg"
+  },
+  {
+    "revision": "60fa77931912b7ccc9503b54147dbb22",
+    "url": "./static/media/nebula_front5.60fa7793.jpg"
+  },
+  {
+    "revision": "2ee6e703264acb327b59903a3c67dd35",
+    "url": "./static/media/nebula_left2.2ee6e703.jpg"
+  },
+  {
+    "revision": "cae753c3668b217aa110e583ddb5bc22",
+    "url": "./static/media/nebula_right1.cae753c3.jpg"
+  },
+  {
+    "revision": "9aff707c882ead329acfad85a3db83ef",
+    "url": "./static/media/nebula_top3.9aff707c.jpg"
+  }
+]);
