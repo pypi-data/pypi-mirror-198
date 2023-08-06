7cad2eb37306e61e4247d3eb57ca1d11e138d7f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:03 +0000

    Add package litex/2022.12/litex-2022.12.tar.gz

---
+++ packages/litex/litex-2022.12.tar.gz/litex-2022.12/litex/build/xilinx/__init__.py
@@ -0,0 +1,2 @@
+from litex.build.xilinx.platform import XilinxPlatform, XilinxSpartan6Platform, Xilinx7SeriesPlatform
+from litex.build.xilinx.programmer import UrJTAG, XC3SProg, FpgaProg, VivadoProgrammer, iMPACT, Adept
