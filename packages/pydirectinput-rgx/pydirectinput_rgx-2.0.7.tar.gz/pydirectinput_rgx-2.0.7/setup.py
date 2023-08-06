b4eb3f2cd3331678c76fd2428fed9dde1b596fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:12 +0000

    Add package pydirectinput-rgx/2.0.7/pydirectinput_rgx-2.0.7.tar.gz

---
+++ packages/pydirectinput-rgx/pydirectinput_rgx-2.0.7.tar.gz/pydirectinput_rgx-2.0.7/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pydirectinput_rgx",
+    version="2.0.7",
+    author="ReggX",
+    author_email="dev@reggx.eu",
+    description=(
+        "Python mouse and keyboard input automation for Windows using "
+        "Direct Input."
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/reggx/pydirectinput_rgx",
+    packages=['pydirectinput'],
+    package_data={
+        'pydirectinput': ['py.typed']
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Win32 (MS Windows)",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries",
+        "Typing :: Typed",
+    ],
+    install_requires=[
+        "typing-extensions>=4.2.0, <5.0; python_version < '3.9'"
+    ],
+    python_requires='>=3.7',
+    license='MIT',
+    keywords='python directinput wrapper abstraction input gui automation'
+)
