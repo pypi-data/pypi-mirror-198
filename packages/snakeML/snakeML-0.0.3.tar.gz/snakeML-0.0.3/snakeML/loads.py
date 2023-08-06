a756bbfc11a606491633c095945b7755b55266b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:51 +0000

    Add package snakeML/0.0.3/snakeML-0.0.3.tar.gz

---
+++ packages/snakeML/snakeML-0.0.3.tar.gz/snakeML-0.0.3/snakeML/loads.py
@@ -0,0 +1,38 @@
+from numpy_transformations import mcol, mrow
+import numpy
+
+def loadData(filename, row_attributes= False, labels= False, separator=',', numpyDataType=None):
+    data=[]
+    with open(filename) as f:
+        if labels:
+            labels=[]
+            for line in f:
+                try:
+                    attrs = line.split(separator)[0:-1]
+                    if row_attributes:
+                        attrs = mcol(numpy.array(attrs,dtype=numpyDataType))
+                    else:
+                        attrs = numpy.array(attrs,dtype=numpyDataType)
+                    label = line.split(separator)[-1].strip()
+                    data.append(attrs)
+                    labels.append(label)
+                except:
+                    pass
+            if row_attributes:
+                return numpy.hstack(data), numpy.array(labels)
+            else:
+                return numpy.array(data), numpy.array(labels)
+        else:
+            for line in f:
+                try:
+                    attrs = line.strip().split(separator)
+                    if row_attributes:
+                        attrs = mcol(numpy.array(attrs,dtype=numpyDataType))
+                    else:
+                        attrs = numpy.array(attrs,dtype=numpyDataType)
+                    data.append(attrs)
+                except:
+                    pass
+            return numpy.array(data)
+        
+
---
+++ packages/snakeML/snakeML-0.0.3.tar.gz/snakeML-0.0.3/snakeML/numpy_transformations.py
@@ -0,0 +1,5 @@
+def mcol(row):
+    return row.reshape(row.size,1)
+
+def mrow(column):
+    return column.reshape(1,column.size)
