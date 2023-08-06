9e7587d2388280db721ec5ffeab9eef85b123083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:46 +0000

    Add package pdivas/0.6.1/pdivas-0.6.1.tar.gz

---
+++ packages/pdivas/pdivas-0.6.1.tar.gz/pdivas-0.6.1/pdivas/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+__version__ = '0.6.1'
+__author__ = 'Ryo Kurosawa'
+__license__      = 'MIT'
+__author_email__ = 'a0160561@yahoo.co.jp'
+__url__          = 'https://github.com/shiro-kur/PDIVAS'
+
+__cur_path__ = __file__
+
+
+
