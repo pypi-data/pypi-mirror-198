41c9bfa637d6b8fb85bd62d4cd470977a53812b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:08 +0000

    Add package zrb/0.0.39/zrb-0.0.39.tar.gz

---
+++ packages/zrb/zrb-0.0.39.tar.gz/zrb-0.0.39/src/zrb/builtin/generator/cmd_task/template/_automate/snake_task_name.py
@@ -0,0 +1,12 @@
+from zrb import CmdTask, runner
+from zrb.builtin._group import project_group
+
+snake_task_name = CmdTask(
+    name='kebab-task-name',
+    description='human readable task name',
+    group=project_group,
+    cmd=[
+        'echo human readable task name'
+    ]
+)
+runner.register(snake_task_name)
