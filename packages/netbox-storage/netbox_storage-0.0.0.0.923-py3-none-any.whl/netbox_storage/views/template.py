550a2fe0845fd1b3e5b45886171c93bbc52df63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:38 +0000

    Add package netbox-storage/0.0.0.0.923/netbox_storage-0.0.0.0.923-py3-none-any.whl

---
+++ packages/netbox-storage/netbox_storage-0.0.0.0.923-py3-none-any.whl/netbox_storage/views/template.py
@@ -0,0 +1,105 @@
+from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import redirect, get_object_or_404
+
+from netbox.views import generic
+from netbox_storage.forms import LVMTemplateForm, LinuxDeviceForm
+
+from netbox_storage.forms.template import PartitionTemplateForm
+from netbox_storage.models import TemplateConfigurationDrive, Partition, LogicalVolume, LinuxDevice, Drive, \
+    StorageConfigurationDrive
+
+from django.http import HttpResponseRedirect
+
+from virtualization.models import VirtualMachine
+
+
+class TemplateLVMAddView(generic.ObjectEditView):
+    queryset = LogicalVolume.objects.all()
+    form = LVMTemplateForm
+    template_name = 'netbox_storage/lvm_create.html'
+
+    def alter_object(self, instance, request, args, kwargs):
+        if not instance.pk:
+            # Assign the object based on URL kwargs
+            content_type = get_object_or_404(ContentType, pk=request.GET.get('content_type'))
+            instance.object = get_object_or_404(content_type.model_class(), pk=request.GET.get('object_id'))
+        return instance
+
+    def get_extra_addanother_params(self, request):
+        return {
+            'content_type': request.GET.get('content_type'),
+            'object_id': request.GET.get('object_id'),
+        }
+
+
+class TemplatePartitionAddView(generic.ObjectEditView):
+    queryset = LinuxDevice.objects.all()
+    form = PartitionTemplateForm
+
+
+class SyncTemplateToVMView(generic.ObjectEditView):
+    queryset = StorageConfigurationDrive.objects.all()
+
+    def get_extra_context(self, request, instance):
+        print(f"Request: {request}")
+        print(f"Self: {self}")
+        print(f"Instance: {instance}")
+        drives_id = TemplateConfigurationDrive.objects.values('drive').filter(platform=instance.platform)
+        partition_dict = {}
+        for drive_id in drives_id:
+            drive = Drive.objects.get(pk=drive_id['drive'])
+
+            if Partition.objects.filter(drive=drive).count() == 0:
+                partition_dict[drive] = None
+            else:
+                partition_dict[drive] = list(Partition.objects.filter(drive=drive))
+
+        for k, v in partition_dict.items():
+            instance_drive = k
+            instance_drive.pk = None
+            instance_drive.save()
+            StorageConfigurationDrive.objects.create(virtual_machine=instance, drive=instance_drive)
+
+            for partition in v:
+                instance_partition = partition
+                instance_partition.pk = None
+                instance_partition.drive = instance_drive
+                instance_partition.save()
+            print(k, v)
+        return {}
+
+
+def sys_topography_file(request):
+    print(request)
+    q = request.GET.get('q', '')
+    vm = VirtualMachine.objects.get(pk=q)
+
+    drives_id = TemplateConfigurationDrive.objects.values('drive').filter(platform=vm.platform)
+    partition_dict = {}
+    for drive_id in drives_id:
+        drive = Drive.objects.get(pk=drive_id['drive'])
+
+        if Partition.objects.filter(drive=drive).count() == 0:
+            partition_dict[drive] = None
+        else:
+            partition_dict[drive] = list(Partition.objects.filter(drive=drive))
+
+    for k, v in partition_dict.items():
+        instance_drive = k
+        instance_drive.pk = None
+
+        number_of_hard_drives = StorageConfigurationDrive.objects.filter(
+            virtual_machine=vm).count() or 0
+        instance_drive.identifier = f'Hard Drive {number_of_hard_drives + 1}'
+
+        instance_drive.save()
+        StorageConfigurationDrive.objects.create(virtual_machine=vm, drive=instance_drive)
+
+        for partition in v:
+            instance_partition = partition
+            instance_partition.pk = None
+            instance_partition.drive = instance_drive
+            instance_partition.save()
+        print(k, v)
+
+    return HttpResponseRedirect(vm.get_absolute_url())
