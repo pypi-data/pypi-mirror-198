bda10101ea4ecfd4cfdf5e67c63025ed4cd467fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:15 +0000

    Add package aloso/0.0.24/aloso-0.0.24-py3-none-any.whl

---
+++ packages/aloso/aloso-0.0.24-py3-none-any.whl/output/database_base.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+import config
+
+Base = declarative_base()
+
+engine = create_engine(f'{config.database_resource}:///{config.database_file}')
+
+from output.contact_database import Contact
+from output.site_database import Site, SitesContacts
+from output.label_database import Label, LabelsContacts
+from output.favorite_links_database import FavoriteLinksData
+from output.activity_logs_database import ActivityLogsData
---
+++ packages/aloso/aloso-0.0.24-py3-none-any.whl/output/install.py
@@ -0,0 +1,28 @@
+from output import directories
+from output.database_base import engine, Base
+from output.tools import config_files_data
+from output.tools.frontend_shell import FrontendShell
+from output.tools.grafana_shell import GrafanaShell
+from output.tools.nginx_shell import NginxShell
+from output.tools.syslog_shell import SyslogShell
+
+if __name__ == '__main__':
+    # config sample
+    with(open('config.py', 'w')) as file:
+        file.write(config_files_data.config_sample)
+    # create directories
+    directories.create_base_dir()
+    # Base de données vide
+    Base.metadata.create_all(engine)
+    '''#frontend
+    FrontendShell.install()
+    #nginx
+    NginxShell.install_nginx('network')
+    #grafana
+    GrafanaShell.install_grafana()
+    #loki
+    GrafanaShell.install_loki()
+    #promtail
+    GrafanaShell.install_promtail()
+    #syslog
+    SyslogShell.install_syslog()'''
