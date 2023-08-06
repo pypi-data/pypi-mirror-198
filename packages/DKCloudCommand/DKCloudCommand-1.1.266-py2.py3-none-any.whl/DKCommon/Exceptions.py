eabfd15b6571ff393194a58734aad5271b77e12d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:53 +0000

    Add package DKCloudCommand/1.1.266/DKCloudCommand-1.1.266-py2.py3-none-any.whl

---
+++ packages/DKCloudCommand/DKCloudCommand-1.1.266-py2.py3-none-any.whl/DKCommon/Exceptions.py
@@ -0,0 +1,7 @@
+class KitchenPreviewOutdatedException(Exception):
+    def __init__(self, message):
+        super(KitchenPreviewOutdatedException, self).__init__(message)
+
+
+class ValidationException(Exception):
+    pass
