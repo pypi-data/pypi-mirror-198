9008ed0ea8949c4e3d03fe03bd036285afa1a066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:17 +0000

    Add package easierscrape/0.1.0/easierscrape-0.1.0.tar.gz

---
+++ packages/easierscrape/easierscrape-0.1.0.tar.gz/easierscrape-0.1.0/README.md
@@ -0,0 +1,78 @@
+# easierscrape
+
+A library for basic web scraping.
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-green.svg)](https://opensource.org/licenses/Apache-2.0)
+[![Issues](https://img.shields.io/github/issues/dag2226/easierscrape)](https://github.com/dag2226/easierscrape/issues)
+[![Build Status](https://github.com/dag2226/easierscrape/workflows/Build%20Status/badge.svg?branch=main)](https://github.com/dag2226/easierscrape/actions?query=workflow%3A%22Build+Status%22)
+[![codecov](https://codecov.io/gh/dag2226/easierscrape/branch/main/graph/badge.svg)](https://codecov.io/gh/dag2226/easierscrape)
+
+## Overview
+easierscrape is a library which helps users do some basic web scraping operations. Oftentimes when doing webscraping code is written and re-written with slightly changed parameters to fit the website to be scraped from. This library is an easy to use tool that can scrape essentials from websites (tables, links, files, etc.). It also has the ability to generate hyperlink trees.
+
+## Details
+This project is a pure python project using modern tooling. It uses a `Makefile` as a command registry, with the following commands:
+- `make`: list available commands
+- `make develop`: install and build this library and its dependencies using `pip`
+- `make build`: build the library using `setuptools`
+- `make lint`: perform static analysis of this library with `flake8` and `black`
+- `make format`: autoformat this library using `black`
+- `make annotate`: run type checking using `mypy`
+- `make test`: run automated tests with `pytest`
+- `make coverage`: run automated tests with `pytest` and collect coverage information
+- `make dist`: package library for distribution
+
+## Basic Usage
+Install with pip: `pip install easierscrape`
+
+Import needed methods from `easierscrape` as seen below:
+```
+from easierscrape import (
+    parse_anchors,
+    parse_files,
+    parse_images,
+    parse_tables,
+    print_tree,
+    tree_gen,
+)
+```
+`parse_anchors`
+- Takes in one argument: The `url` to scrape from
+- Returns a list of anchor tag references
+
+`parse_files`
+- Takes in two arguments:
+  - The `url` to scrape from
+  - A `List` of file extensions to parse (ex: `['pdf', 'txt']`)
+- Downloads results to an `easierscrape_downloads` folder in working directory
+
+`parse_images`
+- Takes in one argument: The `url` to scrape from
+- Downloads results to an `easierscrape_downloads` folder in working directory
+
+`parse_tables`
+- Takes in one argument: The `url` to scrape from
+- Downloads results to an `easierscrape_downloads` folder in working directory
+
+`print_tree`
+- Takes in an [anytree](https://github.com/c0fec0de/anytree) `Node` structure (head of a tree)
+- Prints the tree
+
+`tree_gen`
+- Takes in two arguments;
+  - The head `url` to scrape from
+  - The `maxdepth` to scrape until
+- Returns the head [anytree](https://github.com/c0fec0de/anytree) `Node` of a generated scrape tree
+
+## Command Line Usage
+When installed, you can invoke easierscrape from the command-line:
+```
+usage: easierscrape [-h] url depth
+
+positional arguments:
+  url         the url to scrape
+  depth       the depth of the scrape tree
+
+optional arguments:
+  -h, --help  show this help message and exit
+```
---
+++ packages/easierscrape/easierscrape-0.1.0.tar.gz/easierscrape-0.1.0/easierscrape/easierscrape.py
@@ -0,0 +1,124 @@
+from anytree import Node, RenderTree
+from anytree.search import find
+from bs4 import BeautifulSoup
+from os import makedirs
+from os.path import basename, exists, join
+from pandas import read_html
+from re import compile
+from requests import get
+from urllib.request import urlretrieve, url2pathname
+from uuid import uuid4
+
+
+def parse_anchors(url):
+    return soup_url(url).find_all("a")
+
+
+def parse_files(url, filetypes=[]):
+    if len(filetypes) == 0:
+        print("No filetype specified")
+        return
+    file_download_list = []
+    for filetype in filetypes:
+        files = soup_url(url).find_all("a", href=compile(r"(." + filetype + ")"))
+        file_download_count = 0
+        if len(files) != 0:
+            create_dir(join(".", "easier_scrape_downloads", filetype, url2pathname(url)[2:]))
+        for file in files:
+            try:
+                fileUrl = concat_urls(url, file.attrs["href"])
+                urlretrieve(
+                    fileUrl, join(".", "easier_scrape_downloads", filetype, url2pathname(url)[2:], basename(fileUrl))
+                )
+                file_download_count += 1
+            except Exception:
+                pass
+        file_download_list.append(file_download_count)
+    return file_download_list
+
+
+def parse_images(url):
+    images = soup_url(url).findAll("img")
+    image_download_count = 0
+    if len(images) != 0:
+        create_dir(join(".", "easier_scrape_downloads", "images", url2pathname(url)[2:]))
+    for image in images:
+        try:
+            imageUrl = concat_urls(url, image.attrs["src"])
+            urlretrieve(
+                imageUrl, join(".", "easier_scrape_downloads", "images", url2pathname(url)[2:], basename(imageUrl))
+            )
+            image_download_count += 1
+        except Exception:
+            pass
+    return image_download_count
+
+
+def parse_lists(url):
+    # TODO        The key is "ul"
+    return
+
+
+def parse_tables(url):
+    soup_tables = soup_url(url).findAll("table")
+    if len(soup_tables) != 0:
+        create_dir(join(".", "easier_scrape_downloads", "tables", url2pathname(url)[2:]))
+        tables = read_html(url)
+        for i in range(0, len(tables)):
+            if soup_tables[i].has_attr("id"):
+                table_name = soup_tables["id"]
+            else:
+                table_name = str(uuid4())
+            tables[i].to_csv(join(".", "easier_scrape_downloads", "tables", url2pathname(url)[2:], table_name + ".csv"))
+    return len(soup_tables)
+
+
+def parse_text(url):
+    # TODO
+    return
+
+
+def soup_url(url):
+    return BeautifulSoup(get(url).text, "html.parser")
+
+
+def concat_urls(base_url, child_url):
+    if child_url.startswith("/"):
+        child_url = base_url + child_url
+    if child_url.startswith("./"):
+        child_url = base_url + child_url[1:]
+    if not child_url.startswith("http"):
+        child_url = base_url + "/" + child_url
+    if child_url.endswith("/"):
+        child_url = child_url[:-1]
+    return child_url
+
+
+def create_dir(dir):
+    if not exists(dir):
+        makedirs(dir)
+
+
+def tree_gen(url, maxdepth, tree=None, depth=0):
+    if tree is None:
+        url = url.replace("www.", "")
+        if url.endswith("/"):
+            url = url[:-1]
+        tree = Node(url, url=url)
+    if depth < maxdepth:
+        for a in parse_anchors(url):
+            child_url = concat_urls(url, a.attrs["href"].replace("www.", ""))
+            try:
+                if find(tree.root, lambda node: node.url == child_url) is None:
+                    new_leaf = Node(child_url, url=child_url, parent=tree)
+                    tree = tree_gen(child_url, maxdepth, new_leaf, depth + 1)
+            except Exception:
+                pass
+    return tree.root
+
+
+def print_tree(tree):
+    tree_print = ""
+    for pre, fill, node in RenderTree(tree):
+        tree_print += "%s%s\n" % (pre, node.name)
+    print(tree_print[:-1])
