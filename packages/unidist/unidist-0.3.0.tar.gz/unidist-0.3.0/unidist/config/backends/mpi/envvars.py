5eb7a9c92e148d6603b236282ac2e1a5460f91ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:44 +0000

    Add package unidist/0.3.0/unidist-0.3.0.tar.gz

---
+++ packages/unidist/unidist-0.3.0.tar.gz/unidist-0.3.0/unidist/config/backends/mpi/envvars.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2021-2023 Modin authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""Config entities specific for MPI backend which can be used for unidist behavior tuning."""
+
+from unidist.config.parameter import EnvironmentVariable, ExactStr
+
+
+class IsMpiSpawnWorkers(EnvironmentVariable, type=bool):
+    """Whether to enable MPI spawn or not."""
+
+    default = True
+    varname = "UNIDIST_IS_MPI_SPAWN_WORKERS"
+
+
+class MpiHosts(EnvironmentVariable, type=ExactStr):
+    """MPI hosts to run unidist on."""
+
+    varname = "UNIDIST_MPI_HOSTS"
+
+
+class MpiPickleThreshold(EnvironmentVariable, type=int):
+    """Minimum buffer size for serialization with pickle 5 protocol"""
+
+    default = 1024**2 // 4  # 0.25 MiB
+    varname = "UNIDIST_MPI_PICKLE_THRESHOLD"
---
+++ packages/unidist/unidist-0.3.0.tar.gz/unidist-0.3.0/unidist/core/backends/mpi/core/monitor.py
@@ -0,0 +1,85 @@
+# Copyright (C) 2021-2023 Modin authors
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""Monitoring process."""
+
+try:
+    import mpi4py
+except ImportError:
+    raise ImportError(
+        "Missing dependency 'mpi4py'. Use pip or conda to install it."
+    ) from None
+
+import unidist.core.backends.mpi.core.common as common
+import unidist.core.backends.mpi.core.communication as communication
+from unidist.core.backends.mpi.core.async_operations import AsyncOperations
+
+# TODO: Find a way to move this after all imports
+mpi4py.rc(recv_mprobe=False, initialize=False)
+from mpi4py import MPI  # noqa: E402
+
+
+class TaskCounter:
+    __instance = None
+
+    def __init__(self):
+        self.task_counter = 0
+
+    @classmethod
+    def get_instance(cls):
+        """
+        Get instance of ``TaskCounter``.
+
+        Returns
+        -------
+        TaskCounter
+        """
+        if cls.__instance is None:
+            cls.__instance = TaskCounter()
+        return cls.__instance
+
+    def increment(self):
+        """Increment task counter by one."""
+        self.task_counter += 1
+
+
+def monitor_loop():
+    """
+    Infinite monitor operations processing loop.
+
+    Tracks the number of executed tasks.
+
+    Notes
+    -----
+    The loop exits on special cancelation operation.
+    ``unidist.core.backends.mpi.core.common.Operations`` defines a set of supported operations.
+    """
+    task_counter = TaskCounter.get_instance()
+    mpi_state = communication.MPIState.get_instance()
+    async_operations = AsyncOperations.get_instance()
+
+    while True:
+        # Listen receive operation from any source
+        operation_type, source_rank = communication.recv_operation_type(mpi_state.comm)
+
+        # Proceed the request
+        if operation_type == common.Operation.TASK_DONE:
+            task_counter.increment()
+        elif operation_type == common.Operation.GET_TASK_COUNT:
+            # We use a blocking send here because the receiver is waiting for the result.
+            communication.mpi_send_object(
+                mpi_state.comm,
+                task_counter.task_counter,
+                source_rank,
+            )
+        elif operation_type == common.Operation.CANCEL:
+            async_operations.finish()
+            if not MPI.Is_finalized():
+                MPI.Finalize()
+            break  # leave event loop and shutdown monitoring
+        else:
+            raise ValueError("Unsupported operation!")
+
+        # Check completion status of previous async MPI routines
+        async_operations.check()
