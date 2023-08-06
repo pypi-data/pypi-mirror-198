f40afa4515c5e2b1fd41b6b2e0efe90de2477636
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:53 +0000

    Add package tiled/0.1.0a86/tiled-0.1.0a86.tar.gz

---
+++ packages/tiled/tiled-0.1.0a86.tar.gz/tiled-0.1.0a86/tiled/_tests/test_client.py
@@ -0,0 +1,13 @@
+import httpx
+
+from ..adapters.mapping import MapAdapter
+from ..client import Context
+from ..server.app import build_app
+
+tree = MapAdapter({})
+
+
+def test_configurable_timeout():
+    with Context.from_app(build_app(tree), timeout=httpx.Timeout(17)) as context:
+        assert context.http_client.timeout.connect == 17
+        assert context.http_client.timeout.read == 17
