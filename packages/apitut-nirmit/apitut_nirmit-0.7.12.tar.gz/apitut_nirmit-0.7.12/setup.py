1e3dbc9ceb76d67c28bb911b561b4d8240bca5d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:33 +0000

    Add package apitut-nirmit/0.7.12/apitut_nirmit-0.7.12.tar.gz

---
+++ packages/apitut-nirmit/apitut_nirmit-0.7.12.tar.gz/apitut_nirmit-0.7.12/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='apitut_nirmit',
+    version='0.7.12',
+    description='A tutorial package for creating APIs',
+    author='Nirmit Sakre',
+    author_email='nirmitsakre@gmail.com',
+    packages=['apitut_nirmit'],
+    install_requires=[
+        'requests',
+        'flask',
+    ],
+)
