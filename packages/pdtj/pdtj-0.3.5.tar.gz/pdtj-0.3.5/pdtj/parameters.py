40d809af091d8b108ac235ad230ff9bd5e450cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:55 +0000

    Add package pdtj/0.3.5/pdtj-0.3.5.tar.gz

---
+++ packages/pdtj/pdtj-0.3.5.tar.gz/pdtj-0.3.5/pdtj/parameters.py
@@ -0,0 +1,10 @@
+import yaml
+from yaml.loader import SafeLoader
+
+try:
+    # Open the file and load it
+    with open("./.pdtj.yaml") as file:
+        parameters = yaml.load(file, Loader=SafeLoader)
+
+except FileNotFoundError:
+    parameters = None
