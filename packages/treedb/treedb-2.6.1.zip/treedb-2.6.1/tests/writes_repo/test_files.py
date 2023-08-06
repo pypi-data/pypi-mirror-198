fee81e0e3e2438d5eacff75f21c3cb5c67c28d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:03 +0000

    Add package treedb/2.6.1/treedb-2.6.1.zip

---
+++ packages/treedb/treedb-2.6.1.zip/treedb-2.6.1/tests/writes_repo/test_files.py
@@ -0,0 +1,23 @@
+import pytest
+
+from treedb import glottolog
+
+
+pytestmark = pytest.mark.writes
+
+
+@pytest.mark.xfail_glottolog_tag('master', reason='possibly unnormalized master',
+                                 raises=AssertionError)
+def test_roundtrip(treedb):
+    if not glottolog.git_status_is_clean(treedb.root):
+        raise RuntimeError(f'root must be clean for test: {treedb.root!r}')
+
+    result = treedb.languoids.files.roundtrip()
+
+    clean = glottolog.git_status_is_clean(treedb.root)
+    if not clean:
+        print(glottolog.git_status(treedb.root))
+
+    if result is not None:
+        raise RuntimeError(f'expected result is not None: {result!r}')
+    assert clean
