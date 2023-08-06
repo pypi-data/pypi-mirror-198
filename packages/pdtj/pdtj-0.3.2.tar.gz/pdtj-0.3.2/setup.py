23ff204966c6747d5db514c36a9924f23849b69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:00 +0000

    Add package pdtj/0.3.2/pdtj-0.3.2.tar.gz

---
+++ packages/pdtj/pdtj-0.3.2.tar.gz/pdtj-0.3.2/setup.py
@@ -0,0 +1,51 @@
+import json
+import os
+import pathlib
+
+import setuptools
+
+from pdtj import __version__
+
+long_description = (pathlib.Path(__file__).parent / "README.md").read_text()
+
+
+def get_requirements_from_pipfile_lock(pipfile_lock=None):
+    if pipfile_lock is None:
+        pipfile_lock = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Pipfile.lock")
+    lock_data = json.load(open(pipfile_lock))
+    retval = []
+    for package_name, package_data in lock_data.get("default", {}).items():
+        if package_data.get("file"):
+            package_ref = f'{package_name} @ {package_data["file"]}'
+        else:
+            package_ref = f'{package_name}{package_data["version"]}'
+        retval.append(package_ref)
+    return retval
+
+
+def generate_requirements_txt(pipfile_lock_requirements):
+    for requirement in pipfile_lock_requirements:
+        with open(os.path.join("requirements.txt"), "w") as file:
+            file.writelines(requirement)
+
+
+requirements = get_requirements_from_pipfile_lock()
+generate_requirements_txt(requirements)
+
+setuptools.setup(
+    name="pdtj",
+    version="0.3.2",
+    license="MIT",
+    author="Joao Paulo Euko",
+    url="https://github.com/Joaopeuko/pdtj",
+    keywords=["python", "docstring", "json"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "pdtj = pdtj.json_generator:main",
+        ]
+    },
+)
