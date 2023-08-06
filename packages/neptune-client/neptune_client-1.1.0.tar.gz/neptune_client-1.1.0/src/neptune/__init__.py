e8bd3a0ceb718567fc07da3157023755e73e75f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:48 +0000

    Add package neptune-client/1.1.0/neptune_client-1.1.0.tar.gz

---
+++ packages/neptune-client/neptune_client-1.1.0.tar.gz/neptune_client-1.1.0/src/neptune/__init__.py
@@ -0,0 +1,46 @@
+#
+# Copyright (c) 2023, Neptune Labs Sp. z o.o.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+__all__ = [
+    "ANONYMOUS_API_TOKEN",
+    "init_model",
+    "init_model_version",
+    "init_project",
+    "init_run",
+    "Run",
+    "Model",
+    "ModelVersion",
+    "Project",
+    "__version__",
+]
+
+
+from neptune.common.patches import apply_patches
+from neptune.constants import ANONYMOUS_API_TOKEN
+from neptune.metadata_containers import (
+    Model,
+    ModelVersion,
+    Project,
+    Run,
+)
+from neptune.version import __version__
+
+# Apply patches of external libraries
+apply_patches()
+
+init_run = Run
+init_model = Model
+init_model_version = ModelVersion
+init_project = Project
