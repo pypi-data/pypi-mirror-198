ac5b23b5e9f3be34865101fbf1848c13dcfe9290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:24 +0000

    Add package regext/0.0.2/regext-0.0.2.tar.gz

---
+++ packages/regext/regext-0.0.2.tar.gz/regext-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name='regext',
+    packages=['regext'],
+    version='0.0.2',
+    description='Simple regex assistant with OpenAI API and prompt engineering',
+    author='Sulthan Abiyyu',
+    license='MIT',
+    install_requires=requirements,
+    long_description=long_description,
+)
