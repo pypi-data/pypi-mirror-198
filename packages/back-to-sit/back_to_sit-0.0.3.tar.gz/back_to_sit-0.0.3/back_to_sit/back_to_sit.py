de3f589d2d5248dff42894d84c1c6425a1c04fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:13 +0000

    Add package back-to-sit/0.0.3/back_to_sit-0.0.3.tar.gz

---
+++ packages/back-to-sit/back_to_sit-0.0.3.tar.gz/back_to_sit-0.0.3/back_to_sit/back_to_sit.py
@@ -0,0 +1,38 @@
+import telegram
+import asyncio
+import time 
+import functools
+import wrapt
+
+async def __send_telegram_message(message, chat_id, token):
+    # Set up the bot
+    bot = telegram.Bot(token=token)
+
+    # Send a message
+    await bot.send_message(chat_id=chat_id, text=message)
+
+
+def back_to_sit(message, chat_id, token):
+    """
+    Function that send message when is called
+    """
+    # Create an event loop
+    loop = asyncio.new_event_loop()
+    # Run the coroutine in the event loop
+    loop.run_until_complete(__send_telegram_message(message, chat_id, token))
+
+def back_to_sit_decorator(base_message, chat_id, token):
+    """
+    Decorator that send a message when the function is done
+    and report the time taken to execute the function
+    """
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            NEW_MESSAGE = base_message + f"\n Time taken to execute {func.__name__}: {end_time - start_time:.6f} seconds"
+            back_to_sit(message=NEW_MESSAGE, chat_id=chat_id, token=token)
+            return result
+        return wrapper
+    return decorator
