80e97d6b3eae62a6c1f4adddfb5232f8a6726c2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:11 +0000

    Add package spyrograph/0.14.0/spyrograph-0.14.0.tar.gz

---
+++ packages/spyrograph/spyrograph-0.14.0.tar.gz/spyrograph-0.14.0/spyrograph/epitrochoid/epicycloid.py
@@ -0,0 +1,21 @@
+"""Model of an epicycloid. An epicycloid is a specific case of an epitrochoid
+where the distance from the rolling circle is equal to its radius.
+"""
+
+from typing import List
+from numbers import Number
+
+from spyrograph.epitrochoid.epitrochoid import Epitrochoid
+from spyrograph._cycloid import _Cycloid
+
+class Epicycloid(_Cycloid, Epitrochoid):
+    """Model of a epicycloid which is a special case of a epitrochoid where the
+    circle is rolling around the outside of the fixed circle and has 1/3 the
+    radius of the fixed circle
+    """
+    def __init__(
+            self, R: Number, r: Number, thetas: List[Number] = None,
+            theta_start: Number = None, theta_stop: Number = None,
+            theta_step: Number = None
+        ) -> None:
+        super().__init__(R, r, r, thetas, theta_start, theta_stop, theta_step)
