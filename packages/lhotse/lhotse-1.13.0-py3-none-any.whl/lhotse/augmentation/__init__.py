5d380814f9de21d843eb04abee9f84c224cadfe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:04 +0000

    Add package lhotse/1.13.0/lhotse-1.13.0-py3-none-any.whl

---
+++ packages/lhotse/lhotse-1.13.0-py3-none-any.whl/lhotse/augmentation/__init__.py
@@ -0,0 +1,5 @@
+from .common import AugmentFn
+from .torchaudio import *
+from .transform import AudioTransform
+from .utils import FastRandomRIRGenerator, convolve1d
+from .wpe import DereverbWPE, dereverb_wpe_numpy, dereverb_wpe_torch
