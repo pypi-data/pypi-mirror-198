9d3da9fc01a0b79f10f75edc40464b355104f03b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:48 +0000

    Add package gcloud-aio-taskqueue/6.1.0/gcloud_aio_taskqueue-6.1.0-py3-none-any.whl

---
+++ packages/gcloud-aio-taskqueue/gcloud_aio_taskqueue-6.1.0-py3-none-any.whl/gcloud/aio/taskqueue/__init__.py
@@ -0,0 +1,36 @@
+"""
+This library implements various methods for working with the Google Taskqueue
+APIs.
+
+## Installation
+
+```console
+$ pip install --upgrade gcloud-aio-taskqueue
+```
+
+## Usage
+
+We're still working on documentation -- for now, you can use the
+[smoke tests][smoke-tests] as an example.
+
+## Emulators
+
+For testing purposes, you may want to use `gcloud-aio-taskqueue` along with a
+local emulator. Setting the `$CLOUDTASKS_EMULATOR_HOST` environment variable to
+the address of your emulator should be enough to do the trick.
+
+[smoke-tests]:
+https://github.com/talkiq/gcloud-aio/tree/master/taskqueue/tests/integration
+"""
+from pkg_resources import get_distribution
+__version__ = get_distribution('gcloud-aio-taskqueue').version
+
+from gcloud.aio.taskqueue.queue import PushQueue
+from gcloud.aio.taskqueue.queue import SCOPES
+
+
+__all__ = [
+    'PushQueue',
+    'SCOPES',
+    '__version__',
+]
