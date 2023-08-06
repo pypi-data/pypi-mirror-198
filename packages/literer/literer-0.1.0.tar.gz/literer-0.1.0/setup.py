be8f54cf80fd56859d6b8091e7f7a54967188cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:55 +0000

    Add package literer/0.1.0/literer-0.1.0.tar.gz

---
+++ packages/literer/literer-0.1.0.tar.gz/literer-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="literer",
+    version="0.1.0",
+    license="MIT",
+    description="literer is a package that combines the Semantic Scholar and OpenAI APIs to create automated literature reviews",
+    author="Jonathan Chassot",
+    author_email="jonathan.chassot@unisg.ch",
+    url="https://github.com/JLDC/literer",
+    keywords=["literature review", "openai", "semantic scholar"],
+    install_requires=[
+        "openai>=0.27.2",
+        "requests>=2.28.2",
+    ]
+)
