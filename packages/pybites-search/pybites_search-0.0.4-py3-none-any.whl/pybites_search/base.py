5273a491915e2f749a5c3635f2fe28ae0460fa4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:22 +0000

    Add package pybites-search/0.0.4/pybites_search-0.0.4-py3-none-any.whl

---
+++ packages/pybites-search/pybites_search-0.0.4-py3-none-any.whl/pybites_search/base.py
@@ -0,0 +1,25 @@
+from abc import ABCMeta, abstractmethod
+from typing import NamedTuple
+
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+
+class ContentPiece(NamedTuple):
+    title: str
+    url: str
+
+
+class PybitesSearch(metaclass=ABCMeta):
+    @abstractmethod
+    def match_content(self, search: str) -> list[ContentPiece]:
+        """Search through Pybites content, implement for a specific source"""
+
+    def show_matches(self, content: list[ContentPiece]) -> None:
+        """Show search results in a nice table"""
+        table = Table("Title", "Url")
+        for row in content:
+            table.add_row(row.title, row.url)
+        console.print(table)
