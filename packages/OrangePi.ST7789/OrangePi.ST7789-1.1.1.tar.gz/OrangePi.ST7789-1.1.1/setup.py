548a9125d5d119c27c5ef91d2d6ae5667331dff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:13 +0000

    Add package OrangePi.ST7789/1.1.1/OrangePi.ST7789-1.1.1.tar.gz

---
+++ packages/OrangePi.ST7789/OrangePi.ST7789-1.1.1.tar.gz/OrangePi.ST7789-1.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+classifiers = ['Development Status :: 4 - Beta',
+               'Operating System :: POSIX :: Linux',
+               'License :: OSI Approved :: MIT License',
+               'Intended Audience :: Developers',
+               'Programming Language :: Python :: 2.7',
+               'Programming Language :: Python :: 3',
+               'Topic :: Software Development',
+               'Topic :: System :: Hardware']
+
+setup(name='OrangePi.ST7789',
+      version='1.1.1',
+      description='Library to control ST7789 TFT LCD displays on the Orange Pi.',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      license='MIT',
+      author='Andriy Malyshenko',
+      author_email='andriy@sonocotta.com',
+      classifiers=classifiers,
+      url='https://github.com/sonocotta/st7789-orangepi-python',
+      packages=find_packages(),
+      install_requires=[
+            'OPi.GPIO',
+            'spidev',
+            'Pillow',
+            'NumPy'
+      ])
