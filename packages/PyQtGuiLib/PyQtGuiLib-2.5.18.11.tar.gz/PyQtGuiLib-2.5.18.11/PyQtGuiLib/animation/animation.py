db21fbc7a05d05748bd19b73bc26ef43371dbec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:22 +0000

    Add package PyQtGuiLib/2.5.18.11/PyQtGuiLib-2.5.18.11.tar.gz

---
+++ packages/PyQtGuiLib/PyQtGuiLib-2.5.18.11.tar.gz/PyQtGuiLib-2.5.18.11/PyQtGuiLib/animation/animation.py
@@ -0,0 +1,359 @@
+from PyQtGuiLib.header import (
+    QParallelAnimationGroup,
+    QSequentialAnimationGroup,
+    QPushButton,
+    QObject,
+    qt,
+    QRect,
+    QColor
+)
+from typing import TypeVar
+from collections.abc import Callable
+import copy
+'''
+    重构动画框架
+    Animation 仅创建,管理,播放动画
+'''
+
+from PyQtGuiLib.animation.animationFactory import AnimationFactory
+from PyQtGuiLib.animation.animationDrawType import AniNumber,AniNumbers,AniColor,AniRect
+
+AniMode = int
+ObjMode = str
+AniStartType = TypeVar("AniStartType",AniNumber,AniNumbers,AniColor,AniRect)
+AniEndType = TypeVar("AniEndType",list,QColor,QRect)
+AnimationModeType = TypeVar("AnimationModeType",QParallelAnimationGroup,QSequentialAnimationGroup)
+
+
+# 动画属性类
+class AnimationAttr:
+    Parallel = 1
+    Sequential = 2
+
+    # 动画对象模式
+    Control = "control"  # 动画作用在普通控件上面
+    Draw = "draw"        # 动画作用在绘制的图形上面
+
+    # 动效
+    InCurve = qt.InCurve
+    OutBounce = qt.OutBounce
+    CosineCurve = qt.CosineCurve
+    SineCurve = qt.SineCurve
+
+    def __init__(self,parent:QObject=None,ani_obj_mode="control"):
+        self.__parent = None
+        self.__ani_obj_mode = ani_obj_mode
+        self.__ani_group_obj = None # 动画组对象
+
+        # 默认动画模式(并行)
+        self.__ani_mode = AnimationAttr.Parallel
+        # 默认动画时长
+        self.__ani_duration = 1000
+        # 默认动效
+        self.__ani_special = AnimationAttr.InCurve
+        # 默认动画循环次数
+        self.__ani_loopCount = 0
+
+        if parent:
+            self.setParent(parent)
+
+    def setParent(self,parent):
+        self.__parent = parent
+
+    def setAniMode(self,mode:AniMode):
+        self.__ani_mode = mode
+
+    def setDuration(self,duration:int):
+        self.__ani_duration = duration
+
+    def setSpecial(self, special):
+        self.__ani_special = special
+
+    def setLoopCount(self, count:int):
+        self.__ani_loopCount = count
+
+    def setAniGroupObj(self,obj:AnimationModeType):
+        if isinstance(obj,QParallelAnimationGroup) or isinstance(obj,QSequentialAnimationGroup):
+            self.__ani_group_obj = obj
+        else:
+            raise Exception("Attribute error!")
+
+    def parent(self) -> QObject:
+        return self.__parent
+
+    def aniMode(self) -> AniMode:
+        return self.__ani_mode
+
+    def duration(self) -> int:
+        return self.__ani_duration
+
+    def special(self):
+        return self.__ani_special
+
+    def loopCount(self) -> int:
+        return self.__ani_loopCount
+
+    def aniObjMode(self) -> ObjMode:
+        return self.__ani_obj_mode
+
+    def aniGroupObj(self) -> AnimationModeType:
+        return self.__ani_group_obj
+
+    def isAniGroupObj(self) -> bool:
+        return True if self.aniGroupObj() else False
+
+    def isControlMode(self) -> bool:
+        return True if self.aniObjMode() == AnimationAttr.Control else False
+
+    def isDrawMode(self) -> bool:
+        return True if self.aniObjMode() == AnimationAttr.Draw else False
+
+    # 绘图动画的 值封装
+    @staticmethod
+    def createAniNumber(value) -> AniNumber:
+        return AniNumber(value)
+
+    # 绘图动画的 多值封装
+    @staticmethod
+    def createAniNumbers(*args) -> AniNumbers:
+        return AniNumbers(*args)
+
+    # 绘图动画的 颜色值封装
+    @staticmethod
+    def createAniColor(*args) -> AniColor:
+        return AniColor(*args)
+
+    # 绘图动画的 矩形值封装
+    @staticmethod
+    def createAniRect(*args) -> AniRect:
+        return AniRect(*args)
+
+
+# 动画类
+class Animation(AnimationAttr):
+    def __init__(self, parent: QObject = None, ani_obj_mode="control"):
+        super(Animation, self).__init__(parent,ani_obj_mode)
+
+        # 动画列表(里面每一个元素都是一个完整的动画对象)
+        self.ani_list = []
+
+    # 设置通用属性
+    def __setGeneralAttr(self,ani_data:dict):
+        duration = ani_data.get("duration", None)
+        special = ani_data.get("special", None)
+        loopCount = ani_data.get("loop", None)
+
+        if duration is None:
+            ani_data["duration"] = self.duration()
+        if special is None:
+            ani_data["special"] = self.special()
+        if loopCount is None:
+            ani_data["loopCount"] = self.loopCount()
+
+    def addAni(self,ani_data:dict):
+        '''
+        {
+            "targetObj":xx
+            "propertyName":""
+            "duration":1000,  # 可选参数
+            "special":        # 可选参数
+            "loop":1          # 可选参数
+            "call":fun  回调函数  # 可选参数
+            "argc":tuple    # 回调函数的参数  可选参数
+            "sv":xx  # 该参数在控件模式下,可以写 this 指向自己的属性
+            "atv":[()] 或者 []          # 可选参数
+            "ev":xx
+            "selector":""  # 选择器,这个参数qss样式动画时使用 eg:backgroundColor  可选参数
+            "qss-suffix":"px"  # qss属性单位 如果: 写宽度时单位是px,表示文字大小时,有时候会用到pt  可选参数
+        }
+        注意 sv,atv,ev 中的值的参数类型必须一致
+        这里的每一个动画都是独立的,不会在并联动画连续播放
+        普通控件动画最简化版
+        {
+            "targetObj":xx,
+            "propertyName":"",
+            "sv":xx,
+            "ev":xx
+        }
+       绘图动画最简化版
+        {
+            "propertyName":"",
+            "sv":xx,
+            "ev":xx
+        }
+        :param ani_data:
+        :return:
+        '''
+        if not ani_data:
+            return
+
+        self.__setGeneralAttr(ani_data)
+
+        if self.isControlMode() and self.parent():
+            ani = AnimationFactory(self.parent(),ani_data,self.aniObjMode()).createAni()
+        elif self.isDrawMode():
+            ani_data["targetObj"] = QObject()
+            ani = AnimationFactory(self.parent(), ani_data, self.aniObjMode()).createAni()
+        else:
+            raise Exception("Pattern error,Only Animation.Control or Animation.Draw is supported!")
+
+        self.ani_list.append(ani)
+
+    def addAnis(self,*args):
+        for ani_data in args:
+            self.addAni(ani_data)
+
+    # 添加连续动画(运动必须是相同的)
+    def addSeriesAni(self, ani_data: dict, variation: list):
+        '''
+            连续动画是由单个具有相同的行为动画的组合
+        :param ani_data:
+        :param variation:
+        :return:
+        '''
+        if not ani_data:
+            return
+
+        if self.aniMode() != Animation.Sequential:
+            raise Exception("Current animation mode is not Animation.Sequential")
+
+        if not self.isDrawMode() and not self.isControlMode():
+            raise Exception("Pattern error,Only Animation.Control or Animation.Draw is supported!")
+
+        if not self.parent():
+            raise Exception("No superclass")
+
+        self.__setGeneralAttr(ani_data)
+
+        '''
+                      如果有回调函数,保存之后,先移除掉,最后添加在末尾
+                  '''
+        Call = ani_data.get("call", None)
+        CallAgrc = ani_data.get("argc", None)
+        if Call: del ani_data["call"]
+        if CallAgrc: del ani_data["argc"]
+
+        # 首先创建出 初始动画
+        ani = AnimationFactory(self.parent(), ani_data, self.aniObjMode()).createAni()
+        self.ani_list.append(ani)
+        sv = ani.endValue()
+
+        if self.isDrawMode():
+            ani_data["targetObj"] = QObject()
+
+        # 在创建出后续的连续动画
+        variation_len = len(variation)
+        for i,ev in enumerate(variation):
+            ev = variation[i]
+            if i == variation_len-1:  # 这里判断是否是最后一项数值
+                if Call: ani_data["call"] = Call
+                if CallAgrc: ani_data["argc"] = CallAgrc
+            ani_data["ev"] = ev
+            e_ani = AnimationFactory(self.parent(), ani_data, self.aniObjMode()).createAni()
+            e_ani.setStartValue(sv)
+            e_ani.setEndValue(ev)
+            sv = ev
+            self.ani_list.append(e_ani)
+
+    # 绘图-多值动画
+    def addValuesAni(self,ani_data: dict,startObj:AniStartType,ends:AniEndType):
+        '''
+
+
+        :param ani_data:
+        :param startObj: 参数类型 AniColor,AniNumbers
+        :param ends:
+        :return:
+        '''
+        if self.isDrawMode():
+            # 先移除属性
+            TargetObj = ani_data.get("targetObj")
+            Sv = ani_data.get("sv",None)
+            Ev = ani_data.get("ev",None)
+            Call = ani_data.get("call",None)
+            if TargetObj:del ani_data["targetObj"]
+            if Sv:del ani_data["sv"]
+            if Ev:del ani_data["ev"]
+            if Call:del ani_data["call"]
+
+            # 类型检测,并转换
+            if isinstance(ends,QColor):
+                ends = ends.getRgb()
+            elif isinstance(ends,QRect):
+                ends = ends.getRect()
+
+            for sv,ev in zip(startObj.numberObjs(),ends):
+                copy_ani_data = copy.deepcopy(ani_data)
+                copy_ani_data["targetObj"] = QObject()
+                copy_ani_data["sv"] = sv
+                copy_ani_data["ev"] = ev
+                self.addAni(copy_ani_data)
+        else:
+            raise Exception("addValuesAni() This method supports only the Animation.Draw mode!")
+
+    # Executive function
+    def __exeCall(self,call:Callable, argc=None):
+        if call:
+            if argc:
+                call(*argc)
+            else:
+                call()
+
+    def pause(self,call:Callable=None, argc=None):
+        if self.isAniGroupObj():
+            self.aniGroupObj().pause()
+            self.__exeCall(call,argc)
+
+    def resume(self,call:Callable=None,argc=None):
+        if self.isAniGroupObj():
+            self.aniGroupObj().resume()
+            self.__exeCall(call,argc)
+
+    def state(self):
+        if self.isAniGroupObj():
+            if self.aniGroupObj().state() == self.aniGroupObj().Running:
+                return self.aniGroupObj().Running
+            if self.aniGroupObj() == self.aniGroupObj().Paused:
+                return self.aniGroupObj().Paused
+        else:
+            return None
+
+    def start(self):
+        if self.aniMode() == Animation.Parallel:
+            self.setAniGroupObj(QParallelAnimationGroup(self.parent()))
+        if self.aniMode() == Animation.Sequential:
+            self.setAniGroupObj(QSequentialAnimationGroup(self.parent()))
+
+        for ani in self.ani_list:
+            self.aniGroupObj().addAnimation(ani)
+
+        self.aniGroupObj().start()
+
+    # 开关
+    def aniSwitch(self,btn:QPushButton=None,texts:dict=None):
+        '''
+            动画开关
+            如果动画当前状态是运行,则暂停,反之,运行
+            __________________________________
+            btn: 给定一个按钮(可选)
+            texts:{
+                "pause":"xxx",
+                "resume":"xx"
+            }
+        :return:
+        '''
+        if texts is None:
+            texts = dict()
+
+        if self.isAniGroupObj():
+            pause_ = texts.get("pause", "pause")
+            resume_ = texts.get("resume", "resume")
+
+            if self.aniGroupObj().state() == self.aniGroupObj().Running:
+                self.pause()
+                if btn:
+                    btn.setText(resume_)
+            elif self.aniGroupObj().state() == self.aniGroupObj().Paused:
+                self.resume()
+                if btn:
+                    btn.setText(pause_)
---
+++ packages/PyQtGuiLib/PyQtGuiLib-2.5.18.11.tar.gz/PyQtGuiLib-2.5.18.11/PyQtGuiLib/animation/animationFactory.py
@@ -0,0 +1,273 @@
+import re
+import typing
+from PyQtGuiLib.header import (
+    QPropertyAnimation,
+    QObject,
+    QColor,
+    QRect,
+    QPoint,
+    QSize,
+    pyqtProperty,
+    Signal,
+)
+from PyQtGuiLib.styles import QssStyleAnalysis
+from PyQtGuiLib.animation.animationDrawType import AniNumber
+'''
+    QPropertyAnimation: you're trying to animate a non-existing property value of your QObject
+    该警告出现mac下,部分win可能也会出现,可以忽略该警告
+'''
+
+# 公用动画基类
+class PropertyAnimation(QPropertyAnimation):
+    drawfinished = Signal()
+
+    Parallel = 1
+    Sequential = 2
+    # 动画对象模式
+    Control = "control"  # 动画作用在普通控件上面
+    Draw = "draw"  # 动画作用在绘制的图形上面
+
+    def __init__(self,parent:QObject,ani_data:dict,ani_obj_mode="control"):
+        self.__parent = parent
+        self.attr = ani_data["propertyName"].decode()
+        super().__init__(parent)
+
+        # 动画对象模式
+        self.__ani_obj_mode = ani_obj_mode
+
+        self.__all_ani_data = ani_data
+
+        self.targetObj = ani_data.get("targetObj", None)
+        self.propertyName = ani_data.get("propertyName", None)
+        self.sv = ani_data.get("sv", None)
+        self.atv = ani_data.get("atv", None)
+        self.ev = ani_data.get("ev", None)
+        self.call = ani_data.get("call", None)
+        self.call_argc = ani_data.get("argc", None)
+        self.selector = ani_data.get("selector", None)
+        self.qssSuffix = ani_data.get("qss-suffix","px")
+
+        self.duration = ani_data.get("duration", self.duration())
+        self.special = ani_data.get("special", self.special())
+        self.loopCount = ani_data.get("loop", self.loopCount())
+
+        self.setTargetObject(self.targetObj)
+        self.setDuration(self.duration)
+        self.setSpecial(self.special)
+        self.setLoopCount(self.loopCount)
+
+        # 保存绘图动画的开始对象
+        self.__drawStartObj = self.sv
+
+    def drawSv(self):
+        return self.__drawStartObj
+
+    def allAniDatas(self)->dict:
+        return self.__all_ani_data
+
+    def aniObjMode(self):
+        return self.__ani_obj_mode
+
+    # 设置动效
+    def setSpecial(self,special):
+        self.setEasingCurve(special)
+
+    def special(self):
+        return self.easingCurve()
+
+    # 判断sv是不是this
+    def isThis(self):
+        if isinstance(self.sv,str) and self.sv.lower() == "this":
+            return True
+        return False
+
+    # 刷新绘图动画
+    def updateDraw(self):
+        self.__parent.update()
+
+    # 子类重写(默认模式)
+    def createAni(self):
+        self.setStartValue(self.sv)
+        if self.atv:
+            one_e = self.atv[0]
+            if isinstance(one_e, tuple) or isinstance(one_e, list):
+                for step, value in self.atv:
+                    self.setKeyValueAt(step, value)
+            else:
+                mean_time = 1 / len(self.atv)  # 平均时间
+                step = 0.0
+                for value in self.atv:
+                    step += mean_time
+                    self.setKeyValueAt(step, value)
+        self.setEndValue(self.ev)
+
+        self.aniCall()
+
+    # 执行动画的回调函数
+    def aniCall(self):
+        if self.call:
+            if self.call_argc:
+                self.finished.connect(lambda: self.call(self.targetObj, *self.call_argc))
+            else:
+                self.finished.connect(lambda: self.call(self.targetObj))
+
+
+# 与QSS属性动画相关的基类
+class QSSPropertyAnimation(PropertyAnimation):
+    def __init__(self,parent:QObject,ani_data:dict,ani_obj_mode="control"):
+        super().__init__(parent,ani_data,ani_obj_mode)
+
+        if self.selector is None:
+            raise Exception("backgroundColor The selector must be provided!")
+
+        self.setPropertyName(self.propertyName)
+
+        self.qss = QssStyleAnalysis(self.targetObj)
+        self.qss.setQSS(self.targetObj.styleSheet())
+        self.createAni()
+
+    def setStartValue(self, value) -> None:
+        '''
+            当这个值是 this 的时候,指向自己已经编写的属性值,
+            如果没有这个属性值,将报错,
+            如果这个属性中包含 color 则判定为颜色属性,否则判断为数值形
+            如果这个属性值中包含px,pt 这里的,直接返回数值形
+        :param value:
+        :return:
+        '''
+        if self.isThis():
+            attr = self.attr.lower()
+            if self.qss.selector(self.selector).isAttr(attr):
+                attrValue = self.qss.selector(self.selector).attr(self.attr) # Gets the value of the property
+                if "color" in attr:
+                    color = attrValue
+                    if "rgb" in color.lower() or "rgba" in color.lower():
+                        color_list=[int(i) for i in re.findall(r"[0-9]{1,3}",color)]
+                        value = QColor(*color_list)
+                    else:
+                        value = QColor(color)
+                elif "px" in attrValue or "pt" in attrValue:
+                    value = int(re.findall(r"[0-9]+",attrValue)[0])
+            else:
+                raise Exception("The sv attribute is this, but QSS does not have this attribute!,{}".format(self.attr))
+
+        super().setStartValue(value)
+
+    def updateState(self, newState, oldState) -> None:
+        super().updateState(newState,oldState)
+
+    def interpolated(self, from_, to, progress: float):
+        return super().interpolated(from_,to,progress)
+
+    def __toValue(self,value):
+        if isinstance(value, int):
+            value = "{}{}".format(value,self.qssSuffix)
+        elif isinstance(value, QColor):
+            value = "rgba({},{},{},{})".format(*value.getRgb())
+
+        else:
+            raise Exception("This type is not supported yet.{}".format(value))
+        return value
+
+    def updateCurrentValue(self, value) -> None:
+        self.qss.appendQSS(self.targetObj.styleSheet())
+        self.qss.selector(self.selector).updateAttr(self.propertyName.decode(), self.__toValue(value))
+
+# --------------
+
+
+# 通用普通控件动画
+class AnimationControl(PropertyAnimation):
+    def __init__(self,parent:QObject,ani_data:dict,ani_obj_mode="control"):
+        super().__init__(parent,ani_data,ani_obj_mode)
+
+        self.setPropertyName(self.propertyName)
+        self.createAni()
+
+    def setStartValue(self, value) -> None:
+        if self.propertyName == b"geometry":
+            value = self.targetObject().rect()
+        elif self.propertyName == b"size":
+            value = self.targetObject().size()
+        elif self.propertyName == b"pos":
+            value = self.targetObject().pos()
+        elif self.propertyName == b"windowOpacity":
+            value = self.targetObject().windowOpacity()
+        super().setStartValue(value)
+
+
+
+# 普通绘图 - 单值动画
+class AnimationDrawValue(PropertyAnimation):
+    def __init__(self,parent:QObject,ani_data:dict,ani_obj_mode="control"):
+        super().__init__(parent,ani_data,ani_obj_mode)
+
+        self.setPropertyName(b"value")
+        self.createAni()
+
+    def setStartValue(self, value) -> None:
+        if isinstance(value,int):
+            super().setStartValue(value)
+        elif isinstance(value,AniNumber):
+            super().setStartValue(value.value())
+
+    def updateState(self, newState, oldState) -> None:
+        super().updateState(newState, oldState)
+
+    def updateCurrentValue(self, value) -> None:
+        if self.drawSv():
+            self.drawSv().setNumber(value)
+            self.updateDraw()
+
+
+
+# 返回动画类型
+CreateAni = [AnimationControl,QSSPropertyAnimation,AnimationDrawValue]
+
+
+'''
+    动画工厂
+'''
+class AnimationFactory:
+    # 动画对象模式
+    Control = "control"  # 动画作用在普通控件上面
+    Draw = "draw"        # 动画作用在绘制的图形上面
+
+    def __init__(self,parent,ani_data:dict,ani_obj_mode):
+        self.__ani_data = ani_data
+        self.__parent = parent
+        self.__ani_obj_mode = ani_obj_mode
+
+    def parent(self):
+        return self.__parent
+
+    def aniData(self) -> dict:
+        return self.__ani_data
+
+    def propertyName(self)->str:
+        return self.__ani_data['propertyName']
+
+    def aniObjMode(self):
+        return self.__ani_obj_mode
+
+    def argc(self) -> tuple:
+        return self.parent(),self.aniData(),self.aniObjMode()
+
+    # 返回动画的实例
+    def createAni(self) -> CreateAni:
+        if self.aniObjMode() == AnimationFactory.Control:
+            if self.propertyName() in [b"geometry",b"size",b"pos",b"windowOpacity"]:
+                ani = AnimationControl(*self.argc())
+            elif self.propertyName() and self.aniData().get("selector",None):  # 处理所有qss属性
+                ani = QSSPropertyAnimation(*self.argc())
+            else:
+                raise Exception("There is no animation property,{}!".format(self.propertyName()))
+        elif self.aniObjMode() == AnimationFactory.Draw:
+            if self.propertyName() == b"value":
+                ani = AnimationDrawValue(*self.argc())
+            else:
+                raise Exception("There is no animation property!")
+        else:
+            raise Exception("This animation mode is not available!")
+
+        return ani
