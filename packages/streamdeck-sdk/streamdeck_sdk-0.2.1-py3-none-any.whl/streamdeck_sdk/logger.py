ff55792c9494b623328c240e11c3d86e32a279da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:50 +0000

    Add package streamdeck-sdk/0.2.1/streamdeck_sdk-0.2.1-py3-none-any.whl

---
+++ packages/streamdeck-sdk/streamdeck_sdk-0.2.1-py3-none-any.whl/streamdeck_sdk/logger.py
@@ -0,0 +1,51 @@
+import logging
+from functools import wraps
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+
+from decohints import decohints
+
+logger: logging.Logger = logging.getLogger('default')
+
+
+def init_logger(
+        log_file: Path,
+        log_level: int = logging.DEBUG,
+        log_max_bytes: int = 3 * 1024 * 1024,
+        log_backup_count: int = 2,
+) -> None:
+    logger.setLevel(log_level)
+    logs_dir: Path = log_file.parent
+    logs_dir.mkdir(parents=True, exist_ok=True)
+    rfh = RotatingFileHandler(
+        log_file,
+        mode='a',
+        maxBytes=log_max_bytes,
+        backupCount=log_backup_count,
+        encoding="utf-8",
+        delay=False,
+    )
+    rfh.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        "%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d): %(message)s"
+    )
+    rfh.setFormatter(formatter)
+    logger.addHandler(rfh)
+
+
+@decohints
+def log_errors(func):
+    """
+    A decorator that logs and suppresses errors in the function being decorated.
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+        except BaseException as err:
+            logger.error(str(err), exc_info=True)
+            return
+        return result
+
+    return wrapper
