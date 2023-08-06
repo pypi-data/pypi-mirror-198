5afa1f20491c0892f09a89fcbf49710e64369594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:45 +0000

    Add package bdgd-tools/0.2.3/bdgd_tools-0.2.3.tar.gz

---
+++ packages/bdgd-tools/bdgd_tools-0.2.3.tar.gz/bdgd_tools-0.2.3/bdgd_tools/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for bdgd-tools."""
+
+__author__ = """Ênio Rodrigues"""
+__email__ = 'eniocc@gmail.com'
+__version__ = '0.1.0'
+
---
+++ packages/bdgd-tools/bdgd_tools-0.2.3.tar.gz/bdgd_tools-0.2.3/tests/test_bdgd_tools.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `bdgd_tools` package."""
+
+import pytest
+
+
+from bdgd_tools import bdgd_tools
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+
