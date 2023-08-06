024681d5652ea3ea27146cee4458f0bc36a0f302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:13 +0000

    Add package PyMieSim/0.9.6/PyMieSim-0.9.6-cp310-cp310-macosx_10_13_x86_64.whl

---
+++ packages/PyMieSim/PyMieSim-0.9.6-cp310-cp310-macosx_10_13_x86_64.whl/PyMieSim/examples/experiment/cylinder/plot_cylinder_Qsca_vs_index.py
@@ -0,0 +1,36 @@
+"""
+=============
+Qsca vs index
+=============
+
+"""
+
+import numpy as np
+from PyMieSim.experiment import CylinderSet, SourceSet, Setup
+from PyMieSim import measure
+
+scatterer_set = CylinderSet(
+    diameter=800e-9,
+    index=np.linspace(1.3, 1.9, 1500),
+    n_medium=1
+)
+
+source_set = SourceSet(
+    wavelength=[500e-9, 1000e-9, 1500e-9],
+    polarization=30,
+    amplitude=1
+)
+
+experiment = Setup(
+    scatterer_set=scatterer_set,
+    source_set=source_set
+)
+
+data = experiment.Get([measure.Qsca])
+
+figure = data.plot(
+    y=measure.Qsca,
+    x=scatterer_set.index
+)
+
+figure.show()
