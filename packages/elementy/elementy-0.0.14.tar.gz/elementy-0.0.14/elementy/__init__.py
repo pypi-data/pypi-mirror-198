54db9955624a12df4b06700fed49dc9cab26f895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:50 +0000

    Add package elementy/0.0.14/elementy-0.0.14.tar.gz

---
+++ packages/elementy/elementy-0.0.14.tar.gz/elementy-0.0.14/elementy/__init__.py
@@ -0,0 +1,10 @@
+"""elementy
+
+An interface to data on elements of the periodic table.
+"""
+
+
+from .periodictable import PeriodicTable
+from .element import Element
+
+__all__ = ["PeriodicTable", "Element"]
