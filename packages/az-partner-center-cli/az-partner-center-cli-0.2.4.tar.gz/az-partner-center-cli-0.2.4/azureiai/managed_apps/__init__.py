7644300d671da7c9b30a0614dc5b01df5bf61258
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:42 +0000

    Add package az-partner-center-cli/0.2.4/az-partner-center-cli-0.2.4.tar.gz

---
+++ packages/az-partner-center-cli/az-partner-center-cli-0.2.4.tar.gz/az-partner-center-cli-0.2.4/azureiai/managed_apps/__init__.py
@@ -0,0 +1,10 @@
+#  ---------------------------------------------------------
+#  Copyright (c) Microsoft Corporation. All rights reserved.
+#  ---------------------------------------------------------
+
+"""
+This module contains general functions for Azure Managed Application publication.
+"""
+from azureiai.managed_apps.managed_app import ManagedApplication
+
+__all__ = ["ManagedApplication"]
