9a755292b48560fa46c5ca0f1ff07f6aa3af5e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:58 +0000

    Add package teklib/1.0.9/teklib-1.0.9.tar.gz

---
+++ packages/teklib/teklib-1.0.9.tar.gz/teklib-1.0.9/src/teklib/__init__.py
@@ -0,0 +1,10 @@
+from .info import __doc__
+from . import administration
+from . import application
+from . import wave_theory
+from . import signal_analysis
+from . import miscellaneous
+from . import software
+
+__version__ = "1.0.0"
+
