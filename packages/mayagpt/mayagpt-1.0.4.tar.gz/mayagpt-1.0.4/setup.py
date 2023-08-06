fb78b6fd662d389c34629ed864625213bd259dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:16 +0000

    Add package mayagpt/1.0.4/mayagpt-1.0.4.tar.gz

---
+++ packages/mayagpt/mayagpt-1.0.4.tar.gz/mayagpt-1.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mayagpt",
+    version="1.0.4",
+    author= 'Mahmoud Ellithy',
+    author_email='cmy.mahmoud@hotmail.com',
+    project_urls={
+        "Homepage": "https://github.com/MahmoudGFX/mayagpt",
+    },
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    license="MIT",  # Replace "MIT" with the appropriate SPDX identifier for your license
+    install_requires=["setuptools", "wheel", "PySide2", "openai", "requests"],
+)
+    
