6ad8c49756994404967cbc8f76bac01f1de40989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:27 +0000

    Add package muffin-rest/4.6.7/muffin_rest-4.6.7-py3-none-any.whl

---
+++ packages/muffin-rest/muffin_rest-4.6.7-py3-none-any.whl/muffin_rest/types.py
@@ -0,0 +1,12 @@
+from typing import TYPE_CHECKING, Any, Awaitable, Callable, Type, TypeVar
+
+if TYPE_CHECKING:
+    from .handler import RESTBase
+
+from muffin import Request
+
+TVCollection = TypeVar("TVCollection", bound=Any)
+TVResource = TypeVar("TVResource", bound=Any)
+TAuth = Callable[[Request], Awaitable]
+TVAuth = TypeVar("TVAuth", bound=TAuth)
+TVHandler = TypeVar("TVHandler", bound=Type["RESTBase"])
